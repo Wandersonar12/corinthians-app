1b1663fea6b9f24929b507359cbc74e1db6b3764
GitHub Action <action@github.com>
2026-03-19 15:39:21 +0000

    Dados atualizados

--- src/data.js
+++ src/data.js
@@ -1,8 +1,248 @@
-// Gerado automaticamente em 19/03/2026 15:08
-// Este arquivo é atualizado automaticamente pelo GitHub Actions
+// Gerado automaticamente em 19/03/2026, 15:39:21
 const LIVE_DATA = {
-  "updatedAt": "2026-03-19T15:08:31.070927",
-  "news": [],
+  "updatedAt": "2026-03-19T15:39:21.327Z",
+  "news": [
+    {
+      "source": "Meu Timão",
+      "color": "#1a7a3a",
+      "title": "Corinthians pode ganhar três posições na tabela do Brasileirão caso vença a Chapecoense; confira",
+      "url": "https://www.meutimao.com.br/noticias-do-corinthians/529302/corinthians-pode-ganhar-tres-posicoes-na-tabela-do-brasileirao-caso-venca-a-chapecoense-confira",
+      "time": "19/03 · 15:30",
+      "pubDate": "Thu, 19 Mar 2026 12:30:00 -0300"
+    },
+    {
+      "source": "Meu Timão",
+      "color": "#1a7a3a",
+      "title": "Corinthians inicia venda de ingressos para o clássico contra o Sesi Franca pelo NBB",
+      "url": "https://www.meutimao.com.br/noticias-do-corinthians/529309/corinthians-inicia-venda-de-ingressos-para-o-classico-contra-o-sesi-franca-pelo-nbb",
+      "time": "19/03 · 15:23",
+      "pubDate": "Thu, 19 Mar 2026 12:23:00 -0300"
+    },
+    {
+      "source": "Bolavip",
+      "color": "#0055A5",
+      "title": "Corinthians desiste da contratação do goleiro Bruno Bertinato, da Portuguesa, por falha contra o Paysandu",
+      "url": "<![CDATA[https://br.bolavip.com/corinthians/corinthians-desiste-da-contratacao-do-goleiro-bruno-bertinato-da-portuguesa-por-falha-contra-o-paysandu]]>",
+      "time": "19/03 · 15:10",
+      "pubDate": "Thu, 19 Mar 2026 12:10:01 -0300"
+    },
+    {
+      "source": "Meu Timão",
+      "color": "#1a7a3a",
+      "title": "Corinthians reencontra árbitro da final da Copa do Brasil no duelo contra a Chapecoense",
+      "url": "https://www.meutimao.com.br/noticias-do-corinthians/529300/corinthians-reencontra-arbitro-da-final-da-copa-do-brasil-no-duelo-contra-a-chapecoense",
+      "time": "19/03 · 14:30",
+      "pubDate": "Thu, 19 Mar 2026 11:30:00 -0300"
+    },
+    {
+      "source": "Bolavip",
+      "color": "#0055A5",
+      "title": "Corinthians monitora João Paulo e outros nomes para ser reserva de Hugo Souza no gol",
+      "url": "<![CDATA[https://br.bolavip.com/corinthians/corinthians-monitora-joao-paulo-e-outros-nomes-para-ser-reserva-de-hugo-souza-no-gol]]>",
+      "time": "19/03 · 14:29",
+      "pubDate": "Thu, 19 Mar 2026 11:29:44 -0300"
+    },
+    {
+      "source": "Bolavip",
+      "color": "#0055A5",
+      "title": "Corinthians tenta ampliar série positiva contra a Chapecoense após cinco vitórias seguidas",
+      "url": "<![CDATA[https://br.bolavip.com/corinthians/corinthians-tenta-ampliar-serie-positiva-contra-a-chapecoense-apos-cinco-vitorias-seguidas]]>",
+      "time": "19/03 · 14:04",
+      "pubDate": "Thu, 19 Mar 2026 11:04:44 -0300"
+    },
+    {
+      "source": "Bolavip",
+      "color": "#0055A5",
+      "title": "Manolo Gabbiadini no Corinthians como novo centroavante não tem chance de acontecer após rumores",
+      "url": "<![CDATA[https://br.bolavip.com/corinthians/manolo-gabbiadini-no-corinthians-como-novo-centroavante-nao-tem-chance-de-acontecer-apos-rumores]]>",
+      "time": "19/03 · 13:48",
+      "pubDate": "Thu, 19 Mar 2026 10:48:40 -0300"
+    },
+    {
+      "source": "Folha de S.Paulo",
+      "color": "#1a237e",
+      "title": "Coreia do Norte e Filipinas se classificam para Copa do Mundo feminina no Brasil",
+      "url": "https://redir.folha.com.br/redir/online/esporte/rss091/*https://www1.folha.uol.com.br/esporte/2026/03/coreia-do-norte-e-filipinas-se-classificam-para-copa-do-mundo-feminina-no-brasil.shtml",
+      "time": "19/03 · 13:41",
+      "pubDate": "19 Mar 2026 10:41:00 -0300"
+    },
+    {
+      "source": "Meu Timão",
+      "color": "#1a7a3a",
+      "title": "Corinthians recebe o Bragantino em disputa pela liderança do Brasileiro Feminino Sub-20; saiba tudo",
+      "url": "https://www.meutimao.com.br/noticias-do-corinthians/529275/corinthians-recebe-o-bragantino-em-disputa-pela-lideranca-do-brasileiro-feminino-sub-20-saiba-tudo",
+      "time": "19/03 · 13:33",
+      "pubDate": "Thu, 19 Mar 2026 10:33:00 -0300"
+    },
+    {
+      "source": "Meu Timão",
+      "color": "#1a7a3a",
+      "title": "Presidente e vice do CD apontam irregularidades na convocação de reunião do órgão por Stabile",
+      "url": "https://www.meutimao.com.br/noticias-do-corinthians/529304/presidente-e-vice-do-cd-apontam-irregularidades-na-convocacao-de-reuniao-do-orgao-por-stabile",
+      "time": "19/03 · 13:27",
+      "pubDate": "Thu, 19 Mar 2026 10:27:07 -0300"
+    },
+    {
+      "source": "Folha de S.Paulo",
+      "color": "#1a237e",
+      "title": "Ir� vai boicotar os EUA, n�o a Copa, diz presidente da federa��o iraniana",
+      "url": "https://redir.folha.com.br/redir/online/esporte/rss091/*https://www1.folha.uol.com.br/esporte/2026/03/ira-vai-boicotar-os-eua-nao-a-copa-diz-presidente-da-federacao-iraniana.shtml",
+      "time": "19/03 · 13:20",
+      "pubDate": "19 Mar 2026 10:20:00 -0300"
+    },
+    {
+      "source": "Folha de S.Paulo",
+      "color": "#1a237e",
+      "title": "Sob Roman Abramovich, Chelsea violou regras por anos, diz Premier League",
+      "url": "https://redir.folha.com.br/redir/online/esporte/rss091/*https://www1.folha.uol.com.br/esporte/2026/03/sob-roman-abramovich-chelsea-violou-regras-por-anos-diz-premier-league.shtml",
+      "time": "19/03 · 13:00",
+      "pubDate": "19 Mar 2026 10:00:00 -0300"
+    },
+    {
+      "source": "Folha de S.Paulo",
+      "color": "#1a237e",
+      "title": "Rayane Fogal detalha prepara��o que a levou ao t�tulo do Arnold Classic Ohio",
+      "url": "https://redir.folha.com.br/redir/online/esporte/rss091/*https://www1.folha.uol.com.br/blogs/musculo/2026/03/rayane-fogal-detalha-preparacao-que-a-levou-ao-titulo-do-arnold-classic-ohio.shtml",
+      "time": "19/03 · 13:00",
+      "pubDate": "19 Mar 2026 10:00:00 -0300"
+    },
+    {
+      "source": "Bolavip",
+      "color": "#0055A5",
+      "title": "Corinthians enfrenta a Chapecoense, pelo Brasileirão, com seis jogadores pendurados e gera preocupação",
+      "url": "<![CDATA[https://br.bolavip.com/corinthians/corinthians-enfrenta-a-chapecoense-pelo-brasileirao-com-seis-jogadores-pendurados-e-gera-preocupacao]]>",
+      "time": "19/03 · 12:44",
+      "pubDate": "Thu, 19 Mar 2026 09:44:03 -0300"
+    },
+    {
+      "source": "Meu Timão",
+      "color": "#1a7a3a",
+      "title": "Presidente do Corinthians convoca CD para votar afastamento de Romeu Tuma Júnior",
+      "url": "https://www.meutimao.com.br/noticias-do-corinthians/529301/presidente-do-corinthians-convoca-cd-para-votar-afastamento-de-romeu-tuma-junior",
+      "time": "19/03 · 12:35",
+      "pubDate": "Thu, 19 Mar 2026 09:35:00 -0300"
+    },
+    {
+      "source": "Bolavip",
+      "color": "#0055A5",
+      "title": "Marcelo Paz descarta pressão sobre Dorival e garante permanência do técnico no Corinthians",
+      "url": "<![CDATA[https://br.bolavip.com/corinthians/marcelo-paz-descarta-pressao-sobre-dorival-e-garante-permanencia-do-tecnico-no-corinthians]]>",
+      "time": "19/03 · 12:33",
+      "pubDate": "Thu, 19 Mar 2026 09:33:20 -0300"
+    },
+    {
+      "source": "Meu Timão",
+      "color": "#1a7a3a",
+      "title": "Corinthians conhece grupo da Libertadores 2026 nesta quinta-feira; saiba tudo sobre o sorteio",
+      "url": "https://www.meutimao.com.br/noticias-do-corinthians/529263/corinthians-conhece-grupo-da-libertadores-2026-nesta-quinta-feira-saiba-tudo-sobre-o-sorteio",
+      "time": "19/03 · 12:32",
+      "pubDate": "Thu, 19 Mar 2026 09:32:00 -0300"
+    },
+    {
+      "source": "Bolavip",
+      "color": "#0055A5",
+      "title": "Corinthians terá Yuri Alberto contra Chapecoense após atacante adiar retorno para ganhar mais confiança",
+      "url": "<![CDATA[https://br.bolavip.com/corinthians/corinthians-tera-yuri-alberto-contra-chapecoense-apos-atacante-adiar-retorno-para-ganhar-mais-confianca]]>",
+      "time": "19/03 · 12:07",
+      "pubDate": "Thu, 19 Mar 2026 09:07:00 -0300"
+    },
+    {
+      "source": "Bolavip",
+      "color": "#0055A5",
+      "title": "Maceió é colocado na mesa do Corinthians e contratação é recusada pela diretoria nos bastidores",
+      "url": "<![CDATA[https://br.bolavip.com/corinthians/maceio-e-colocado-na-mesa-do-corinthians-e-contratacao-e-recusada-pela-diretoria-nos-bastidores]]>",
+      "time": "19/03 · 11:33",
+      "pubDate": "Thu, 19 Mar 2026 08:33:51 -0300"
+    },
+    {
+      "source": "Meu Timão",
+      "color": "#1a7a3a",
+      "title": "Saiba a única opção para assistir ao confronto entre Corinthians e Chapecoense pelo Brasileirão",
+      "url": "https://www.meutimao.com.br/noticias-do-corinthians/529273/saiba-a-unica-opcao-para-assistir-ao-confronto-entre-corinthians-e-chapecoense-pelo-brasileirao",
+      "time": "19/03 · 11:30",
+      "pubDate": "Thu, 19 Mar 2026 08:30:00 -0300"
+    },
+    {
+      "source": "Folha de S.Paulo",
+      "color": "#1a237e",
+      "title": "Santos demite Vojvoda e anuncia contrata��o de Cuca",
+      "url": "https://redir.folha.com.br/redir/online/esporte/rss091/*https://www1.folha.uol.com.br/esporte/2026/03/santos-demite-vojvoda-e-tem-cuca-como-substituto-preferido.shtml",
+      "time": "19/03 · 04:36",
+      "pubDate": "19 Mar 2026 01:36:00 -0300"
+    },
+    {
+      "source": "Folha de S.Paulo",
+      "color": "#1a237e",
+      "title": "Programa gratuito promove trilhas por parques de S�o Paulo",
+      "url": "https://redir.folha.com.br/redir/online/esporte/rss091/*https://www1.folha.uol.com.br/blogs/e-logo-ali/2026/03/programa-gratuito-promove-trilhas-por-parques-de-sao-paulo.shtml",
+      "time": "19/03 · 02:00",
+      "pubDate": "18 Mar 2026 23:00:00 -0300"
+    },
+    {
+      "source": "Folha de S.Paulo",
+      "color": "#1a237e",
+      "title": "Messi alcan�a marca de 900 gols na carreira",
+      "url": "https://redir.folha.com.br/redir/online/esporte/rss091/*https://www1.folha.uol.com.br/esporte/2026/03/messi-alcanca-marca-de-900-gols-na-carreira.shtml",
+      "time": "19/03 · 00:54",
+      "pubDate": "18 Mar 2026 21:54:00 -0300"
+    },
+    {
+      "source": "Folha de S.Paulo",
+      "color": "#1a237e",
+      "title": "&apos;Po, Ancelotti, e eu?&apos;, reage Neymar durante a convoca��o",
+      "url": "https://redir.folha.com.br/redir/online/esporte/rss091/*https://www1.folha.uol.com.br/esporte/2026/03/po-ancelotti-e-eu-reage-neymar-durante-a-convocacao.shtml",
+      "time": "18/03 · 23:44",
+      "pubDate": "18 Mar 2026 20:44:00 -0300"
+    },
+    {
+      "source": "Gazeta Esportiva",
+      "color": "#E5820A",
+      "title": "Memphis decepciona em nova função no Corinthians",
+      "url": "https://www.gazetaesportiva.com/times/corinthians/memphis-decepciona-em-nova-funcao-no-corinthians/",
+      "time": "02/03 · 10:00",
+      "pubDate": "Mon, 02 Mar 2026 10:00:10 +0000"
+    },
+    {
+      "source": "Gazeta Esportiva",
+      "color": "#E5820A",
+      "title": "Sem Yuri Alberto, média de gols do Corinthians despenca",
+      "url": "https://www.gazetaesportiva.com/times/corinthians/sem-yuri-alberto-media-de-gols-do-corinthians-despenca/",
+      "time": "02/03 · 09:00",
+      "pubDate": "Mon, 02 Mar 2026 09:00:19 +0000"
+    },
+    {
+      "source": "Gazeta Esportiva",
+      "color": "#E5820A",
+      "title": "Saiba os números e os concorrentes de Lingard, meia próximo de reforçar o Corinthians",
+      "url": "https://www.gazetaesportiva.com/times/corinthians/saiba-os-numeros-e-os-concorrentes-de-lingard-atacante-proximo-de-reforcar-o-corinthians/",
+      "time": "02/03 · 08:00",
+      "pubDate": "Mon, 02 Mar 2026 08:00:34 +0000"
+    },
+    {
+      "source": "Gazeta Esportiva",
+      "color": "#E5820A",
+      "title": "Grêmio x Corinthians: veja prováveis escalações e onde assistir ao jogo pelo Brasileiro sub-20",
+      "url": "https://www.gazetaesportiva.com/times/corinthians/gremio-x-corinthians-veja-provaveis-escalacoes-e-onde-assistir-ao-jogo-pelo-brasileiro-sub-20/",
+      "time": "02/03 · 01:05",
+      "pubDate": "Mon, 02 Mar 2026 01:05:07 +0000"
+    },
+    {
+      "source": "Gazeta Esportiva",
+      "color": "#E5820A",
+      "title": "Presidente do Corinthians veta saída de André, e Milan ameaça ir à Fifa",
+      "url": "https://www.gazetaesportiva.com/times/corinthians/presidente-do-corinthians-veta-saida-de-andre-e-milan-ameaca-ir-a-fifa/",
+      "time": "02/03 · 00:27",
+      "pubDate": "Mon, 02 Mar 2026 00:27:33 +0000"
+    },
+    {
+      "source": "Gazeta Esportiva",
+      "color": "#E5820A",
+      "title": "Corinthians: conheça os números da equipe na temporada 2026",
+      "url": "https://www.gazetaesportiva.com/times/corinthians/corinthians-conheca-os-numeros-da-equipe-na-temporada-2026/",
+      "time": "01/03 · 17:29",
+      "pubDate": "Sun, 01 Mar 2026 17:29:03 +0000"
+    }
+  ],
   "upcoming": [],
   "results": [],
   "standings": []
