88129d4e32fe5167eab332c5b70be049f6eef08f
GitHub Action <action@github.com>
2026-03-20 19:51:06 +0000

    Dados atualizados

--- src/data.js
+++ src/data.js
@@ -1,14 +1,70 @@
-// Gerado automaticamente em 20/03/2026, 16:24:55
+// Gerado automaticamente em 20/03/2026, 19:51:06
 const LIVE_DATA = {
-  "updatedAt": "2026-03-20T16:24:55.080Z",
+  "updatedAt": "2026-03-20T19:51:06.403Z",
   "news": [
+    {
+      "source": "Meu Timao",
+      "color": "#1a7a3a",
+      "title": "Pantaleão alega ameaças no Corinthians e se nega a participar de votação sobre afastamento de Tuma",
+      "url": "https://www.meutimao.com.br/noticias-do-corinthians/529444/pantaleao-alega-ameacas-no-corinthians-e-se-nega-a-participar-de-votacao-sobre-afastamento-de-tuma",
+      "time": "20/03 · 19:37",
+      "pubDate": "Fri, 20 Mar 2026 16:37:00 -0300"
+    },
+    {
+      "source": "Meu Timao",
+      "color": "#1a7a3a",
+      "title": "Corinthians terá a chance de revanche, confronto inédito e reencontro de uma década na Libertadores",
+      "url": "https://www.meutimao.com.br/noticias-do-corinthians/529404/corinthians-tera-a-chance-de-revanche-confronto-inedito-e-reencontro-de-uma-decada-na-libertadores",
+      "time": "20/03 · 19:31",
+      "pubDate": "Fri, 20 Mar 2026 16:31:00 -0300"
+    },
+    {
+      "source": "Bolavip",
+      "color": "#0055A5",
+      "title": "Corinthians avança por Arthur Cabral e formaliza proposta ao Botafogo por empréstimo até dezembro",
+      "url": "<![CDATA[https://br.bolavip.com/corinthians/corinthians-avanca-por-arthur-cabral-e-formaliza-proposta-ao-botafogo-por-emprestimo-ate-dezembro]]>",
+      "time": "20/03 · 19:28",
+      "pubDate": "Fri, 20 Mar 2026 16:28:40 -0300"
+    },
+    {
+      "source": "Meu Timao",
+      "color": "#1a7a3a",
+      "title": "MP recebe denúncia de associados do Corinthians sobre convocação de Stabile contra Tuma",
+      "url": "https://www.meutimao.com.br/noticias-do-corinthians/529438/mp-recebe-denuncia-de-associados-do-corinthians-sobre-convocacao-de-stabile-contra-tuma",
+      "time": "20/03 · 18:47",
+      "pubDate": "Fri, 20 Mar 2026 15:47:00 -0300"
+    },
+    {
+      "source": "Meu Timao",
+      "color": "#1a7a3a",
+      "title": "Corinthians mantém invencibilidade e alcança melhor início no Brasileiro Feminino Sub-20",
+      "url": "https://www.meutimao.com.br/noticias-do-corinthians/529338/corinthians-mantem-invencibilidade-e-alcanca-melhor-inicio-no-brasileiro-feminino-sub-20",
+      "time": "20/03 · 18:33",
+      "pubDate": "Fri, 20 Mar 2026 15:33:00 -0300"
+    },
     {
       "source": "Bolavip",
       "color": "#0055A5",
       "title": "Hugo Souza, do Corinthians, é convocado por Ancelotti para substituir lesionado Alisson nesta Data FIFA",
       "url": "<![CDATA[https://br.bolavip.com/corinthians/hugo-souza-do-corinthians-e-convocado-por-ancelotti-para-substituir-lesionado-alisson-nesta-data-fifa]]>",
-      "time": "20/03 · 16:14",
-      "pubDate": "Fri, 20 Mar 2026 13:14:56 -0300"
+      "time": "20/03 · 17:44",
+      "pubDate": "Fri, 20 Mar 2026 14:44:06 -0300"
+    },
+    {
+      "source": "Meu Timao",
+      "color": "#1a7a3a",
+      "title": "Corinthians mantém baixo aproveitamento em gramados sintéticos após empate com a Chapecoense",
+      "url": "https://www.meutimao.com.br/noticias-do-corinthians/529395/corinthians-mantem-baixo-aproveitamento-em-gramados-sinteticos-apos-empate-com-a-chapecoense",
+      "time": "20/03 · 17:34",
+      "pubDate": "Fri, 20 Mar 2026 14:34:00 -0300"
+    },
+    {
+      "source": "Meu Timao",
+      "color": "#1a7a3a",
+      "title": "André Ramalho reconhece insatisfação da torcida e projeta busca por reação do Corinthians",
+      "url": "https://www.meutimao.com.br/noticias-do-corinthians/529419/andre-ramalho-reconhece-insatisfacao-da-torcida-e-projeta-busca-por-reacao-do-corinthians",
+      "time": "20/03 · 16:33",
+      "pubDate": "Fri, 20 Mar 2026 13:33:00 -0300"
     },
     {
       "source": "Meu Timao",
@@ -35,28 +91,12 @@ const LIVE_DATA = {
       "pubDate": "Fri, 20 Mar 2026 11:54:30 -0300"
     },
     {
-      "source": "Meu Timao",
-      "color": "#1a7a3a",
-      "title": "Corinthians vê sequência de vitórias contra a Chapecoense encerrar após o empate pelo Brasileirão",
-      "url": "https://www.meutimao.com.br/noticias-do-corinthians/529372/corinthians-ve-sequencia-de-vitorias-contra-a-chapecoense-encerrar-apos-o-empate-pelo-brasileirao",
-      "time": "20/03 · 14:31",
-      "pubDate": "Fri, 20 Mar 2026 11:31:00 -0300"
-    },
-    {
       "source": "Bolavip",
       "color": "#0055A5",
       "title": "Corinthians define futuro de Dorival Júnior após sequência negativa e treinador foi mantido no cargo",
       "url": "<![CDATA[https://br.bolavip.com/corinthians/corinthians-define-futuro-de-dorival-junior-apos-sequencia-negativa-e-treinador-foi-mantido-no-cargo]]>",
       "time": "20/03 · 13:57",
       "pubDate": "Fri, 20 Mar 2026 10:57:54 -0300"
-    },
-    {
-      "source": "Meu Timao",
-      "color": "#1a7a3a",
-      "title": "Dorival coloca eventual demissão nas mãos da diretoria e confia na reação do Corinthians",
-      "url": "https://www.meutimao.com.br/noticias-do-corinthians/529398/dorival-coloca-eventual-demissao-nas-maos-da-diretoria-e-confia-na-reacao-do-corinthians",
-      "time": "20/03 · 13:31",
-      "pubDate": "Fri, 20 Mar 2026 10:31:00 -0300"
     },
     {
       "source": "Bolavip",
@@ -75,30 +115,6 @@ const LIVE_DATA = {
       "pubDate": "Fri, 20 Mar 2026 09:38:50 -0300"
     },
     {
-      "source": "Meu Timao",
-      "color": "#1a7a3a",
-      "title": "Zagueiro do Corinthians se destaca após empate fora de casa no Brasileirão",
-      "url": "https://www.meutimao.com.br/noticias-do-corinthians/529380/zagueiro-do-corinthians-se-destaca-apos-empate-fora-de-casa-no-brasileirao",
-      "time": "20/03 · 12:33",
-      "pubDate": "Fri, 20 Mar 2026 09:33:00 -0300"
-    },
-    {
-      "source": "Meu Timao",
-      "color": "#1a7a3a",
-      "title": "Memphis é convocado pela Holanda e pode desfalcar o Corinthians no Brasileirão",
-      "url": "https://www.meutimao.com.br/noticias-do-corinthians/529414/memphis-e-convocado-pela-holanda-e-pode-desfalcar-o-corinthians-no-brasileirao",
-      "time": "20/03 · 11:31",
-      "pubDate": "Fri, 20 Mar 2026 08:31:00 -0300"
-    },
-    {
-      "source": "Meu Timao",
-      "color": "#1a7a3a",
-      "title": "Corinthians visita o Brasília para manter sequência histórica no NBB; veja detalhes",
-      "url": "https://www.meutimao.com.br/noticias-do-corinthians/529327/corinthians-visita-o-brasilia-para-manter-sequencia-historica-no-nbb-veja-detalhes",
-      "time": "20/03 · 11:30",
-      "pubDate": "Fri, 20 Mar 2026 08:30:00 -0300"
-    },
-    {
       "source": "Bolavip",
       "color": "#0055A5",
       "title": "Corinthians encara Flamengo embalado, e Jardim avisa: “temos muito respeito” antes do duelo",
@@ -107,28 +123,12 @@ const LIVE_DATA = {
       "pubDate": "Fri, 20 Mar 2026 08:05:52 -0300"
     },
     {
-      "source": "Meu Timao",
-      "color": "#1a7a3a",
-      "title": "Últimas do Corinthians: empate no Brasileirão, sorteio da Libertadores e votação do Conselho",
-      "url": "https://www.meutimao.com.br/noticias-do-corinthians/529402/ultimas-do-corinthians-empate-no-brasileirao-sorteio-da-libertadores-e-votacao-do-conselho",
-      "time": "20/03 · 10:30",
-      "pubDate": "Fri, 20 Mar 2026 07:30:00 -0300"
-    },
-    {
       "source": "Bolavip",
       "color": "#0055A5",
       "title": "Eduardo Doma pede apoio da torcida da Chapecoense para enfrentar o Corinthians: “Vai fazer a diferença”",
       "url": "<![CDATA[https://br.bolavip.com/corinthians/eduardo-doma-pede-apoio-da-torcida-da-chapecoense-para-enfrentar-o-corinthians-vai-fazer-a-diferenca]]>",
       "time": "19/03 · 21:12",
       "pubDate": "Thu, 19 Mar 2026 18:12:11 -0300"
-    },
-    {
-      "source": "Bolavip",
-      "color": "#0055A5",
-      "title": "Chapecoense x Corinthians: inteligência artificial projeta empate na Arena Condá pelo Brasileirão",
-      "url": "<![CDATA[https://br.bolavip.com/corinthians/chapecoense-x-corinthians-inteligencia-artificial-projeta-empate-na-arena-conda-pelo-brasileirao]]>",
-      "time": "19/03 · 19:28",
-      "pubDate": "Thu, 19 Mar 2026 16:28:40 -0300"
     },
     {
       "source": "Gazeta Esportiva",
