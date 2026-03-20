8b69cbbdcb9d75d73ec12d0ef77d94db51800280
GitHub Action <action@github.com>
2026-03-20 10:01:45 +0000

    Dados atualizados

--- src/data.js
+++ src/data.js
@@ -1,22 +1,102 @@
-// Gerado automaticamente em 19/03/2026, 23:01:23
+// Gerado automaticamente em 20/03/2026, 10:01:45
 const LIVE_DATA = {
-  "updatedAt": "2026-03-19T23:01:23.060Z",
+  "updatedAt": "2026-03-20T10:01:45.477Z",
   "news": [
     {
-      "source": "Meu Timão",
-      "color": "#1a7a3a",
-      "title": "Corinthians enfrenta a Chapecoense em busca da sexta vitória seguida no duelo; veja o retrospecto",
-      "url": "https://www.meutimao.com.br/noticias-do-corinthians/529312/corinthians-enfrenta-a-chapecoense-em-busca-da-sexta-vitoria-seguida-no-duelo-veja-o-retrospecto",
-      "time": "19/03 · 22:30",
-      "pubDate": "Thu, 19 Mar 2026 19:30:00 -0300"
+      "source": "Bolavip",
+      "color": "#0055A5",
+      "title": "Corinthians oferece Angileri e Dieguinho por João Paulo, mas Santos recusa proposta; conversam seguem",
+      "url": "<![CDATA[https://br.bolavip.com/corinthians/corinthians-oferece-angileri-e-dieguinho-por-joao-paulo-mas-santos-recusa-proposta-conversam-seguem]]>",
+      "time": "20/03 · 08:35",
+      "pubDate": "Fri, 20 Mar 2026 05:35:22 -0300"
     },
     {
       "source": "Meu Timão",
       "color": "#1a7a3a",
-      "title": "Corinthians organiza evento para torcedores antes de duelo no domingo",
-      "url": "https://www.meutimao.com.br/noticias-do-corinthians/529337/corinthians-organiza-evento-para-torcedores-antes-de-duelo-no-domingo",
-      "time": "19/03 · 22:23",
-      "pubDate": "Thu, 19 Mar 2026 19:23:07 -0300"
+      "title": "VÍDEO: Hugo Souza comenta sobre a sua ausência na última convocação da Seleção Brasileira",
+      "url": "https://www.meutimao.com.br/videos-do-corinthians/126783/hugo-souza-comenta-sobre-a-sua-ausencia-na-ultima-convocacao-da-selecao-brasileira",
+      "time": "20/03 · 04:26",
+      "pubDate": "Fri, 20 Mar 2026 01:26:51 -0300"
+    },
+    {
+      "source": "Meu Timão",
+      "color": "#1a7a3a",
+      "title": "VÍDEO: Zona Mista Matheus Pereira: Empate com a Chapecoense e necessidade de resultados pressionam o elenco",
+      "url": "https://www.meutimao.com.br/videos-do-corinthians/126781/zona-mista-matheus-pereira-empate-com-a-chapecoense-e-necessidade-de-resultados-pressionam-o-elenco",
+      "time": "20/03 · 04:20",
+      "pubDate": "Fri, 20 Mar 2026 01:20:00 -0300"
+    },
+    {
+      "source": "Meu Timão",
+      "color": "#1a7a3a",
+      "title": "VÍDEO: André Ramalho projeta partida entre Corinthians e Flamengo pelo Brasileirão 2026",
+      "url": "https://www.meutimao.com.br/videos-do-corinthians/126780/andre-ramalho-projeta-partida-entre-corinthians-e-flamengo-pelo-brasileirao-2026",
+      "time": "20/03 · 04:15",
+      "pubDate": "Fri, 20 Mar 2026 01:15:18 -0300"
+    },
+    {
+      "source": "Meu Timão",
+      "color": "#1a7a3a",
+      "title": "Goleiro e atacante são poupados em noite de notas baixas da Fiel após empate contra a Chapecoense",
+      "url": "https://www.meutimao.com.br/noticias-do-corinthians/529397/goleiro-e-atacante-sao-poupados-em-noite-de-notas-baixas-da-fiel-apos-empate-contra-a-chapecoense",
+      "time": "20/03 · 03:56",
+      "pubDate": "Fri, 20 Mar 2026 00:56:00 -0300"
+    },
+    {
+      "source": "Meu Timão",
+      "color": "#1a7a3a",
+      "title": "Dorival lamenta novos desfalques no Corinthians e revela dificuldade física de Memphis",
+      "url": "https://www.meutimao.com.br/noticias-do-corinthians/529396/dorival-lamenta-novos-desfalques-no-corinthians-e-revela-dificuldade-fisica-de-memphis",
+      "time": "20/03 · 03:53",
+      "pubDate": "Fri, 20 Mar 2026 00:53:00 -0300"
+    },
+    {
+      "source": "Meu Timão",
+      "color": "#1a7a3a",
+      "title": "Técnico do Corinthians avalia jogo intenso e aponta ineficácia ofensiva em empate com a Chapecoense",
+      "url": "https://www.meutimao.com.br/noticias-do-corinthians/529388/tecnico-do-corinthians-avalia-jogo-intenso-e-aponta-ineficacia-ofensiva-em-empate-com-a-chapecoense",
+      "time": "20/03 · 03:46",
+      "pubDate": "Fri, 20 Mar 2026 00:46:00 -0300"
+    },
+    {
+      "source": "Meu Timão",
+      "color": "#1a7a3a",
+      "title": "VÍDEO: Possibilidade de demissão, retorno de Yuri Alberto e mais: coletiva de Dorival Jr. no Corinthians",
+      "url": "https://www.meutimao.com.br/videos-do-corinthians/126777/possibilidade-de-demissao-retorno-de-yuri-alberto-e-mais-coletiva-de-dorival-jr-no-corinthians",
+      "time": "20/03 · 03:31",
+      "pubDate": "Fri, 20 Mar 2026 00:31:00 -0300"
+    },
+    {
+      "source": "Meu Timão",
+      "color": "#1a7a3a",
+      "title": "Atacante do Corinthians volta a atuar depois de um mês lesionado",
+      "url": "https://www.meutimao.com.br/noticias-do-corinthians/529367/atacante-do-corinthians-volta-a-atuar-depois-de-um-mes-lesionado",
+      "time": "20/03 · 03:18",
+      "pubDate": "Fri, 20 Mar 2026 00:18:00 -0300"
+    },
+    {
+      "source": "Bolavip",
+      "color": "#0055A5",
+      "title": "Corinthians e Chapecoense empatam sem gols pela 7ª rodada do Brasileirão",
+      "url": "<![CDATA[https://br.bolavip.com/corinthians/corinthians-e-chapecoense-empatam-sem-gols-pela-7a-rodada-do-brasileirao]]>",
+      "time": "20/03 · 02:45",
+      "pubDate": "Thu, 19 Mar 2026 23:45:11 -0300"
+    },
+    {
+      "source": "Folha de S.Paulo",
+      "color": "#1a237e",
+      "title": "Fifa aplica san��es � Associa��o de Futebol de Israel por discrimina��o contra palestinos",
+      "url": "https://redir.folha.com.br/redir/online/esporte/rss091/*https://www1.folha.uol.com.br/esporte/2026/03/fifa-aplica-sancoes-a-associacao-de-futebol-de-israel-por-discriminacao-contra-palestinos.shtml",
+      "time": "20/03 · 02:00",
+      "pubDate": "19 Mar 2026 23:00:00 -0300"
+    },
+    {
+      "source": "Folha de S.Paulo",
+      "color": "#1a237e",
+      "title": "Libertadores define grupos; veja advers�rios dos times brasileiros",
+      "url": "https://redir.folha.com.br/redir/online/esporte/rss091/*https://www1.folha.uol.com.br/esporte/2026/03/libertadores-define-grupos-veja-adversarios-dos-times-brasileiros.shtml",
+      "time": "20/03 · 00:32",
+      "pubDate": "19 Mar 2026 21:32:00 -0300"
     },
     {
       "source": "Folha de S.Paulo",
@@ -29,58 +109,10 @@ const LIVE_DATA = {
     {
       "source": "Bolavip",
       "color": "#0055A5",
-      "title": "Corinthians oferece Angileri e Dieguinho por João Paulo, mas Santos recusa proposta; conversam seguem",
-      "url": "<![CDATA[https://br.bolavip.com/corinthians/corinthians-oferece-angileri-e-dieguinho-por-joao-paulo-mas-santos-recusa-proposta-conversam-seguem]]>",
-      "time": "19/03 · 21:50",
-      "pubDate": "Thu, 19 Mar 2026 18:50:14 -0300"
-    },
-    {
-      "source": "Meu Timão",
-      "color": "#1a7a3a",
-      "title": "Em busca de reação e apostando em 'lei do ex': como vem a Chapecoense para enfrentar Corinthians",
-      "url": "https://www.meutimao.com.br/noticias-do-corinthians/529318/em-busca-de-reacao-e-apostando-em-lei-do-ex-como-vem-a-chapecoense-para-enfrentar-corinthians",
-      "time": "19/03 · 21:31",
-      "pubDate": "Thu, 19 Mar 2026 18:31:00 -0300"
-    },
-    {
-      "source": "Meu Timão",
-      "color": "#1a7a3a",
-      "title": "Corinthians conhece adversários e formato da décima edição do Paulistão Feminino Sub-17",
-      "url": "https://www.meutimao.com.br/noticias-do-corinthians/529331/corinthians-conhece-adversarios-e-formato-da-decima-edicao-do-paulistao-feminino-sub-17",
-      "time": "19/03 · 21:29",
-      "pubDate": "Thu, 19 Mar 2026 18:29:00 -0300"
-    },
-    {
-      "source": "Bolavip",
-      "color": "#0055A5",
       "title": "Eduardo Doma pede apoio da torcida da Chapecoense para enfrentar o Corinthians: “Vai fazer a diferença”",
       "url": "<![CDATA[https://br.bolavip.com/corinthians/eduardo-doma-pede-apoio-da-torcida-da-chapecoense-para-enfrentar-o-corinthians-vai-fazer-a-diferenca]]>",
       "time": "19/03 · 21:12",
       "pubDate": "Thu, 19 Mar 2026 18:12:11 -0300"
-    },
-    {
-      "source": "Meu Timão",
-      "color": "#1a7a3a",
-      "title": "Zagueiro do Corinthians deve retornar ao time titular contra a Chapecoense após três jogos no banco",
-      "url": "https://www.meutimao.com.br/noticias-do-corinthians/529308/zagueiro-do-corinthians-deve-retornar-ao-time-titular-contra-a-chapecoense-apos-tres-jogos-no-banco",
-      "time": "19/03 · 20:31",
-      "pubDate": "Thu, 19 Mar 2026 17:31:00 -0300"
-    },
-    {
-      "source": "Meu Timão",
-      "color": "#1a7a3a",
-      "title": "Corinthians domina Red Bull Bragantino e vence a terceira seguida no Brasileirão Feminino Sub-20",
-      "url": "https://www.meutimao.com.br/noticias-do-corinthians/529323/corinthians-domina-red-bull-bragantino-e-vence-a-terceira-seguida-no-brasileirao-feminino-sub-20",
-      "time": "19/03 · 20:10",
-      "pubDate": "Thu, 19 Mar 2026 17:10:00 -0300"
-    },
-    {
-      "source": "Meu Timão",
-      "color": "#1a7a3a",
-      "title": "Atacantes reservas do Corinthians tentam retomar protagonismo contra a Chapecoense",
-      "url": "https://www.meutimao.com.br/noticias-do-corinthians/529307/atacantes-reservas-do-corinthians-tentam-retomar-protagonismo-contra-a-chapecoense",
-      "time": "19/03 · 19:31",
-      "pubDate": "Thu, 19 Mar 2026 16:31:00 -0300"
     },
     {
       "source": "Bolavip",
@@ -89,14 +121,6 @@ const LIVE_DATA = {
       "url": "<![CDATA[https://br.bolavip.com/corinthians/chapecoense-x-corinthians-inteligencia-artificial-projeta-empate-na-arena-conda-pelo-brasileirao]]>",
       "time": "19/03 · 19:28",
       "pubDate": "Thu, 19 Mar 2026 16:28:40 -0300"
-    },
-    {
-      "source": "Meu Timão",
-      "color": "#1a7a3a",
-      "title": "Corinthians atinge marca histórica de jogos no Brasileirão Feminino",
-      "url": "https://www.meutimao.com.br/noticias-do-corinthians/529320/corinthians-atinge-marca-historica-de-jogos-no-brasileirao-feminino",
-      "time": "19/03 · 19:16",
-      "pubDate": "Thu, 19 Mar 2026 16:16:00 -0300"
     },
     {
       "source": "Folha de S.Paulo",
@@ -139,14 +163,6 @@ const LIVE_DATA = {
       "pubDate": "Thu, 19 Mar 2026 12:48:08 -0300"
     },
     {
-      "source": "Bolavip",
-      "color": "#0055A5",
-      "title": "Corinthians tenta ampliar série positiva contra a Chapecoense após cinco vitórias seguidas",
-      "url": "<![CDATA[https://br.bolavip.com/corinthians/corinthians-tenta-ampliar-serie-positiva-contra-a-chapecoense-apos-cinco-vitorias-seguidas]]>",
-      "time": "19/03 · 14:04",
-      "pubDate": "Thu, 19 Mar 2026 11:04:44 -0300"
-    },
-    {
       "source": "Folha de S.Paulo",
       "color": "#1a237e",
       "title": "Coreia do Norte e Filipinas se classificam para Copa do Mundo feminina no Brasil",
@@ -177,22 +193,6 @@ const LIVE_DATA = {
       "url": "https://redir.folha.com.br/redir/online/esporte/rss091/*https://www1.folha.uol.com.br/blogs/musculo/2026/03/rayane-fogal-detalha-preparacao-que-a-levou-ao-titulo-do-arnold-classic-ohio.shtml",
       "time": "19/03 · 13:00",
       "pubDate": "19 Mar 2026 10:00:00 -0300"
-    },
-    {
-      "source": "Folha de S.Paulo",
-      "color": "#1a237e",
-      "title": "Santos demite Vojvoda e anuncia contrata��o de Cuca",
-      "url": "https://redir.folha.com.br/redir/online/esporte/rss091/*https://www1.folha.uol.com.br/esporte/2026/03/santos-demite-vojvoda-e-tem-cuca-como-substituto-preferido.shtml",
-      "time": "19/03 · 04:36",
-      "pubDate": "19 Mar 2026 01:36:00 -0300"
-    },
-    {
-      "source": "Folha de S.Paulo",
-      "color": "#1a237e",
-      "title": "Programa gratuito promove trilhas por parques de S�o Paulo",
-      "url": "https://redir.folha.com.br/redir/online/esporte/rss091/*https://www1.folha.uol.com.br/blogs/e-logo-ali/2026/03/programa-gratuito-promove-trilhas-por-parques-de-sao-paulo.shtml",
-      "time": "19/03 · 02:00",
-      "pubDate": "18 Mar 2026 23:00:00 -0300"
     },
     {
       "source": "Gazeta Esportiva",
