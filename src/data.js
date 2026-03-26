be997b2f735302d26a4bc410ad90c4ca0c10c661
GitHub Action <action@github.com>
2026-03-26 10:39:59 +0000

    Dados atualizados

--- src/data.js
+++ src/data.js
@@ -1,14 +1,102 @@
-// Gerado automaticamente em 25/03/2026, 23:52:38
+// Gerado automaticamente em 26/03/2026, 10:39:59
 const LIVE_DATA = {
-  "updatedAt": "2026-03-25T23:52:38.214Z",
+  "updatedAt": "2026-03-26T10:39:59.224Z",
   "news": [
     {
       "source": "Meu Timao",
       "color": "#1a7a3a",
-      "title": "Zagueiro do Corinthians se destaca em quesitos defensivos entre os jogadores do Brasileirão",
-      "url": "https://www.meutimao.com.br/noticias-do-corinthians/529727/zagueiro-do-corinthians-se-destaca-em-quesitos-defensivos-entre-os-jogadores-do-brasileirao",
-      "time": "25/03 · 23:30",
-      "pubDate": "Wed, 25 Mar 2026 20:30:00 -0300"
+      "title": "Últimas do Corinthians: troca por Arthur Cabral, visto de Lingard e volante cobiçado",
+      "url": "https://www.meutimao.com.br/noticias-do-corinthians/529808/ultimas-do-corinthians-troca-por-arthur-cabral-visto-de-lingard-e-volante-cobicado",
+      "time": "26/03 · 10:30",
+      "pubDate": "Thu, 26 Mar 2026 07:30:00 -0300"
+    },
+    {
+      "source": "Bolavip",
+      "color": "#0055A5",
+      "title": "Corinthians acelera busca por um goleiro para fazer ‘sombra’ a Hugo Souza no decorrer da temporada",
+      "url": "<![CDATA[https://br.bolavip.com/corinthians/corinthians-acelera-busca-por-um-goleiro-para-fazer-sombra-a-hugo-souza-no-decorrer-da-temporada]]>",
+      "time": "26/03 · 09:14",
+      "pubDate": "Thu, 26 Mar 2026 06:14:05 -0300"
+    },
+    {
+      "source": "Bolavip",
+      "color": "#0055A5",
+      "title": "André segue em alta no Corinthians e desperta interesse do Benfica e +2 clubes europeus",
+      "url": "<![CDATA[https://br.bolavip.com/corinthians/andre-segue-em-alta-no-corinthians-e-desperta-interesse-do-benfica]]>",
+      "time": "26/03 · 09:13",
+      "pubDate": "Thu, 26 Mar 2026 06:13:20 -0300"
+    },
+    {
+      "source": "Bolavip",
+      "color": "#0055A5",
+      "title": "Osmar Stabile confirma que Arthur Cabral foi oferecido ao Corinthians e prega cautela",
+      "url": "<![CDATA[https://br.bolavip.com/corinthians/osmar-stabile-confirma-que-arthur-cabral-foi-oferecido-ao-corinthians-e-prega-cautela]]>",
+      "time": "26/03 · 02:55",
+      "pubDate": "Wed, 25 Mar 2026 23:55:04 -0300"
+    },
+    {
+      "source": "Bolavip",
+      "color": "#0055A5",
+      "title": "Ex-Corinthians, Kazim vira técnico e tenta salvar Crawley Town de queda na League Two",
+      "url": "<![CDATA[https://br.bolavip.com/corinthians/ex-corinthians-kazim-vira-tecnico-e-tenta-salvar-crawley-town-de-queda-na-league-two]]>",
+      "time": "26/03 · 01:47",
+      "pubDate": "Wed, 25 Mar 2026 22:47:18 -0300"
+    },
+    {
+      "source": "Meu Timao",
+      "color": "#1a7a3a",
+      "title": "Presidente do Corinthians confirma conversas por Arthur Cabral e projeta renovação com Memphis",
+      "url": "https://www.meutimao.com.br/noticias-do-corinthians/529807/presidente-do-corinthians-confirma-conversas-por-arthur-cabral-e-projeta-renovacao-com-memphis",
+      "time": "26/03 · 01:32",
+      "pubDate": "Wed, 25 Mar 2026 22:32:00 -0300"
+    },
+    {
+      "source": "Meu Timao",
+      "color": "#1a7a3a",
+      "title": "Dorival Júnior fala sobre pressão no Corinthians e diz que 'coisas boas' estão acontecendo",
+      "url": "https://www.meutimao.com.br/noticias-do-corinthians/529600/dorival-junior-fala-sobre-pressao-no-corinthians-e-diz-que-coisas-boas-estao-acontecendo",
+      "time": "26/03 · 01:30",
+      "pubDate": "Wed, 25 Mar 2026 22:30:00 -0300"
+    },
+    {
+      "source": "Meu Timao",
+      "color": "#1a7a3a",
+      "title": "Presidente do Corinthians adota otimismo e projeta acordo com Talleres por dívida de Garro",
+      "url": "https://www.meutimao.com.br/noticias-do-corinthians/529806/presidente-do-corinthians-adota-otimismo-e-projeta-acordo-com-talleres-por-divida-de-garro",
+      "time": "26/03 · 01:12",
+      "pubDate": "Wed, 25 Mar 2026 22:12:00 -0300"
+    },
+    {
+      "source": "Meu Timao",
+      "color": "#1a7a3a",
+      "title": "Gabi Zanotti se torna a segunda jogadora com mais partidas na história do Corinthians",
+      "url": "https://www.meutimao.com.br/noticias-do-corinthians/529680/gabi-zanotti-se-torna-a-segunda-jogadora-com-mais-partidas-na-historia-do-corinthians",
+      "time": "26/03 · 01:00",
+      "pubDate": "Wed, 25 Mar 2026 22:00:00 -0300"
+    },
+    {
+      "source": "Meu Timao",
+      "color": "#1a7a3a",
+      "title": "Patrocinadora máster do Corinthians promove jantar para as Brabas no gramado da Neo Química Arena",
+      "url": "https://www.meutimao.com.br/noticias-do-corinthians/529805/patrocinadora-master-do-corinthians-promove-jantar-para-as-brabas-no-gramado-da-neo-quimica-arena",
+      "time": "26/03 · 00:44",
+      "pubDate": "Wed, 25 Mar 2026 21:44:00 -0300"
+    },
+    {
+      "source": "Meu Timao",
+      "color": "#1a7a3a",
+      "title": "Volante completa um mês sem entrar em campo pelo Corinthians",
+      "url": "https://www.meutimao.com.br/noticias-do-corinthians/529544/volante-completa-um-mes-sem-entrar-em-campo-pelo-corinthians",
+      "time": "26/03 · 00:30",
+      "pubDate": "Wed, 25 Mar 2026 21:30:00 -0300"
+    },
+    {
+      "source": "Meu Timao",
+      "color": "#1a7a3a",
+      "title": "Corinthians vê Zanotti manter liderança e meia entrar na lista de assistentes em 2026",
+      "url": "https://www.meutimao.com.br/noticias-do-corinthians/529732/corinthians-ve-zanotti-manter-lideranca-e-meia-entrar-na-lista-de-assistentes-em-2026",
+      "time": "26/03 · 00:02",
+      "pubDate": "Wed, 25 Mar 2026 21:02:00 -0300"
     },
     {
       "source": "Bolavip",
@@ -27,30 +115,6 @@ const LIVE_DATA = {
       "pubDate": "Wed, 25 Mar 2026 20:06:39 -0300"
     },
     {
-      "source": "Meu Timao",
-      "color": "#1a7a3a",
-      "title": "Ídolo do Corinthians faz treinamento junto do Sub-20 do futsal",
-      "url": "https://www.meutimao.com.br/noticias-do-corinthians/529797/idolo-do-corinthians-faz-treinamento-junto-do-sub-20-do-futsal",
-      "time": "25/03 · 23:03",
-      "pubDate": "Wed, 25 Mar 2026 20:03:00 -0300"
-    },
-    {
-      "source": "Meu Timao",
-      "color": "#1a7a3a",
-      "title": "Corinthians dispensa atacante do Sub-17 após menos de um ano no clube",
-      "url": "https://www.meutimao.com.br/noticias-do-corinthians/529803/corinthians-dispensa-atacante-do-sub-17-apos-menos-de-um-ano-no-clube",
-      "time": "25/03 · 22:47",
-      "pubDate": "Wed, 25 Mar 2026 19:47:00 -0300"
-    },
-    {
-      "source": "Meu Timao",
-      "color": "#1a7a3a",
-      "title": "Armador do Corinthians substitui lesionado e disputará Jogo das Estrelas do NBB",
-      "url": "https://www.meutimao.com.br/noticias-do-corinthians/529794/armador-do-corinthians-substitui-lesionado-e-disputara-jogo-das-estrelas-do-nbb",
-      "time": "25/03 · 22:32",
-      "pubDate": "Wed, 25 Mar 2026 19:32:00 -0300"
-    },
-    {
       "source": "Bolavip",
       "color": "#0055A5",
       "title": "Corinthians se reapresenta e intensifica preparação para duelo diante do Fluminense pelo Brasileirão",
@@ -59,76 +123,12 @@ const LIVE_DATA = {
       "pubDate": "Wed, 25 Mar 2026 19:27:57 -0300"
     },
     {
-      "source": "Meu Timao",
-      "color": "#1a7a3a",
-      "title": "Zagueiro do Corinthians valoriza pausa para Data Fifa antes de 'maratona' em abril",
-      "url": "https://www.meutimao.com.br/noticias-do-corinthians/529801/zagueiro-do-corinthians-valoriza-pausa-para-data-fifa-antes-de-maratona-em-abril",
-      "time": "25/03 · 22:12",
-      "pubDate": "Wed, 25 Mar 2026 19:12:00 -0300"
-    },
-    {
-      "source": "Meu Timao",
-      "color": "#1a7a3a",
-      "title": "Corinthians se reapresenta após folga na Data Fifa com atualizações do departamento médico",
-      "url": "https://www.meutimao.com.br/noticias-do-corinthians/529800/corinthians-se-reapresenta-apos-folga-na-data-fifa-com-atualizacoes-do-departamento-medico",
-      "time": "25/03 · 21:58",
-      "pubDate": "Wed, 25 Mar 2026 18:58:00 -0300"
-    },
-    {
-      "source": "Bolavip",
-      "color": "#0055A5",
-      "title": "Atlético de Madrid inicia contato com Corinthians pela contratação de André; Timão pede € 35 milhões",
-      "url": "<![CDATA[https://br.bolavip.com/corinthians/atletico-de-madrid-inicia-contato-com-corinthians-pela-contratacao-de-andre-timao-pede-eur-35-milhoes]]>",
-      "time": "25/03 · 21:46",
-      "pubDate": "Wed, 25 Mar 2026 18:46:13 -0300"
-    },
-    {
-      "source": "Meu Timao",
-      "color": "#1a7a3a",
-      "title": "Ala do Corinthians ultrapassa mil pontos no Novo Basquete Brasil",
-      "url": "https://www.meutimao.com.br/noticias-do-corinthians/529755/ala-do-corinthians-ultrapassa-mil-pontos-no-nbb",
-      "time": "25/03 · 21:30",
-      "pubDate": "Wed, 25 Mar 2026 18:30:00 -0300"
-    },
-    {
-      "source": "Meu Timao",
-      "color": "#1a7a3a",
-      "title": "Corinthians entrega parte de documentos solicitados pelo MP; EY e ata de reunião seguem pendentes",
-      "url": "https://www.meutimao.com.br/noticias-do-corinthians/529798/corinthians-entrega-parte-de-documentos-solicitados-pelo-mp-ey-e-ata-de-reuniao-seguem-pendentes",
-      "time": "25/03 · 21:16",
-      "pubDate": "Wed, 25 Mar 2026 18:16:48 -0300"
-    },
-    {
       "source": "Bolavip",
       "color": "#0055A5",
       "title": "Memphis Depay ganha força para deixar o Corinthians após a Copa; renovação fica difícil",
       "url": "<![CDATA[https://br.bolavip.com/corinthians/memphis-depay-ganha-forca-para-deixar-o-corinthians-apos-a-copa-renovacao-fica-dificil]]>",
       "time": "25/03 · 21:04",
       "pubDate": "Wed, 25 Mar 2026 18:04:43 -0300"
-    },
-    {
-      "source": "Bolavip",
-      "color": "#0055A5",
-      "title": "Lingard deve aparecer no BID até sexta-feira e pode estrear no Corinthians contra o Fluminense",
-      "url": "<![CDATA[https://br.bolavip.com/corinthians/lingard-corinthians-fluminense-maracana]]>",
-      "time": "25/03 · 19:50",
-      "pubDate": "Wed, 25 Mar 2026 16:50:27 -0300"
-    },
-    {
-      "source": "Bolavip",
-      "color": "#0055A5",
-      "title": "Corinthians pode negociar Matheus Bidu com o Flamengo em caso de “proposta irrecusável”, diz site",
-      "url": "<![CDATA[https://br.bolavip.com/corinthians/corinthians-pode-negociar-matheus-bidu-com-o-flamengo-em-caso-de-proposta-irrecusavel-diz-site]]>",
-      "time": "25/03 · 19:08",
-      "pubDate": "Wed, 25 Mar 2026 16:08:47 -0300"
-    },
-    {
-      "source": "Bolavip",
-      "color": "#0055A5",
-      "title": "Dorival Jr. tem ‘dor de cabeça’ no Corinthians e pode ter 7 desfalques contra o Fluminense",
-      "url": "<![CDATA[https://br.bolavip.com/corinthians/dorival-jr-tem-dor-de-cabeca-no-corinthians-e-pode-ter-7-desfalques-contra-o-fluminense]]>",
-      "time": "25/03 · 17:15",
-      "pubDate": "Wed, 25 Mar 2026 14:15:46 -0300"
     },
     {
       "source": "Gazeta Esportiva",
@@ -210,12 +210,6 @@ const LIVE_DATA = {
       "url": "https://www.meutimao.com.br/videos-do-corinthians/126854/entrevista-com-juca-kfouri-e-socrates-jr-em-evento-sobre-a-redemocratizacao-do-corinthians",
       "pubDate": "Wed, 25 Mar 2026 14:32:03 -0300",
       "date": "25/03 · 17:32"
-    },
-    {
-      "title": "Corinthians ainda busca goleiro experiente antes do fim da janela de transferências",
-      "url": "https://www.meutimao.com.br/noticias-do-corinthians/529781/corinthians-ainda-busca-goleiro-experiente-antes-do-fim-da-janela-de-transferencias",
-      "pubDate": "Wed, 25 Mar 2026 13:15:00 -0300",
-      "date": "25/03 · 16:15"
     }
   ]
 };
