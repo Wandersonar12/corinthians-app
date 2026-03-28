7c492711c425e283d2a703d32f4a121c011180b0
GitHub Action <action@github.com>
2026-03-28 23:02:02 +0000

    Dados atualizados

--- src/data.js
+++ src/data.js
@@ -1,7 +1,55 @@
-// Gerado automaticamente em 28/03/2026, 19:27:32
+// Gerado automaticamente em 28/03/2026, 23:02:02
 const LIVE_DATA = {
-  "updatedAt": "2026-03-28T19:27:32.723Z",
+  "updatedAt": "2026-03-28T23:02:02.144Z",
   "news": [
+    {
+      "source": "Meu Timao",
+      "color": "#1a7a3a",
+      "title": "Zanotti comenta sobre feito histórico com a camisa do Corinthians",
+      "url": "https://www.meutimao.com.br/noticias-do-corinthians/529932/zanotti-comenta-sobre-feito-historico-com-a-camisa-do-corinthians",
+      "time": "28/03 · 22:35",
+      "pubDate": "Sat, 28 Mar 2026 19:35:00 -0300"
+    },
+    {
+      "source": "Bolavip",
+      "color": "#0055A5",
+      "title": "Corinthians e Grêmio entram na disputa por Helitão e zagueiro vira alvo no mercado europeu",
+      "url": "<![CDATA[https://br.bolavip.com/corinthians/corinthians-e-gremio-entram-na-disputa-por-helitao-e-zagueiro-vira-alvo-no-mercado-europeu]]>",
+      "time": "28/03 · 21:50",
+      "pubDate": "Sat, 28 Mar 2026 18:50:49 -0300"
+    },
+    {
+      "source": "Meu Timao",
+      "color": "#1a7a3a",
+      "title": "Ex-líbero do Corinthians relembra quando tentou se autossabotar em peneira em clube rival",
+      "url": "https://www.meutimao.com.br/noticias-do-corinthians/529868/ex-libero-do-corinthians-relembra-quando-tentou-se-autossabotar-em-peneira-em-clube-rival",
+      "time": "28/03 · 21:29",
+      "pubDate": "Sat, 28 Mar 2026 18:29:00 -0300"
+    },
+    {
+      "source": "Bolavip",
+      "color": "#0055A5",
+      "title": "Zakaria Labyad ainda não convence no Corinthians e precisa melhorar aspectos para jogar com Dorival Jr",
+      "url": "<![CDATA[https://br.bolavip.com/corinthians/zakaria-labyad-ainda-nao-convence-no-corinthians-e-precisa-melhorar-aspectos-para-jogar-com-dorival-jr]]>",
+      "time": "28/03 · 21:14",
+      "pubDate": "Sat, 28 Mar 2026 18:14:08 -0300"
+    },
+    {
+      "source": "Bolavip",
+      "color": "#0055A5",
+      "title": "Campeão brasileiro pelo Corinthians, Vagner Love anuncia aposentadoria do futebol aos 41 anos",
+      "url": "<![CDATA[https://br.bolavip.com/corinthians/campeao-brasileiro-pelo-corinthians-vagner-love-anuncia-aposentadoria]]>",
+      "time": "28/03 · 21:11",
+      "pubDate": "Sat, 28 Mar 2026 18:11:20 -0300"
+    },
+    {
+      "source": "Meu Timao",
+      "color": "#1a7a3a",
+      "title": "Corinthians supera a marca de 1300 gols marcados; veja histórico completo",
+      "url": "https://www.meutimao.com.br/noticias-do-corinthians/529924/corinthians-supera-a-marca-de-1300-gols-marcados-veja-historico-completo",
+      "time": "28/03 · 20:31",
+      "pubDate": "Sat, 28 Mar 2026 17:31:00 -0300"
+    },
     {
       "source": "Meu Timao",
       "color": "#1a7a3a",
@@ -43,30 +91,6 @@ const LIVE_DATA = {
       "pubDate": "Sat, 28 Mar 2026 13:31:00 -0300"
     },
     {
-      "source": "Meu Timao",
-      "color": "#1a7a3a",
-      "title": "Corinthians projeta prazo para quitar dívida junto ao Shakhtar por Maycon",
-      "url": "https://www.meutimao.com.br/noticias-do-corinthians/529938/corinthians-projeta-prazo-para-quitar-divida-junto-ao-shakhtar-por-maycon",
-      "time": "28/03 · 15:28",
-      "pubDate": "Sat, 28 Mar 2026 12:28:27 -0300"
-    },
-    {
-      "source": "Meu Timao",
-      "color": "#1a7a3a",
-      "title": "Corinthians tenta encerrar tabu de quase quatro anos contra o Joinville na LNF",
-      "url": "https://www.meutimao.com.br/noticias-do-corinthians/529812/corinthians-tenta-encerrar-tabu-de-quase-quatro-anos-contra-o-joinville-na-lnf",
-      "time": "28/03 · 14:30",
-      "pubDate": "Sat, 28 Mar 2026 11:30:00 -0300"
-    },
-    {
-      "source": "Meu Timao",
-      "color": "#1a7a3a",
-      "title": "Tuma denuncia Stabile e diretor do Corinthians no MP por falso testemunho e coação de associado",
-      "url": "https://www.meutimao.com.br/noticias-do-corinthians/529934/tuma-denuncia-stabile-e-diretor-do-corinthians-no-mp-por-falso-testemunho-e-coacao-de-associado",
-      "time": "28/03 · 13:31",
-      "pubDate": "Sat, 28 Mar 2026 10:31:00 -0300"
-    },
-    {
       "source": "Bolavip",
       "color": "#0055A5",
       "title": "Lingard fica à disposição do Corinthians e Dorival Júnior toma decisão sobre onde escalar o inglês",
@@ -105,30 +129,6 @@ const LIVE_DATA = {
       "url": "<![CDATA[https://br.bolavip.com/corinthians/torcida-do-corinthians-critica-ausencia-de-murillo-na-convocacao-da-selecao-brasileira]]>",
       "time": "27/03 · 20:39",
       "pubDate": "Fri, 27 Mar 2026 17:39:43 -0300"
-    },
-    {
-      "source": "Bolavip",
-      "color": "#0055A5",
-      "title": "Corinthians é condenado pela CAS por dívida de Maycon e pode sofrer novo transfer ban da FIFA",
-      "url": "<![CDATA[https://br.bolavip.com/corinthians/corinthians-e-condenado-pela-cas-por-divida-de-maycon-e-pode-sofrer-novo-transfer-ban-da-fifa]]>",
-      "time": "27/03 · 18:19",
-      "pubDate": "Fri, 27 Mar 2026 15:19:13 -0300"
-    },
-    {
-      "source": "Bolavip",
-      "color": "#0055A5",
-      "title": "Gui Negão sofre lesão, é cortado da Seleção Brasileira Sub-20 e preocupa o Corinthians",
-      "url": "<![CDATA[https://br.bolavip.com/corinthians/gui-negao-sofre-lesao-e-cortado-da-selecao-brasileira-sub-20-e-preocupa-o-corinthians]]>",
-      "time": "27/03 · 17:47",
-      "pubDate": "Fri, 27 Mar 2026 14:47:52 -0300"
-    },
-    {
-      "source": "Bolavip",
-      "color": "#0055A5",
-      "title": "Corinthians decide não contratar nenhum centroavante após cancelar negociações por Arthur Cabral e Renê",
-      "url": "<![CDATA[https://br.bolavip.com/corinthians/corinthians-decide-nao-contratar-nenhum-centroavante-apos-cancelar-negociacoes-por-arthur-cabral-e-rene]]>",
-      "time": "27/03 · 17:41",
-      "pubDate": "Fri, 27 Mar 2026 14:41:02 -0300"
     },
     {
       "source": "Gazeta Esportiva",
@@ -199,6 +199,12 @@ const LIVE_DATA = {
   "results": [],
   "standings": [],
   "highlights": [
+    {
+      "title": "Corinthians supera a marca de 1300 gols marcados; veja histórico completo",
+      "url": "https://www.meutimao.com.br/noticias-do-corinthians/529924/corinthians-supera-a-marca-de-1300-gols-marcados-veja-historico-completo",
+      "pubDate": "Sat, 28 Mar 2026 17:31:00 -0300",
+      "date": "28/03 · 20:31"
+    },
     {
       "title": "Carrillo entra em campo pelo Peru contra Senegal na estreia de ex-Corinthians; saiba onde assistir",
       "url": "https://www.meutimao.com.br/noticias-do-corinthians/529899/carrillo-entra-em-campo-pelo-peru-contra-senegal-na-estreia-de-ex-corinthians-saiba-onde-assistir",
