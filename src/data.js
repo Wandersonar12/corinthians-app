1d3cb2f6f59655c33ced4e6ded54dacaba8c38bf
GitHub Action <action@github.com>
2026-04-06 11:14:41 +0000

    Dados atualizados

--- src/data.js
+++ src/data.js
@@ -1,46 +1,110 @@
-// Gerado automaticamente em 05/04/2026, 23:04:09
+// Gerado automaticamente em 06/04/2026, 11:14:41
 const LIVE_DATA = {
-  "updatedAt": "2026-04-05T23:04:09.503Z",
+  "updatedAt": "2026-04-06T11:14:41.026Z",
   "news": [
     {
       "source": "Meu Timao",
       "color": "#1a7a3a",
-      "title": "Estádio do Corinthians avança na instalação da estátua de São Jorge e se aproxima de inauguração",
-      "url": "https://www.meutimao.com.br/noticias-do-corinthians/530370/estadio-do-corinthians-avanca-na-instalacao-da-estatua-de-sao-jorge-e-se-aproxima-de-inauguracao",
-      "time": "05/04 · 22:15",
-      "pubDate": "Sun, 05 Apr 2026 19:15:00 -0300"
+      "title": "Últimas do Corinthians: derrota para Internacional, Dorival demitido e protesto da torcida",
+      "url": "https://www.meutimao.com.br/noticias-do-corinthians/530435/ultimas-do-corinthians-derrota-para-internacional-dorival-demitido-e-protesto-da-torcida",
+      "time": "06/04 · 10:30",
+      "pubDate": "Mon, 06 Apr 2026 07:30:00 -0300"
+    },
+    {
+      "source": "Bolavip",
+      "color": "#0055A5",
+      "title": "Carlos Carvalhal e Bruno Lage ganham força para substituir Dorival Júnior no Corinthians",
+      "url": "<![CDATA[https://br.bolavip.com/corinthians/carlos-carvalhal-e-bruno-lage-ganham-forca-para-substituir-dorival-junior-no-corinthians]]>",
+      "time": "06/04 · 09:12",
+      "pubDate": "Mon, 06 Apr 2026 06:12:36 -0300"
+    },
+    {
+      "source": "Bolavip",
+      "color": "#0055A5",
+      "title": "Dorival não resiste pressão no Corinthians e é demitido após derrota para o Internacional",
+      "url": "<![CDATA[https://br.bolavip.com/corinthians/dorival-nao-resiste-pressao-no-corinthians-e-e-demitido-apos-derrota-para-o-internacional]]>",
+      "time": "06/04 · 09:12",
+      "pubDate": "Mon, 06 Apr 2026 06:12:00 -0300"
+    },
+    {
+      "source": "Bolavip",
+      "color": "#0055A5",
+      "title": "Corinthians demite Dorival Júnior e pode receber interesse de Emiliano Díaz para assumir comando",
+      "url": "<![CDATA[https://br.bolavip.com/corinthians/corinthians-demite-dorival-junior-e-pode-receber-interesse-de-emiliano-diaz-para-assumir-comando]]>",
+      "time": "06/04 · 03:35",
+      "pubDate": "Mon, 06 Apr 2026 00:35:21 -0300"
     },
     {
       "source": "Meu Timao",
       "color": "#1a7a3a",
-      "title": "Corinthians vence o Tarumã e garante ida às quartas de final do Brasileirão Feminino Sub-20",
-      "url": "https://www.meutimao.com.br/noticias-do-corinthians/530367/corinthians-vence-o-taruma-e-garante-ida-as-quartas-de-final-do-brasileirao-feminino-sub-20",
-      "time": "05/04 · 21:32",
-      "pubDate": "Sun, 05 Apr 2026 18:32:00 -0300"
+      "title": "Corinthians divulga 19 jogadoras inscritas para a disputa da Teal Rising Cup",
+      "url": "https://www.meutimao.com.br/noticias-do-corinthians/530436/corinthians-divulga-19-jogadoras-inscritas-para-a-disputa-da-teal-rising-cup-veja-lista",
+      "time": "06/04 · 03:35",
+      "pubDate": "Mon, 06 Apr 2026 00:35:00 -0300"
     },
     {
       "source": "Meu Timao",
       "color": "#1a7a3a",
-      "title": "Corinthians está escalado para enfrentar o Internacional na Neo Química Arena",
-      "url": "https://www.meutimao.com.br/noticias-do-corinthians/530358/corinthians-esta-escalado-para-enfrentar-o-internacional-na-neo-quimica-arena",
-      "time": "05/04 · 21:31",
-      "pubDate": "Sun, 05 Apr 2026 18:31:00 -0300"
+      "title": "Elenco do Corinthians recebe avaliações bem abaixo da média após derrota para o Internacional",
+      "url": "https://www.meutimao.com.br/noticias-do-corinthians/530431/elenco-do-corinthians-recebe-avaliacoes-bem-abaixo-da-media-apos-derrota-para-o-internacional",
+      "time": "06/04 · 03:04",
+      "pubDate": "Mon, 06 Apr 2026 00:04:00 -0300"
+    },
+    {
+      "source": "Bolavip",
+      "color": "#0055A5",
+      "title": "Bernabei decide no fim, Inter vence o Corinthians e pune estratégia de Dorival em Itaquera",
+      "url": "<![CDATA[https://br.bolavip.com/corinthians/bernabei-decide-no-fim-inter-vence-o-corinthians-e-pune-estrategia-de-dorival-em-itaquera]]>",
+      "time": "06/04 · 02:30",
+      "pubDate": "Sun, 05 Apr 2026 23:30:39 -0300"
     },
     {
       "source": "Meu Timao",
       "color": "#1a7a3a",
-      "title": "VÍDEO: Ao vivo &#128308; Corinthians x Internacional | Narração e Imagens da NQA | 10ª rodada | BR 2026",
-      "url": "https://www.meutimao.com.br/videos-do-corinthians/126988/ao-vivo-corinthians-x-internacional-narracao-e-imagens-da-nqa-10-rodada-br-2026",
-      "time": "05/04 · 19:30",
-      "pubDate": "Sun, 05 Apr 2026 16:30:00 -0300"
+      "title": "José Martínez dispara contra Dorival Júnior após demissão do treinador pelo Corinthians",
+      "url": "https://www.meutimao.com.br/noticias-do-corinthians/530425/jose-martinez-dispara-contra-dorival-junior-apos-demissao-do-treinador-pelo-corinthians",
+      "time": "06/04 · 02:30",
+      "pubDate": "Sun, 05 Apr 2026 23:30:00 -0300"
     },
     {
       "source": "Meu Timao",
       "color": "#1a7a3a",
-      "title": "Corinthians busca manter vantagem em retrospecto diante do Internacional; veja números",
-      "url": "https://www.meutimao.com.br/noticias-do-corinthians/530352/corinthians-busca-manter-vantagem-em-retrospecto-diante-do-internacional-veja-numeros",
-      "time": "05/04 · 19:30",
-      "pubDate": "Sun, 05 Apr 2026 16:30:00 -0300"
+      "title": "Executivo do Corinthians explica motivos para demissão de Dorival Júnior",
+      "url": "https://www.meutimao.com.br/noticias-do-corinthians/530422/executivo-do-corinthians-explica-motivos-para-demissao-de-dorival-junior",
+      "time": "06/04 · 02:22",
+      "pubDate": "Sun, 05 Apr 2026 23:22:00 -0300"
+    },
+    {
+      "source": "Meu Timao",
+      "color": "#1a7a3a",
+      "title": "Zagueiro do Corinthians recebe terceiro cartão amarelo e será desfalque no Dérbi",
+      "url": "https://www.meutimao.com.br/noticias-do-corinthians/530420/zagueiro-do-corinthians-recebe-terceiro-cartao-amarelo-e-sera-desfalque-no-derbi",
+      "time": "06/04 · 02:17",
+      "pubDate": "Sun, 05 Apr 2026 23:17:00 -0300"
+    },
+    {
+      "source": "Meu Timao",
+      "color": "#1a7a3a",
+      "title": "VÍDEO: Demitido! Dorival Jr. deixa o comando do Corinthians após derrota para o Internacional.",
+      "url": "https://www.meutimao.com.br/videos-do-corinthians/127014/demitido-dorival-jr-deixa-o-comando-do-corinthians-apos-derrota-para-o-internacional",
+      "time": "06/04 · 02:14",
+      "pubDate": "Sun, 05 Apr 2026 23:14:14 -0300"
+    },
+    {
+      "source": "Meu Timao",
+      "color": "#1a7a3a",
+      "title": "Meia-atacante do Corinthians faz sua estreia como titular em derrota para o Internacional",
+      "url": "https://www.meutimao.com.br/noticias-do-corinthians/530378/meia-atacante-do-corinthians-faz-sua-estreia-como-titular-em-derrota-para-o-internacional",
+      "time": "06/04 · 02:10",
+      "pubDate": "Sun, 05 Apr 2026 23:10:00 -0300"
+    },
+    {
+      "source": "Bolavip",
+      "color": "#0055A5",
+      "title": "Corinthians perde para o Internacional por 1 a 0 e se aproxima da Z-4 do Brasileirão",
+      "url": "<![CDATA[https://br.bolavip.com/corinthians/corinthians-perde-para-o-internacional-por-1-a-0-e-se-aproxima-da-z-4-do-brasileirao]]>",
+      "time": "06/04 · 01:47",
+      "pubDate": "Sun, 05 Apr 2026 22:47:02 -0300"
     },
     {
       "source": "Bolavip",
@@ -49,30 +113,6 @@ const LIVE_DATA = {
       "url": "<![CDATA[https://br.bolavip.com/corinthians/hat-trick-de-yuri-alberto-marcou-ultima-vitoria-do-corinthians-sobre-o-internacional-na-neo-quimica-arena]]>",
       "time": "05/04 · 19:10",
       "pubDate": "Sun, 05 Apr 2026 16:10:17 -0300"
-    },
-    {
-      "source": "Meu Timao",
-      "color": "#1a7a3a",
-      "title": "Fixo do Corinthians fala sobre novas lideranças no elenco e elogia atletas promovidos da base",
-      "url": "https://www.meutimao.com.br/noticias-do-corinthians/530290/fixo-do-corinthians-fala-sobre-novas-liderancas-no-elenco-e-elogia-atletas-promovidos-da-base",
-      "time": "05/04 · 18:32",
-      "pubDate": "Sun, 05 Apr 2026 15:32:00 -0300"
-    },
-    {
-      "source": "Meu Timao",
-      "color": "#1a7a3a",
-      "title": "Corinthians pode subir até sete posições na tabela em caso de vitória diante do Internacional",
-      "url": "https://www.meutimao.com.br/noticias-do-corinthians/530350/corinthians-pode-subir-ate-sete-posicoes-na-tabela-em-caso-de-vitoria-diante-do-internacional",
-      "time": "05/04 · 17:30",
-      "pubDate": "Sun, 05 Apr 2026 14:30:00 -0300"
-    },
-    {
-      "source": "Meu Timao",
-      "color": "#1a7a3a",
-      "title": "IA projeta resultado do confronto entre Corinthians e Internacional na Neo Química Arena",
-      "url": "https://www.meutimao.com.br/noticias-do-corinthians/530348/ia-projeta-resultado-do-confronto-entre-corinthians-e-internacional-na-neo-quimica-arena",
-      "time": "05/04 · 17:00",
-      "pubDate": "Sun, 05 Apr 2026 14:00:00 -0300"
     },
     {
       "source": "Bolavip",
@@ -89,46 +129,6 @@ const LIVE_DATA = {
       "url": "<![CDATA[https://br.bolavip.com/corinthians/dorival-junior-enfrenta-internacional-pressionado-e-se-nao-vencer-deve-ser-demitido-pelo-corinthians]]>",
       "time": "05/04 · 11:00",
       "pubDate": "Sun, 05 Apr 2026 08:00:37 -0300"
-    },
-    {
-      "source": "Bolavip",
-      "color": "#0055A5",
-      "title": "Angileri ganha chance e destaca desafio do Corinthians contra o Internacional: “Jogo muito difícil”",
-      "url": "<![CDATA[https://br.bolavip.com/corinthians/angileri-ganha-chance-e-destaca-desafio-do-corinthians-contra-o-internacional-jogo-muito-dificil]]>",
-      "time": "05/04 · 02:09",
-      "pubDate": "Sat, 04 Apr 2026 23:09:11 -0300"
-    },
-    {
-      "source": "Bolavip",
-      "color": "#0055A5",
-      "title": "Escalação do Corinthians: Dorival Júnior prepara cinco alterações para enfrentar o Internacional",
-      "url": "<![CDATA[https://br.bolavip.com/corinthians/escalacao-do-corinthians-dorival-junior-prepara-cinco-alteracoes-para-enfrentar-o-internacional]]>",
-      "time": "04/04 · 23:51",
-      "pubDate": "Sat, 04 Apr 2026 20:51:49 -0300"
-    },
-    {
-      "source": "Bolavip",
-      "color": "#0055A5",
-      "title": "Andrés Vombergar é oferecido ao Corinthians, mas permanência de Pedro Raul impede a negociação",
-      "url": "<![CDATA[https://br.bolavip.com/corinthians/andres-vombergar-e-oferecido-ao-corinthians-mas-permanencia-de-pedro-raul-impede-um-acordo]]>",
-      "time": "04/04 · 21:50",
-      "pubDate": "Sat, 04 Apr 2026 18:50:42 -0300"
-    },
-    {
-      "source": "Bolavip",
-      "color": "#0055A5",
-      "title": "Vidente aponta energias positivas para o Corinthians em duelo contra o Internacional",
-      "url": "<![CDATA[https://br.bolavip.com/corinthians/vidente-aponta-energias-positivas-para-o-corinthians-em-duelo-contra-o-internacional]]>",
-      "time": "04/04 · 19:09",
-      "pubDate": "Sat, 04 Apr 2026 16:09:35 -0300"
-    },
-    {
-      "source": "Bolavip",
-      "color": "#0055A5",
-      "title": "Corinthians x Internacional: onde assistir ao vivo, horário e prováveis escalações pelo Brasileirão",
-      "url": "<![CDATA[https://br.bolavip.com/corinthians/corinthians-x-internacional-onde-assistir-ao-vivo-horario-e-provaveis-escalacoes-pelo-brasileirao]]>",
-      "time": "04/04 · 13:09",
-      "pubDate": "Sat, 04 Apr 2026 10:09:47 -0300"
     },
     {
       "source": "Gazeta Esportiva",
@@ -200,52 +200,22 @@ const LIVE_DATA = {
   "standings": [],
   "highlights": [
     {
-      "title": "VÍDEO: Ao vivo &#128308; Corinthians x Internacional | Narração e Imagens da NQA | 10ª rodada | BR 2026",
-      "url": "https://www.meutimao.com.br/videos-do-corinthians/126988/ao-vivo-corinthians-x-internacional-narracao-e-imagens-da-nqa-10-rodada-br-2026",
-      "pubDate": "Sun, 05 Apr 2026 16:30:00 -0300",
-      "date": "05/04 · 19:30"
+      "title": "VÍDEO: Demitido! Dorival Jr. deixa o comando do Corinthians após derrota para o Internacional.",
+      "url": "https://www.meutimao.com.br/videos-do-corinthians/127014/demitido-dorival-jr-deixa-o-comando-do-corinthians-apos-derrota-para-o-internacional",
+      "pubDate": "Sun, 05 Apr 2026 23:14:14 -0300",
+      "date": "06/04 · 02:14"
     },
     {
-      "title": "Técnica do Corinthians reforça importância de trabalho em grupo após mais uma goleada das Brabas",
-      "url": "https://www.meutimao.com.br/noticias-do-corinthians/530308/tecnica-do-corinthians-reforca-importancia-de-trabalho-em-grupo-apos-mais-uma-goleada-das-brabas",
-      "pubDate": "Sun, 05 Apr 2026 12:35:00 -0300",
-      "date": "05/04 · 15:35"
+      "title": "VÍDEO: Melhores Momentos: Corinthians 0x1 Internacional | 10ª rodada | Campeonato Brasileiro 2026",
+      "url": "https://www.meutimao.com.br/videos-do-corinthians/127009/melhores-momentos-corinthians-0x1-internacional-10-rodada-campeonato-brasileiro-2026",
+      "pubDate": "Sun, 05 Apr 2026 21:54:24 -0300",
+      "date": "06/04 · 00:54"
     },
     {
-      "title": "Clima leve e foco em melhora marcam bastidores da goleada do Corinthians no Brasileirão Feminino",
-      "url": "https://www.meutimao.com.br/noticias-do-corinthians/530339/clima-leve-e-foco-em-melhora-marcam-bastidores-da-goleada-do-corinthians-no-brasileirao-feminino",
-      "pubDate": "Sun, 05 Apr 2026 10:00:00 -0300",
-      "date": "05/04 · 13:00"
-    },
-    {
-      "title": "Saiba qual a única opção para assistir ao confronto entre Corinthians e Internacional",
-      "url": "https://www.meutimao.com.br/noticias-do-corinthians/530325/saiba-qual-a-unica-opcao-para-assistir-ao-confronto-entre-corinthians-e-internacional",
-      "pubDate": "Sun, 05 Apr 2026 08:30:00 -0300",
-      "date": "05/04 · 11:30"
-    },
-    {
-      "title": "Volante emprestado pelo Corinthians marca primeiro gol pelo Fortaleza; saiba como foi",
-      "url": "https://www.meutimao.com.br/noticias-do-corinthians/530335/volante-emprestado-pelo-corinthians-marca-primeiro-gol-pelo-fortaleza-saiba-como-foi",
-      "pubDate": "Sat, 04 Apr 2026 22:03:00 -0300",
-      "date": "05/04 · 01:03"
-    },
-    {
-      "title": "Goleiro do Corinthians comenta objetivos na LNF e papel de liderança no grupo",
-      "url": "https://www.meutimao.com.br/noticias-do-corinthians/530286/goleiro-do-corinthians-comenta-objetivos-na-lnf-e-papel-de-lideranca-no-grupo",
-      "pubDate": "Sat, 04 Apr 2026 21:00:00 -0300",
-      "date": "05/04 · 00:00"
-    },
-    {
-      "title": "Gabi Zanotti se aproxima dos 100 gols pelo Corinthians",
-      "url": "https://www.meutimao.com.br/noticias-do-corinthians/530313/gabi-zanotti-se-aproxima-dos-100-gols-pelo-corinthians",
-      "pubDate": "Sat, 04 Apr 2026 19:33:00 -0300",
-      "date": "04/04 · 22:33"
-    },
-    {
-      "title": "Fixo do Corinthians celebra gol contra o Magnus após período afastado por grave lesão",
-      "url": "https://www.meutimao.com.br/noticias-do-corinthians/530289/fixo-do-corinthians-celebra-gol-contra-o-magnus-apos-periodo-afastado-por-grave-lesao",
-      "pubDate": "Sat, 04 Apr 2026 18:33:00 -0300",
-      "date": "04/04 · 21:33"
+      "title": "Zagueiro do Corinthians fala sobre o momento ruim e estende responsabilidade para toda a equipe",
+      "url": "https://www.meutimao.com.br/noticias-do-corinthians/530397/zagueiro-do-corinthians-fala-sobre-o-momento-ruim-e-estende-responsabilidade-para-toda-a-equipe",
+      "pubDate": "Sun, 05 Apr 2026 21:54:00 -0300",
+      "date": "06/04 · 00:54"
     }
   ]
 };
