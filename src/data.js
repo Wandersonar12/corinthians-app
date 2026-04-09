25aa467126e3961bda311494637a9d53d88ef8f7
GitHub Action <action@github.com>
2026-04-09 11:33:27 +0000

    Dados atualizados

--- src/data.js
+++ src/data.js
@@ -1,7 +1,87 @@
-// Gerado automaticamente em 08/04/2026, 23:54:46
+// Gerado automaticamente em 09/04/2026, 11:33:27
 const LIVE_DATA = {
-  "updatedAt": "2026-04-08T23:54:46.887Z",
+  "updatedAt": "2026-04-09T11:33:27.871Z",
   "news": [
+    {
+      "source": "Bolavip",
+      "color": "#0055A5",
+      "title": "Sem tempo para treinar, Diniz lista problemas a corrigir em plena estreia do Corinthians na Libertadores",
+      "url": "<![CDATA[https://br.bolavip.com/corinthians/sem-tempo-para-treinar-diniz-lista-problemas-a-corrigir-em-plena-estreia-do-corinthians-na-libertadores]]>",
+      "time": "09/04 · 11:17",
+      "pubDate": "Thu, 09 Apr 2026 08:17:35 -0300"
+    },
+    {
+      "source": "Meu Timao",
+      "color": "#1a7a3a",
+      "title": "Últimas do Corinthians: provável escalação, demissão de gerente e manifestação do MP",
+      "url": "https://www.meutimao.com.br/noticias-do-corinthians/530643/ultimas-do-corinthians-provavel-escalacao-demissao-de-gerente-e-manifestacao-do-mp",
+      "time": "09/04 · 10:30",
+      "pubDate": "Thu, 09 Apr 2026 07:30:00 -0300"
+    },
+    {
+      "source": "Meu Timao",
+      "color": "#1a7a3a",
+      "title": "Corinthians visita o Platense pela primeira rodada da Copa Libertadores; saiba tudo",
+      "url": "https://www.meutimao.com.br/noticias-do-corinthians/530620/corinthians-visita-o-platense-pela-primeira-rodada-da-copa-libertadores-saiba-tudo",
+      "time": "09/04 · 03:01",
+      "pubDate": "Thu, 09 Apr 2026 00:01:00 -0300"
+    },
+    {
+      "source": "Bolavip",
+      "color": "#0055A5",
+      "title": "Martinelli sinaliza desejo de jogar no Corinthians e sonha com Libertadores: “Meu time do coração”",
+      "url": "<![CDATA[https://br.bolavip.com/corinthians/martinelli-sinaliza-desejo-de-jogar-no-corinthians]]>",
+      "time": "09/04 · 02:32",
+      "pubDate": "Wed, 08 Apr 2026 23:32:59 -0300"
+    },
+    {
+      "source": "Meu Timao",
+      "color": "#1a7a3a",
+      "title": "Corinthians enfrenta o Kansas City Current pela semifinal da Teal Rising Cup; saiba tudo",
+      "url": "https://www.meutimao.com.br/noticias-do-corinthians/530618/corinthians-enfrenta-o-kansas-city-current-pela-semifinal-da-teal-rising-cup-saiba-tudo",
+      "time": "09/04 · 02:30",
+      "pubDate": "Wed, 08 Apr 2026 23:30:00 -0300"
+    },
+    {
+      "source": "Meu Timao",
+      "color": "#1a7a3a",
+      "title": "Novo técnico do Corinthians elogia laterais e projeta possibilidade de convocações à Seleção",
+      "url": "https://www.meutimao.com.br/noticias-do-corinthians/530583/novo-tecnico-do-corinthians-elogia-laterais-e-projeta-possibilidade-de-convocacoes-a-selecao",
+      "time": "09/04 · 01:30",
+      "pubDate": "Wed, 08 Apr 2026 22:30:00 -0300"
+    },
+    {
+      "source": "Meu Timao",
+      "color": "#1a7a3a",
+      "title": "Corinthians faz jogo dominante e goleia o Praia Grande na estreia do Paulista de Futsal",
+      "url": "https://www.meutimao.com.br/noticias-do-corinthians/530633/corinthians-faz-jogo-dominante-e-goleia-o-praia-grande-na-estreia-do-paulista-de-futsal",
+      "time": "09/04 · 00:34",
+      "pubDate": "Wed, 08 Apr 2026 21:34:00 -0300"
+    },
+    {
+      "source": "Meu Timao",
+      "color": "#1a7a3a",
+      "title": "Começo de ano abaixo e destaques: conheça mais sobre o adversário do Corinthians na Teal Rising Cup",
+      "url": "https://www.meutimao.com.br/noticias-do-corinthians/530607/comeco-de-anoabaixo-e-destaques-conheca-mais-sobre-o-adversario-do-corinthians-na-teal-rising-cup",
+      "time": "09/04 · 00:32",
+      "pubDate": "Wed, 08 Apr 2026 21:32:00 -0300"
+    },
+    {
+      "source": "Meu Timao",
+      "color": "#1a7a3a",
+      "title": "Corinthians faz último treino antes de enfrentar o Kansas City Current pela Teal Rising Cup",
+      "url": "https://www.meutimao.com.br/noticias-do-corinthians/530641/corinthians-faz-ultimo-treino-antes-de-enfrentar-o-kansas-city-current-pela-teal-rising-cup",
+      "time": "09/04 · 00:19",
+      "pubDate": "Wed, 08 Apr 2026 21:19:00 -0300"
+    },
+    {
+      "source": "Meu Timao",
+      "color": "#1a7a3a",
+      "title": "Atacante do Corinthians sofre lesão no tornozelo e deve desfalcar início da temporada do Sub-17",
+      "url": "https://www.meutimao.com.br/noticias-do-corinthians/530639/atacante-do-corinthians-sofre-lesao-no-tornozelo-e-deve-desfalcar-inicio-da-temporada-do-sub-17",
+      "time": "09/04 · 00:09",
+      "pubDate": "Wed, 08 Apr 2026 21:09:00 -0300"
+    },
     {
       "source": "Bolavip",
       "color": "#0055A5",
@@ -9,38 +89,6 @@ const LIVE_DATA = {
       "url": "<![CDATA[https://br.bolavip.com/corinthians/escalacao-do-corinthians-diniz-deve-fazer-mudancas-no-ataque]]>",
       "time": "08/04 · 23:49",
       "pubDate": "Wed, 08 Apr 2026 20:49:36 -0300"
-    },
-    {
-      "source": "Meu Timao",
-      "color": "#1a7a3a",
-      "title": "Fernando Diniz explica escolhas para a comissão técnica que trouxe ao Corinthians",
-      "url": "https://www.meutimao.com.br/noticias-do-corinthians/530576/fernando-diniz-explica-escolhas-para-a-comissao-tecnica-que-trouxe-ao-corinthians",
-      "time": "08/04 · 23:31",
-      "pubDate": "Wed, 08 Apr 2026 20:31:00 -0300"
-    },
-    {
-      "source": "Meu Timao",
-      "color": "#1a7a3a",
-      "title": "Lateral do Corinthians celebra nova chance na Seleção Feminina após lesão",
-      "url": "https://www.meutimao.com.br/noticias-do-corinthians/530631/lateral-do-corinthians-celebra-nova-chance-na-selecao-feminina-apos-lesao",
-      "time": "08/04 · 23:03",
-      "pubDate": "Wed, 08 Apr 2026 20:03:00 -0300"
-    },
-    {
-      "source": "Meu Timao",
-      "color": "#1a7a3a",
-      "title": "Ala do Corinthians registra maior pontuação da carreira em vitória sobre o Osasco no NBB",
-      "url": "https://www.meutimao.com.br/noticias-do-corinthians/530593/ala-do-corinthians-registra-maior-pontuacao-da-carreira-em-vitoria-sobre-o-osasco-no-nbb",
-      "time": "08/04 · 22:32",
-      "pubDate": "Wed, 08 Apr 2026 19:32:00 -0300"
-    },
-    {
-      "source": "Meu Timao",
-      "color": "#1a7a3a",
-      "title": "VÍDEO: Desembarque do Corinthians em Buenos Aires para enfrentar o Platense pela Libertadores 2026",
-      "url": "https://www.meutimao.com.br/videos-do-corinthians/127073/desembarque-do-corinthians-em-buenos-aires-para-enfrentar-o-platense-pela-libertadores-2026",
-      "time": "08/04 · 22:30",
-      "pubDate": "Wed, 08 Apr 2026 19:30:43 -0300"
     },
     {
       "source": "Bolavip",
@@ -51,44 +99,12 @@ const LIVE_DATA = {
       "pubDate": "Wed, 08 Apr 2026 19:29:32 -0300"
     },
     {
-      "source": "Meu Timao",
-      "color": "#1a7a3a",
-      "title": "Volante do Corinthians celebra gol em empate no Brasileirão Sub-20",
-      "url": "https://www.meutimao.com.br/noticias-do-corinthians/530629/volante-do-corinthians-celebra-gol-em-empate-no-brasileirao-sub-20",
-      "time": "08/04 · 22:05",
-      "pubDate": "Wed, 08 Apr 2026 19:05:00 -0300"
-    },
-    {
       "source": "Bolavip",
       "color": "#0055A5",
       "title": "Diniz mira ‘resgate técnico’ no Corinthians e quer recuperar Garro, André, Bidon e até Alex Santana",
       "url": "<![CDATA[https://br.bolavip.com/corinthians/diniz-mira-resgate-tecnico-no-corinthians-e-quer-recuperar-garro-andre-bidon-e-ate-alex-santana]]>",
       "time": "08/04 · 21:50",
       "pubDate": "Wed, 08 Apr 2026 18:50:13 -0300"
-    },
-    {
-      "source": "Meu Timao",
-      "color": "#1a7a3a",
-      "title": "Fernando Diniz só trabalhou com um jogador do atual elenco do Corinthians na carreira",
-      "url": "https://www.meutimao.com.br/noticias-do-corinthians/530600/fernando-diniz-so-trabalhou-com-um-jogador-do-atual-elenco-do-corinthians-na-carreira",
-      "time": "08/04 · 21:30",
-      "pubDate": "Wed, 08 Apr 2026 18:30:00 -0300"
-    },
-    {
-      "source": "Meu Timao",
-      "color": "#1a7a3a",
-      "title": "Armador do Corinthians valoriza terceiro triunfo consecutivo e minimiza nova marca histórica",
-      "url": "https://www.meutimao.com.br/noticias-do-corinthians/530592/armador-do-corinthians-valoriza-terceiro-triunfo-consecutivo-e-minimiza-nova-marca-historica",
-      "time": "08/04 · 21:01",
-      "pubDate": "Wed, 08 Apr 2026 18:01:00 -0300"
-    },
-    {
-      "source": "Meu Timao",
-      "color": "#1a7a3a",
-      "title": "Volante do Corinthians conta como chegou ao clube e fala sobre dificuldades no início da carreira",
-      "url": "https://www.meutimao.com.br/noticias-do-corinthians/530603/volante-do-corinthians-conta-como-chegou-ao-clube-e-fala-sobre-dificuldades-no-inicio-da-carreira",
-      "time": "08/04 · 20:31",
-      "pubDate": "Wed, 08 Apr 2026 17:31:00 -0300"
     },
     {
       "source": "Bolavip",
@@ -113,22 +129,6 @@ const LIVE_DATA = {
       "url": "<![CDATA[https://br.bolavip.com/corinthians/antes-de-platense-x-corinthians-yuri-alberto-tem-desempenho-3-vezes-superior-ao-de-tomas-nasif]]>",
       "time": "08/04 · 19:41",
       "pubDate": "Wed, 08 Apr 2026 16:41:13 -0300"
-    },
-    {
-      "source": "Bolavip",
-      "color": "#0055A5",
-      "title": "Fernando Diniz ‘manda a real’ para os atletas do Corinthians e espera protagonismo na Libertadores",
-      "url": "<![CDATA[https://br.bolavip.com/corinthians/fernando-diniz-manda-a-real-para-os-atletas-do-corinthians-e-espera-protagonismo-na-libertadores]]>",
-      "time": "08/04 · 19:13",
-      "pubDate": "Wed, 08 Apr 2026 16:13:31 -0300"
-    },
-    {
-      "source": "Bolavip",
-      "color": "#0055A5",
-      "title": "Corinthians demite gerente Renan Bloise, tradutor de Memphis Depay, por ordem de Osmar Stábile",
-      "url": "<![CDATA[https://br.bolavip.com/corinthians/corinthians-demite-gerente-renan-bloise-tradutor-de-memphis-depay-por-ordem-de-osmar-stabile]]>",
-      "time": "08/04 · 19:10",
-      "pubDate": "Wed, 08 Apr 2026 16:10:37 -0300"
     },
     {
       "source": "Gazeta Esportiva",
@@ -199,6 +199,12 @@ const LIVE_DATA = {
   "results": [],
   "standings": [],
   "highlights": [
+    {
+      "title": "Corinthians faz jogo dominante e goleia o Praia Grande na estreia do Paulista de Futsal",
+      "url": "https://www.meutimao.com.br/noticias-do-corinthians/530633/corinthians-faz-jogo-dominante-e-goleia-o-praia-grande-na-estreia-do-paulista-de-futsal",
+      "pubDate": "Wed, 08 Apr 2026 21:34:00 -0300",
+      "date": "09/04 · 00:34"
+    },
     {
       "title": "VÍDEO: Desembarque do Corinthians em Buenos Aires para enfrentar o Platense pela Libertadores 2026",
       "url": "https://www.meutimao.com.br/videos-do-corinthians/127073/desembarque-do-corinthians-em-buenos-aires-para-enfrentar-o-platense-pela-libertadores-2026",
