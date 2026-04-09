4844018ca3eef73b6af6d5b364d4cee1bf8b710c
GitHub Action <action@github.com>
2026-04-09 23:55:47 +0000

    Dados atualizados

--- src/data.js
+++ src/data.js
@@ -1,7 +1,55 @@
-// Gerado automaticamente em 09/04/2026, 20:36:07
+// Gerado automaticamente em 09/04/2026, 23:55:47
 const LIVE_DATA = {
-  "updatedAt": "2026-04-09T20:36:07.533Z",
+  "updatedAt": "2026-04-09T23:55:47.897Z",
   "news": [
+    {
+      "source": "Meu Timao",
+      "color": "#1a7a3a",
+      "title": "Corinthians confirma lesões de trio antes de partida contra o Platense pela Libertadores",
+      "url": "https://www.meutimao.com.br/noticias-do-corinthians/530696/corinthians-confirma-lesoes-de-trio-antes-de-partida-contra-o-platense-pela-libertadores",
+      "time": "09/04 · 23:28",
+      "pubDate": "Thu, 09 Apr 2026 20:28:00 -0300"
+    },
+    {
+      "source": "Meu Timao",
+      "color": "#1a7a3a",
+      "title": "Corinthians está escalado para enfrentar o Platense na estreia da Libertadores",
+      "url": "https://www.meutimao.com.br/noticias-do-corinthians/530656/corinthians-esta-escalado-para-enfrentar-o-platense-na-estreia-da-libertadores",
+      "time": "09/04 · 22:46",
+      "pubDate": "Thu, 09 Apr 2026 19:46:00 -0300"
+    },
+    {
+      "source": "Bolavip",
+      "color": "#0055A5",
+      "title": "Platense x Corinthians: Alvinegro tem elenco cinco vezes mais valioso que o clube argentino",
+      "url": "<![CDATA[https://br.bolavip.com/corinthians/platense-x-corinthians-alvinegro-tem-elenco-cinco-vezes-mais-valioso-que-o-clube-argentino]]>",
+      "time": "09/04 · 22:29",
+      "pubDate": "Thu, 09 Apr 2026 19:29:34 -0300"
+    },
+    {
+      "source": "Meu Timao",
+      "color": "#1a7a3a",
+      "title": "Corinthians registra lateral destaque de time sensação na Copinha",
+      "url": "https://www.meutimao.com.br/noticias-do-corinthians/530684/corinthians-registra-lateral-destaque-de-time-sensacao-na-copinha",
+      "time": "09/04 · 22:13",
+      "pubDate": "Thu, 09 Apr 2026 19:13:00 -0300"
+    },
+    {
+      "source": "Meu Timao",
+      "color": "#1a7a3a",
+      "title": "Presidente do Conselho Fiscal do Corinthians é afastado de maneira cautelar; defesa está em curso",
+      "url": "https://www.meutimao.com.br/noticias-do-corinthians/530683/presidente-do-conselho-fiscal-do-corinthians-e-afastado-de-maneira-cautelar-defesa-esta-em-curso",
+      "time": "09/04 · 21:39",
+      "pubDate": "Thu, 09 Apr 2026 18:39:00 -0300"
+    },
+    {
+      "source": "Meu Timao",
+      "color": "#1a7a3a",
+      "title": "Corinthians apresenta a coleção de Sabotage com familiares do artista na Neo Química Arena",
+      "url": "https://www.meutimao.com.br/noticias-do-corinthians/530670/corinthians-apresenta-a-colecao-de-sabotage-com-familiares-do-artista-na-neo-quimica-arena",
+      "time": "09/04 · 21:32",
+      "pubDate": "Thu, 09 Apr 2026 18:32:00 -0300"
+    },
     {
       "source": "Bolavip",
       "color": "#0055A5",
@@ -9,6 +57,14 @@ const LIVE_DATA = {
       "url": "<![CDATA[https://br.bolavip.com/corinthians/vidente-projeta-resultado-positivo-do-corinthians-contra-o-platense-pela-libertadores-2026]]>",
       "time": "09/04 · 20:26",
       "pubDate": "Thu, 09 Apr 2026 17:26:57 -0300"
+    },
+    {
+      "source": "Meu Timao",
+      "color": "#1a7a3a",
+      "title": "Torcida do Corinthians relata dificuldades para comprar ingressos no Fiel Torcedor; veja comentários",
+      "url": "https://www.meutimao.com.br/noticias-do-corinthians/530675/torcida-do-corinthians-relata-dificuldades-para-comprar-ingressos-no-fiel-torcedor-veja-comentarios",
+      "time": "09/04 · 20:26",
+      "pubDate": "Thu, 09 Apr 2026 17:26:36 -0300"
     },
     {
       "source": "Meu Timao",
@@ -43,46 +99,6 @@ const LIVE_DATA = {
       "pubDate": "Thu, 09 Apr 2026 15:13:26 -0300"
     },
     {
-      "source": "Meu Timao",
-      "color": "#1a7a3a",
-      "title": "Corinthians tem sete desfalques e uma dúvida para estreia na Libertadores contra o Platense",
-      "url": "https://www.meutimao.com.br/noticias-do-corinthians/530654/corinthians-tem-sete-desfalques-e-uma-duvida-para-estreia-na-libertadores-contra-o-platense",
-      "time": "09/04 · 17:31",
-      "pubDate": "Thu, 09 Apr 2026 14:31:00 -0300"
-    },
-    {
-      "source": "Meu Timao",
-      "color": "#1a7a3a",
-      "title": "Corinthians conhece tabela detalhada das rodadas finais do Brasileirão Feminino",
-      "url": "https://www.meutimao.com.br/noticias-do-corinthians/530673/corinthians-conhece-tabela-detalhada-das-rodadas-finais-do-brasileirao-feminino",
-      "time": "09/04 · 17:27",
-      "pubDate": "Thu, 09 Apr 2026 14:27:00 -0300"
-    },
-    {
-      "source": "Meu Timao",
-      "color": "#1a7a3a",
-      "title": "Ala do Corinthians amplia liderança na lista de artilheiros do futsal em 2026",
-      "url": "https://www.meutimao.com.br/noticias-do-corinthians/530659/ala-do-corinthians-amplia-lideranca-na-lista-de-artilheiros-do-futsal-em-2026",
-      "time": "09/04 · 17:01",
-      "pubDate": "Thu, 09 Apr 2026 14:01:00 -0300"
-    },
-    {
-      "source": "Meu Timao",
-      "color": "#1a7a3a",
-      "title": "Memphis Depay é advertido pelo STJD por uso de celular em jogo do Corinthians",
-      "url": "https://www.meutimao.com.br/noticias-do-corinthians/530672/memphis-depay-e-advertido-pelo-stjd-por-uso-de-celular-em-jogo-do-corinthians",
-      "time": "09/04 · 16:32",
-      "pubDate": "Thu, 09 Apr 2026 13:32:00 -0300"
-    },
-    {
-      "source": "Meu Timao",
-      "color": "#1a7a3a",
-      "title": "Diniz projeta primeiros jogos no Corinthians e cobra retomada de força na Neo Química Arena",
-      "url": "https://www.meutimao.com.br/noticias-do-corinthians/530581/diniz-projeta-primeiros-jogos-no-corinthians-e-cobra-retomada-de-forca-na-neo-quimica-arena",
-      "time": "09/04 · 16:30",
-      "pubDate": "Thu, 09 Apr 2026 13:30:00 -0300"
-    },
-    {
       "source": "Bolavip",
       "color": "#0055A5",
       "title": "Corinthians tenta quebrar tabu de quatro anos na Argentina em estreia de Fernando Diniz",
@@ -105,22 +121,6 @@ const LIVE_DATA = {
       "url": "<![CDATA[https://br.bolavip.com/corinthians/abel-ferreira-destaca-cansaco-antes-de-corinthians-x-palmeiras-teremos-tempo-para-falar-disso-depois]]>",
       "time": "09/04 · 16:18",
       "pubDate": "Thu, 09 Apr 2026 13:18:29 -0300"
-    },
-    {
-      "source": "Meu Timao",
-      "color": "#1a7a3a",
-      "title": "Corinthians encara sétimo argentino diferente na Libertadores em meio a histórico ruim",
-      "url": "https://www.meutimao.com.br/noticias-do-corinthians/530630/corinthians-encara-setimo-argentino-diferente-na-libertadores-em-meio-a-historico-ruim",
-      "time": "09/04 · 16:01",
-      "pubDate": "Thu, 09 Apr 2026 13:01:00 -0300"
-    },
-    {
-      "source": "Bolavip",
-      "color": "#0055A5",
-      "title": "Fernando Diniz fará primeiro jogo pelo Corinthians contra o Platense; relembre as estreias do técnico",
-      "url": "<![CDATA[https://br.bolavip.com/corinthians/fernando-diniz-fara-primeiro-jogo-pelo-corinthians-contra-o-platense-relembre-as-estreias-do-tecnico]]>",
-      "time": "09/04 · 16:00",
-      "pubDate": "Thu, 09 Apr 2026 13:00:15 -0300"
     },
     {
       "source": "Bolavip",
@@ -200,6 +200,12 @@ const LIVE_DATA = {
   "standings": [],
   "highlights": [
     {
+      "title": "Momento instável após temporada histórica: veja como chega o Platense contra o Timão",
+      "url": "https://www.meutimao.com.br/noticias-do-corinthians/530657/momento-instavel-apos-temporada-historica-veja-como-chega-o-platense-contra-o-timao",
+      "pubDate": "Thu, 09 Apr 2026 17:32:00 -0300",
+      "date": "09/04 · 20:32"
+    },
+    {
       "title": "Joia da base do Corinthians estreia no profissional com assistência em goleada sobre o Praia Grande",
       "url": "https://www.meutimao.com.br/noticias-do-corinthians/530662/joia-da-base-do-corinthians-estreia-no-profissional-com-assistencia-em-goleada-sobre-o-praia-grande",
       "pubDate": "Thu, 09 Apr 2026 11:01:00 -0300",
@@ -228,18 +234,6 @@ const LIVE_DATA = {
       "url": "https://www.meutimao.com.br/noticias-do-corinthians/530633/corinthians-faz-jogo-dominante-e-goleia-o-praia-grande-na-estreia-do-paulista-de-futsal",
       "pubDate": "Wed, 08 Apr 2026 21:34:00 -0300",
       "date": "09/04 · 00:34"
-    },
-    {
-      "title": "VÍDEO: Desembarque do Corinthians em Buenos Aires para enfrentar o Platense pela Libertadores 2026",
-      "url": "https://www.meutimao.com.br/videos-do-corinthians/127073/desembarque-do-corinthians-em-buenos-aires-para-enfrentar-o-platense-pela-libertadores-2026",
-      "pubDate": "Wed, 08 Apr 2026 19:30:43 -0300",
-      "date": "08/04 · 22:30"
-    },
-    {
-      "title": "Volante do Corinthians celebra gol em empate no Brasileirão Sub-20",
-      "url": "https://www.meutimao.com.br/noticias-do-corinthians/530629/volante-do-corinthians-celebra-gol-em-empate-no-brasileirao-sub-20",
-      "pubDate": "Wed, 08 Apr 2026 19:05:00 -0300",
-      "date": "08/04 · 22:05"
     }
   ]
 };
