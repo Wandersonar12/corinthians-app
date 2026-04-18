d17460f3988b3943fa0433e080f822b811826863
GitHub Action <action@github.com>
2026-04-18 18:29:22 +0000

    Dados atualizados

--- src/data.js
+++ src/data.js
@@ -1,7 +1,95 @@
-// Gerado automaticamente em 18/04/2026, 12:32:14
+// Gerado automaticamente em 18/04/2026, 18:29:22
 const LIVE_DATA = {
-  "updatedAt": "2026-04-18T12:32:14.846Z",
+  "updatedAt": "2026-04-18T18:29:22.624Z",
   "news": [
+    {
+      "source": "Bolavip",
+      "color": "#0055A5",
+      "title": "Corinthians abre os cofres, paga dívida de Pedro Raul com o Toluca e evita novo problema com a Fifa",
+      "url": "<![CDATA[https://br.bolavip.com/corinthians/corinthians-abre-os-cofres-paga-divida-de-pedro-raul-com-o-toluca-e-evita-novo-problema-com-a-fifa]]>",
+      "time": "18/04 · 17:48",
+      "pubDate": "Sat, 18 Apr 2026 14:48:43 -0300"
+    },
+    {
+      "source": "Meu Timao",
+      "color": "#1a7a3a",
+      "title": "Corinthians tem sete desfalques e uma dúvida para enfrentar o Vitória pelo Brasileirão",
+      "url": "https://www.meutimao.com.br/noticias-do-corinthians/531298/corinthians-tem-sete-desfalques-e-uma-duvida-para-enfrentar-o-vitoria-pelo-brasileirao",
+      "time": "18/04 · 17:31",
+      "pubDate": "Sat, 18 Apr 2026 14:31:00 -0300"
+    },
+    {
+      "source": "Meu Timao",
+      "color": "#1a7a3a",
+      "title": "Vitória volta atrás e indica que terá Cacá em duelo contra o Corinthians; saiba o valor da multa",
+      "url": "https://www.meutimao.com.br/noticias-do-corinthians/531310/vitoria-volta-atras-e-indica-que-tera-caca-em-duelo-contra-o-corinthians-saiba-o-valor-da-multa",
+      "time": "18/04 · 16:52",
+      "pubDate": "Sat, 18 Apr 2026 13:52:59 -0300"
+    },
+    {
+      "source": "Meu Timao",
+      "color": "#1a7a3a",
+      "title": "Corinthians quita parcela da CNRD e paga nova prestação de dívida por Pedro Raul",
+      "url": "https://www.meutimao.com.br/noticias-do-corinthians/531311/corinthians-quita-parcela-da-cnrd-e-paga-nova-prestacao-de-divida-por-pedro-raul",
+      "time": "18/04 · 16:38",
+      "pubDate": "Sat, 18 Apr 2026 13:38:00 -0300"
+    },
+    {
+      "source": "Meu Timao",
+      "color": "#1a7a3a",
+      "title": "Corinthians pode entrar na zona de rebaixamento do Brasileirão se não vencer o Vitória; veja tabela",
+      "url": "https://www.meutimao.com.br/noticias-do-corinthians/531299/corinthians-pode-entrar-na-zona-de-rebaixamento-do-brasileirao-se-nao-vencer-o-vitoria-veja-tabela",
+      "time": "18/04 · 16:34",
+      "pubDate": "Sat, 18 Apr 2026 13:34:00 -0300"
+    },
+    {
+      "source": "Meu Timao",
+      "color": "#1a7a3a",
+      "title": "Dupla do Corinthians conquista ouro com o Brasil nos Jogos Sul-Americanos da Juventude",
+      "url": "https://www.meutimao.com.br/noticias-do-corinthians/531309/dupla-do-corinthians-conquista-ouro-com-o-brasil-nos-jogos-sul-americanos-da-juventude",
+      "time": "18/04 · 16:01",
+      "pubDate": "Sat, 18 Apr 2026 13:01:00 -0300"
+    },
+    {
+      "source": "Bolavip",
+      "color": "#0055A5",
+      "title": "Oscar Schmidt marcou Palmeiras e Corinthians e mudou até sua paixão no futebol",
+      "url": "<![CDATA[https://br.bolavip.com/corinthians/oscar-schmidt-marcou-palmeiras-e-corinthians-e-mudou-ate-sua-paixao-no-futebol]]>",
+      "time": "18/04 · 15:59",
+      "pubDate": "Sat, 18 Apr 2026 12:59:35 -0300"
+    },
+    {
+      "source": "Meu Timao",
+      "color": "#1a7a3a",
+      "title": "Ídolo do Corinthians pede busto em sua homenagem e elogia Fernando Diniz",
+      "url": "https://www.meutimao.com.br/noticias-do-corinthians/531256/idolo-do-corinthians-pede-busto-em-sua-homenagem-e-elogia-fernando-diniz",
+      "time": "18/04 · 15:35",
+      "pubDate": "Sat, 18 Apr 2026 12:35:00 -0300"
+    },
+    {
+      "source": "Bolavip",
+      "color": "#0055A5",
+      "title": "Corinthians inicia contatos por Everton Cebolinha e planeja investida para próxima janela",
+      "url": "<![CDATA[https://br.bolavip.com/corinthians/corinthians-inicia-contatos-por-everton-cebolinha-e-planeja-investida-para-proxima-janela]]>",
+      "time": "18/04 · 14:45",
+      "pubDate": "Sat, 18 Apr 2026 11:45:09 -0300"
+    },
+    {
+      "source": "Meu Timao",
+      "color": "#1a7a3a",
+      "title": "Jogadoras do Corinthians entram em campo pela Fifa Series e duelos na Liga das Nações",
+      "url": "https://www.meutimao.com.br/noticias-do-corinthians/531280/jogadoras-do-corinthians-entram-em-campo-pela-fifa-series-e-duelos-na-liga-das-nacoes",
+      "time": "18/04 · 14:33",
+      "pubDate": "Sat, 18 Apr 2026 11:33:00 -0300"
+    },
+    {
+      "source": "Meu Timao",
+      "color": "#1a7a3a",
+      "title": "Zagueiro do Corinthians ganha vaga no time ideal da segunda rodada da Libertadores",
+      "url": "https://www.meutimao.com.br/noticias-do-corinthians/531301/zagueiro-do-corinthians-ganha-vaga-no-time-ideal-da-segunda-rodada-da-libertadores",
+      "time": "18/04 · 14:01",
+      "pubDate": "Sat, 18 Apr 2026 11:01:00 -0300"
+    },
     {
       "source": "Bolavip",
       "color": "#0055A5",
@@ -11,36 +99,12 @@ const LIVE_DATA = {
       "pubDate": "Sat, 18 Apr 2026 09:28:45 -0300"
     },
     {
-      "source": "Meu Timao",
-      "color": "#1a7a3a",
-      "title": "Saiba onde assistir à partida entre Corinthians e Vitória pelo Brasileirão",
-      "url": "https://www.meutimao.com.br/noticias-do-corinthians/531273/saiba-onde-assistir-a-partida-entre-corinthians-e-vitoria-pelo-brasileirao",
-      "time": "18/04 · 11:30",
-      "pubDate": "Sat, 18 Apr 2026 08:30:00 -0300"
-    },
-    {
       "source": "Bolavip",
       "color": "#0055A5",
       "title": "Lenda do basquete mundial, Oscar Schmidt morre aos 68 anos; ídolo eternizou seu nome no Corinthians",
       "url": "<![CDATA[https://br.bolavip.com/corinthians/lenda-do-basquete-mundial-oscar-schmidt-morre-aos-68-anos-idolo-eternizou-seu-nome-no-corinthians]]>",
       "time": "18/04 · 11:08",
       "pubDate": "Sat, 18 Apr 2026 08:08:09 -0300"
-    },
-    {
-      "source": "Meu Timao",
-      "color": "#1a7a3a",
-      "title": "Últimas do Corinthians: punições ao Corinthians, ausência de emprestado e mudança na Arena",
-      "url": "https://www.meutimao.com.br/noticias-do-corinthians/531292/ultimas-do-corinthians-punicoes-ao-corinthians-ausencia-de-emprestado-e-mudanca-na-arena",
-      "time": "18/04 · 10:30",
-      "pubDate": "Sat, 18 Apr 2026 07:30:00 -0300"
-    },
-    {
-      "source": "Meu Timao",
-      "color": "#1a7a3a",
-      "title": "Corinthians encara o Vitória em busca de sequenciar vitórias na temporada; saiba tudo",
-      "url": "https://www.meutimao.com.br/noticias-do-corinthians/531265/corinthians-encara-o-vitoria-em-busca-de-sequenciar-vitorias-na-temporada-saiba-tudo",
-      "time": "18/04 · 03:01",
-      "pubDate": "Sat, 18 Apr 2026 00:01:00 -0300"
     },
     {
       "source": "Bolavip",
@@ -59,76 +123,12 @@ const LIVE_DATA = {
       "pubDate": "Fri, 17 Apr 2026 22:32:43 -0300"
     },
     {
-      "source": "Meu Timao",
-      "color": "#1a7a3a",
-      "title": "Passagem por Barcelona e formação humana: conheça Herlon Silva, técnico do Sub-13 do Corinthians",
-      "url": "https://www.meutimao.com.br/noticias-do-corinthians/531194/passagem-por-barcelona-e-formacao-humana-conheca-herlon-silva-tecnico-do-sub-13-do-corinthians",
-      "time": "18/04 · 00:32",
-      "pubDate": "Fri, 17 Apr 2026 21:32:00 -0300"
-    },
-    {
-      "source": "Meu Timao",
-      "color": "#1a7a3a",
-      "title": "Corinthians é decisivo no ataque e vence o Caxias do Sul pelo fechamento do NBB",
-      "url": "https://www.meutimao.com.br/noticias-do-corinthians/531283/corinthians-e-decisivo-no-ataque-e-vence-o-caxias-do-sul-pelo-fechamento-do-nbb",
-      "time": "18/04 · 00:23",
-      "pubDate": "Fri, 17 Apr 2026 21:23:00 -0300"
-    },
-    {
-      "source": "Meu Timao",
-      "color": "#1a7a3a",
-      "title": "Técnico do Sub-20 avalia empate do Corinthians no Brasileiro e exalta chegada de Fernando Diniz",
-      "url": "https://www.meutimao.com.br/noticias-do-corinthians/531277/tecnico-do-sub-20-avalia-empate-do-corinthians-no-brasileiro-e-exalta-chegada-de-fernando-diniz",
-      "time": "18/04 · 00:04",
-      "pubDate": "Fri, 17 Apr 2026 21:04:00 -0300"
-    },
-    {
-      "source": "Meu Timao",
-      "color": "#1a7a3a",
-      "title": "Corinthians conhece grupo e adversários da primeira fase do Paulista Sub-20; confira",
-      "url": "https://www.meutimao.com.br/noticias-do-corinthians/531288/corinthians-conhece-grupo-e-adversarios-da-primeira-fase-do-paulista-sub-20-confira",
-      "time": "17/04 · 23:43",
-      "pubDate": "Fri, 17 Apr 2026 20:43:00 -0300"
-    },
-    {
-      "source": "Meu Timao",
-      "color": "#1a7a3a",
-      "title": "Titular do Corinthians assume segunda posição entre zagueiros em participações em gols na década",
-      "url": "https://www.meutimao.com.br/noticias-do-corinthians/531251/titular-do-corinthians-assume-segunda-posicao-entre-zagueiros-em-participacoes-em-gols-na-decada",
-      "time": "17/04 · 23:31",
-      "pubDate": "Fri, 17 Apr 2026 20:31:00 -0300"
-    },
-    {
       "source": "Bolavip",
       "color": "#0055A5",
       "title": "Fernando Diniz pede lateral e Corinthians mira quatro reforços no mercado da bola, crava jornalista",
       "url": "<![CDATA[https://br.bolavip.com/corinthians/corinthians-diniz-contratacao-lateral]]>",
       "time": "17/04 · 21:33",
       "pubDate": "Fri, 17 Apr 2026 18:33:16 -0300"
-    },
-    {
-      "source": "Bolavip",
-      "color": "#0055A5",
-      "title": "Matheuzinho, do Corinthians, sofre suspensão de 4 jogos no STJD; André e Hugo Souza também são punidos",
-      "url": "<![CDATA[https://br.bolavip.com/corinthians/matheuzinho-do-corinthians-sofre-suspensao-de-4-jogos-no-stjd-andre-e-hugo-souza-tambem-sao-punidos]]>",
-      "time": "17/04 · 20:27",
-      "pubDate": "Fri, 17 Apr 2026 17:27:29 -0300"
-    },
-    {
-      "source": "Bolavip",
-      "color": "#0055A5",
-      "title": "Corinthians defende invencibilidade de quase uma década no Barradão em duelo decisivo contra o Vitória",
-      "url": "<![CDATA[https://br.bolavip.com/corinthians/corinthians-defende-invencibilidade-de-quase-uma-decada-no-barradao-em-duelo-decisivo-contra-o-vitoria]]>",
-      "time": "17/04 · 18:29",
-      "pubDate": "Fri, 17 Apr 2026 15:29:33 -0300"
-    },
-    {
-      "source": "Bolavip",
-      "color": "#0055A5",
-      "title": "Corinthians enfrenta barreira do Flamengo para contratar Cebolinha",
-      "url": "<![CDATA[https://br.bolavip.com/corinthians/corinthians-enfrenta-barreira-do-flamengo-para-contratar-cebolinha]]>",
-      "time": "17/04 · 15:48",
-      "pubDate": "Fri, 17 Apr 2026 12:48:35 -0300"
     },
     {
       "source": "Gazeta Esportiva",
@@ -200,6 +200,12 @@ const LIVE_DATA = {
   "standings": [],
   "highlights": [
     {
+      "title": "Corinthians visita o Carlos Barbosa para manter bom momento na LNF; saiba tudo",
+      "url": "https://www.meutimao.com.br/noticias-do-corinthians/531261/corinthians-visita-o-carlos-barbosa-para-manter-bom-momento-na-lnf-saiba-tudo",
+      "pubDate": "Sat, 18 Apr 2026 09:32:00 -0300",
+      "date": "18/04 · 12:32"
+    },
+    {
       "title": "Saiba onde assistir à partida entre Corinthians e Vitória pelo Brasileirão",
       "url": "https://www.meutimao.com.br/noticias-do-corinthians/531273/saiba-onde-assistir-a-partida-entre-corinthians-e-vitoria-pelo-brasileirao",
       "pubDate": "Sat, 18 Apr 2026 08:30:00 -0300",
@@ -216,12 +222,6 @@ const LIVE_DATA = {
       "url": "https://www.meutimao.com.br/videos-do-corinthians/127241/corinthians-desembarca-em-salvador-para-enfrentar-o-vitoria-pelo-brasileirao",
       "pubDate": "Fri, 17 Apr 2026 20:16:51 -0300",
       "date": "17/04 · 23:16"
-    },
-    {
-      "title": "Corinthians pode emendar sequência sem sofrer gols que não se repete há quase um ano",
-      "url": "https://www.meutimao.com.br/noticias-do-corinthians/531185/corinthians-pode-emendar-sequencia-sem-sofrer-gols-que-nao-se-repete-ha-quase-um-ano",
-      "pubDate": "Fri, 17 Apr 2026 16:32:00 -0300",
-      "date": "17/04 · 19:32"
     }
   ]
 };
