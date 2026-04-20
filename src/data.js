0c4c8e4b9c1e577f5c1600561705e8ee583027f3
GitHub Action <action@github.com>
2026-04-20 18:46:47 +0000

    Dados atualizados

--- src/data.js
+++ src/data.js
@@ -1,14 +1,94 @@
-// Gerado automaticamente em 20/04/2026, 12:58:41
+// Gerado automaticamente em 20/04/2026, 18:46:47
 const LIVE_DATA = {
-  "updatedAt": "2026-04-20T12:58:41.066Z",
+  "updatedAt": "2026-04-20T18:46:47.951Z",
   "news": [
+    {
+      "source": "Bolavip",
+      "color": "#0055A5",
+      "title": "Corinthians oferece R$ 2,5 milhões/mês para renovar com Memphis Depay; dívida seria paga até 2028",
+      "url": "<![CDATA[https://br.bolavip.com/corinthians/corinthians-oferece-r-25-milhoes-mes-para-renovar-com-memphis-depay-divida-seria-paga-ate-2028]]>",
+      "time": "20/04 · 18:45",
+      "pubDate": "Mon, 20 Apr 2026 15:45:46 -0300"
+    },
+    {
+      "source": "Bolavip",
+      "color": "#0055A5",
+      "title": "Barra x Corinthians: onde assistir ao vivo, horário e escalações do jogo desta terça",
+      "url": "<![CDATA[https://br.bolavip.com/corinthians/barra-corinthians-onde-assistir-ao-vivo-jogo-copa-do-brasil-hoje-horario-transmissao-tv-internet-streaming-escalacoes]]>",
+      "time": "20/04 · 18:42",
+      "pubDate": "Mon, 20 Apr 2026 15:42:18 -0300"
+    },
+    {
+      "source": "Bolavip",
+      "color": "#0055A5",
+      "title": "Fernando Diniz pede novo camisa 9 e Corinthians também quer fechar “pacotão” com Everton Cebolinha",
+      "url": "<![CDATA[https://br.bolavip.com/corinthians/fernando-diniz-pede-novo-camisa-9-e-corinthians-tambem-quer-fechar-pacotao-com-everton-cebolinha]]>",
+      "time": "20/04 · 18:38",
+      "pubDate": "Mon, 20 Apr 2026 15:38:18 -0300"
+    },
+    {
+      "source": "Meu Timao",
+      "color": "#1a7a3a",
+      "title": "Corinthians busca vitória sobre Juventude para atingir marca que não alcança há seis meses",
+      "url": "https://www.meutimao.com.br/noticias-do-corinthians/531398/corinthians-busca-vitoria-sobre-juventude-para-atingir-marca-que-nao-alcanca-ha-seis-meses",
+      "time": "20/04 · 18:32",
+      "pubDate": "Mon, 20 Apr 2026 15:32:00 -0300"
+    },
+    {
+      "source": "Meu Timao",
+      "color": "#1a7a3a",
+      "title": "Volante do Corinthians fica fora da estreia na Copa do Brasil contra o Barra",
+      "url": "https://www.meutimao.com.br/noticias-do-corinthians/531433/volante-do-corinthians-fica-fora-da-estreia-na-copa-do-brasil-contra-o-barra",
+      "time": "20/04 · 18:07",
+      "pubDate": "Mon, 20 Apr 2026 15:07:56 -0300"
+    },
+    {
+      "source": "Meu Timao",
+      "color": "#1a7a3a",
+      "title": "Guia do Meu Timão: saiba todos os detalhes da disputa do Corinthians na Copa do Brasil 2026",
+      "url": "https://www.meutimao.com.br/noticias-do-corinthians/531414/guia-do-meu-timao-saiba-todos-os-detalhes-da-disputa-do-corinthians-na-copa-do-brasil-2026",
+      "time": "20/04 · 18:02",
+      "pubDate": "Mon, 20 Apr 2026 15:02:00 -0300"
+    },
+    {
+      "source": "Meu Timao",
+      "color": "#1a7a3a",
+      "title": "Presidente do Cuiabá confirma o recebimento de parcela da dívida do Corinthians por Raniele",
+      "url": "https://www.meutimao.com.br/noticias-do-corinthians/531412/presidente-do-cuiaba-confirma-o-recebimento-de-parcela-da-divida-com-o-corinthians-por-raniele",
+      "time": "20/04 · 17:30",
+      "pubDate": "Mon, 20 Apr 2026 14:30:00 -0300"
+    },
+    {
+      "source": "Meu Timao",
+      "color": "#1a7a3a",
+      "title": "Corinthians ensaia mudanças para estreia na Copa do Brasil contra o Barra; veja provável escalação",
+      "url": "https://www.meutimao.com.br/noticias-do-corinthians/531424/corinthians-ensaia-mudancas-para-estreia-na-copa-do-brasil-contra-o-barra-veja-provavel-escalacao",
+      "time": "20/04 · 16:46",
+      "pubDate": "Mon, 20 Apr 2026 13:46:00 -0300"
+    },
+    {
+      "source": "Meu Timao",
+      "color": "#1a7a3a",
+      "title": "Corinthians prepara homenagem para Oscar Schmidt antes da estreia nos playoffs do NBB",
+      "url": "https://www.meutimao.com.br/noticias-do-corinthians/531422/corinthians-prepara-homenagem-para-oscar-schmidt-antes-da-estreia-nos-playoffs-do-nbb",
+      "time": "20/04 · 15:31",
+      "pubDate": "Mon, 20 Apr 2026 12:31:00 -0300"
+    },
     {
       "source": "Bolavip",
       "color": "#0055A5",
       "title": "Kayke é aprovado por Fernando Diniz, mas pode sair do Corinthians rumo à Europa após receber sondagens",
       "url": "<![CDATA[https://br.bolavip.com/corinthians/kayke-e-aprovado-por-fernando-diniz-mas-pode-sair-do-corinthians-rumo-a-europa-apos-receber-sondagens]]>",
-      "time": "20/04 · 12:45",
-      "pubDate": "Mon, 20 Apr 2026 09:45:54 -0300"
+      "time": "20/04 · 13:50",
+      "pubDate": "Mon, 20 Apr 2026 10:50:24 -0300"
+    },
+    {
+      "source": "Meu Timao",
+      "color": "#1a7a3a",
+      "title": "Corinthians inscreve volante reintegrado ao elenco na Copa do Brasil",
+      "url": "https://www.meutimao.com.br/noticias-do-corinthians/531415/corinthians-inscreve-volante-reintegrado-ao-elenco-na-copa-do-brasil",
+      "time": "20/04 · 13:36",
+      "pubDate": "Mon, 20 Apr 2026 10:36:00 -0300"
     },
     {
       "source": "Bolavip",
@@ -43,92 +123,12 @@ const LIVE_DATA = {
       "pubDate": "Mon, 20 Apr 2026 08:54:40 -0300"
     },
     {
-      "source": "Meu Timao",
-      "color": "#1a7a3a",
-      "title": "Saiba onde assistir à partida entre Corinthians e Juventude pelo Brasileirão Feminino",
-      "url": "https://www.meutimao.com.br/noticias-do-corinthians/531382/saiba-onde-assistir-a-partida-entre-corinthians-e-juventude-pelo-brasileirao-feminino",
-      "time": "20/04 · 11:30",
-      "pubDate": "Mon, 20 Apr 2026 08:30:00 -0300"
-    },
-    {
       "source": "Bolavip",
       "color": "#0055A5",
       "title": "Diniz tenta recuperar Pedro Raul e evitar nova contratação no Corinthians",
       "url": "<![CDATA[https://br.bolavip.com/corinthians/diniz-tenta-recuperar-pedro-raul-e-evitar-nova-contratacao-no-corinthians]]>",
       "time": "20/04 · 11:15",
       "pubDate": "Mon, 20 Apr 2026 08:15:48 -0300"
-    },
-    {
-      "source": "Bolavip",
-      "color": "#0055A5",
-      "title": "Alex Santana agrada Diniz e postura nos bastidores é vital para volta no Corinthians",
-      "url": "<![CDATA[https://br.bolavip.com/corinthians/alex-santana-agrada-diniz-e-postura-nos-bastidores-e-vital-para-volta-no-corinthians]]>",
-      "time": "20/04 · 10:40",
-      "pubDate": "Mon, 20 Apr 2026 07:40:31 -0300"
-    },
-    {
-      "source": "Meu Timao",
-      "color": "#1a7a3a",
-      "title": "Últimas do Corinthians: comentários de Diniz e jogadoras do Corinthians brilham na Seleção",
-      "url": "https://www.meutimao.com.br/noticias-do-corinthians/531400/ultimas-do-corinthians-comentarios-de-diniz-e-jogadoras-do-corinthians-brilham-na-selecao",
-      "time": "20/04 · 10:30",
-      "pubDate": "Mon, 20 Apr 2026 07:30:00 -0300"
-    },
-    {
-      "source": "Meu Timao",
-      "color": "#1a7a3a",
-      "title": "Corinthians enfrenta o Juventude buscando manter a liderança do Brasileirão; saiba tudo",
-      "url": "https://www.meutimao.com.br/noticias-do-corinthians/531385/corinthians-enfrenta-o-juventude-buscando-manter-a-lideranca-do-brasileirao-saiba-tudo",
-      "time": "20/04 · 03:01",
-      "pubDate": "Mon, 20 Apr 2026 00:01:00 -0300"
-    },
-    {
-      "source": "Meu Timao",
-      "color": "#1a7a3a",
-      "title": "Reforço do Corinthians assume vice-artilharia da equipe na temporada",
-      "url": "https://www.meutimao.com.br/noticias-do-corinthians/531379/reforco-do-corinthians-assume-vice-artilharia-da-equipe-na-temporada",
-      "time": "20/04 · 01:31",
-      "pubDate": "Sun, 19 Apr 2026 22:31:00 -0300"
-    },
-    {
-      "source": "Meu Timao",
-      "color": "#1a7a3a",
-      "title": "Corinthians enfrenta Juventude pela primeira vez como visitante e defende retrospecto perfeito",
-      "url": "https://www.meutimao.com.br/noticias-do-corinthians/531381/corinthians-enfrenta-juventude-pela-primeira-vez-como-visitante-e-defende-retrospecto-perfeito",
-      "time": "20/04 · 00:35",
-      "pubDate": "Sun, 19 Apr 2026 21:35:00 -0300"
-    },
-    {
-      "source": "Meu Timao",
-      "color": "#1a7a3a",
-      "title": "Corinthians encera a 12ª rodada do Campeonato Brasileiro na zona do rebaixamento; veja a tabela",
-      "url": "https://www.meutimao.com.br/noticias-do-corinthians/531387/corinthians-encera-a-12-rodada-do-campeonato-brasileiro-na-zona-do-rebaixamento-veja-a-tabela",
-      "time": "20/04 · 00:34",
-      "pubDate": "Sun, 19 Apr 2026 21:34:00 -0300"
-    },
-    {
-      "source": "Meu Timao",
-      "color": "#1a7a3a",
-      "title": "Ataque do Corinthians repete marca negativa após cinco meses",
-      "url": "https://www.meutimao.com.br/noticias-do-corinthians/531378/ataque-do-corinthians-repete-marca-negativa-apos-cinco-meses",
-      "time": "19/04 · 23:35",
-      "pubDate": "Sun, 19 Apr 2026 20:35:00 -0300"
-    },
-    {
-      "source": "Bolavip",
-      "color": "#0055A5",
-      "title": "Diniz segura decisão sobre Pedro Milans e Corinthians avalia lateral antes de ir ao mercado",
-      "url": "<![CDATA[https://br.bolavip.com/corinthians/diniz-segura-decisao-sobre-pedro-milans-e-corinthians-avalia-lateral-antes-de-ir-ao-mercado]]>",
-      "time": "19/04 · 16:29",
-      "pubDate": "Sun, 19 Apr 2026 13:29:12 -0300"
-    },
-    {
-      "source": "Bolavip",
-      "color": "#0055A5",
-      "title": "Rodrigo Garro lamenta empate do Corinthians e liga alerta para o Z-4 no Brasileirão",
-      "url": "<![CDATA[https://br.bolavip.com/corinthians/rodrigo-garro-lamenta-empate-do-corinthians-e-liga-alerta-para-o-z-4-no-brasileirao]]>",
-      "time": "19/04 · 15:53",
-      "pubDate": "Sun, 19 Apr 2026 12:53:44 -0300"
     },
     {
       "source": "Gazeta Esportiva",
@@ -216,24 +216,6 @@ const LIVE_DATA = {
       "url": "https://www.meutimao.com.br/noticias-do-corinthians/531384/corinthians-participa-de-50-por-cento-dos-empates-sem-gols-no-campeonato-brasileiro",
       "pubDate": "Sun, 19 Apr 2026 15:32:00 -0300",
       "date": "19/04 · 18:32"
-    },
-    {
-      "title": "VÍDEO: Rodrigo Garro lamenta empate, comenta trabalho de Diniz e queda para a zona de rebaixamento",
-      "url": "https://www.meutimao.com.br/videos-do-corinthians/127258/rodrigo-garro-lamenta-empate-comenta-trabalho-de-diniz-e-queda-para-a-zona-de-rebaixamento",
-      "pubDate": "Sun, 19 Apr 2026 00:05:43 -0300",
-      "date": "19/04 · 03:05"
-    },
-    {
-      "title": "Goleiro do Corinthians é o craque da partida em noite de notas baixas contra o Vitória",
-      "url": "https://www.meutimao.com.br/noticias-do-corinthians/531366/goleiro-do-corinthians-e-o-craque-da-partida-em-noite-de-notas-baixas-contra-o-vitoria",
-      "pubDate": "Sat, 18 Apr 2026 23:52:00 -0300",
-      "date": "19/04 · 02:52"
-    },
-    {
-      "title": "VÍDEO: M. Bidu fala de empate contra o Vitória, cita posição do Corinthians no Z4 e comenta efetividade",
-      "url": "https://www.meutimao.com.br/videos-do-corinthians/127256/m-bidu-fala-de-empate-contra-o-vitoria-cita-posicao-do-corinthians-no-z4-e-comenta-efetividade",
-      "pubDate": "Sat, 18 Apr 2026 23:49:41 -0300",
-      "date": "19/04 · 02:49"
     }
   ]
 };
