283cd07861aa5cb3c21e43592ad1bc79bf2541a5
GitHub Action <action@github.com>
2026-05-02 18:39:10 +0000

    Dados atualizados

--- src/data.js
+++ src/data.js
@@ -1,70 +1,94 @@
-// Gerado automaticamente em 02/05/2026, 12:39:19
+// Gerado automaticamente em 02/05/2026, 18:39:10
 const LIVE_DATA = {
-  "updatedAt": "2026-05-02T12:39:19.587Z",
+  "updatedAt": "2026-05-02T18:39:10.667Z",
   "news": [
     {
       "source": "Meu Timao",
       "color": "#1a7a3a",
-      "title": "Corinthians visita a Ferroviária na estreia do Paulista Feminino Sub-17; saiba tudo",
-      "url": "https://www.meutimao.com.br/noticias-do-corinthians/532151/corinthians-visita-ferroviaria-pela-estreia-do-paulista-feminino-sub-17-saiba-tudo",
-      "time": "02/05 · 12:31",
-      "pubDate": "Sat, 02 May 2026 09:31:00 -0300"
+      "title": "VÍDEO: Lingard titular de novo? Provável escalação de Mirassol x Corinthians pelo Brasileirão 2026",
+      "url": "https://www.meutimao.com.br/videos-do-corinthians/127535/lingard-titular-de-novo-provavel-escalacao-de-mirassol-x-corinthians-pelo-brasileirao-2026",
+      "time": "02/05 · 18:08",
+      "pubDate": "Sat, 02 May 2026 15:08:49 -0300"
     },
     {
       "source": "Meu Timao",
       "color": "#1a7a3a",
-      "title": "Corinthians recebe o Terceiro Milênio na estreia do Paulista Sub-17; saiba tudo",
-      "url": "https://www.meutimao.com.br/noticias-do-corinthians/532145/corinthians-recebe-o-terceiro-milenio-na-estreia-do-paulista-sub-17-saiba-tudo",
-      "time": "02/05 · 11:30",
-      "pubDate": "Sat, 02 May 2026 08:30:00 -0300"
+      "title": "Atacante sente dores no quadril e desfalca o Corinthians contra o Mirassol",
+      "url": "https://www.meutimao.com.br/noticias-do-corinthians/532184/atacante-sente-dores-no-quadril-e-desfalca-o-corinthians-contra-o-mirassol",
+      "time": "02/05 · 17:43",
+      "pubDate": "Sat, 02 May 2026 14:43:00 -0300"
     },
     {
       "source": "Meu Timao",
       "color": "#1a7a3a",
-      "title": "Últimas do Corinthians: premiação milionária, vitória no feminino e definição no Paulistão",
-      "url": "https://www.meutimao.com.br/noticias-do-corinthians/532164/ultimas-do-corinthians-premiacao-milionaria-vitoria-no-feminino-e-definicao-no-paulistao",
-      "time": "02/05 · 10:30",
-      "pubDate": "Sat, 02 May 2026 07:30:00 -0300"
+      "title": "Goleiro do Corinthians comenta expectativa por convocação para a Copa do Mundo",
+      "url": "https://www.meutimao.com.br/noticias-do-corinthians/532157/goleiro-do-corinthians-comenta-expectativa-por-convocacao-para-a-copa-do-mundo",
+      "time": "02/05 · 17:31",
+      "pubDate": "Sat, 02 May 2026 14:31:00 -0300"
     },
     {
       "source": "Meu Timao",
       "color": "#1a7a3a",
-      "title": "Felicidade por defesa e união marcam os bastidores da vitória do Corinthians sobre o Peñarol",
-      "url": "https://www.meutimao.com.br/noticias-do-corinthians/531594/felicidade-por-defesa-e-uniao-marcam-os-bastidores-da-vitoria-do-corinthians-sobre-o-penarol",
-      "time": "02/05 · 01:23",
-      "pubDate": "Fri, 01 May 2026 22:23:00 -0300"
+      "title": "Mídia europeia repercute gol de Lingard pelo Corinthians na Libertadores: 'Se dedicando ao máximo'",
+      "url": "https://www.meutimao.com.br/noticias-do-corinthians/532171/midia-europeia-repercute-gol-de-lingard-pelo-corinthians-na-libertadores-se-dedicando-ao-maximo",
+      "time": "02/05 · 16:34",
+      "pubDate": "Sat, 02 May 2026 13:34:00 -0300"
     },
     {
       "source": "Meu Timao",
       "color": "#1a7a3a",
-      "title": "Meia do Corinthians celebra vitória na Libertadores e exalta impacto do trabalho de Diniz",
-      "url": "https://www.meutimao.com.br/noticias-do-corinthians/532158/meia-do-corinthians-celebra-vitoria-na-libertadores-e-exalta-impacto-do-trabalho-de-diniz",
-      "time": "02/05 · 01:15",
-      "pubDate": "Fri, 01 May 2026 22:15:00 -0300"
+      "title": "Corinthians goleia o Terceiro Milênio na estreia do Paulistão Sub-17",
+      "url": "https://www.meutimao.com.br/noticias-do-corinthians/532180/corinthians-goleia-o-terceiro-milenio-na-estreia-do-paulistao-sub-17",
+      "time": "02/05 · 15:41",
+      "pubDate": "Sat, 02 May 2026 12:41:00 -0300"
     },
     {
       "source": "Meu Timao",
       "color": "#1a7a3a",
-      "title": "Garro se isola e atacante sobe em lista de assistentes do Corinthians em 2026; veja o ranking",
-      "url": "https://www.meutimao.com.br/noticias-do-corinthians/532077/garro-se-isola-e-atacante-sobe-em-lista-de-assistentes-do-corinthians-em-2026-veja-o-ranking",
-      "time": "02/05 · 01:06",
-      "pubDate": "Fri, 01 May 2026 22:06:00 -0300"
+      "title": "Técnico do Corinthians elogia versatilidade de Breno Bidon e detalha funções do volante em campo",
+      "url": "https://www.meutimao.com.br/noticias-do-corinthians/532118/tecnico-do-corinthians-elogia-versatilidade-de-breno-bidon-e-detalha-funcoes-do-volante-em-campo",
+      "time": "02/05 · 15:39",
+      "pubDate": "Sat, 02 May 2026 12:39:00 -0300"
     },
     {
       "source": "Meu Timao",
       "color": "#1a7a3a",
-      "title": "Gustavo Henrique celebra gol na Libertadores e exalta trabalho defensivo do Corinthians",
-      "url": "https://www.meutimao.com.br/noticias-do-corinthians/532160/gustavo-henrique-celebra-gol-na-libertadores-e-exalta-trabalho-defensivo-do-corinthians",
-      "time": "02/05 · 00:45",
-      "pubDate": "Fri, 01 May 2026 21:45:00 -0300"
+      "title": "Corinthians deve repetir Raniele na lateral contra o Mirassol; veja a provável escalação",
+      "url": "https://www.meutimao.com.br/noticias-do-corinthians/532172/corinthians-deve-repetir-raniele-na-lateral-contra-o-mirassol-veja-a-provavel-escalacao",
+      "time": "02/05 · 15:18",
+      "pubDate": "Sat, 02 May 2026 12:18:00 -0300"
     },
     {
       "source": "Meu Timao",
       "color": "#1a7a3a",
-      "title": "Meia do Corinthians iguala quantidade de assistências de 2025 em início de temporada",
-      "url": "https://www.meutimao.com.br/noticias-do-corinthians/532078/meia-do-corinthians-iguala-quantidade-de-assistencias-de-2025-em-inicio-de-temporada",
-      "time": "02/05 · 00:33",
-      "pubDate": "Fri, 01 May 2026 21:33:00 -0300"
+      "title": "Comitê de reestruturação do Corinthians sofre baixas e tem continuidade dos trabalhos ameaçada",
+      "url": "https://www.meutimao.com.br/noticias-do-corinthians/532173/comite-de-reestruturacao-do-corinthians-sofre-baixas-e-tem-continuidade-dos-trabalhos-ameacada",
+      "time": "02/05 · 14:34",
+      "pubDate": "Sat, 02 May 2026 11:34:00 -0300"
+    },
+    {
+      "source": "Bolavip",
+      "color": "#0055A5",
+      "title": "Mirassol x Corinthians: onde assistir ao vivo, horário e escalações do jogo deste domingo",
+      "url": "<![CDATA[https://br.bolavip.com/corinthians/mirassol-corinthians-onde-assistir-ao-vivo-jogo-brasileirao-hoje-horario-transmissao-tv-internet-streaming-escalacoes]]>",
+      "time": "02/05 · 11:52",
+      "pubDate": "Sat, 02 May 2026 08:52:19 -0300"
+    },
+    {
+      "source": "Bolavip",
+      "color": "#0055A5",
+      "title": "Corinthians inicia maratona fora de casa e aposta em estratégia para manter desempenho",
+      "url": "<![CDATA[https://br.bolavip.com/corinthians/corinthians-inicia-maratona-fora-de-casa-e-aposta-em-estrategia-para-manter-desempenho]]>",
+      "time": "02/05 · 11:49",
+      "pubDate": "Sat, 02 May 2026 08:49:04 -0300"
+    },
+    {
+      "source": "Bolavip",
+      "color": "#0055A5",
+      "title": "Marcelo Paz define estratégia no mercado e Corinthians mira novo atacante para 2026",
+      "url": "<![CDATA[https://br.bolavip.com/corinthians/marcelo-paz-define-estrategia-no-mercado-e-corinthians-mira-novo-atacante-para-2026]]>",
+      "time": "02/05 · 11:21",
+      "pubDate": "Sat, 02 May 2026 08:21:05 -0300"
     },
     {
       "source": "Bolavip",
@@ -107,98 +131,98 @@ const LIVE_DATA = {
       "pubDate": "Fri, 01 May 2026 12:33:21 -0300"
     },
     {
-      "source": "Bolavip",
-      "color": "#0055A5",
-      "title": "Lingard avalia ambiente em Corinthians 2 x 0 Peñarol e cita adaptação com Fernando Diniz",
-      "url": "<![CDATA[https://br.bolavip.com/corinthians/lingard-avalia-ambiente-em-corinthians-2-x-0-penarol-e-cita-adaptacao-com-fernando-diniz]]>",
-      "time": "01/05 · 14:04",
-      "pubDate": "Fri, 01 May 2026 11:04:59 -0300"
-    },
-    {
-      "source": "Bolavip",
-      "color": "#0055A5",
-      "title": "Corinthians iguala inícios históricos e embala campanha perfeita na Libertadores",
-      "url": "<![CDATA[https://br.bolavip.com/corinthians/corinthians-iguala-inicios-historicos-e-embala-campanha-perfeita-na-libertadores]]>",
-      "time": "01/05 · 12:33",
-      "pubDate": "Fri, 01 May 2026 09:33:29 -0300"
-    },
-    {
-      "source": "Bolavip",
-      "color": "#0055A5",
-      "title": "Memphis Depay aceita abrir mão de parte dos valores pelo avanço na renovação contratual no Corinthians",
-      "url": "<![CDATA[https://br.bolavip.com/corinthians/memphis-depay-aceita-abrir-mao-de-parte-dos-valores-pelo-avanco-na-renovacao-contratual-no-corinthians]]>",
-      "time": "01/05 · 09:41",
-      "pubDate": "Fri, 01 May 2026 06:41:18 -0300"
+      "source": "Gazeta Esportiva",
+      "color": "#E5820A",
+      "title": "Garro se diz preocupado com Z4, mas confia em reação do Corinthians: &#8220;Vamos acabar lá em cima&#8221;",
+      "url": "https://www.gazetaesportiva.com/times/corinthians/garro-se-diz-preocupado-com-z4-mas-confia-em-reacao-do-corinthians-vamos-acabar-la-em-cima/",
+      "time": "19/04 · 10:00",
+      "pubDate": "Sun, 19 Apr 2026 10:00:16 +0000"
     },
     {
       "source": "Gazeta Esportiva",
       "color": "#E5820A",
-      "title": "Memphis decepciona em nova função no Corinthians",
-      "url": "https://www.gazetaesportiva.com/times/corinthians/memphis-decepciona-em-nova-funcao-no-corinthians/",
-      "time": "02/03 · 10:00",
-      "pubDate": "Mon, 02 Mar 2026 10:00:10 +0000"
+      "title": "Atuações do Corinthians: veja quem foi bem e quem foi mal no empate contra o Vitória",
+      "url": "https://www.gazetaesportiva.com/times/corinthians/atuacoes-do-corinthians-veja-quem-foi-bem-e-quem-foi-mal-no-empate-contra-o-vitoria/",
+      "time": "19/04 · 09:00",
+      "pubDate": "Sun, 19 Apr 2026 09:00:00 +0000"
     },
     {
       "source": "Gazeta Esportiva",
       "color": "#E5820A",
-      "title": "Sem Yuri Alberto, média de gols do Corinthians despenca",
-      "url": "https://www.gazetaesportiva.com/times/corinthians/sem-yuri-alberto-media-de-gols-do-corinthians-despenca/",
-      "time": "02/03 · 09:00",
-      "pubDate": "Mon, 02 Mar 2026 09:00:19 +0000"
+      "title": "Análise: Corinthians volta a ser inofensivo e precisa ligar alerta no Brasileiro",
+      "url": "https://www.gazetaesportiva.com/times/corinthians/analise-corinthians-volta-a-ser-inofensivo-e-precisa-ligar-alerta-no-brasileiro/",
+      "time": "19/04 · 08:00",
+      "pubDate": "Sun, 19 Apr 2026 08:00:50 +0000"
     },
     {
       "source": "Gazeta Esportiva",
       "color": "#E5820A",
-      "title": "Saiba os números e os concorrentes de Lingard, meia próximo de reforçar o Corinthians",
-      "url": "https://www.gazetaesportiva.com/times/corinthians/saiba-os-numeros-e-os-concorrentes-de-lingard-atacante-proximo-de-reforcar-o-corinthians/",
-      "time": "02/03 · 08:00",
-      "pubDate": "Mon, 02 Mar 2026 08:00:34 +0000"
+      "title": "Diniz admite preocupação com Z4 e cobra melhora ofensiva do Corinthians: &#8220;Pior partida desde que assumi&#8221;",
+      "url": "https://www.gazetaesportiva.com/times/corinthians/diniz-admite-preocupacao-com-z4-e-cobra-melhora-ofensiva-do-corinthians-pior-partida-desde-que-assumi/",
+      "time": "19/04 · 02:11",
+      "pubDate": "Sun, 19 Apr 2026 02:11:37 +0000"
     },
     {
       "source": "Gazeta Esportiva",
       "color": "#E5820A",
-      "title": "Grêmio x Corinthians: veja prováveis escalações e onde assistir ao jogo pelo Brasileiro sub-20",
-      "url": "https://www.gazetaesportiva.com/times/corinthians/gremio-x-corinthians-veja-provaveis-escalacoes-e-onde-assistir-ao-jogo-pelo-brasileiro-sub-20/",
-      "time": "02/03 · 01:05",
-      "pubDate": "Mon, 02 Mar 2026 01:05:07 +0000"
+      "title": "Veja fotos da partida entre Vitória e Corinthians pelo Brasileirão",
+      "url": "https://www.gazetaesportiva.com/campeonatos/brasileiro-serie-a/veja-fotos-da-partida-entre-vitoria-e-corinthians-pelo-brasileirao/",
+      "time": "19/04 · 01:24",
+      "pubDate": "Sun, 19 Apr 2026 01:24:44 +0000"
     },
     {
       "source": "Gazeta Esportiva",
       "color": "#E5820A",
-      "title": "Presidente do Corinthians veta saída de André, e Milan ameaça ir à Fifa",
-      "url": "https://www.gazetaesportiva.com/times/corinthians/presidente-do-corinthians-veta-saida-de-andre-e-milan-ameaca-ir-a-fifa/",
-      "time": "02/03 · 00:27",
-      "pubDate": "Mon, 02 Mar 2026 00:27:33 +0000"
+      "title": "Bidu vê &#8220;ponto importante&#8221; fora de casa e destaca consistência defensiva do Corinthians em empate",
+      "url": "https://www.gazetaesportiva.com/times/corinthians/bidu-ve-ponto-importante-fora-de-casa-e-destaca-consistencia-defensiva-do-corinthians-em-empate/",
+      "time": "19/04 · 01:14",
+      "pubDate": "Sun, 19 Apr 2026 01:14:55 +0000"
     },
     {
       "source": "Gazeta Esportiva",
       "color": "#E5820A",
-      "title": "Corinthians: conheça os números da equipe na temporada 2026",
-      "url": "https://www.gazetaesportiva.com/times/corinthians/corinthians-conheca-os-numeros-da-equipe-na-temporada-2026/",
-      "time": "01/03 · 17:29",
-      "pubDate": "Sun, 01 Mar 2026 17:29:03 +0000"
+      "title": "Corinthians empata com Vitória, vê jejum aumentar e entra no Z4 do Brasileiro",
+      "url": "https://www.gazetaesportiva.com/campeonatos/brasileiro-serie-a/corinthians-empata-com-vitoria-ve-jejum-aumentar-e-entra-no-z4-do-brasileiro/",
+      "time": "19/04 · 01:01",
+      "pubDate": "Sun, 19 Apr 2026 01:01:48 +0000"
     },
     {
       "source": "Gazeta Esportiva",
       "color": "#E5820A",
-      "title": "Lingard chega ao Brasil para realizar exames e assinar com o Corinthians",
-      "url": "https://www.gazetaesportiva.com/times/corinthians/lingard-chega-ao-brasil-para-realizar-exames-e-assinar-com-o-corinthians/",
-      "time": "01/03 · 12:25",
-      "pubDate": "Sun, 01 Mar 2026 12:25:50 +0000"
-    },
-    {
-      "source": "Gazeta Esportiva",
-      "color": "#E5820A",
-      "title": "Após queda no Paulista, Corinthians terá maior pausa no calendário desde início da temporada",
-      "url": "https://www.gazetaesportiva.com/times/corinthians/apos-queda-no-paulista-corinthians-tera-maior-pausa-no-calendario-desde-inicio-da-temporada/",
-      "time": "01/03 · 11:00",
-      "pubDate": "Sun, 01 Mar 2026 11:00:25 +0000"
+      "title": "Corinthians terá Hugo no gol e duas mudanças para encarar o Vitória; veja escalações",
+      "url": "https://www.gazetaesportiva.com/times/corinthians/escalacoes-corinthians-vitoria-18-04-2026/",
+      "time": "18/04 · 22:02",
+      "pubDate": "Sat, 18 Apr 2026 22:02:31 +0000"
     }
   ],
   "upcoming": [],
   "results": [],
   "standings": [],
   "highlights": [
+    {
+      "title": "VÍDEO: Lingard titular de novo? Provável escalação de Mirassol x Corinthians pelo Brasileirão 2026",
+      "url": "https://www.meutimao.com.br/videos-do-corinthians/127535/lingard-titular-de-novo-provavel-escalacao-de-mirassol-x-corinthians-pelo-brasileirao-2026",
+      "pubDate": "Sat, 02 May 2026 15:08:49 -0300",
+      "date": "02/05 · 18:08"
+    },
+    {
+      "title": "Goleiro do Corinthians comenta expectativa por convocação para a Copa do Mundo",
+      "url": "https://www.meutimao.com.br/noticias-do-corinthians/532157/goleiro-do-corinthians-comenta-expectativa-por-convocacao-para-a-copa-do-mundo",
+      "pubDate": "Sat, 02 May 2026 14:31:00 -0300",
+      "date": "02/05 · 17:31"
+    },
+    {
+      "title": "Mídia europeia repercute gol de Lingard pelo Corinthians na Libertadores: 'Se dedicando ao máximo'",
+      "url": "https://www.meutimao.com.br/noticias-do-corinthians/532171/midia-europeia-repercute-gol-de-lingard-pelo-corinthians-na-libertadores-se-dedicando-ao-maximo",
+      "pubDate": "Sat, 02 May 2026 13:34:00 -0300",
+      "date": "02/05 · 16:34"
+    },
+    {
+      "title": "Corinthians goleia o Terceiro Milênio na estreia do Paulistão Sub-17",
+      "url": "https://www.meutimao.com.br/noticias-do-corinthians/532180/corinthians-goleia-o-terceiro-milenio-na-estreia-do-paulistao-sub-17",
+      "pubDate": "Sat, 02 May 2026 12:41:00 -0300",
+      "date": "02/05 · 15:41"
+    },
     {
       "title": "Gustavo Henrique celebra gol na Libertadores e exalta trabalho defensivo do Corinthians",
       "url": "https://www.meutimao.com.br/noticias-do-corinthians/532160/gustavo-henrique-celebra-gol-na-libertadores-e-exalta-trabalho-defensivo-do-corinthians",
