5dc6a173861bab6ff6aaa9901a018b056d5a440f
GitHub Action <action@github.com>
2026-05-04 13:16:41 +0000

    Dados atualizados

--- src/data.js
+++ src/data.js
@@ -1,11 +1,67 @@
-// Gerado automaticamente em 04/05/2026, 10:03:52
+// Gerado automaticamente em 04/05/2026, 13:16:41
 const LIVE_DATA = {
-  "updatedAt": "2026-05-04T10:03:52.380Z",
+  "updatedAt": "2026-05-04T13:16:41.955Z",
   "news": [
     {
       "source": "Meu Timao",
       "color": "#1a7a3a",
-      "title": "Técnico do Corinthians atribui o desempenho ruim contra o Mirassol à baixa concentração da equipe",
+      "title": "Diniz sofre primeira derrota no comando do Corinthians após sete jogos de invencibilidade",
+      "url": "https://www.meutimao.com.br/noticias-do-corinthians/532244/diniz-sofre-primeira-derrota-no-comando-do-corinthians-apos-sete-jogos-de-invencibilidade",
+      "time": "04/05 · 12:30",
+      "pubDate": "Mon, 04 May 2026 09:30:00 -0300"
+    },
+    {
+      "source": "Meu Timao",
+      "color": "#1a7a3a",
+      "title": "Corinthians visita o Minas na abertura das quartas de final do NBB; saiba tudo",
+      "url": "https://www.meutimao.com.br/noticias-do-corinthians/532221/corinthians-visita-o-minas-na-abertura-das-quartas-de-final-do-nbb-saiba-tudo",
+      "time": "04/05 · 11:30",
+      "pubDate": "Mon, 04 May 2026 08:30:00 -0300"
+    },
+    {
+      "source": "Meu Timao",
+      "color": "#1a7a3a",
+      "title": "Últimas do Corinthians: derrota no Brasileirão, críticas de executivo à arbitragem e retorno ao Z4",
+      "url": "https://www.meutimao.com.br/noticias-do-corinthians/532282/ultimas-do-corinthians-derrota-no-brasileirao-criticas-de-executivo-a-arbitragem-e-retorno-ao-z4",
+      "time": "04/05 · 10:30",
+      "pubDate": "Mon, 04 May 2026 07:30:00 -0300"
+    },
+    {
+      "source": "Bolavip",
+      "color": "#0055A5",
+      "title": "Diniz critica falta de ânimo do Corinthians e cobra concentração após derrota",
+      "url": "<![CDATA[https://br.bolavip.com/corinthians/diniz-critica-falta-de-animo-do-corinthians-e-cobra-concentracao-apos-derrota]]>",
+      "time": "04/05 · 09:54",
+      "pubDate": "Mon, 04 May 2026 06:54:59 -0300"
+    },
+    {
+      "source": "Bolavip",
+      "color": "#0055A5",
+      "title": "Corinthians reage e cobra CBF após polêmica de arbitragem",
+      "url": "<![CDATA[https://br.bolavip.com/corinthians/corinthians-reage-e-cobra-cbf-apos-polemica-de-arbitragem]]>",
+      "time": "04/05 · 09:54",
+      "pubDate": "Mon, 04 May 2026 06:54:41 -0300"
+    },
+    {
+      "source": "Bolavip",
+      "color": "#0055A5",
+      "title": "Corinthians sofre primeira derrota com Fernando Diniz após reação tardia diante do Mirassol",
+      "url": "<![CDATA[https://br.bolavip.com/corinthians/corinthians-sofre-primeira-derrota-com-fernando-diniz-apos-reacao-tardia-diante-do-mirassol]]>",
+      "time": "04/05 · 09:15",
+      "pubDate": "Mon, 04 May 2026 06:15:05 -0300"
+    },
+    {
+      "source": "Bolavip",
+      "color": "#0055A5",
+      "title": "Fernando Diniz desabafa sobre arbitragem em derrota do Corinthians para o Mirassol pelo Brasileirão",
+      "url": "<![CDATA[https://br.bolavip.com/corinthians/fernando-diniz-desabafa-sobre-arbitragem-em-derrota-do-corinthians-para-o-mirassol-pelo-brasileirao]]>",
+      "time": "04/05 · 04:07",
+      "pubDate": "Mon, 04 May 2026 01:07:35 -0300"
+    },
+    {
+      "source": "Meu Timao",
+      "color": "#1a7a3a",
+      "title": "Técnico do Corinthians atribui desempenho ruim contra o Mirassol à baixa concentração da equipe",
       "url": "https://www.meutimao.com.br/noticias-do-corinthians/532274/tecnico-do-corinthians-atribui-o-desempenho-ruim-contra-o-mirassol-a-baixa-concentracao-da-equipe",
       "time": "04/05 · 04:00",
       "pubDate": "Mon, 04 May 2026 01:00:00 -0300"
@@ -43,28 +99,28 @@ const LIVE_DATA = {
       "pubDate": "Mon, 04 May 2026 00:09:00 -0300"
     },
     {
-      "source": "Meu Timao",
-      "color": "#1a7a3a",
-      "title": "VÍDEO: Marcelo Paz comenta decisões da arbitragem em jogos do Corinthians e se diz insatisfeito",
-      "url": "https://www.meutimao.com.br/videos-do-corinthians/127560/marcelo-paz-comenta-decisoes-da-arbitragem-em-jogos-do-corinthians-e-se-diz-insatisfeito",
-      "time": "04/05 · 02:57",
-      "pubDate": "Sun, 03 May 2026 23:57:26 -0300"
-    },
-    {
-      "source": "Meu Timao",
-      "color": "#1a7a3a",
-      "title": "Meia registra primeira assistência pelo Corinthians em derrota no Brasileirão",
-      "url": "https://www.meutimao.com.br/noticias-do-corinthians/532250/meia-registra-primeira-assistencia-pelo-corinthians-em-derrota-no-brasileirao",
+      "source": "Bolavip",
+      "color": "#0055A5",
+      "title": "Matheus Candançan vira alvo de críticas por pênalti marcado em Mirassol x Corinthians: “Decisão absurda”",
+      "url": "<![CDATA[https://br.bolavip.com/corinthians/matheus-candancan-vira-alvo-de-criticas-por-penalti-marcado-em-mirassol-x-corinthians-decisao-absurda]]>",
       "time": "04/05 · 02:28",
-      "pubDate": "Sun, 03 May 2026 23:28:00 -0300"
+      "pubDate": "Sun, 03 May 2026 23:28:02 -0300"
     },
     {
-      "source": "Meu Timao",
-      "color": "#1a7a3a",
-      "title": "Filho do Terrão volta a marcar pelo Corinthians após quase três meses em branco",
-      "url": "https://www.meutimao.com.br/noticias-do-corinthians/532251/filho-do-terrao-volta-a-marcar-pelo-corinthians-apos-quase-tres-meses-em-branco",
-      "time": "04/05 · 02:26",
-      "pubDate": "Sun, 03 May 2026 23:26:00 -0300"
+      "source": "Bolavip",
+      "color": "#0055A5",
+      "title": "Corinthians busca reação, mas sai derrotado diante do Mirassol em jogo marcado por polêmicas de arbitragem",
+      "url": "<![CDATA[https://br.bolavip.com/corinthians/corinthians-busca-reacao-mas-sai-derrotado-diante-do-mirassol-em-jogo-marcado-por-polemicas-de-arbitragem]]>",
+      "time": "04/05 · 01:40",
+      "pubDate": "Sun, 03 May 2026 22:40:37 -0300"
+    },
+    {
+      "source": "Bolavip",
+      "color": "#0055A5",
+      "title": "Fernando Diniz chama árbitro de mascarado em derrota do Corinthians para o Mirassol pelo Campeonato Brasileiro",
+      "url": "<![CDATA[https://br.bolavip.com/corinthians/fernando-diniz-chama-arbitro-de-mascarado-em-mirassol-x-corinthians]]>",
+      "time": "04/05 · 01:32",
+      "pubDate": "Sun, 03 May 2026 22:32:29 -0300"
     },
     {
       "source": "Bolavip",
@@ -75,124 +131,68 @@ const LIVE_DATA = {
       "pubDate": "Sun, 03 May 2026 16:14:02 -0300"
     },
     {
-      "source": "Bolavip",
-      "color": "#0055A5",
-      "title": "Mirassol x Corinthians: Cicinho prevê resultado e indica tropeço do Timão no Brasileirão",
-      "url": "<![CDATA[https://br.bolavip.com/corinthians/mirassol-x-corinthians-cicinho-preve-resultado-e-indica-tropeco-do-timao-no-brasileirao]]>",
-      "time": "03/05 · 15:34",
-      "pubDate": "Sun, 03 May 2026 12:34:37 -0300"
-    },
-    {
-      "source": "Bolavip",
-      "color": "#0055A5",
-      "title": "Nícollas, de 17 anos, é chamado por Diniz após desfalques do Corinthians contra o Mirassol",
-      "url": "<![CDATA[https://br.bolavip.com/corinthians/nicollas-de-17-anos-e-chamado-por-diniz-apos-desfalques-do-corinthians-contra-o-mirassol]]>",
-      "time": "03/05 · 14:40",
-      "pubDate": "Sun, 03 May 2026 11:40:29 -0300"
-    },
-    {
-      "source": "Bolavip",
-      "color": "#0055A5",
-      "title": "Allan deve ganhar chance no Corinthians após suspensão de André",
-      "url": "<![CDATA[https://br.bolavip.com/corinthians/allan-deve-ganhar-chance-no-corinthians-apos-suspensao-de-andre]]>",
-      "time": "03/05 · 13:09",
-      "pubDate": "Sun, 03 May 2026 10:09:52 -0300"
-    },
-    {
-      "source": "Bolavip",
-      "color": "#0055A5",
-      "title": "Corinthians segue de olho em Matheus Magalhães e avalia Strefezza por empréstimo",
-      "url": "<![CDATA[https://br.bolavip.com/corinthians/corinthians-segue-de-olho-em-matheus-magalhaes-e-avalia-strefezza-por-emprestimo]]>",
-      "time": "02/05 · 21:36",
-      "pubDate": "Sat, 02 May 2026 18:36:28 -0300"
-    },
-    {
-      "source": "Bolavip",
-      "color": "#0055A5",
-      "title": "Vidente projeta possível tropeço do Corinthians contra o Mirassol pelo Brasileirão",
-      "url": "<![CDATA[https://br.bolavip.com/corinthians/vidente-projeta-possivel-tropeco-do-corinthians-contra-o-mirassol-pelo-brasileirao]]>",
-      "time": "02/05 · 20:58",
-      "pubDate": "Sat, 02 May 2026 17:58:40 -0300"
-    },
-    {
-      "source": "Bolavip",
-      "color": "#0055A5",
-      "title": "Diniz impõe mudança no Corinthians e bastidor de ataque é exposto",
-      "url": "<![CDATA[https://br.bolavip.com/corinthians/diniz-impoe-mudanca-no-corinthians-e-bastidor-de-ataque-e-exposto]]>",
-      "time": "02/05 · 19:11",
-      "pubDate": "Sat, 02 May 2026 16:11:28 -0300"
-    },
-    {
-      "source": "Bolavip",
-      "color": "#0055A5",
-      "title": "Rafael Guanaes tem duas dúvidas e um reforço para Mirassol x Corinthians pelo Brasileirão",
-      "url": "<![CDATA[https://br.bolavip.com/corinthians/rafael-guanaes-tem-duas-duvidas-e-um-reforco-para-mirassol-x-corinthians-pelo-brasileirao]]>",
-      "time": "02/05 · 17:49",
-      "pubDate": "Sat, 02 May 2026 14:49:23 -0300"
+      "source": "Gazeta Esportiva",
+      "color": "#E5820A",
+      "title": "Garro se diz preocupado com Z4, mas confia em reação do Corinthians: &#8220;Vamos acabar lá em cima&#8221;",
+      "url": "https://www.gazetaesportiva.com/times/corinthians/garro-se-diz-preocupado-com-z4-mas-confia-em-reacao-do-corinthians-vamos-acabar-la-em-cima/",
+      "time": "19/04 · 10:00",
+      "pubDate": "Sun, 19 Apr 2026 10:00:16 +0000"
     },
     {
       "source": "Gazeta Esportiva",
       "color": "#E5820A",
-      "title": "Corinthians vence Juventude e retoma liderança do Brasileirão feminino",
-      "url": "https://www.gazetaesportiva.com/times/corinthians/corinthians-vence-juventude-brasileirao-feminino-rodada-7/",
-      "time": "21/04 · 02:02",
-      "pubDate": "Tue, 21 Apr 2026 02:02:11 +0000"
+      "title": "Atuações do Corinthians: veja quem foi bem e quem foi mal no empate contra o Vitória",
+      "url": "https://www.gazetaesportiva.com/times/corinthians/atuacoes-do-corinthians-veja-quem-foi-bem-e-quem-foi-mal-no-empate-contra-o-vitoria/",
+      "time": "19/04 · 09:00",
+      "pubDate": "Sun, 19 Apr 2026 09:00:00 +0000"
     },
     {
       "source": "Gazeta Esportiva",
       "color": "#E5820A",
-      "title": "Barra x Corinthians pela Copa do Brasil: onde assistir ao vivo, estatísticas, histórico e escalações",
-      "url": "https://www.gazetaesportiva.com/campeonatos/copa-do-brasil/barra-x-corinthians-pela-copa-do-brasil-onde-assistir-ao-vivo-estatisticas-historico-e-escalacoes/",
-      "time": "20/04 · 23:00",
-      "pubDate": "Mon, 20 Apr 2026 23:00:25 +0000"
+      "title": "Análise: Corinthians volta a ser inofensivo e precisa ligar alerta no Brasileiro",
+      "url": "https://www.gazetaesportiva.com/times/corinthians/analise-corinthians-volta-a-ser-inofensivo-e-precisa-ligar-alerta-no-brasileiro/",
+      "time": "19/04 · 08:00",
+      "pubDate": "Sun, 19 Apr 2026 08:00:50 +0000"
     },
     {
       "source": "Gazeta Esportiva",
       "color": "#E5820A",
-      "title": "Corinthians perde dupla e desembarca em Santa Catarina para estreia na Copa do Brasil",
-      "url": "https://www.gazetaesportiva.com/times/corinthians/corinthians-perde-dupla-e-desembarca-em-santa-catarina-para-estreia-na-copa-do-brasil/",
-      "time": "20/04 · 22:31",
-      "pubDate": "Mon, 20 Apr 2026 22:31:46 +0000"
+      "title": "Diniz admite preocupação com Z4 e cobra melhora ofensiva do Corinthians: &#8220;Pior partida desde que assumi&#8221;",
+      "url": "https://www.gazetaesportiva.com/times/corinthians/diniz-admite-preocupacao-com-z4-e-cobra-melhora-ofensiva-do-corinthians-pior-partida-desde-que-assumi/",
+      "time": "19/04 · 02:11",
+      "pubDate": "Sun, 19 Apr 2026 02:11:37 +0000"
     },
     {
       "source": "Gazeta Esportiva",
       "color": "#E5820A",
-      "title": "Matheuzinho projeta estreia do Corinthians e pede &#8220;pés no chão&#8221; em defesa do título da Copa do Brasil",
-      "url": "https://www.gazetaesportiva.com/times/corinthians/matheuzinho-projeta-estreia-do-corinthians-e-pede-pes-no-chao-em-defesa-do-titulo-da-copa-do-brasil/",
-      "time": "20/04 · 20:30",
-      "pubDate": "Mon, 20 Apr 2026 20:30:22 +0000"
+      "title": "Veja fotos da partida entre Vitória e Corinthians pelo Brasileirão",
+      "url": "https://www.gazetaesportiva.com/campeonatos/brasileiro-serie-a/veja-fotos-da-partida-entre-vitoria-e-corinthians-pelo-brasileirao/",
+      "time": "19/04 · 01:24",
+      "pubDate": "Sun, 19 Apr 2026 01:24:44 +0000"
     },
     {
       "source": "Gazeta Esportiva",
       "color": "#E5820A",
-      "title": "Escalação: Corinthians fecha preparação e pode ter mudanças para estreia na Copa do Brasil",
-      "url": "https://www.gazetaesportiva.com/times/corinthians/escalacao-corinthians-fecha-preparacao-e-pode-ter-mudancas-para-estreia-na-copa-do-brasil/",
-      "time": "20/04 · 19:00",
-      "pubDate": "Mon, 20 Apr 2026 19:00:52 +0000"
+      "title": "Bidu vê &#8220;ponto importante&#8221; fora de casa e destaca consistência defensiva do Corinthians em empate",
+      "url": "https://www.gazetaesportiva.com/times/corinthians/bidu-ve-ponto-importante-fora-de-casa-e-destaca-consistencia-defensiva-do-corinthians-em-empate/",
+      "time": "19/04 · 01:14",
+      "pubDate": "Sun, 19 Apr 2026 01:14:55 +0000"
     },
     {
       "source": "Gazeta Esportiva",
       "color": "#E5820A",
-      "title": "Veja fotos do último treino do Corinthians antes da estreia na Copa do Brasil",
-      "url": "https://www.gazetaesportiva.com/times/corinthians/veja-fotos-do-ultimo-treino-do-corinthians-antes-da-estreia-na-copa-do-brasil/",
-      "time": "20/04 · 18:55",
-      "pubDate": "Mon, 20 Apr 2026 18:55:03 +0000"
+      "title": "Corinthians empata com Vitória, vê jejum aumentar e entra no Z4 do Brasileiro",
+      "url": "https://www.gazetaesportiva.com/campeonatos/brasileiro-serie-a/corinthians-empata-com-vitoria-ve-jejum-aumentar-e-entra-no-z4-do-brasileiro/",
+      "time": "19/04 · 01:01",
+      "pubDate": "Sun, 19 Apr 2026 01:01:48 +0000"
     },
     {
       "source": "Gazeta Esportiva",
       "color": "#E5820A",
-      "title": "Alex Santana é inscrito pelo Corinthians na Copa do Brasil após reintegração",
-      "url": "https://www.gazetaesportiva.com/times/corinthians/alex-santana-inscrito-corinthians-copa-do-brasil/",
-      "time": "20/04 · 15:21",
-      "pubDate": "Mon, 20 Apr 2026 15:21:02 +0000"
-    },
-    {
-      "source": "Gazeta Esportiva",
-      "color": "#E5820A",
-      "title": "Barra x Corinthians: veja onde assistir à partida pela Copa do Brasil",
-      "url": "https://www.gazetaesportiva.com/times/corinthians/barra-x-corinthians-veja-onde-assistir-a-partida-pela-copa-do-brasil/",
-      "time": "20/04 · 10:00",
-      "pubDate": "Mon, 20 Apr 2026 10:00:50 +0000"
+      "title": "Corinthians terá Hugo no gol e duas mudanças para encarar o Vitória; veja escalações",
+      "url": "https://www.gazetaesportiva.com/times/corinthians/escalacoes-corinthians-vitoria-18-04-2026/",
+      "time": "18/04 · 22:02",
+      "pubDate": "Sat, 18 Apr 2026 22:02:31 +0000"
     }
   ],
   "upcoming": [],
