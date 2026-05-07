635eb55ff4d2131319d35dfe32482c2319637fcd
GitHub Action <action@github.com>
2026-05-07 22:47:56 +0000

    Dados atualizados

--- src/data.js
+++ src/data.js
@@ -1,70 +1,86 @@
-// Gerado automaticamente em 07/05/2026, 19:21:29
+// Gerado automaticamente em 07/05/2026, 22:47:56
 const LIVE_DATA = {
-  "updatedAt": "2026-05-07T19:21:29.492Z",
+  "updatedAt": "2026-05-07T22:47:56.619Z",
   "news": [
     {
       "source": "Meu Timao",
       "color": "#1a7a3a",
-      "title": "Ídolo do Corinthians critica a atuação de Jesse Lingard contra o Santa Fe: 'Anda em campo'",
-      "url": "https://www.meutimao.com.br/noticias-do-corinthians/532497/idolo-do-corinthians-critica-a-atuacao-de-jesse-lingard-contra-o-santa-fe-anda-em-campo",
-      "time": "07/05 · 19:01",
-      "pubDate": "Thu, 07 May 2026 16:01:00 -0300"
+      "title": "Lateral do Corinthians avalia jogo na altitude de Bogotá e projeta nova sequência sem sofrer gols",
+      "url": "https://www.meutimao.com.br/noticias-do-corinthians/532487/lateral-do-corinthians-avalia-jogo-na-altitude-de-bogota-e-projeta-nova-sequencia-sem-sofrer-gols",
+      "time": "07/05 · 22:28",
+      "pubDate": "Thu, 07 May 2026 19:28:00 -0300"
     },
     {
       "source": "Meu Timao",
       "color": "#1a7a3a",
-      "title": "Gustavo Henrique comenta empate heroico do Corinthians e critica condições do gramado em Bogotá",
-      "url": "https://www.meutimao.com.br/noticias-do-corinthians/532485/gustavo-henrique-comenta-empate-heroico-do-corinthians-e-critica-condicoes-do-gramado-em-bogota",
-      "time": "07/05 · 18:42",
-      "pubDate": "Thu, 07 May 2026 15:42:00 -0300"
+      "title": "Corinthians denuncia atos racistas e violência em jogo contra o Santa Fe pela Libertadores",
+      "url": "https://www.meutimao.com.br/noticias-do-corinthians/532517/corinthians-denuncia-atos-racistas-e-violencia-em-jogo-contra-o-santa-fe-pela-libertadores",
+      "time": "07/05 · 22:12",
+      "pubDate": "Thu, 07 May 2026 19:12:36 -0300"
     },
     {
       "source": "Meu Timao",
       "color": "#1a7a3a",
-      "title": "Justiça condena Corinthians em ação movida por ex-lateral aposentado aos 23 anos",
-      "url": "https://www.meutimao.com.br/noticias-do-corinthians/532502/justica-condena-corinthians-em-acao-movida-por-ex-lateral-aposentado-aos-23-anos",
-      "time": "07/05 · 18:36",
-      "pubDate": "Thu, 07 May 2026 15:36:00 -0300"
+      "title": "Corinthians registro novo meio-campista e chega a sete contratações para o Sub-17 em 2026",
+      "url": "https://www.meutimao.com.br/noticias-do-corinthians/532511/corinthians-registro-novo-meio-campista-e-chega-a-sete-contratacoes-para-o-sub-17-em-2026",
+      "time": "07/05 · 22:03",
+      "pubDate": "Thu, 07 May 2026 19:03:00 -0300"
     },
     {
       "source": "Meu Timao",
       "color": "#1a7a3a",
-      "title": "Corinthians é condenado a pagar valor milionário por dívida relacionada à compra de Charles",
-      "url": "https://www.meutimao.com.br/noticias-do-corinthians/532501/corinthians-e-condenado-a-pagar-valor-milionario-por-divida-relacionada-a-compra-de-charles",
-      "time": "07/05 · 18:30",
-      "pubDate": "Thu, 07 May 2026 15:30:00 -0300"
+      "title": "Corinthians contará com três atletas ligados ao clube na final da Champions League de 2026",
+      "url": "https://www.meutimao.com.br/noticias-do-corinthians/532514/corinthians-contara-com-tres-atletas-ligados-ao-clube-na-final-da-champions-league-de-2026",
+      "time": "07/05 · 21:55",
+      "pubDate": "Thu, 07 May 2026 18:55:19 -0300"
     },
     {
       "source": "Meu Timao",
       "color": "#1a7a3a",
-      "title": "Meia-atacante tenta encerrar jejum e transformar São Paulo em maior vítima pelo Corinthians",
-      "url": "https://www.meutimao.com.br/noticias-do-corinthians/532427/meia-atacante-tenta-encerrar-jejum-e-transformar-sao-paulo-em-maior-vitima-pelo-corinthians",
-      "time": "07/05 · 17:33",
-      "pubDate": "Thu, 07 May 2026 14:33:00 -0300"
+      "title": "Gustavo Henrique registra 14 participações em gols desde chegada ao Corinthians",
+      "url": "https://www.meutimao.com.br/noticias-do-corinthians/532439/gustavo-henrique-registra-14-participacoes-em-gols-desde-chegada-ao-corinthians",
+      "time": "07/05 · 21:30",
+      "pubDate": "Thu, 07 May 2026 18:30:00 -0300"
     },
     {
       "source": "Meu Timao",
       "color": "#1a7a3a",
-      "title": "Rodrigo Garro celebra fase positiva do Corinthians e projeta sequência da temporada",
-      "url": "https://www.meutimao.com.br/noticias-do-corinthians/532467/rodrigo-garro-celebra-fase-positiva-do-corinthians-e-projeta-sequencia-da-temporada",
-      "time": "07/05 · 16:38",
-      "pubDate": "Thu, 07 May 2026 13:38:00 -0300"
+      "title": "Corinthians quebra sequência de derrotas na altitude com empate contra o Santa Fe na Libertadores",
+      "url": "https://www.meutimao.com.br/noticias-do-corinthians/532444/corinthians-quebra-sequencia-de-derrotas-na-altitude-com-empate-contra-o-santa-fe-na-libertadores",
+      "time": "07/05 · 21:01",
+      "pubDate": "Thu, 07 May 2026 18:01:00 -0300"
     },
     {
       "source": "Meu Timao",
       "color": "#1a7a3a",
-      "title": "Corinthians cede a Neo Química Arena para jogo de rival pelo Campeonato Brasileiro",
-      "url": "https://www.meutimao.com.br/noticias-do-corinthians/532498/corinthians-cede-a-neo-quimica-arena-para-jogo-de-rival-pelo-campeonato-brasileiro",
-      "time": "07/05 · 16:19",
-      "pubDate": "Thu, 07 May 2026 13:19:00 -0300"
+      "title": "Técnico do Corinthians elogia desempenho de inglês e defende escalação repetida",
+      "url": "https://www.meutimao.com.br/noticias-do-corinthians/532479/tecnico-do-corinthians-elogia-desempenho-de-ingles-e-defende-escalacao-repetida",
+      "time": "07/05 · 20:32",
+      "pubDate": "Thu, 07 May 2026 17:32:00 -0300"
     },
     {
       "source": "Meu Timao",
       "color": "#1a7a3a",
-      "title": "Sem errar passes e com gol salvador: veja os números de Gustavo Henrique em empate do Corinthians",
-      "url": "https://www.meutimao.com.br/noticias-do-corinthians/532466/sem-errar-passes-e-com-gol-salvador-veja-os-numeros-de-gustavo-henrique-em-empate-do-corinthians",
-      "time": "07/05 · 15:39",
-      "pubDate": "Thu, 07 May 2026 12:39:00 -0300"
+      "title": "Meio-campista do Corinthians lidera ações com bola e duelos ganhos contra o Santa Fe",
+      "url": "https://www.meutimao.com.br/noticias-do-corinthians/532465/meio-campista-do-corinthians-lidera-acoes-com-bola-e-duelos-ganhos-contra-o-santa-fe",
+      "time": "07/05 · 19:33",
+      "pubDate": "Thu, 07 May 2026 16:33:00 -0300"
+    },
+    {
+      "source": "Bolavip",
+      "color": "#0055A5",
+      "title": "Corinthians é condenado a pagar R$ 6 milhões por Charles e pode sofrer transfer ban da FIFA",
+      "url": "<![CDATA[https://br.bolavip.com/corinthians/corinthians-e-condenado-a-pagar-r-6-milhoes-por-charles-e-pode-sofrer-transfer-ban-da-fifa]]>",
+      "time": "07/05 · 19:08",
+      "pubDate": "Thu, 07 May 2026 16:08:57 -0300"
+    },
+    {
+      "source": "Bolavip",
+      "color": "#0055A5",
+      "title": "Corinthians amplia invencibilidade no El Campín após empate pela Libertadores",
+      "url": "<![CDATA[https://br.bolavip.com/corinthians/corinthians-amplia-invencibilidade-no-el-campin-apos-empate-pela-libertadores]]>",
+      "time": "07/05 · 13:38",
+      "pubDate": "Thu, 07 May 2026 10:38:00 -0300"
     },
     {
       "source": "Bolavip",
@@ -115,90 +131,86 @@ const LIVE_DATA = {
       "pubDate": "Thu, 07 May 2026 01:26:01 -0300"
     },
     {
-      "source": "Bolavip",
-      "color": "#0055A5",
-      "title": "Matheus Bidu é atingido por objeto e cai no gramado durante empate do Corinthians",
-      "url": "<![CDATA[https://br.bolavip.com/corinthians/matheus-bidu-e-atingido-por-objeto-e-cai-no-gramado-durante-empate-do-corinthians]]>",
-      "time": "07/05 · 02:49",
-      "pubDate": "Wed, 06 May 2026 23:49:00 -0300"
-    },
-    {
-      "source": "Bolavip",
-      "color": "#0055A5",
-      "title": "Fernando Diniz adota postura defensiva, sofre pressão do Santa Fe, mas Corinthians arranca empate no fim",
-      "url": "<![CDATA[https://br.bolavip.com/corinthians/fernando-diniz-aposta-na-defesa-e-corinthians-empata-no-fim-do-jogo]]>",
-      "time": "07/05 · 02:30",
-      "pubDate": "Wed, 06 May 2026 23:30:46 -0300"
+      "source": "Gazeta Esportiva",
+      "color": "#E5820A",
+      "title": "Memphis decepciona em nova função no Corinthians",
+      "url": "https://www.gazetaesportiva.com/times/corinthians/memphis-decepciona-em-nova-funcao-no-corinthians/",
+      "time": "02/03 · 10:00",
+      "pubDate": "Mon, 02 Mar 2026 10:00:10 +0000"
     },
     {
       "source": "Gazeta Esportiva",
       "color": "#E5820A",
-      "title": "Corinthians vence Juventude e retoma liderança do Brasileirão feminino",
-      "url": "https://www.gazetaesportiva.com/times/corinthians/corinthians-vence-juventude-brasileirao-feminino-rodada-7/",
-      "time": "21/04 · 02:02",
-      "pubDate": "Tue, 21 Apr 2026 02:02:11 +0000"
+      "title": "Sem Yuri Alberto, média de gols do Corinthians despenca",
+      "url": "https://www.gazetaesportiva.com/times/corinthians/sem-yuri-alberto-media-de-gols-do-corinthians-despenca/",
+      "time": "02/03 · 09:00",
+      "pubDate": "Mon, 02 Mar 2026 09:00:19 +0000"
     },
     {
       "source": "Gazeta Esportiva",
       "color": "#E5820A",
-      "title": "Barra x Corinthians pela Copa do Brasil: onde assistir ao vivo, estatísticas, histórico e escalações",
-      "url": "https://www.gazetaesportiva.com/campeonatos/copa-do-brasil/barra-x-corinthians-pela-copa-do-brasil-onde-assistir-ao-vivo-estatisticas-historico-e-escalacoes/",
-      "time": "20/04 · 23:00",
-      "pubDate": "Mon, 20 Apr 2026 23:00:25 +0000"
+      "title": "Saiba os números e os concorrentes de Lingard, meia próximo de reforçar o Corinthians",
+      "url": "https://www.gazetaesportiva.com/times/corinthians/saiba-os-numeros-e-os-concorrentes-de-lingard-atacante-proximo-de-reforcar-o-corinthians/",
+      "time": "02/03 · 08:00",
+      "pubDate": "Mon, 02 Mar 2026 08:00:34 +0000"
     },
     {
       "source": "Gazeta Esportiva",
       "color": "#E5820A",
-      "title": "Corinthians perde dupla e desembarca em Santa Catarina para estreia na Copa do Brasil",
-      "url": "https://www.gazetaesportiva.com/times/corinthians/corinthians-perde-dupla-e-desembarca-em-santa-catarina-para-estreia-na-copa-do-brasil/",
-      "time": "20/04 · 22:31",
-      "pubDate": "Mon, 20 Apr 2026 22:31:46 +0000"
+      "title": "Grêmio x Corinthians: veja prováveis escalações e onde assistir ao jogo pelo Brasileiro sub-20",
+      "url": "https://www.gazetaesportiva.com/times/corinthians/gremio-x-corinthians-veja-provaveis-escalacoes-e-onde-assistir-ao-jogo-pelo-brasileiro-sub-20/",
+      "time": "02/03 · 01:05",
+      "pubDate": "Mon, 02 Mar 2026 01:05:07 +0000"
     },
     {
       "source": "Gazeta Esportiva",
       "color": "#E5820A",
-      "title": "Matheuzinho projeta estreia do Corinthians e pede &#8220;pés no chão&#8221; em defesa do título da Copa do Brasil",
-      "url": "https://www.gazetaesportiva.com/times/corinthians/matheuzinho-projeta-estreia-do-corinthians-e-pede-pes-no-chao-em-defesa-do-titulo-da-copa-do-brasil/",
-      "time": "20/04 · 20:30",
-      "pubDate": "Mon, 20 Apr 2026 20:30:22 +0000"
+      "title": "Presidente do Corinthians veta saída de André, e Milan ameaça ir à Fifa",
+      "url": "https://www.gazetaesportiva.com/times/corinthians/presidente-do-corinthians-veta-saida-de-andre-e-milan-ameaca-ir-a-fifa/",
+      "time": "02/03 · 00:27",
+      "pubDate": "Mon, 02 Mar 2026 00:27:33 +0000"
     },
     {
       "source": "Gazeta Esportiva",
       "color": "#E5820A",
-      "title": "Escalação: Corinthians fecha preparação e pode ter mudanças para estreia na Copa do Brasil",
-      "url": "https://www.gazetaesportiva.com/times/corinthians/escalacao-corinthians-fecha-preparacao-e-pode-ter-mudancas-para-estreia-na-copa-do-brasil/",
-      "time": "20/04 · 19:00",
-      "pubDate": "Mon, 20 Apr 2026 19:00:52 +0000"
+      "title": "Corinthians: conheça os números da equipe na temporada 2026",
+      "url": "https://www.gazetaesportiva.com/times/corinthians/corinthians-conheca-os-numeros-da-equipe-na-temporada-2026/",
+      "time": "01/03 · 17:29",
+      "pubDate": "Sun, 01 Mar 2026 17:29:03 +0000"
     },
     {
       "source": "Gazeta Esportiva",
       "color": "#E5820A",
-      "title": "Veja fotos do último treino do Corinthians antes da estreia na Copa do Brasil",
-      "url": "https://www.gazetaesportiva.com/times/corinthians/veja-fotos-do-ultimo-treino-do-corinthians-antes-da-estreia-na-copa-do-brasil/",
-      "time": "20/04 · 18:55",
-      "pubDate": "Mon, 20 Apr 2026 18:55:03 +0000"
+      "title": "Lingard chega ao Brasil para realizar exames e assinar com o Corinthians",
+      "url": "https://www.gazetaesportiva.com/times/corinthians/lingard-chega-ao-brasil-para-realizar-exames-e-assinar-com-o-corinthians/",
+      "time": "01/03 · 12:25",
+      "pubDate": "Sun, 01 Mar 2026 12:25:50 +0000"
     },
     {
       "source": "Gazeta Esportiva",
       "color": "#E5820A",
-      "title": "Alex Santana é inscrito pelo Corinthians na Copa do Brasil após reintegração",
-      "url": "https://www.gazetaesportiva.com/times/corinthians/alex-santana-inscrito-corinthians-copa-do-brasil/",
-      "time": "20/04 · 15:21",
-      "pubDate": "Mon, 20 Apr 2026 15:21:02 +0000"
-    },
-    {
-      "source": "Gazeta Esportiva",
-      "color": "#E5820A",
-      "title": "Barra x Corinthians: veja onde assistir à partida pela Copa do Brasil",
-      "url": "https://www.gazetaesportiva.com/times/corinthians/barra-x-corinthians-veja-onde-assistir-a-partida-pela-copa-do-brasil/",
-      "time": "20/04 · 10:00",
-      "pubDate": "Mon, 20 Apr 2026 10:00:50 +0000"
+      "title": "Após queda no Paulista, Corinthians terá maior pausa no calendário desde início da temporada",
+      "url": "https://www.gazetaesportiva.com/times/corinthians/apos-queda-no-paulista-corinthians-tera-maior-pausa-no-calendario-desde-inicio-da-temporada/",
+      "time": "01/03 · 11:00",
+      "pubDate": "Sun, 01 Mar 2026 11:00:25 +0000"
     }
   ],
   "upcoming": [],
   "results": [],
   "standings": [],
   "highlights": [
+    {
+      "title": "Lateral do Corinthians avalia jogo na altitude de Bogotá e projeta nova sequência sem sofrer gols",
+      "url": "https://www.meutimao.com.br/noticias-do-corinthians/532487/lateral-do-corinthians-avalia-jogo-na-altitude-de-bogota-e-projeta-nova-sequencia-sem-sofrer-gols",
+      "pubDate": "Thu, 07 May 2026 19:28:00 -0300",
+      "date": "07/05 · 22:28"
+    },
+    {
+      "title": "Gustavo Henrique registra 14 participações em gols desde chegada ao Corinthians",
+      "url": "https://www.meutimao.com.br/noticias-do-corinthians/532439/gustavo-henrique-registra-14-participacoes-em-gols-desde-chegada-ao-corinthians",
+      "pubDate": "Thu, 07 May 2026 18:30:00 -0300",
+      "date": "07/05 · 21:30"
+    },
     {
       "title": "Sem errar passes e com gol salvador: veja os números de Gustavo Henrique em empate do Corinthians",
       "url": "https://www.meutimao.com.br/noticias-do-corinthians/532466/sem-errar-passes-e-com-gol-salvador-veja-os-numeros-de-gustavo-henrique-em-empate-do-corinthians",
@@ -234,18 +246,6 @@ const LIVE_DATA = {
       "url": "https://www.meutimao.com.br/noticias-do-corinthians/532461/zagueiro-e-goleiro-saem-como-herois-em-empate-do-corinthians-diante-do-santa-fe-veja-notas",
       "pubDate": "Thu, 07 May 2026 00:46:00 -0300",
       "date": "07/05 · 03:46"
-    },
-    {
-      "title": "Corinthians sofre gols em jogos seguidos após sequência defensiva histórica",
-      "url": "https://www.meutimao.com.br/noticias-do-corinthians/532441/corinthians-sofre-gols-em-jogos-seguidos-apos-sequencia-defensiva-historica",
-      "pubDate": "Thu, 07 May 2026 00:23:00 -0300",
-      "date": "07/05 · 03:23"
-    },
-    {
-      "title": "Zagueiro do Corinthians comemora gol marcado e analisa confusão após empate com o Santa Fe",
-      "url": "https://www.meutimao.com.br/noticias-do-corinthians/532455/zagueiro-do-corinthians-comemora-gol-marcado-e-analisa-confusao-apos-empate-com-o-santa-fe",
-      "pubDate": "Thu, 07 May 2026 00:14:00 -0300",
-      "date": "07/05 · 03:14"
     }
   ]
 };
