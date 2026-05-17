e650b169746097e4dfd8b00d7119da5cdb1b9be4
GitHub Action <action@github.com>
2026-05-17 09:25:57 +0000

    Dados atualizados

--- src/data.js
+++ src/data.js
@@ -1,70 +1,86 @@
-// Gerado automaticamente em 16/05/2026, 22:33:28
+// Gerado automaticamente em 17/05/2026, 09:25:56
 const LIVE_DATA = {
-  "updatedAt": "2026-05-16T22:33:28.755Z",
+  "updatedAt": "2026-05-17T09:25:56.987Z",
   "news": [
     {
       "source": "Meu Timao",
       "color": "#1a7a3a",
-      "title": "Meia marroquino desfalca o Corinthians contra o Botafogo pelo Brasileirão",
-      "url": "https://www.meutimao.com.br/noticias-do-corinthians/533088/meia-marroquino-desfalca-o-corinthians-contra-o-botafogo-pelo-brasileirao",
-      "time": "16/05 · 22:07",
-      "pubDate": "Sat, 16 May 2026 19:07:00 -0300"
+      "title": "Corinthians visita o Botafogo para embalar no Campeonato Brasileiro; saiba tudo",
+      "url": "https://www.meutimao.com.br/noticias-do-corinthians/533077/corinthians-visita-o-botafogo-para-embalar-no-campeonato-brasileiro-saiba-tudo",
+      "time": "17/05 · 03:01",
+      "pubDate": "Sun, 17 May 2026 00:01:00 -0300"
     },
     {
       "source": "Meu Timao",
       "color": "#1a7a3a",
-      "title": "VÍDEO: Sem Memphis e Labyad! Corinthians desembarca no Rio de Janeiro para enfrentar o Botafogo",
-      "url": "https://www.meutimao.com.br/videos-do-corinthians/127875/sem-memphis-e-labyad-corinthians-desembarca-no-rio-de-janeiro-para-enfrentar-o-botafogo",
-      "time": "16/05 · 22:03",
-      "pubDate": "Sat, 16 May 2026 19:03:12 -0300"
+      "title": "Técnico do Corinthians exalta equilíbrio defensivo em campanha histórica no NBB",
+      "url": "https://www.meutimao.com.br/noticias-do-corinthians/533071/tecnico-do-corinthians-exalta-equilibrio-defensivo-em-campanha-historica-no-nbb",
+      "time": "17/05 · 01:30",
+      "pubDate": "Sat, 16 May 2026 22:30:00 -0300"
     },
     {
       "source": "Meu Timao",
       "color": "#1a7a3a",
-      "title": "Lateral do Corinthians completa 100 dias fora da equipe por conta de lesão no joelho direito",
-      "url": "https://www.meutimao.com.br/noticias-do-corinthians/533067/lateral-do-corinthians-completa-100-dias-fora-da-equipe-por-conta-de-lesao-no-joelho-direito",
-      "time": "16/05 · 22:02",
-      "pubDate": "Sat, 16 May 2026 19:02:00 -0300"
+      "title": "Zagueiro do Corinthians destaca importância da comissão técnica em vitória pelo Paulista Sub-17",
+      "url": "https://www.meutimao.com.br/noticias-do-corinthians/533069/zagueiro-do-corinthians-destaca-importancia-da-comissao-tecnica-em-vitoria-pelo-paulista-sub-17",
+      "time": "17/05 · 00:31",
+      "pubDate": "Sat, 16 May 2026 21:31:00 -0300"
     },
     {
       "source": "Meu Timao",
       "color": "#1a7a3a",
-      "title": "Corinthians volta a vencer em estreias de camisas I e II depois de três anos; veja o retrospecto",
-      "url": "https://www.meutimao.com.br/noticias-do-corinthians/533020/corinthians-volta-a-vencer-em-estreias-de-camisas-i-e-ii-depois-de-tres-anos-veja-o-retrospecto",
-      "time": "16/05 · 21:34",
-      "pubDate": "Sat, 16 May 2026 18:34:00 -0300"
+      "title": "Corinthians tem destaque individual e dupla no quinteto ideal das quartas do NBB",
+      "url": "https://www.meutimao.com.br/noticias-do-corinthians/533093/corinthians-tem-destaque-individual-e-dupla-no-quinteto-ideal-das-quartas-do-nbb",
+      "time": "17/05 · 00:01",
+      "pubDate": "Sat, 16 May 2026 21:01:00 -0300"
     },
     {
       "source": "Meu Timao",
       "color": "#1a7a3a",
-      "title": "Corinthians desembarca no Rio de Janeiro com 24 jogadores para duelo contra o Botafogo",
-      "url": "https://www.meutimao.com.br/noticias-do-corinthians/533057/corinthians-desembarca-no-rio-de-janeiro-com-24-jogadores-para-duelo-contra-o-botafogo",
-      "time": "16/05 · 21:33",
-      "pubDate": "Sat, 16 May 2026 18:33:00 -0300"
+      "title": "Com ótimo aproveitamento, Emily Lima alcança marca de dez jogos oficiais no comando do Corinthians",
+      "url": "https://www.meutimao.com.br/noticias-do-corinthians/533068/com-otimo-aproveitamento-emily-lima-alcanca-marca-de-dez-jogos-oficiais-no-comando-do-corinthians",
+      "time": "16/05 · 23:30",
+      "pubDate": "Sat, 16 May 2026 20:30:00 -0300"
+    },
+    {
+      "source": "Bolavip",
+      "color": "#0055A5",
+      "title": "Corinthians se posiciona após movimentações envolvendo Yuri Alberto e Hugo Souza",
+      "url": "<![CDATA[https://br.bolavip.com/corinthians/corinthians-se-posiciona-apos-movimentacoes-envolvendo-yuri-alberto-e-hugo-souza]]>",
+      "time": "16/05 · 23:09",
+      "pubDate": "Sat, 16 May 2026 20:09:43 -0300"
     },
     {
       "source": "Meu Timao",
       "color": "#1a7a3a",
-      "title": "Zagueiro do Corinthians explica discussão com Fernando Diniz e garante clima resolvido",
-      "url": "https://www.meutimao.com.br/noticias-do-corinthians/533082/zagueiro-do-corinthians-explica-discussao-com-fernando-diniz-e-garante-clima-resolvido",
-      "time": "16/05 · 21:01",
-      "pubDate": "Sat, 16 May 2026 18:01:00 -0300"
+      "title": "Presidente do Corinthians nega arrependimento por manter André e reforça necessidade de vendas",
+      "url": "https://www.meutimao.com.br/noticias-do-corinthians/533081/presidente-do-corinthians-nega-arrependimento-por-manter-andre-e-reforca-necessidade-de-vendas",
+      "time": "16/05 · 23:03",
+      "pubDate": "Sat, 16 May 2026 20:03:00 -0300"
     },
     {
       "source": "Meu Timao",
       "color": "#1a7a3a",
-      "title": "Fernando Diniz destaca resposta positiva do elenco desde sua chegada ao Corinthians",
-      "url": "https://www.meutimao.com.br/noticias-do-corinthians/532997/fernando-diniz-destaca-resposta-positiva-do-elenco-desde-sua-chegada-ao-corinthians",
-      "time": "16/05 · 20:36",
-      "pubDate": "Sat, 16 May 2026 17:36:00 -0300"
+      "title": "Volante do Corinthians se desculpa por agressão a árbitro em derrota no Brasileirão Sub-20",
+      "url": "https://www.meutimao.com.br/noticias-do-corinthians/533089/volante-do-corinthians-se-desculpa-por-agressao-a-arbitro-em-derrota-no-brasileirao-sub-20",
+      "time": "16/05 · 22:51",
+      "pubDate": "Sat, 16 May 2026 19:51:09 -0300"
     },
     {
       "source": "Meu Timao",
       "color": "#1a7a3a",
-      "title": "Elenco do Corinthians tira fotos com troféu da NBA no CT",
-      "url": "https://www.meutimao.com.br/noticias-do-corinthians/533072/elenco-do-corinthians-tira-fotos-com-trofeu-da-nba-no-ct",
-      "time": "16/05 · 20:01",
-      "pubDate": "Sat, 16 May 2026 17:01:00 -0300"
+      "title": "Técnico do Corinthians defende jovem volante de críticas após classificação na Copa do Brasil",
+      "url": "https://www.meutimao.com.br/noticias-do-corinthians/532998/tecnico-do-corinthians-defende-jovem-volante-de-criticas-apos-classificacao-na-copa-do-brasil",
+      "time": "16/05 · 22:33",
+      "pubDate": "Sat, 16 May 2026 19:33:00 -0300"
+    },
+    {
+      "source": "Bolavip",
+      "color": "#0055A5",
+      "title": "Com novidade na zaga, Fernando Diniz define escalação titular do Corinthians contra o Botafogo",
+      "url": "<![CDATA[https://br.bolavip.com/corinthians/com-novidade-na-zaga-fernando-diniz-define-escalacao-titular-do-corinthians-contra-o-botafogo]]>",
+      "time": "16/05 · 22:19",
+      "pubDate": "Sat, 16 May 2026 19:19:44 -0300"
     },
     {
       "source": "Bolavip",
@@ -113,22 +129,6 @@ const LIVE_DATA = {
       "url": "<![CDATA[https://br.bolavip.com/corinthians/osmar-stabile-confirma-que-yuri-alberto-nao-quer-ficar-no-corinthians]]>",
       "time": "15/05 · 19:17",
       "pubDate": "Fri, 15 May 2026 16:17:18 -0300"
-    },
-    {
-      "source": "Bolavip",
-      "color": "#0055A5",
-      "title": "Além de Yuri Alberto, Corinthians tem acordo para vender Hugo Souza",
-      "url": "<![CDATA[https://br.bolavip.com/corinthians/alem-de-yuri-alberto-corinthians-tem-acordo-para-vender-hugo-souza]]>",
-      "time": "15/05 · 19:01",
-      "pubDate": "Fri, 15 May 2026 16:01:13 -0300"
-    },
-    {
-      "source": "Bolavip",
-      "color": "#0055A5",
-      "title": "Corinthians aguarda proposta oficial de R$ 118 milhões da Roma-ITA por 40% de Yuri Alberto",
-      "url": "<![CDATA[https://br.bolavip.com/corinthians/corinthians-aguarda-proposta-oficial-de-r-118-milhoes-da-roma-ita-por-40-de-yuri-alberto]]>",
-      "time": "15/05 · 17:36",
-      "pubDate": "Fri, 15 May 2026 14:36:30 -0300"
     },
     {
       "source": "Gazeta Esportiva",
@@ -206,6 +206,12 @@ const LIVE_DATA = {
       "date": "16/05 · 22:03"
     },
     {
+      "title": "Corinthians recebe taça da NBA no CT e jogadores aproveitam momento para fotos",
+      "url": "https://www.meutimao.com.br/noticias-do-corinthians/533072/corinthians-recebe-taca-da-nba-no-ct-e-jogadores-aproveitam-momento-para-fotos",
+      "pubDate": "Sat, 16 May 2026 17:01:00 -0300",
+      "date": "16/05 · 20:01"
+    },
+    {
       "title": "VÍDEO: Provável escalação do Corinthians para enfrentar o botafogo pelo Campeonato Brasileiro 2026",
       "url": "https://www.meutimao.com.br/videos-do-corinthians/127871/provavel-escalacao-do-corinthians-para-enfrentar-o-botafogo-pelo-campeonato-brasileiro-2026",
       "pubDate": "Sat, 16 May 2026 15:12:13 -0300",
@@ -216,12 +222,6 @@ const LIVE_DATA = {
       "url": "https://www.meutimao.com.br/noticias-do-corinthians/533018/ariel-godoi-marca-primeiro-hat-trick-pelo-corinthians-em-goleada-no-paulistao-feminino",
       "pubDate": "Sat, 16 May 2026 14:34:00 -0300",
       "date": "16/05 · 17:34"
-    },
-    {
-      "title": "Motivação sobre o passado e novo uniforme marcam bastidores da goleada sobre o Red Bull Bragantino",
-      "url": "https://www.meutimao.com.br/noticias-do-corinthians/533046/motivacao-sobre-o-passado-e-novo-uniforme-marcam-bastidores-da-goleada-sobre-o-red-bull-bragantino",
-      "pubDate": "Fri, 15 May 2026 22:00:00 -0300",
-      "date": "16/05 · 01:00"
     }
   ]
 };
