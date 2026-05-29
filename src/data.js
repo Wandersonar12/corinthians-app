fcdce828e47441251bb4f935af2187131ab78b07
GitHub Action <action@github.com>
2026-05-29 23:01:18 +0000

    Dados atualizados

--- src/data.js
+++ src/data.js
@@ -1,70 +1,102 @@
-// Gerado automaticamente em 29/05/2026, 20:03:07
+// Gerado automaticamente em 29/05/2026, 23:01:18
 const LIVE_DATA = {
-  "updatedAt": "2026-05-29T20:03:07.701Z",
+  "updatedAt": "2026-05-29T23:01:18.785Z",
   "news": [
     {
       "source": "Meu Timao",
       "color": "#1a7a3a",
-      "title": "Meia brilha e Corinthians vence o Vitória na estreia do Brasileirão Sub-17",
-      "url": "https://www.meutimao.com.br/noticias-do-corinthians/534002/meia-brilha-e-corinthians-vence-o-vitoria-na-estreia-do-brasileirao-sub-17",
-      "time": "29/05 · 19:57",
-      "pubDate": "Fri, 29 May 2026 16:57:01 -0300"
+      "title": "Fernando Diniz busca soluções para o ataque do Corinthians em duelo com o Grêmio",
+      "url": "https://www.meutimao.com.br/noticias-do-corinthians/533986/fernando-diniz-busca-solucoes-para-o-ataque-do-corinthians-em-duelo-com-o-gremio",
+      "time": "29/05 · 22:32",
+      "pubDate": "Fri, 29 May 2026 19:32:00 -0300"
     },
     {
       "source": "Meu Timao",
       "color": "#1a7a3a",
-      "title": "Jovem atacante busca primeira chance com Fernando Diniz no Corinthians",
-      "url": "https://www.meutimao.com.br/noticias-do-corinthians/533988/jovem-atacante-busca-primeira-chance-com-fernando-diniz-no-corinthians",
-      "time": "29/05 · 19:33",
-      "pubDate": "Fri, 29 May 2026 16:33:00 -0300"
+      "title": "VÍDEO: Desembarque do Corinthians em Porto Alegre para partida contra o Grêmio pelo Brasileirão 2026",
+      "url": "https://www.meutimao.com.br/videos-do-corinthians/128121/desembarque-do-corinthians-em-porto-alegre-para-partida-contra-o-gremio-pelo-brasileirao-2026",
+      "time": "29/05 · 22:15",
+      "pubDate": "Fri, 29 May 2026 19:15:27 -0300"
     },
     {
       "source": "Meu Timao",
       "color": "#1a7a3a",
-      "title": "Atacante se isola e zagueiros se destacam em artilharia do Sub-20 do Corinthians em 2026",
-      "url": "https://www.meutimao.com.br/noticias-do-corinthians/533964/atacante-se-isola-e-zagueiros-se-destacam-em-artilharia-do-sub-20-do-corinthians-em-2026",
-      "time": "29/05 · 18:33",
-      "pubDate": "Fri, 29 May 2026 15:33:00 -0300"
+      "title": "Corinthians desembarca em Porto Alegre com 24 jogadores para duelo contra o Grêmio pelo Brasileirão",
+      "url": "https://www.meutimao.com.br/noticias-do-corinthians/534004/corinthians-desembarca-em-porto-alegre-com-24-jogadores-para-duelo-contra-o-gremio-pelo-brasileirao",
+      "time": "29/05 · 21:59",
+      "pubDate": "Fri, 29 May 2026 18:59:00 -0300"
     },
     {
       "source": "Meu Timao",
       "color": "#1a7a3a",
-      "title": "Eliminação há mais de vinte anos: Confira o retrospecto entre Corinthians e Rosario Central",
-      "url": "https://www.meutimao.com.br/noticias-do-corinthians/533999/eliminacao-ha-mais-de-vinte-anos-confira-o-retrospecto-entre-corinthians-e-rosario-central",
-      "time": "29/05 · 18:31",
-      "pubDate": "Fri, 29 May 2026 15:31:00 -0300"
+      "title": "Atacante do Corinthians é convocada pela Colômbia para últimas rodadas da Liga das Nações",
+      "url": "https://www.meutimao.com.br/noticias-do-corinthians/534020/atacante-do-corinthians-e-convocada-pela-colombia-para-ultimas-rodadas-da-liga-das-nacoes",
+      "time": "29/05 · 21:49",
+      "pubDate": "Fri, 29 May 2026 18:49:58 -0300"
     },
     {
       "source": "Meu Timao",
       "color": "#1a7a3a",
-      "title": "Atacante do Corinthians antecipa 'final' contra o Grêmio em duelo pelo Brasileirão",
-      "url": "https://www.meutimao.com.br/noticias-do-corinthians/534003/atacante-do-corinthians-antecipa-final-contra-o-gremio-em-duelo-pelo-brasileirao",
-      "time": "29/05 · 18:28",
-      "pubDate": "Fri, 29 May 2026 15:28:00 -0300"
+      "title": "Edu Gaspar elege melhores contratações feitas durante sua passagem como gerente do Corinthians",
+      "url": "https://www.meutimao.com.br/noticias-do-corinthians/533983/edu-gaspar-elege-melhores-contratacoes-feitas-durante-sua-passagem-como-gerente-do-corinthians",
+      "time": "29/05 · 21:31",
+      "pubDate": "Fri, 29 May 2026 18:31:00 -0300"
     },
     {
       "source": "Meu Timao",
       "color": "#1a7a3a",
-      "title": "Corinthians encerra preparação para enfrentar o Grêmio pelo Brasileirão; veja a provável escalação",
-      "url": "https://www.meutimao.com.br/noticias-do-corinthians/533989/corinthians-encerra-preparacao-para-enfrentar-o-gremio-pelo-brasileirao-veja-a-provavel-escalacao",
-      "time": "29/05 · 18:15",
-      "pubDate": "Fri, 29 May 2026 15:15:00 -0300"
+      "title": "VÍDEO: Yuri Alberto segue titular? Confira a provável escalação do Corinthians para enfrentar o Grêmio",
+      "url": "https://www.meutimao.com.br/videos-do-corinthians/128119/yuri-alberto-segue-titular-confira-a-provavel-escalacao-do-corinthians-para-enfrentar-o-gremio",
+      "time": "29/05 · 20:42",
+      "pubDate": "Fri, 29 May 2026 17:42:00 -0300"
     },
     {
       "source": "Meu Timao",
       "color": "#1a7a3a",
-      "title": "Kaio César deseja sorte a Memphis na Copa do Mundo e reforça torcida por permanência no Corinthians",
-      "url": "https://www.meutimao.com.br/noticias-do-corinthians/533932/kaio-cesar-deseja-sorte-a-memphis-na-copa-do-mundo-e-reforca-torcida-por-permanencia-no-corinthians",
-      "time": "29/05 · 18:01",
-      "pubDate": "Fri, 29 May 2026 15:01:00 -0300"
+      "title": "Corinthians busca evitar sequência inédita sem vitórias contra o Palmeiras",
+      "url": "https://www.meutimao.com.br/noticias-do-corinthians/533968/corinthians-busca-evitar-sequencia-inedita-sem-vitorias-contra-o-palmeiras",
+      "time": "29/05 · 20:33",
+      "pubDate": "Fri, 29 May 2026 17:33:00 -0300"
     },
     {
       "source": "Meu Timao",
       "color": "#1a7a3a",
-      "title": "Corinthians busca melhorar retrospecto em oitavas de final da Copa Libertadores; veja números",
-      "url": "https://www.meutimao.com.br/noticias-do-corinthians/534000/corinthians-busca-melhorar-retrospecto-em-oitavas-de-final-da-copa-libertadores-veja-numeros",
-      "time": "29/05 · 17:32",
-      "pubDate": "Fri, 29 May 2026 14:32:00 -0300"
+      "title": "MP-SP cobra novas explicações do Corinthians por contratação de empresa de segurança irregular",
+      "url": "https://www.meutimao.com.br/noticias-do-corinthians/534011/mp-sp-cobra-novas-explicacoes-do-corinthians-por-contratacao-de-empresa-de-seguranca-irregular",
+      "time": "29/05 · 20:14",
+      "pubDate": "Fri, 29 May 2026 17:14:00 -0300"
+    },
+    {
+      "source": "Bolavip",
+      "color": "#0055A5",
+      "title": "Corinthians trabalha para derrubar transfer ban e sonha com Roberto Firmino e Everton Cebolinha",
+      "url": "<![CDATA[https://br.bolavip.com/corinthians/corinthians-trabalha-para-derrubar-transfer-ban-e-sonha-com-roberto-firmino-e-everton-cebolinha]]>",
+      "time": "29/05 · 19:28",
+      "pubDate": "Fri, 29 May 2026 16:28:00 -0300"
+    },
+    {
+      "source": "Bolavip",
+      "color": "#0055A5",
+      "title": "Sem Memphis Depay, confira a provável escalação do Corinthians diante do Grêmio pelo Brasileirão",
+      "url": "<![CDATA[https://br.bolavip.com/corinthians/sem-memphis-depay-confira-a-provavel-escalacao-do-corinthians-diante-do-gremio-pelo-brasileirao]]>",
+      "time": "29/05 · 19:22",
+      "pubDate": "Fri, 29 May 2026 16:22:12 -0300"
+    },
+    {
+      "source": "Bolavip",
+      "color": "#0055A5",
+      "title": "Novo transfer ban? Corinthians lida com 4 cobranças e trava em problema bancário",
+      "url": "<![CDATA[https://br.bolavip.com/corinthians/novo-transfer-ban-corinthians-lida-com-4-cobrancas-e-trava-em-problema-bancario]]>",
+      "time": "29/05 · 14:58",
+      "pubDate": "Fri, 29 May 2026 11:58:34 -0300"
+    },
+    {
+      "source": "Bolavip",
+      "color": "#0055A5",
+      "title": "Corinthians amplia crise contra argentinos após nova derrota para o Platense",
+      "url": "<![CDATA[https://br.bolavip.com/corinthians/corinthians-amplia-crise-contra-argentinos-apos-nova-derrota-para-o-platense]]>",
+      "time": "29/05 · 14:32",
+      "pubDate": "Fri, 29 May 2026 11:32:48 -0300"
     },
     {
       "source": "Bolavip",
@@ -93,42 +125,10 @@ const LIVE_DATA = {
     {
       "source": "Bolavip",
       "color": "#0055A5",
-      "title": "Novo transfer ban? Corinthians lida com 4 cobranças e trava em problema bancário",
-      "url": "<![CDATA[https://br.bolavip.com/corinthians/novo-transfer-ban-corinthians-lida-com-4-cobrancas-e-trava-em-problema-bancario]]>",
-      "time": "29/05 · 11:00",
-      "pubDate": "Fri, 29 May 2026 08:00:08 -0300"
-    },
-    {
-      "source": "Bolavip",
-      "color": "#0055A5",
-      "title": "Corinthians encaminha renovação com Memphis Depay até 2028 e anúncio pode acontecer antes da Copa",
-      "url": "<![CDATA[https://br.bolavip.com/corinthians/corinthians-encaminha-renovacao-depay-2028]]>",
-      "time": "29/05 · 10:59",
-      "pubDate": "Fri, 29 May 2026 07:59:11 -0300"
-    },
-    {
-      "source": "Bolavip",
-      "color": "#0055A5",
       "title": "Memphis usa bandana com número 80 em jogo do Corinthians e motivo é campanha de marketing",
       "url": "<![CDATA[https://br.bolavip.com/corinthians/memphis-usa-bandana-com-numero-80-em-jogo-do-corinthians-e-motivo-e-campanha-de-marketing]]>",
       "time": "28/05 · 22:34",
       "pubDate": "Thu, 28 May 2026 19:34:31 -0300"
-    },
-    {
-      "source": "Bolavip",
-      "color": "#0055A5",
-      "title": "Duílio Monteiro renuncia ao cargo de conselheiro do Corinthians em momento de pressão",
-      "url": "<![CDATA[https://br.bolavip.com/corinthians/duilio-monteiro-renuncia-ao-cargo-de-conselheiro-do-corinthians-em-momento-de-pressao]]>",
-      "time": "28/05 · 22:02",
-      "pubDate": "Thu, 28 May 2026 19:02:06 -0300"
-    },
-    {
-      "source": "Bolavip",
-      "color": "#0055A5",
-      "title": "Corinthians amplia problemas e perde Lingard para último jogo antes da Copa do Mundo",
-      "url": "<![CDATA[https://br.bolavip.com/corinthians/corinthians-perde-lingard-para-ultimo-jogo-antes-da-copa-do-mundo]]>",
-      "time": "28/05 · 21:06",
-      "pubDate": "Thu, 28 May 2026 18:06:48 -0300"
     },
     {
       "source": "Gazeta Esportiva",
@@ -199,6 +199,24 @@ const LIVE_DATA = {
   "results": [],
   "standings": [],
   "highlights": [
+    {
+      "title": "VÍDEO: Desembarque do Corinthians em Porto Alegre para partida contra o Grêmio pelo Brasileirão 2026",
+      "url": "https://www.meutimao.com.br/videos-do-corinthians/128121/desembarque-do-corinthians-em-porto-alegre-para-partida-contra-o-gremio-pelo-brasileirao-2026",
+      "pubDate": "Fri, 29 May 2026 19:15:27 -0300",
+      "date": "29/05 · 22:15"
+    },
+    {
+      "title": "Edu Gaspar elege melhores contratações feitas durante sua passagem como gerente do Corinthians",
+      "url": "https://www.meutimao.com.br/noticias-do-corinthians/533983/edu-gaspar-elege-melhores-contratacoes-feitas-durante-sua-passagem-como-gerente-do-corinthians",
+      "pubDate": "Fri, 29 May 2026 18:31:00 -0300",
+      "date": "29/05 · 21:31"
+    },
+    {
+      "title": "VÍDEO: Yuri Alberto segue titular? Confira a provável escalação do Corinthians para enfrentar o Grêmio",
+      "url": "https://www.meutimao.com.br/videos-do-corinthians/128119/yuri-alberto-segue-titular-confira-a-provavel-escalacao-do-corinthians-para-enfrentar-o-gremio",
+      "pubDate": "Fri, 29 May 2026 17:42:00 -0300",
+      "date": "29/05 · 20:42"
+    },
     {
       "title": "Sorteio das oitavas de final da Libertadores 2026: horário, onde assistir e classificados",
       "url": "https://www.meutimao.com.br/noticias-do-corinthians/533967/sorteio-das-oitavas-de-final-da-libertadores-2026-horario-onde-assistir-e-classificados",
