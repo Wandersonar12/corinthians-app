728c8f1639b919409f0430beb46c1549d9ff5204
GitHub Action <action@github.com>
2026-05-31 13:05:17 +0000

    Dados atualizados

--- src/data.js
+++ src/data.js
@@ -1,7 +1,63 @@
-// Gerado automaticamente em 31/05/2026, 10:05:24
+// Gerado automaticamente em 31/05/2026, 13:05:17
 const LIVE_DATA = {
-  "updatedAt": "2026-05-31T10:05:24.893Z",
+  "updatedAt": "2026-05-31T13:05:17.648Z",
   "news": [
+    {
+      "source": "Meu Timao",
+      "color": "#1a7a3a",
+      "title": "Jovem do Corinthians segue sem oportunidades com Fernando Diniz mesmo com ataque desfalcado",
+      "url": "https://www.meutimao.com.br/noticias-do-corinthians/534099/jovem-do-corinthians-segue-sem-oportunidades-com-fernando-diniz-mesmo-com-ataque-desfalcado",
+      "time": "31/05 · 12:33",
+      "pubDate": "Sun, 31 May 2026 09:33:00 -0300"
+    },
+    {
+      "source": "Meu Timao",
+      "color": "#1a7a3a",
+      "title": "Goleira do Corinthians vê convocação como fruto do trabalho e quer aproveitar chance na Seleção",
+      "url": "https://www.meutimao.com.br/noticias-do-corinthians/534061/goleira-do-corinthians-ve-convocacao-como-fruto-do-trabalho-e-quer-aproveitar-chance-na-selecao",
+      "time": "31/05 · 11:31",
+      "pubDate": "Sun, 31 May 2026 08:31:00 -0300"
+    },
+    {
+      "source": "Meu Timao",
+      "color": "#1a7a3a",
+      "title": "Últimas do Corinthians: classificação no feminino, vitória no Brasileirão e suspensão de Garro",
+      "url": "https://www.meutimao.com.br/noticias-do-corinthians/534125/ultimas-do-corinthians-classificacao-no-feminino-vitoria-no-brasileirao-e-suspensao-de-garro",
+      "time": "31/05 · 10:30",
+      "pubDate": "Sun, 31 May 2026 07:30:00 -0300"
+    },
+    {
+      "source": "Bolavip",
+      "color": "#0055A5",
+      "title": "Vídeo: Hugo Souza projeta possível duelo contra Flamengo e manda recado após vitória do Corinthians",
+      "url": "<![CDATA[https://br.bolavip.com/corinthians/video-hugo-souza-projeta-possivel-duelo-contra-flamengo-e-manda-recado-apos-vitoria-do-corinthians]]>",
+      "time": "31/05 · 02:59",
+      "pubDate": "Sat, 30 May 2026 23:59:43 -0300"
+    },
+    {
+      "source": "Bolavip",
+      "color": "#0055A5",
+      "title": "Vídeo: Matheus Bidu ressalta nova função com Fernando Diniz após vitória do Corinthians contra o Grêmio",
+      "url": "<![CDATA[https://br.bolavip.com/corinthians/video-matheus-bidu-ressalta-nova-funcao-com-fernando-diniz-apos-vitoria-do-corinthians-contra-o-gremio]]>",
+      "time": "31/05 · 02:59",
+      "pubDate": "Sat, 30 May 2026 23:59:05 -0300"
+    },
+    {
+      "source": "Bolavip",
+      "color": "#0055A5",
+      "title": "Vídeo: Hugo Souza salienta importância de seu preparador de goleiros após vitória do Corinthians",
+      "url": "<![CDATA[https://br.bolavip.com/corinthians/video-hugo-souza-salienta-importancia-de-seu-preparador-de-goleiros-apos-vitoria-do-corinthians]]>",
+      "time": "31/05 · 02:42",
+      "pubDate": "Sat, 30 May 2026 23:42:08 -0300"
+    },
+    {
+      "source": "Bolavip",
+      "color": "#0055A5",
+      "title": "Diniz e Garro chamam atenção de Lucas Paulo Torezin em Grêmio x Corinthians",
+      "url": "<![CDATA[https://br.bolavip.com/corinthians/diniz-e-garro-chamam-atencao-de-lucas-paulo-torezin-em-gremio-x-corinthians]]>",
+      "time": "31/05 · 01:31",
+      "pubDate": "Sat, 30 May 2026 22:31:31 -0300"
+    },
     {
       "source": "Meu Timao",
       "color": "#1a7a3a",
@@ -16,7 +72,7 @@ const LIVE_DATA = {
       "title": "Corinthians entra na parte de cima da tabela após vitória sobre o Grêmio; confira",
       "url": "https://www.meutimao.com.br/noticias-do-corinthians/534103/corinthians-entra-na-parte-de-cima-da-tabela-apos-vitoria-sobre-o-gremio-confira",
       "time": "31/05 · 01:00",
-      "pubDate": "Sat, 30 May 2026 22:00:17 -0300"
+      "pubDate": "Sat, 30 May 2026 22:00:00 -0300"
     },
     {
       "source": "Meu Timao",
@@ -32,7 +88,7 @@ const LIVE_DATA = {
       "title": "Kaio César é eleito craque do Corinthians em avaliações da Fiel após vitória no Brasileirão",
       "url": "https://www.meutimao.com.br/noticias-do-corinthians/534121/kaio-cesar-e-eleito-craque-do-corinthians-em-avaliacoes-da-fiel-apos-vitoria-no-brasileirao",
       "time": "31/05 · 00:45",
-      "pubDate": "Sat, 30 May 2026 21:45:36 -0300"
+      "pubDate": "Sat, 30 May 2026 21:45:00 -0300"
     },
     {
       "source": "Meu Timao",
@@ -43,28 +99,12 @@ const LIVE_DATA = {
       "pubDate": "Sat, 30 May 2026 21:41:00 -0300"
     },
     {
-      "source": "Meu Timao",
-      "color": "#1a7a3a",
-      "title": "Volante do Corinthians volta a marcar após quase dois meses",
-      "url": "https://www.meutimao.com.br/noticias-do-corinthians/534081/volante-do-corinthians-volta-a-marcar-apos-quase-dois-meses",
-      "time": "31/05 · 00:12",
-      "pubDate": "Sat, 30 May 2026 21:12:00 -0300"
-    },
-    {
-      "source": "Meu Timao",
-      "color": "#1a7a3a",
-      "title": "Volante do Corinthians se destaca em números ofensivos e defensivos durante vitória no Brasileirão",
-      "url": "https://www.meutimao.com.br/noticias-do-corinthians/534097/volante-do-corinthians-se-destaca-em-numeros-ofensivos-e-defensivos-durante-vitoria-no-brasileirao",
-      "time": "31/05 · 00:03",
-      "pubDate": "Sat, 30 May 2026 21:03:00 -0300"
-    },
-    {
-      "source": "Meu Timao",
-      "color": "#1a7a3a",
-      "title": "Corinthians encerra jejum de mais de três meses como visitante no Brasileirão",
-      "url": "https://www.meutimao.com.br/noticias-do-corinthians/534083/corinthians-encerra-jejum-de-mais-de-tres-meses-como-visitante-no-brasileirao",
-      "time": "30/05 · 23:47",
-      "pubDate": "Sat, 30 May 2026 20:47:00 -0300"
+      "source": "Bolavip",
+      "color": "#0055A5",
+      "title": "Vídeo: Garro perde a paciência com árbitro de Grêmio x Corinthians e recebe amarelo",
+      "url": "<![CDATA[https://br.bolavip.com/corinthians/video-garro-perde-a-paciencia-com-arbitro-de-gremio-x-corinthians-e-recebe-amarelo]]>",
+      "time": "30/05 · 23:36",
+      "pubDate": "Sat, 30 May 2026 20:36:39 -0300"
     },
     {
       "source": "Bolavip",
@@ -85,50 +125,10 @@ const LIVE_DATA = {
     {
       "source": "Bolavip",
       "color": "#0055A5",
-      "title": "Vídeo: Garro perde a paciência com árbitro de Grêmio x Corinthians e recebe amarelo",
-      "url": "<![CDATA[https://br.bolavip.com/corinthians/video-garro-perde-a-paciencia-com-arbitro-de-gremio-x-corinthians-e-recebe-amarelo]]>",
-      "time": "30/05 · 22:19",
-      "pubDate": "Sat, 30 May 2026 19:19:05 -0300"
-    },
-    {
-      "source": "Bolavip",
-      "color": "#0055A5",
-      "title": "Grêmio x Corinthians: Relembre duelo histórico de 2023 que terminou no placar de 4 a 4",
-      "url": "<![CDATA[https://br.bolavip.com/corinthians/gremio-x-corinthians-relembre-duelo-historico-de-2023-que-terminou-no-placar-de-4-a-4]]>",
-      "time": "30/05 · 21:38",
-      "pubDate": "Sat, 30 May 2026 18:38:24 -0300"
-    },
-    {
-      "source": "Bolavip",
-      "color": "#0055A5",
-      "title": "Vídeo: Corinthians começa mal em Porto Alegre, sofre gol cedo do Grêmio e irrita torcida nas redes",
-      "url": "<![CDATA[https://br.bolavip.com/corinthians/corinthians-falha-no-inicio-e-gremio-abre-o-placar-em-porto-alegre]]>",
-      "time": "30/05 · 21:37",
-      "pubDate": "Sat, 30 May 2026 18:37:54 -0300"
-    },
-    {
-      "source": "Bolavip",
-      "color": "#0055A5",
       "title": "Torcida do Corinthians elogia Kaio César durante primeiro tempo contra o Grêmio no Brasileirão",
       "url": "<![CDATA[https://br.bolavip.com/corinthians/torcida-do-corinthians-elogia-kaio-cesar-durante-primeiro-tempo-contra-o-gremio-no-brasileirao]]>",
       "time": "30/05 · 21:35",
       "pubDate": "Sat, 30 May 2026 18:35:56 -0300"
-    },
-    {
-      "source": "Bolavip",
-      "color": "#0055A5",
-      "title": "Corinthians define futuro de atletas em duelo decisivo contra o Grêmio",
-      "url": "<![CDATA[https://br.bolavip.com/corinthians/corinthians-define-futuro-de-atletas-em-duelo-decisivo-contra-o-gremio]]>",
-      "time": "30/05 · 16:19",
-      "pubDate": "Sat, 30 May 2026 13:19:43 -0300"
-    },
-    {
-      "source": "Bolavip",
-      "color": "#0055A5",
-      "title": "Diniz mira sua primeira vitória como visitante no Brasileirão em Grêmio x Corinthians",
-      "url": "<![CDATA[https://br.bolavip.com/corinthians/diniz-mira-sua-primeira-vitoria-como-visitante-no-brasileirao-em-gremio-x-corinthians]]>",
-      "time": "30/05 · 13:43",
-      "pubDate": "Sat, 30 May 2026 10:43:11 -0300"
     },
     {
       "source": "Gazeta Esportiva",
@@ -200,6 +200,12 @@ const LIVE_DATA = {
   "standings": [],
   "highlights": [
     {
+      "title": "Goleira do Corinthians vê convocação como fruto do trabalho e quer aproveitar chance na Seleção",
+      "url": "https://www.meutimao.com.br/noticias-do-corinthians/534061/goleira-do-corinthians-ve-convocacao-como-fruto-do-trabalho-e-quer-aproveitar-chance-na-selecao",
+      "pubDate": "Sun, 31 May 2026 08:31:00 -0300",
+      "date": "31/05 · 11:31"
+    },
+    {
       "title": "VÍDEO: Melhores momentos: Grêmio 1x3 Corinthians | 18ª rodada | Brasileirão 2026",
       "url": "https://www.meutimao.com.br/videos-do-corinthians/128143/melhores-momentos-gremio-1x3-corinthians-18-rodada-brasileirao-2026",
       "pubDate": "Sat, 30 May 2026 21:41:00 -0300",
@@ -212,15 +218,15 @@ const LIVE_DATA = {
       "date": "31/05 · 00:26"
     },
     {
-      "title": "Atacante marca primeiro gol pelo Corinthians em vitória sobre o Grêmio",
-      "url": "https://www.meutimao.com.br/noticias-do-corinthians/534085/atacante-marca-primeiro-gol-pelo-corinthians-em-vitoria-sobre-o-gremio",
-      "pubDate": "Sat, 30 May 2026 20:10:20 -0300",
+      "title": "Kaio César marca primeiro gol pelo Corinthians em vitória sobre o Grêmio",
+      "url": "https://www.meutimao.com.br/noticias-do-corinthians/534085/kaio-cesar-marca-primeiro-gol-pelo-corinthians-em-vitoria-sobre-o-gremio",
+      "pubDate": "Sat, 30 May 2026 20:10:00 -0300",
       "date": "30/05 · 23:10"
     },
     {
-      "title": "Goleira valoriza classificação sobre o Palmeiras pela Copa do Brasil Feminina",
-      "url": "https://www.meutimao.com.br/noticias-do-corinthians/534060/goleira-valoriza-classificacao-sobre-o-palmeiras-pela-copa-do-brasil-feminina",
-      "pubDate": "Sat, 30 May 2026 16:13:14 -0300",
+      "title": "Goleira do Corinthians valoriza classificação sobre o Palmeiras pela Copa do Brasil Feminina",
+      "url": "https://www.meutimao.com.br/noticias-do-corinthians/534060/goleira-do-corinthians-valoriza-classificacao-sobre-o-palmeiras-pela-copa-do-brasil-feminina",
+      "pubDate": "Sat, 30 May 2026 16:13:00 -0300",
       "date": "30/05 · 19:13"
     },
     {
