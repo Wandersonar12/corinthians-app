ead91d2b6468cef3b4f3b7fd90bb3ba88416f9d1
GitHub Action <action@github.com>
2026-06-04 19:51:13 +0000

    Dados atualizados

--- src/data.js
+++ src/data.js
@@ -1,14 +1,62 @@
-// Gerado automaticamente em 04/06/2026, 14:26:09
+// Gerado automaticamente em 04/06/2026, 19:51:13
 const LIVE_DATA = {
-  "updatedAt": "2026-06-04T14:26:09.872Z",
+  "updatedAt": "2026-06-04T19:51:13.367Z",
   "news": [
+    {
+      "source": "Meu Timao",
+      "color": "#1a7a3a",
+      "title": "Vitória do Corinthians no Paulista de Futsal movimenta ranking de artilheiros da temporada",
+      "url": "https://www.meutimao.com.br/noticias-do-corinthians/534322/vitoria-do-corinthians-no-paulista-de-futsal-movimenta-ranking-de-artilheiros-da-temporada",
+      "time": "04/06 · 19:34",
+      "pubDate": "Thu, 04 Jun 2026 16:34:00 -0300"
+    },
+    {
+      "source": "Meu Timao",
+      "color": "#1a7a3a",
+      "title": "Corinthians está entre os times brasileiros com menor números de convocados nesta Copa do Mundo",
+      "url": "https://www.meutimao.com.br/noticias-do-corinthians/534281/corinthians-esta-entre-os-times-brasileiros-com-menor-numeros-de-convocados-nesta-copa-do-mundo",
+      "time": "04/06 · 18:32",
+      "pubDate": "Thu, 04 Jun 2026 15:32:00 -0300"
+    },
+    {
+      "source": "Meu Timao",
+      "color": "#1a7a3a",
+      "title": "Corinthians tem terceiro melhor início de Paulistão da década após vencer o Indaiatuba",
+      "url": "https://www.meutimao.com.br/noticias-do-corinthians/534319/corinthians-tem-terceiro-melhor-inicio-de-paulistao-da-decada-apos-vencer-o-indaiatuba",
+      "time": "04/06 · 17:33",
+      "pubDate": "Thu, 04 Jun 2026 14:33:00 -0300"
+    },
+    {
+      "source": "Meu Timao",
+      "color": "#1a7a3a",
+      "title": "Arthur Elias retorna ao CT Joaquim Grava e destaca importância do Corinthians em sua carreira",
+      "url": "https://www.meutimao.com.br/noticias-do-corinthians/534265/arthur-elias-retorna-ao-ct-joaquim-grava-e-destaca-importancia-do-corinthians-em-sua-carreira",
+      "time": "04/06 · 16:32",
+      "pubDate": "Thu, 04 Jun 2026 13:32:00 -0300"
+    },
+    {
+      "source": "Meu Timao",
+      "color": "#1a7a3a",
+      "title": "Empresário detalha motivo que o levou a acionar o Corinthians na Justiça e critica Augusto Melo",
+      "url": "https://www.meutimao.com.br/noticias-do-corinthians/534299/empresario-detalha-motivo-que-o-levou-a-acionar-o-corinthians-na-justica-e-critica-augusto-melo",
+      "time": "04/06 · 15:29",
+      "pubDate": "Thu, 04 Jun 2026 12:29:00 -0300"
+    },
+    {
+      "source": "Meu Timao",
+      "color": "#1a7a3a",
+      "title": "Olheiro do Corinthians participa de observação de atletas no continente africano",
+      "url": "https://www.meutimao.com.br/noticias-do-corinthians/534292/olheiro-do-corinthians-participa-de-observacao-de-atletas-no-continente-africano",
+      "time": "04/06 · 14:32",
+      "pubDate": "Thu, 04 Jun 2026 11:32:00 -0300"
+    },
     {
       "source": "Meu Timao",
       "color": "#1a7a3a",
       "title": "Corinthians faz treino aberto e encerra preparação para amistoso contra sub-23 dos Estados Unidos",
       "url": "https://www.meutimao.com.br/noticias-do-corinthians/534330/corinthians-faz-treino-aberto-e-encerra-preparacao-para-amistoso-contra-sub-23-dos-estados-unidos",
       "time": "04/06 · 13:32",
-      "pubDate": "Thu, 04 Jun 2026 10:32:46 -0300"
+      "pubDate": "Thu, 04 Jun 2026 10:32:00 -0300"
     },
     {
       "source": "Meu Timao",
@@ -19,28 +67,12 @@ const LIVE_DATA = {
       "pubDate": "Thu, 04 Jun 2026 10:31:00 -0300"
     },
     {
-      "source": "Meu Timao",
-      "color": "#1a7a3a",
-      "title": "Corinthians registra a terceira maior dívida líquida do futebol brasileiro em 2025; veja os números",
-      "url": "https://www.meutimao.com.br/noticias-do-corinthians/534283/corinthians-registra-a-terceira-maior-divida-liquida-do-futebol-brasileiro-em-2025-veja-os-numeros",
-      "time": "04/06 · 12:32",
-      "pubDate": "Thu, 04 Jun 2026 09:32:00 -0300"
-    },
-    {
-      "source": "Meu Timao",
-      "color": "#1a7a3a",
-      "title": "Corinthians projeta custo milionário de ampliação da Arena bancada por gestora do estacionamento",
-      "url": "https://www.meutimao.com.br/noticias-do-corinthians/534327/corinthians-projeta-custo-milionario-de-ampliacao-da-arena-bancada-por-gestora-do-estacionamento",
-      "time": "04/06 · 11:31",
-      "pubDate": "Thu, 04 Jun 2026 08:31:00 -0300"
-    },
-    {
-      "source": "Meu Timao",
-      "color": "#1a7a3a",
-      "title": "Últimas do Corinthians: possível novo patrocínio, caso Flora e acordo por estacionamento",
-      "url": "https://www.meutimao.com.br/noticias-do-corinthians/534321/ultimas-do-corinthians-possivel-novo-patrocinio-caso-flora-e-acordo-por-estacionamento",
-      "time": "04/06 · 10:30",
-      "pubDate": "Thu, 04 Jun 2026 07:30:00 -0300"
+      "source": "Bolavip",
+      "color": "#0055A5",
+      "title": "Corinthians faz sondagem pela contratação de Bitello e multa no Dínamo Moscou é de R$ 106 milhões",
+      "url": "<![CDATA[https://br.bolavip.com/corinthians/corinthians-faz-sondagem-pela-contratacao-de-bitello-e-multa-no-dinamo-moscou-e-de-r-106-milhoes]]>",
+      "time": "04/06 · 12:25",
+      "pubDate": "Thu, 04 Jun 2026 09:25:13 -0300"
     },
     {
       "source": "Bolavip",
@@ -57,30 +89,6 @@ const LIVE_DATA = {
       "url": "<![CDATA[https://br.bolavip.com/corinthians/osmar-stabile-define-preco-de-yuri-alberto-e-crava-permanencia-no-corinthians]]>",
       "time": "04/06 · 02:36",
       "pubDate": "Wed, 03 Jun 2026 23:36:59 -0300"
-    },
-    {
-      "source": "Meu Timao",
-      "color": "#1a7a3a",
-      "title": "Dupla do Corinthians participa de derrota em amistoso da Seleção Feminina Sub-20",
-      "url": "https://www.meutimao.com.br/noticias-do-corinthians/534320/dupla-do-corinthians-participa-de-derrota-em-amistoso-da-selecao-feminina-sub-20",
-      "time": "04/06 · 01:22",
-      "pubDate": "Wed, 03 Jun 2026 22:22:00 -0300"
-    },
-    {
-      "source": "Meu Timao",
-      "color": "#1a7a3a",
-      "title": "Presidente do Corinthians explica contratação de empresa de segurança irregular para uso particular",
-      "url": "https://www.meutimao.com.br/noticias-do-corinthians/534308/presidente-do-corinthians-explica-contratacao-de-empresa-de-seguranca-irregular-para-uso-particular",
-      "time": "04/06 · 00:56",
-      "pubDate": "Wed, 03 Jun 2026 21:56:00 -0300"
-    },
-    {
-      "source": "Meu Timao",
-      "color": "#1a7a3a",
-      "title": "MP denuncia vice-presidente do Corinthians por quatro crimes em caso de materiais da Nike",
-      "url": "https://www.meutimao.com.br/noticias-do-corinthians/534317/mp-denuncia-vice-presidente-do-corinthians-por-quatro-crimes-em-caso-de-materiais-da-nike",
-      "time": "04/06 · 00:26",
-      "pubDate": "Wed, 03 Jun 2026 21:26:00 -0300"
     },
     {
       "source": "Bolavip",
@@ -121,14 +129,6 @@ const LIVE_DATA = {
       "url": "<![CDATA[https://br.bolavip.com/corinthians/corinthians-avanca-nas-conversas-com-a-empresa-fatal-fans-e-detalhes-sao-costurados]]>",
       "time": "03/06 · 18:13",
       "pubDate": "Wed, 03 Jun 2026 15:13:25 -0300"
-    },
-    {
-      "source": "Bolavip",
-      "color": "#0055A5",
-      "title": "Corinthians deixa transfer ban de lado e prioriza dívida de R$ 42 milhões com Talleres por Garro",
-      "url": "<![CDATA[https://br.bolavip.com/corinthians/corinthians-deixa-transfer-ban-de-lado-e-prioriza-divida-de-r-42-milhoes-com-talleres-por-garro]]>",
-      "time": "03/06 · 14:12",
-      "pubDate": "Wed, 03 Jun 2026 11:12:17 -0300"
     },
     {
       "source": "Gazeta Esportiva",
