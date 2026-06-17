78facade63092cca1c58844cf906a7d7ebedca6e
GitHub Action <action@github.com>
2026-06-17 12:08:17 +0000

    Dados atualizados

--- src/data.js
+++ src/data.js
@@ -1,7 +1,79 @@
-// Gerado automaticamente em 16/06/2026, 23:16:29
+// Gerado automaticamente em 17/06/2026, 12:08:17
 const LIVE_DATA = {
-  "updatedAt": "2026-06-16T23:16:29.824Z",
+  "updatedAt": "2026-06-17T12:08:17.060Z",
   "news": [
+    {
+      "source": "Meu Timao",
+      "color": "#1a7a3a",
+      "title": "Corinthians recebe o Vitória visando subir na tabela do Brasileirão Sub-20; saiba tudo",
+      "url": "https://www.meutimao.com.br/noticias-do-corinthians/534712/corinthians-recebe-o-vitoria-visando-subir-na-tabela-do-brasileirao-sub-20-saiba-tudo",
+      "time": "17/06 · 11:30",
+      "pubDate": "Wed, 17 Jun 2026 08:30:00 -0300"
+    },
+    {
+      "source": "Meu Timao",
+      "color": "#1a7a3a",
+      "title": "Últimas do Corinthians: falta de pagamento, reunião suspensa e zagueiro comentarista",
+      "url": "https://www.meutimao.com.br/noticias-do-corinthians/534729/ultimas-do-corinthians-falta-de-pagamento-reuniao-suspensa-e-zagueiro-comentarista",
+      "time": "17/06 · 10:30",
+      "pubDate": "Wed, 17 Jun 2026 07:30:00 -0300"
+    },
+    {
+      "source": "Gazeta Esportiva",
+      "color": "#E5820A",
+      "title": "Corinthians tem 12 atletas que não podem mais trocar de clube no Brasileirão; veja lista",
+      "url": "https://www.gazetaesportiva.com/times/corinthians/corinthians-tem-12-atletas-que-nao-podem-mais-trocar-de-clube-no-brasileirao-veja-lista/",
+      "time": "17/06 · 10:00",
+      "pubDate": "Wed, 17 Jun 2026 10:00:25 +0000"
+    },
+    {
+      "source": "Meu Timao",
+      "color": "#1a7a3a",
+      "title": "Gui Negão relembra primeira convocação para jogo do time profissional do Corinthians",
+      "url": "https://www.meutimao.com.br/noticias-do-corinthians/534706/gui-negao-relembra-primeira-convocacao-para-jogo-do-time-profissional-do-corinthians",
+      "time": "17/06 · 01:30",
+      "pubDate": "Tue, 16 Jun 2026 22:30:00 -0300"
+    },
+    {
+      "source": "Meu Timao",
+      "color": "#1a7a3a",
+      "title": "Corinthians derrota o Diadema e assume liderança do Paulista de Futsal",
+      "url": "https://www.meutimao.com.br/noticias-do-corinthians/534726/corinthians-derrota-o-diadema-e-assume-lideranca-do-paulista-de-futsal",
+      "time": "17/06 · 00:29",
+      "pubDate": "Tue, 16 Jun 2026 21:29:00 -0300"
+    },
+    {
+      "source": "Meu Timao",
+      "color": "#1a7a3a",
+      "title": "Menor país a participar de uma Copa do Mundo também foi goleado pelo Corinthians em 1981; relembre",
+      "url": "https://www.meutimao.com.br/noticias-do-corinthians/534711/menor-pais-a-participar-de-uma-copa-do-mundo-tambem-foi-goleado-pelo-corinthians-em-1981-relembre",
+      "time": "17/06 · 00:29",
+      "pubDate": "Tue, 16 Jun 2026 21:29:00 -0300"
+    },
+    {
+      "source": "Meu Timao",
+      "color": "#1a7a3a",
+      "title": "Justiça de São Paulo suspende Assembleia da reforma estatutária do Corinthians",
+      "url": "https://www.meutimao.com.br/noticias-do-corinthians/534723/justica-de-sao-paulo-suspende-assembleia-da-reforma-estatutaria-do-corinthians",
+      "time": "16/06 · 23:33",
+      "pubDate": "Tue, 16 Jun 2026 20:33:00 -0300"
+    },
+    {
+      "source": "Meu Timao",
+      "color": "#1a7a3a",
+      "title": "Corinthians perde prazo e aguarda conclusão burocrática para encerrar impasse com Talleres",
+      "url": "https://www.meutimao.com.br/noticias-do-corinthians/534722/corinthians-perde-prazo-e-aguarda-conclusao-burocratica-para-encerrar-impasse-com-talleres",
+      "time": "16/06 · 23:33",
+      "pubDate": "Tue, 16 Jun 2026 20:33:00 -0300"
+    },
+    {
+      "source": "Meu Timao",
+      "color": "#1a7a3a",
+      "title": "Guia do Meu Timão: saiba tudo sobre a busca do Corinthians pelo título do Paulista Feminino Sub-20",
+      "url": "https://www.meutimao.com.br/noticias-do-corinthians/534717/guia-do-meu-timao-saiba-tudo-sobre-a-busca-do-corinthians-pelo-titulo-do-paulista-feminino-sub-20",
+      "time": "16/06 · 23:33",
+      "pubDate": "Tue, 16 Jun 2026 20:33:00 -0300"
+    },
     {
       "source": "Gazeta Esportiva",
       "color": "#E5820A",
@@ -11,76 +83,12 @@ const LIVE_DATA = {
       "pubDate": "Tue, 16 Jun 2026 23:00:50 +0000"
     },
     {
-      "source": "Meu Timao",
-      "color": "#1a7a3a",
-      "title": "Torcedor de Cabo Verde se declara ao Corinthians antes da estreia do país na Copa do Mundo",
-      "url": "https://www.meutimao.com.br/noticias-do-corinthians/534709/torcedor-de-cabo-verde-se-declara-ao-corinthians-antes-da-estreia-do-pais-na-copa-do-mundo",
-      "time": "16/06 · 22:31",
-      "pubDate": "Tue, 16 Jun 2026 19:31:00 -0300"
-    },
-    {
-      "source": "Meu Timao",
-      "color": "#1a7a3a",
-      "title": "Corinthians libera zagueiro do Sub-20 e aumenta dispensas na base",
-      "url": "https://www.meutimao.com.br/noticias-do-corinthians/534719/corinthians-libera-zagueiro-do-sub-20-e-aumenta-dispensas-na-base",
-      "time": "16/06 · 21:47",
-      "pubDate": "Tue, 16 Jun 2026 18:47:00 -0300"
-    },
-    {
-      "source": "Meu Timao",
-      "color": "#1a7a3a",
-      "title": "Atacante do Corinthians destaca preparação e projeta sequência positiva no Brasileiro Sub-20",
-      "url": "https://www.meutimao.com.br/noticias-do-corinthians/534713/atacante-do-corinthians-destaca-preparacao-e-projeta-sequencia-positiva-no-brasileiro-sub-20",
-      "time": "16/06 · 21:33",
-      "pubDate": "Tue, 16 Jun 2026 18:33:00 -0300"
-    },
-    {
-      "source": "Meu Timao",
-      "color": "#1a7a3a",
-      "title": "Fiel coloca zagueiro como melhor contratação do Corinthians no primeiro semestre; veja a enquete",
-      "url": "https://www.meutimao.com.br/noticias-do-corinthians/534697/fiel-coloca-zagueiro-como-melhor-contratacao-do-corinthians-no-primeiro-semestre-veja-a-enquete",
-      "time": "16/06 · 20:33",
-      "pubDate": "Tue, 16 Jun 2026 17:33:00 -0300"
-    },
-    {
       "source": "Gazeta Esportiva",
       "color": "#E5820A",
       "title": "De virada, Corinthians perde do Cruzeiro pelo Brasileiro sub-17",
       "url": "https://www.gazetaesportiva.com/times/corinthians/de-virada-corinthians-perde-do-cruzeiro-pelo-brasileiro-sub-17/",
       "time": "16/06 · 20:32",
       "pubDate": "Tue, 16 Jun 2026 20:32:15 +0000"
-    },
-    {
-      "source": "Meu Timao",
-      "color": "#1a7a3a",
-      "title": "Corinthians sofre com nova expulsão e leva virada do Cruzeiro no Brasileiro Sub-17",
-      "url": "https://www.meutimao.com.br/noticias-do-corinthians/534707/corinthians-sofre-com-nova-expulsao-e-leva-virada-do-cruzeiro-no-brasileiro-sub-17",
-      "time": "16/06 · 20:18",
-      "pubDate": "Tue, 16 Jun 2026 17:18:00 -0300"
-    },
-    {
-      "source": "Meu Timao",
-      "color": "#1a7a3a",
-      "title": "Pantaleão pede que Stabile explique fala sobre conselheiros e contratos no Corinthians",
-      "url": "https://www.meutimao.com.br/noticias-do-corinthians/534708/pantaleao-pede-que-stabile-explique-fala-sobre-conselheiros-e-contratos-no-corinthians",
-      "time": "16/06 · 19:35",
-      "pubDate": "Tue, 16 Jun 2026 16:35:00 -0300"
-    },
-    {
-      "source": "Meu Timao",
-      "color": "#1a7a3a",
-      "title": "Corinthians celebra aniversário do Maracanã e relembra conquistas históricas no estádio",
-      "url": "https://www.meutimao.com.br/noticias-do-corinthians/534705/corinthians-celebra-aniversario-do-maracana-e-relembra-conquistas-historicas-no-estadio",
-      "time": "16/06 · 18:32",
-      "pubDate": "Tue, 16 Jun 2026 15:32:00 -0300"
-    },
-    {
-      "source": "Meu Timao",
-      "color": "#1a7a3a",
-      "title": "Corinthians Feminino foi visitante em 87,5% dos sorteios da CBF desde 2024",
-      "url": "https://www.meutimao.com.br/noticias-do-corinthians/534699/corinthians-feminino-foi-visitante-em-875-por-cento-dos-sorteios-da-cbf-desde-2024",
-      "time": "16/06 · 17:32",
-      "pubDate": "Tue, 16 Jun 2026 14:32:00 -0300"
     },
     {
       "source": "Bolavip",
@@ -153,20 +161,18 @@ const LIVE_DATA = {
       "url": "<![CDATA[https://br.bolavip.com/corinthians/osmar-stabile-presidente-do-corinthians-se-pronuncia-sobre-viagem-aos-eua-durante-a-copa-vim-a-new-york-a-trabalho]]>",
       "time": "14/06 · 20:53",
       "pubDate": "Sun, 14 Jun 2026 17:53:45 -0300"
-    },
-    {
-      "source": "Gazeta Esportiva",
-      "color": "#E5820A",
-      "title": "Dupla do Corinthians se encontra em viagem de férias para a Grécia",
-      "url": "https://www.gazetaesportiva.com/times/corinthians/dupla-do-corinthians-se-encontra-em-viagem-de-ferias-para-a-grecia/",
-      "time": "14/06 · 20:13",
-      "pubDate": "Sun, 14 Jun 2026 20:13:09 +0000"
     }
   ],
   "upcoming": [],
   "results": [],
   "standings": [],
   "highlights": [
+    {
+      "title": "Menor país a participar de uma Copa do Mundo também foi goleado pelo Corinthians em 1981; relembre",
+      "url": "https://www.meutimao.com.br/noticias-do-corinthians/534711/menor-pais-a-participar-de-uma-copa-do-mundo-tambem-foi-goleado-pelo-corinthians-em-1981-relembre",
+      "pubDate": "Tue, 16 Jun 2026 21:29:00 -0300",
+      "date": "17/06 · 00:29"
+    },
     {
       "title": "Pivô do Corinthians consolida vice-liderança em ranking de artilharia com gol em derrota na Copa LNF",
       "url": "https://www.meutimao.com.br/noticias-do-corinthians/534627/pivo-do-corinthians-consolida-vice-lideranca-em-ranking-de-artilharia-com-gol-em-derrota-na-copa-lnf",
@@ -178,18 +184,6 @@ const LIVE_DATA = {
       "url": "https://www.meutimao.com.br/noticias-do-corinthians/534628/zagueiro-do-corinthians-marca-seu-primeiro-gol-na-vitoria-contra-a-portuguesa-santista",
       "pubDate": "Mon, 15 Jun 2026 19:02:00 -0300",
       "date": "15/06 · 22:02"
-    },
-    {
-      "title": "Centroavante do Sub-17 do Corinthians projeta partida contra o Cruzeiro e valoriza bom momento",
-      "url": "https://www.meutimao.com.br/noticias-do-corinthians/534679/centroavante-do-sub-17-do-corinthians-projeta-partida-contra-o-cruzeiro-e-valoriza-bom-momento",
-      "pubDate": "Mon, 15 Jun 2026 17:28:00 -0300",
-      "date": "15/06 · 20:28"
-    },
-    {
-      "title": "Goleiro destaque contra Espanha assumiu vaga em time português por ida de Hugo Souza ao Corinthians",
-      "url": "https://www.meutimao.com.br/noticias-do-corinthians/534675/goleiro-destaque-contra-espanha-assumiu-vaga-em-time-portugues-por-ida-de-hugo-souza-ao-corinthians",
-      "pubDate": "Mon, 15 Jun 2026 15:53:00 -0300",
-      "date": "15/06 · 18:53"
     }
   ]
 };
