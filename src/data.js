2947916f1473caf873c31c1242173ea4f9b15303
GitHub Action <action@github.com>
2026-07-04 22:43:29 +0000

    Dados atualizados

--- src/data.js
+++ src/data.js
@@ -1,7 +1,39 @@
-// Gerado automaticamente em 04/07/2026, 18:49:27
+// Gerado automaticamente em 04/07/2026, 22:43:29
 const LIVE_DATA = {
-  "updatedAt": "2026-07-04T18:49:27.755Z",
+  "updatedAt": "2026-07-04T22:43:29.739Z",
   "news": [
+    {
+      "source": "Meu Timao",
+      "color": "#1a7a3a",
+      "title": "Corinthians é o clube com o menor patrimônio líquido da Série A do Brasileirão",
+      "url": "https://www.meutimao.com.br/noticias-do-corinthians/535362/corinthians-e-o-clube-com-o-menor-patrimonio-liquido-da-serie-a-do-brasileirao",
+      "time": "04/07 · 22:02",
+      "pubDate": "Sat, 04 Jul 2026 19:02:00 -0300"
+    },
+    {
+      "source": "Meu Timao",
+      "color": "#1a7a3a",
+      "title": "Artilheiro do Sub-15 projeta título do Campeonato Paulista pelo Corinthians",
+      "url": "https://www.meutimao.com.br/noticias-do-corinthians/535357/artilheiro-do-sub-15-projeta-titulo-do-campeonato-paulista-pelo-corinthians",
+      "time": "04/07 · 21:32",
+      "pubDate": "Sat, 04 Jul 2026 18:32:00 -0300"
+    },
+    {
+      "source": "Meu Timao",
+      "color": "#1a7a3a",
+      "title": "Corinthians vence Gazin Porto Velho e garante 100% de aproveitamento no Brasileirão Sub-17",
+      "url": "https://www.meutimao.com.br/noticias-do-corinthians/535374/corinthians-vence-gazin-porto-velho-e-garante-100-por-cento-de-aproveitamento-no-brasileirao-sub-17",
+      "time": "04/07 · 19:57",
+      "pubDate": "Sat, 04 Jul 2026 16:57:00 -0300"
+    },
+    {
+      "source": "Meu Timao",
+      "color": "#1a7a3a",
+      "title": "Pivô supera marca de gols da temporada passada e assume protagonismo no Corinthians",
+      "url": "https://www.meutimao.com.br/noticias-do-corinthians/535311/pivo-supera-marca-de-gols-da-temporada-passada-e-assume-protagonismo-no-corinthians",
+      "time": "04/07 · 19:32",
+      "pubDate": "Sat, 04 Jul 2026 16:32:00 -0300"
+    },
     {
       "source": "Meu Timao",
       "color": "#1a7a3a",
@@ -9,6 +41,22 @@ const LIVE_DATA = {
       "url": "https://www.meutimao.com.br/noticias-do-corinthians/535331/corinthians-busca-primeira-vitoria-fora-de-casa-na-lnf-apos-quase-oito-meses",
       "time": "04/07 · 18:31",
       "pubDate": "Sat, 04 Jul 2026 15:31:00 -0300"
+    },
+    {
+      "source": "Gazeta Esportiva",
+      "color": "#E5820A",
+      "title": "Corinthians conclui décimo dia de intertemporada com ajustes táticos de Diniz",
+      "url": "https://www.gazetaesportiva.com/times/corinthians/corinthians-treino-fernando-diniz-intertemporada/",
+      "time": "04/07 · 18:13",
+      "pubDate": "Sat, 04 Jul 2026 18:13:57 +0000"
+    },
+    {
+      "source": "Gazeta Esportiva",
+      "color": "#E5820A",
+      "title": "Hugo Souza analisa preparação do Corinthians na intertemporada",
+      "url": "https://www.gazetaesportiva.com/times/corinthians/hugo-souza-analisa-preparacao-do-corinthians-na-intertemporada/",
+      "time": "04/07 · 17:38",
+      "pubDate": "Sat, 04 Jul 2026 17:38:04 +0000"
     },
     {
       "source": "Meu Timao",
@@ -19,12 +67,28 @@ const LIVE_DATA = {
       "pubDate": "Sat, 04 Jul 2026 14:32:00 -0300"
     },
     {
+      "source": "Gazeta Esportiva",
+      "color": "#E5820A",
+      "title": "Romero completa 34 anos; relembre gols do paraguaio pelo Corinthians na Neo Química Arena",
+      "url": "https://www.gazetaesportiva.com/times/corinthians/romero-completa-34-anos-relembre-gols-do-paraguaio-pelo-corinthians-na-neo-quimica-arena/",
+      "time": "04/07 · 17:23",
+      "pubDate": "Sat, 04 Jul 2026 17:23:31 +0000"
+    },
+    {
       "source": "Meu Timao",
       "color": "#1a7a3a",
       "title": "Corinthians completa décimo dia seguido de treinamento com atividades no CT",
       "url": "https://www.meutimao.com.br/noticias-do-corinthians/535367/corinthians-completa-decimo-dia-seguido-de-treinamento-com-atividades-no-ct",
       "time": "04/07 · 17:18",
       "pubDate": "Sat, 04 Jul 2026 14:18:00 -0300"
+    },
+    {
+      "source": "Gazeta Esportiva",
+      "color": "#E5820A",
+      "title": "Há 14 anos, Corinthians conquistava a Libertadores; relembre gols da campanha invicta",
+      "url": "https://www.gazetaesportiva.com/times/corinthians/ha-14-anos-corinthians-conquistava-a-libertadores-relembre-gols-da-campanha-invicta/",
+      "time": "04/07 · 17:09",
+      "pubDate": "Sat, 04 Jul 2026 17:09:49 +0000"
     },
     {
       "source": "Meu Timao",
@@ -35,36 +99,28 @@ const LIVE_DATA = {
       "pubDate": "Sat, 04 Jul 2026 13:15:00 -0300"
     },
     {
-      "source": "Meu Timao",
-      "color": "#1a7a3a",
-      "title": "Corinthians vence duelo direto contra o Juventus e sobe na tabela do Paulistão Sub-17",
-      "url": "https://www.meutimao.com.br/noticias-do-corinthians/535361/corinthians-vence-duelo-direto-contra-o-juventus-e-sobe-na-tabela-do-paulistao-sub-17",
-      "time": "04/07 · 15:41",
-      "pubDate": "Sat, 04 Jul 2026 12:41:00 -0300"
+      "source": "Gazeta Esportiva",
+      "color": "#E5820A",
+      "title": "Hugo Souza celebra dois anos de Corinthians e valoriza intertemporada",
+      "url": "https://www.gazetaesportiva.com/times/corinthians/hugo-souza-celebra-dois-anos-de-corinthians-e-valoriza-intertemporada/",
+      "time": "04/07 · 10:00",
+      "pubDate": "Sat, 04 Jul 2026 10:00:11 +0000"
     },
     {
-      "source": "Meu Timao",
-      "color": "#1a7a3a",
-      "title": "Atacante revelada pelo Corinthians é anunciada por equipe universitária dos Estados Unidos",
-      "url": "https://www.meutimao.com.br/noticias-do-corinthians/535360/atacante-revelada-pelo-corinthians-e-anunciada-por-equipe-universitaria-dos-estados-unidos",
-      "time": "04/07 · 15:15",
-      "pubDate": "Sat, 04 Jul 2026 12:15:00 -0300"
+      "source": "Gazeta Esportiva",
+      "color": "#E5820A",
+      "title": "Com Corinthians, Palmeiras e Santos, CBF detalha tabela das quartas do Brasileirão sub-20",
+      "url": "https://www.gazetaesportiva.com/times/palmeiras/brasileirao-sub-20-quartas-de-final/",
+      "time": "03/07 · 22:48",
+      "pubDate": "Fri, 03 Jul 2026 22:48:55 +0000"
     },
     {
-      "source": "Meu Timao",
-      "color": "#1a7a3a",
-      "title": "Organizadas do Corinthians e do Boca Juniors se reúnem para discutir ações no combate ao racismo",
-      "url": "https://www.meutimao.com.br/noticias-do-corinthians/535347/organizadas-do-corinthians-e-do-boca-juniors-se-reunem-para-discutir-acoes-no-combate-ao-racismo",
-      "time": "04/07 · 14:31",
-      "pubDate": "Sat, 04 Jul 2026 11:31:00 -0300"
-    },
-    {
-      "source": "Meu Timao",
-      "color": "#1a7a3a",
-      "title": "Você sabia? Haaland jogou ao lado de André Ramalho e já vestiu a camisa do Corinthians",
-      "url": "https://www.meutimao.com.br/noticias-do-corinthians/535328/voce-sabia-haaland-jogou-ao-lado-de-andre-ramalho-e-ja-vestiu-a-camisa-do-corinthians",
-      "time": "04/07 · 13:36",
-      "pubDate": "Sat, 04 Jul 2026 10:36:00 -0300"
+      "source": "Gazeta Esportiva",
+      "color": "#E5820A",
+      "title": "Corinthians intensifica movimentações ofensivas durante intertemporada",
+      "url": "https://www.gazetaesportiva.com/times/corinthians/corinthians-intensifica-movimentacoes-ofensivas-durante-intertemporada/",
+      "time": "03/07 · 21:23",
+      "pubDate": "Fri, 03 Jul 2026 21:23:24 +0000"
     },
     {
       "source": "Bolavip",
@@ -81,6 +137,14 @@ const LIVE_DATA = {
       "url": "<![CDATA[https://br.bolavip.com/corinthians/memphis-depay-faz-exigencias-e-trava-renovacao-no-corinthians-holandes-quer-r-50-milhoes-por-ano]]>",
       "time": "03/07 · 17:58",
       "pubDate": "Fri, 03 Jul 2026 14:58:08 -0300"
+    },
+    {
+      "source": "Gazeta Esportiva",
+      "color": "#E5820A",
+      "title": "Veja fotos do treino do Corinthians desta sexta-feira",
+      "url": "https://www.gazetaesportiva.com/times/corinthians/veja-fotos-do-treino-do-corinthians-desta-sexta-feira-33/",
+      "time": "03/07 · 17:26",
+      "pubDate": "Fri, 03 Jul 2026 17:26:13 +0000"
     },
     {
       "source": "Bolavip",
@@ -129,76 +193,18 @@ const LIVE_DATA = {
       "url": "<![CDATA[https://br.bolavip.com/corinthians/memphis-depay-recebe-10-dias-de-ferias-do-corinthians-antes-de-reapresentacao-e-provavel-renovacao]]>",
       "time": "30/06 · 17:26",
       "pubDate": "Tue, 30 Jun 2026 14:26:21 -0300"
-    },
-    {
-      "source": "Gazeta Esportiva",
-      "color": "#E5820A",
-      "title": "Allan aponta foco do Corinthians para buscar melhor campanha da Libertadores",
-      "url": "https://www.gazetaesportiva.com/times/corinthians/allan-aponta-foco-do-corinthians-para-buscar-melhor-campanha-da-libertadores/",
-      "time": "27/05 · 00:00",
-      "pubDate": "Wed, 27 May 2026 00:00:28 +0000"
-    },
-    {
-      "source": "Gazeta Esportiva",
-      "color": "#E5820A",
-      "title": "Corinthians x Platense pela Libertadores: onde assistir ao vivo, estatísticas e prováveis escalações",
-      "url": "https://www.gazetaesportiva.com/campeonatos/libertadores-da-america/corinthians-x-platense-libertadores-27-05-26/",
-      "time": "26/05 · 23:00",
-      "pubDate": "Tue, 26 May 2026 23:00:00 +0000"
-    },
-    {
-      "source": "Gazeta Esportiva",
-      "color": "#E5820A",
-      "title": "Corinthians faz treino tático e fecha preparação para pegar o Platense; veja provável escalação",
-      "url": "https://www.gazetaesportiva.com/times/corinthians/corinthians-faz-treino-tatico-e-fecha-preparacao-para-pegar-o-platense-veja-provavel-escalacao/",
-      "time": "26/05 · 22:26",
-      "pubDate": "Tue, 26 May 2026 22:26:53 +0000"
-    },
-    {
-      "source": "Gazeta Esportiva",
-      "color": "#E5820A",
-      "title": "Veja fotos do último treino do Corinthians antes de enfrentar o Platense",
-      "url": "https://www.gazetaesportiva.com/times/corinthians/veja-fotos-do-ultimo-treino-do-corinthians-antes-de-enfrentar-o-platense/",
-      "time": "26/05 · 22:26",
-      "pubDate": "Tue, 26 May 2026 22:26:51 +0000"
-    },
-    {
-      "source": "Gazeta Esportiva",
-      "color": "#E5820A",
-      "title": "Diretor do Corinthians atualiza situação de Memphis e abre o jogo sobre Yuri Alberto",
-      "url": "https://www.gazetaesportiva.com/times/corinthians/diretor-do-corinthians-atualiza-situacao-de-memphis-e-abre-o-jogo-sobre-yuri-alberto/",
-      "time": "26/05 · 17:45",
-      "pubDate": "Tue, 26 May 2026 17:45:53 +0000"
-    },
-    {
-      "source": "Gazeta Esportiva",
-      "color": "#E5820A",
-      "title": "Memphis pode fazer último jogo na Arena, e Corinthians corre contra o tempo por renovação",
-      "url": "https://www.gazetaesportiva.com/times/corinthians/memphis-pode-fazer-ultimo-jogo-na-arena-e-corinthians-corre-contra-o-tempo-por-renovacao/",
-      "time": "26/05 · 10:00",
-      "pubDate": "Tue, 26 May 2026 10:00:49 +0000"
-    },
-    {
-      "source": "Gazeta Esportiva",
-      "color": "#E5820A",
-      "title": "Corinthians x Platense: veja onde assistir ao duelo pela última rodada da Libertadores",
-      "url": "https://www.gazetaesportiva.com/times/corinthians/corinthians-x-platense-veja-onde-assistir-ao-duelo-pela-ultima-rodada-da-libertadores/",
-      "time": "26/05 · 09:00",
-      "pubDate": "Tue, 26 May 2026 09:00:32 +0000"
-    },
-    {
-      "source": "Gazeta Esportiva",
-      "color": "#E5820A",
-      "title": "Corinthians goleia o Mixto e assume a ponta do Brasileiro feminino",
-      "url": "https://www.gazetaesportiva.com/times/corinthians/corinthians-goleia-o-mixto-e-assume-a-ponta-do-brasileiro-feminino/",
-      "time": "26/05 · 02:04",
-      "pubDate": "Tue, 26 May 2026 02:04:37 +0000"
     }
   ],
   "upcoming": [],
   "results": [],
   "standings": [],
   "highlights": [
+    {
+      "title": "Pivô supera marca de gols da temporada passada e assume protagonismo no Corinthians",
+      "url": "https://www.meutimao.com.br/noticias-do-corinthians/535311/pivo-supera-marca-de-gols-da-temporada-passada-e-assume-protagonismo-no-corinthians",
+      "pubDate": "Sat, 04 Jul 2026 16:32:00 -0300",
+      "date": "04/07 · 19:32"
+    },
     {
       "title": "VÍDEO: 10 maiores vendas da história do Corinthians",
       "url": "https://www.meutimao.com.br/videos-do-corinthians/128448/10-maiores-vendas-da-historia-do-corinthians",
