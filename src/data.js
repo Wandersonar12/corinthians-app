efdd39947450c970fb48a060097fba08ad89cdbe
GitHub Action <action@github.com>
2026-07-06 22:57:13 +0000

    Dados atualizados

--- src/data.js
+++ src/data.js
@@ -1,7 +1,55 @@
-// Gerado automaticamente em 06/07/2026, 19:27:07
+// Gerado automaticamente em 06/07/2026, 22:57:13
 const LIVE_DATA = {
-  "updatedAt": "2026-07-06T19:27:07.779Z",
+  "updatedAt": "2026-07-06T22:57:13.089Z",
   "news": [
+    {
+      "source": "Meu Timao",
+      "color": "#1a7a3a",
+      "title": "O Corinthians deve renovar com Jesse Lingard para 2027? Vote na enquete do Meu Timão!",
+      "url": "https://www.meutimao.com.br/noticias-do-corinthians/535408/o-corinthians-deve-renovar-com-jesse-lingard-para-2027-vote-na-enquete-do-meu-timao",
+      "time": "06/07 · 22:34",
+      "pubDate": "Mon, 06 Jul 2026 19:34:00 -0300"
+    },
+    {
+      "source": "Meu Timao",
+      "color": "#1a7a3a",
+      "title": "Corinthians conhece datas e horários de mais três jogos no Brasileiro Sub-17; confira",
+      "url": "https://www.meutimao.com.br/noticias-do-corinthians/535417/corinthians-conhece-datas-e-horarios-de-mais-tres-jogos-no-brasileiro-sub-17-confira",
+      "time": "06/07 · 21:36",
+      "pubDate": "Mon, 06 Jul 2026 18:36:00 -0300"
+    },
+    {
+      "source": "Meu Timao",
+      "color": "#1a7a3a",
+      "title": "Enquete aponta rejeição da torcida do Corinthians à renovação de atacante para 2027",
+      "url": "https://www.meutimao.com.br/noticias-do-corinthians/535407/enquete-aponta-rejeicao-da-torcida-do-corinthians-a-renovacao-de-atacante-para-2027",
+      "time": "06/07 · 20:35",
+      "pubDate": "Mon, 06 Jul 2026 17:35:00 -0300"
+    },
+    {
+      "source": "Gazeta Esportiva",
+      "color": "#E5820A",
+      "title": "Veja fotos do treino do Corinthians desta segunda-feira",
+      "url": "https://www.gazetaesportiva.com/times/corinthians/veja-fotos-do-treino-do-corinthians-desta-segunda-feira-21/",
+      "time": "06/07 · 19:30",
+      "pubDate": "Mon, 06 Jul 2026 19:30:01 +0000"
+    },
+    {
+      "source": "Meu Timao",
+      "color": "#1a7a3a",
+      "title": "Corinthians faz treino em dois períodos em retorno da equipe após folga",
+      "url": "https://www.meutimao.com.br/noticias-do-corinthians/535418/corinthians-faz-treino-em-dois-periodos-em-retorno-da-equipe-apos-folga",
+      "time": "06/07 · 19:29",
+      "pubDate": "Mon, 06 Jul 2026 16:29:00 -0300"
+    },
+    {
+      "source": "Gazeta Esportiva",
+      "color": "#E5820A",
+      "title": "Corinthians se reapresenta com treino técnico-tático em 11º dia de intertemporada",
+      "url": "https://www.gazetaesportiva.com/times/corinthians/corinthians-se-reapresenta-com-treino-tecnico-tatico-em-11o-dia-de-intertemporada/",
+      "time": "06/07 · 19:27",
+      "pubDate": "Mon, 06 Jul 2026 19:27:27 +0000"
+    },
     {
       "source": "Meu Timao",
       "color": "#1a7a3a",
@@ -9,6 +57,14 @@ const LIVE_DATA = {
       "url": "https://www.meutimao.com.br/noticias-do-corinthians/535396/atacante-do-corinthians-estreia-pelo-clube-em-vitoria-no-paulista-sub-20",
       "time": "06/07 · 18:33",
       "pubDate": "Mon, 06 Jul 2026 15:33:00 -0300"
+    },
+    {
+      "source": "Gazeta Esportiva",
+      "color": "#E5820A",
+      "title": "Corinthians chega ao 10º dia de intertemporada com ajustes táticos de Fernando Diniz",
+      "url": "https://www.gazetaesportiva.com/todas-as-noticias/corinthians-chega-ao-10o-dia-de-intertemporada-com-ajustes-taticos-de-fernando-diniz/",
+      "time": "06/07 · 18:08",
+      "pubDate": "Mon, 06 Jul 2026 18:08:51 +0000"
     },
     {
       "source": "Meu Timao",
@@ -35,36 +91,12 @@ const LIVE_DATA = {
       "pubDate": "Mon, 06 Jul 2026 13:31:00 -0300"
     },
     {
-      "source": "Meu Timao",
-      "color": "#1a7a3a",
-      "title": "Destaque na base, meia-atacante deixa o Corinthians com apenas dez jogos pelo profissional",
-      "url": "https://www.meutimao.com.br/noticias-do-corinthians/535411/destaque-na-base-meia-atacante-deixa-o-corinthians-com-apenas-dez-jogos-pelo-profissional",
-      "time": "06/07 · 15:37",
-      "pubDate": "Mon, 06 Jul 2026 12:37:00 -0300"
-    },
-    {
-      "source": "Meu Timao",
-      "color": "#1a7a3a",
-      "title": "Corinthians vê jejum aumentar após nova derrota fora de casa na LNF",
-      "url": "https://www.meutimao.com.br/noticias-do-corinthians/535381/corinthians-ve-jejum-aumentar-apos-nova-derrota-fora-de-casa-na-lnf",
-      "time": "06/07 · 14:36",
-      "pubDate": "Mon, 06 Jul 2026 11:36:00 -0300"
-    },
-    {
-      "source": "Meu Timao",
-      "color": "#1a7a3a",
-      "title": "Corinthians terá semana com amistoso pelo profissional e Dérbi duplo nas categorias de base",
-      "url": "https://www.meutimao.com.br/noticias-do-corinthians/535410/corinthians-tera-semana-com-amistoso-pelo-profissional-e-derbi-duplo-nas-categorias-de-base",
-      "time": "06/07 · 13:31",
-      "pubDate": "Mon, 06 Jul 2026 10:31:00 -0300"
-    },
-    {
-      "source": "Meu Timao",
-      "color": "#1a7a3a",
-      "title": "Atacante marca pela primeira vez e entra na lista de artilheiros do Corinthians Sub-20; confira",
-      "url": "https://www.meutimao.com.br/noticias-do-corinthians/535397/atacante-marca-pela-primeira-vez-e-entra-na-lista-de-artilheiros-do-corinthians-sub-20-confira",
-      "time": "06/07 · 12:32",
-      "pubDate": "Mon, 06 Jul 2026 09:32:00 -0300"
+      "source": "Gazeta Esportiva",
+      "color": "#E5820A",
+      "title": "Corinthians abre semana de preparação para amistoso em meio à intertemporada",
+      "url": "https://www.gazetaesportiva.com/times/corinthians/corinthians-abre-semana-de-preparacao-para-amistoso-em-meio-a-intertemporada/",
+      "time": "06/07 · 10:00",
+      "pubDate": "Mon, 06 Jul 2026 10:00:59 +0000"
     },
     {
       "source": "Bolavip",
@@ -75,12 +107,44 @@ const LIVE_DATA = {
       "pubDate": "Sun, 05 Jul 2026 10:59:40 -0300"
     },
     {
+      "source": "Gazeta Esportiva",
+      "color": "#E5820A",
+      "title": "Corinthians folga em meio à intertemporada antes de se preparar para amistoso",
+      "url": "https://www.gazetaesportiva.com/times/corinthians/corinthians-folga-em-meio-a-intertemporada-antes-de-se-preparar-para-amistoso/",
+      "time": "05/07 · 09:00",
+      "pubDate": "Sun, 05 Jul 2026 09:00:45 +0000"
+    },
+    {
       "source": "Bolavip",
       "color": "#0055A5",
       "title": "Pedro Raul é oferecido ao León, do México e Corinthians planeja negociar o atacante nesta janela",
       "url": "<![CDATA[https://br.bolavip.com/corinthians/pedro-raul-e-oferecido-ao-leon-do-mexico-e-corinthians-planeja-negociar-o-atacante-nesta-janela]]>",
       "time": "05/07 · 01:44",
       "pubDate": "Sat, 04 Jul 2026 22:44:21 -0300"
+    },
+    {
+      "source": "Gazeta Esportiva",
+      "color": "#E5820A",
+      "title": "Corinthians conclui décimo dia de intertemporada com ajustes táticos de Diniz",
+      "url": "https://www.gazetaesportiva.com/times/corinthians/corinthians-treino-fernando-diniz-intertemporada/",
+      "time": "04/07 · 18:13",
+      "pubDate": "Sat, 04 Jul 2026 18:13:57 +0000"
+    },
+    {
+      "source": "Gazeta Esportiva",
+      "color": "#E5820A",
+      "title": "Hugo Souza analisa preparação do Corinthians na intertemporada",
+      "url": "https://www.gazetaesportiva.com/times/corinthians/hugo-souza-analisa-preparacao-do-corinthians-na-intertemporada/",
+      "time": "04/07 · 17:38",
+      "pubDate": "Sat, 04 Jul 2026 17:38:04 +0000"
+    },
+    {
+      "source": "Gazeta Esportiva",
+      "color": "#E5820A",
+      "title": "Romero completa 34 anos; relembre gols do paraguaio pelo Corinthians na Neo Química Arena",
+      "url": "https://www.gazetaesportiva.com/times/corinthians/romero-completa-34-anos-relembre-gols-do-paraguaio-pelo-corinthians-na-neo-quimica-arena/",
+      "time": "04/07 · 17:23",
+      "pubDate": "Sat, 04 Jul 2026 17:23:31 +0000"
     },
     {
       "source": "Bolavip",
@@ -129,70 +193,6 @@ const LIVE_DATA = {
       "url": "<![CDATA[https://br.bolavip.com/corinthians/corinthians-planeja-venda-de-breno-bidon-ou-andre-luiz-em-busca-de-reequilibrio-financeiro]]>",
       "time": "01/07 · 03:34",
       "pubDate": "Wed, 01 Jul 2026 00:34:54 -0300"
-    },
-    {
-      "source": "Gazeta Esportiva",
-      "color": "#E5820A",
-      "title": "Allan aponta foco do Corinthians para buscar melhor campanha da Libertadores",
-      "url": "https://www.gazetaesportiva.com/times/corinthians/allan-aponta-foco-do-corinthians-para-buscar-melhor-campanha-da-libertadores/",
-      "time": "27/05 · 00:00",
-      "pubDate": "Wed, 27 May 2026 00:00:28 +0000"
-    },
-    {
-      "source": "Gazeta Esportiva",
-      "color": "#E5820A",
-      "title": "Corinthians x Platense pela Libertadores: onde assistir ao vivo, estatísticas e prováveis escalações",
-      "url": "https://www.gazetaesportiva.com/campeonatos/libertadores-da-america/corinthians-x-platense-libertadores-27-05-26/",
-      "time": "26/05 · 23:00",
-      "pubDate": "Tue, 26 May 2026 23:00:00 +0000"
-    },
-    {
-      "source": "Gazeta Esportiva",
-      "color": "#E5820A",
-      "title": "Corinthians faz treino tático e fecha preparação para pegar o Platense; veja provável escalação",
-      "url": "https://www.gazetaesportiva.com/times/corinthians/corinthians-faz-treino-tatico-e-fecha-preparacao-para-pegar-o-platense-veja-provavel-escalacao/",
-      "time": "26/05 · 22:26",
-      "pubDate": "Tue, 26 May 2026 22:26:53 +0000"
-    },
-    {
-      "source": "Gazeta Esportiva",
-      "color": "#E5820A",
-      "title": "Veja fotos do último treino do Corinthians antes de enfrentar o Platense",
-      "url": "https://www.gazetaesportiva.com/times/corinthians/veja-fotos-do-ultimo-treino-do-corinthians-antes-de-enfrentar-o-platense/",
-      "time": "26/05 · 22:26",
-      "pubDate": "Tue, 26 May 2026 22:26:51 +0000"
-    },
-    {
-      "source": "Gazeta Esportiva",
-      "color": "#E5820A",
-      "title": "Diretor do Corinthians atualiza situação de Memphis e abre o jogo sobre Yuri Alberto",
-      "url": "https://www.gazetaesportiva.com/times/corinthians/diretor-do-corinthians-atualiza-situacao-de-memphis-e-abre-o-jogo-sobre-yuri-alberto/",
-      "time": "26/05 · 17:45",
-      "pubDate": "Tue, 26 May 2026 17:45:53 +0000"
-    },
-    {
-      "source": "Gazeta Esportiva",
-      "color": "#E5820A",
-      "title": "Memphis pode fazer último jogo na Arena, e Corinthians corre contra o tempo por renovação",
-      "url": "https://www.gazetaesportiva.com/times/corinthians/memphis-pode-fazer-ultimo-jogo-na-arena-e-corinthians-corre-contra-o-tempo-por-renovacao/",
-      "time": "26/05 · 10:00",
-      "pubDate": "Tue, 26 May 2026 10:00:49 +0000"
-    },
-    {
-      "source": "Gazeta Esportiva",
-      "color": "#E5820A",
-      "title": "Corinthians x Platense: veja onde assistir ao duelo pela última rodada da Libertadores",
-      "url": "https://www.gazetaesportiva.com/times/corinthians/corinthians-x-platense-veja-onde-assistir-ao-duelo-pela-ultima-rodada-da-libertadores/",
-      "time": "26/05 · 09:00",
-      "pubDate": "Tue, 26 May 2026 09:00:32 +0000"
-    },
-    {
-      "source": "Gazeta Esportiva",
-      "color": "#E5820A",
-      "title": "Corinthians goleia o Mixto e assume a ponta do Brasileiro feminino",
-      "url": "https://www.gazetaesportiva.com/times/corinthians/corinthians-goleia-o-mixto-e-assume-a-ponta-do-brasileiro-feminino/",
-      "time": "26/05 · 02:04",
-      "pubDate": "Tue, 26 May 2026 02:04:37 +0000"
     }
   ],
   "upcoming": [],
@@ -210,12 +210,6 @@ const LIVE_DATA = {
       "url": "https://www.meutimao.com.br/videos-do-corinthians/128458/corinthianos-em-campo-pelo-brasil-na-copa-timao-segue-treinando-na-intertemporada",
       "pubDate": "Sun, 05 Jul 2026 12:47:19 -0300",
       "date": "05/07 · 15:47"
-    },
-    {
-      "title": "Goleada do Corinthians sobre o Juventus movimenta artilharia do Sub-17; confira a lista",
-      "url": "https://www.meutimao.com.br/noticias-do-corinthians/535365/goleada-do-corinthians-sobre-o-juventus-movimenta-artilharia-do-sub-17-confira-a-lista",
-      "pubDate": "Sat, 04 Jul 2026 21:04:00 -0300",
-      "date": "05/07 · 00:04"
     }
   ]
 };
