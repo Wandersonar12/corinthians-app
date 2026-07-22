3f46aa3207d19837d27017c285ab10e110c47b42
GitHub Action <action@github.com>
2026-07-22 22:48:09 +0000

    Dados atualizados

--- src/data.js
+++ src/data.js
@@ -1,62 +1,102 @@
-// Gerado automaticamente em 22/07/2026, 18:52:29
+// Gerado automaticamente em 22/07/2026, 22:48:09
 const LIVE_DATA = {
-  "updatedAt": "2026-07-22T18:52:29.956Z",
+  "updatedAt": "2026-07-22T22:48:09.761Z",
   "news": [
     {
       "source": "Meu Timao",
       "color": "#1a7a3a",
-      "title": "Artilheiro do Corinthians avalia goleada no Brasileiro Sub-17 e cita referências no profissional",
-      "url": "https://www.meutimao.com.br/noticias-do-corinthians/536175/artilheiro-do-corinthians-avalia-goleada-no-brasileiro-sub-17-e-cita-referencias-no-profissional",
-      "time": "22/07 · 18:33",
-      "pubDate": "Wed, 22 Jul 2026 15:33:00 -0300"
+      "title": "Corinthians Sub-17 emplaca cinco vitórias seguidas depois de dois anos após bater o Internacional",
+      "url": "https://www.meutimao.com.br/noticias-do-corinthians/536172/corinthians-sub-17-emplaca-cinco-vitorias-seguidas-depois-de-dois-anos-apos-bater-o-internacional",
+      "time": "22/07 · 22:33",
+      "pubDate": "Wed, 22 Jul 2026 19:33:00 -0300"
+    },
+    {
+      "source": "Gazeta Esportiva",
+      "color": "#E5820A",
+      "title": "Organizada do Corinthians cobra explicações de Osmar Stabile sobre SAF e transfer bans",
+      "url": "https://www.gazetaesportiva.com/times/corinthians/organizada-do-corinthians-cobra-explicacoes-de-osmar-stabile-sobre-saf-e-transfer-bans/",
+      "time": "22/07 · 22:32",
+      "pubDate": "Wed, 22 Jul 2026 22:32:52 +0000"
     },
     {
       "source": "Meu Timao",
       "color": "#1a7a3a",
-      "title": "Defesa de vice do Corinthians reforça pedido para afastar promotor do caso dos materiais da Nike",
-      "url": "https://www.meutimao.com.br/noticias-do-corinthians/536183/defesa-de-vice-do-corinthians-reforca-pedido-para-afastar-promotor-do-caso-dos-materiais-da-nike",
-      "time": "22/07 · 18:00",
-      "pubDate": "Wed, 22 Jul 2026 15:00:00 -0300"
+      "title": "Gaviões da Fiel vai ao Parque São Jorge e cobra presidente do Corinthians por SAF e transfer ban",
+      "url": "https://www.meutimao.com.br/noticias-do-corinthians/536198/gavioes-da-fiel-vai-ao-parque-sao-jorge-e-cobra-presidente-do-corinthians-por-saf-e-transfer-ban",
+      "time": "22/07 · 22:19",
+      "pubDate": "Wed, 22 Jul 2026 19:19:00 -0300"
     },
     {
       "source": "Meu Timao",
       "color": "#1a7a3a",
-      "title": "Corinthians iguala temporada com maior número de derrotas desde a reativação do futebol feminino",
-      "url": "https://www.meutimao.com.br/noticias-do-corinthians/536127/corinthians-iguala-temporada-com-maior-numero-de-derrotas-desde-a-reativacao-do-futebol-feminino",
-      "time": "22/07 · 17:01",
-      "pubDate": "Wed, 22 Jul 2026 14:01:00 -0300"
+      "title": "Pivô da Seleção Brasileira se declara ao Corinthians em visita ao Parque São Jorge",
+      "url": "https://www.meutimao.com.br/noticias-do-corinthians/536160/pivo-da-selecao-brasileira-se-declara-ao-corinthians-em-visita-ao-parque-sao-jorge",
+      "time": "22/07 · 21:31",
+      "pubDate": "Wed, 22 Jul 2026 18:31:00 -0300"
     },
     {
       "source": "Meu Timao",
       "color": "#1a7a3a",
-      "title": "Corinthians reencontra a Neo Química Arena em busca da décima vitória em casa na temporada",
-      "url": "https://www.meutimao.com.br/noticias-do-corinthians/536182/corinthians-reencontra-a-neo-quimica-arena-em-busca-da-decima-vitoria-em-casa-na-temporada",
-      "time": "22/07 · 16:04",
-      "pubDate": "Wed, 22 Jul 2026 13:04:00 -0300"
+      "title": "Osmar Stabile recebe atletas do Corinthians convocadas para a Seleção Brasileira de Handebol",
+      "url": "https://www.meutimao.com.br/noticias-do-corinthians/536168/osmar-stabile-recebe-atletas-do-corinthians-convocadas-para-a-selecao-brasileira-de-handebol",
+      "time": "22/07 · 21:00",
+      "pubDate": "Wed, 22 Jul 2026 18:00:00 -0300"
     },
     {
       "source": "Meu Timao",
       "color": "#1a7a3a",
-      "title": "Corinthians transfere jogo do Brasileirão Feminino para o Canindé",
-      "url": "https://www.meutimao.com.br/noticias-do-corinthians/536181/corinthians-transfere-jogo-do-brasileirao-feminino-para-o-caninde",
-      "time": "22/07 · 15:00",
-      "pubDate": "Wed, 22 Jul 2026 12:00:00 -0300"
+      "title": "Corinthians vence o Lemense e chega à vice-liderança do Paulista Feminino Sub-20",
+      "url": "https://www.meutimao.com.br/noticias-do-corinthians/536192/corinthians-vence-o-lemense-e-chega-a-vice-lideranca-do-paulista-feminino-sub-20",
+      "time": "22/07 · 20:56",
+      "pubDate": "Wed, 22 Jul 2026 17:56:00 -0300"
     },
     {
       "source": "Meu Timao",
       "color": "#1a7a3a",
-      "title": "Zagueira do Corinthians pede união do elenco para sequência da temporada após eliminação",
-      "url": "https://www.meutimao.com.br/noticias-do-corinthians/536124/zagueira-do-corinthians-pede-uniao-do-elenco-para-sequencia-da-temporada-apos-eliminacao",
-      "time": "22/07 · 14:36",
-      "pubDate": "Wed, 22 Jul 2026 11:36:00 -0300"
+      "title": "Goleiro do Corinthians celebra atuação em vitória e revela sonho de chegar ao time profissional",
+      "url": "https://www.meutimao.com.br/noticias-do-corinthians/536177/goleiro-do-corinthians-celebra-atuacao-em-vitoria-e-revela-sonho-de-chegar-ao-time-profissional",
+      "time": "22/07 · 20:32",
+      "pubDate": "Wed, 22 Jul 2026 17:32:00 -0300"
+    },
+    {
+      "source": "Gazeta Esportiva",
+      "color": "#E5820A",
+      "title": "Corinthians x Remo: veja onde assistir ao jogo pelo Brasileirão",
+      "url": "https://www.gazetaesportiva.com/campeonatos/brasileiro-serie-a/corinthians-x-remo-veja-onde-assistir-ao-jogo-pelo-brasileirao/",
+      "time": "22/07 · 20:21",
+      "pubDate": "Wed, 22 Jul 2026 20:21:02 +0000"
     },
     {
       "source": "Meu Timao",
       "color": "#1a7a3a",
-      "title": "Meia amplia vantagem nas artilharias do Brasileiro Sub-17 e do elenco do Corinthians após goleada",
-      "url": "https://www.meutimao.com.br/noticias-do-corinthians/536153/meia-amplia-vantagem-nas-artilharias-do-brasileiro-sub-17-e-do-elenco-do-corinthians-apos-goleada",
-      "time": "22/07 · 13:33",
-      "pubDate": "Wed, 22 Jul 2026 10:33:00 -0300"
+      "title": "Corinthians confirma saída de cinco atletas para a próxima temporada do basquete",
+      "url": "https://www.meutimao.com.br/noticias-do-corinthians/536189/corinthians-confirma-saida-de-cinco-atletas-para-a-proxima-temporada-do-basquete",
+      "time": "22/07 · 20:16",
+      "pubDate": "Wed, 22 Jul 2026 17:16:00 -0300"
+    },
+    {
+      "source": "Gazeta Esportiva",
+      "color": "#E5820A",
+      "title": "Corinthians se reapresenta na Neo Química Arena e inicia reta final de treinos para pegar o Remo",
+      "url": "https://www.gazetaesportiva.com/todas-as-noticias/corinthians-se-reapresenta-na-neo-quimica-arena-e-inicia-reta-final-de-treinos-para-pegar-o-remo/",
+      "time": "22/07 · 20:00",
+      "pubDate": "Wed, 22 Jul 2026 20:00:42 +0000"
+    },
+    {
+      "source": "Meu Timao",
+      "color": "#1a7a3a",
+      "title": "Corinthians chega a quatro campeonatos sem título pela primeira vez na história do futebol feminino",
+      "url": "https://www.meutimao.com.br/noticias-do-corinthians/536125/corinthians-chega-a-quatro-campeonatos-sem-titulo-pela-primeira-vez-na-historia-do-futebol-feminino",
+      "time": "22/07 · 19:37",
+      "pubDate": "Wed, 22 Jul 2026 16:37:00 -0300"
+    },
+    {
+      "source": "Bolavip",
+      "color": "#0055A5",
+      "title": "Vidente não pipoca e prevê vitória do Corinthians contra o Remo pelo Brasileirão 2026",
+      "url": "<![CDATA[https://br.bolavip.com/corinthians/vidente-nao-pipoca-e-preve-vitoria-do-corinthians-contra-o-remo-pelo-brasileirao-2026]]>",
+      "time": "22/07 · 17:53",
+      "pubDate": "Wed, 22 Jul 2026 14:53:54 -0300"
     },
     {
       "source": "Bolavip",
@@ -73,14 +113,6 @@ const LIVE_DATA = {
       "url": "<![CDATA[https://br.bolavip.com/corinthians/memphis-depay-liga-para-presidente-do-corinthians-buscando-renovacao]]>",
       "time": "22/07 · 12:50",
       "pubDate": "Wed, 22 Jul 2026 09:50:10 -0300"
-    },
-    {
-      "source": "Meu Timao",
-      "color": "#1a7a3a",
-      "title": "Corinthians inicia novo procedimento no gramado da Neo Química Arena após incômodo de jogadores",
-      "url": "https://www.meutimao.com.br/noticias-do-corinthians/536169/corinthians-inicia-novo-procedimento-no-gramado-da-neo-quimica-arena-apos-incomodo-de-jogadores",
-      "time": "22/07 · 12:29",
-      "pubDate": "Wed, 22 Jul 2026 09:29:00 -0300"
     },
     {
       "source": "Bolavip",
@@ -147,14 +179,6 @@ const LIVE_DATA = {
       "pubDate": "Tue, 21 Jul 2026 17:27:26 -0300"
     },
     {
-      "source": "Bolavip",
-      "color": "#0055A5",
-      "title": "Corinthians sofre nova punição da FIFA por dívida de Charles e chega ao 4º transfer ban",
-      "url": "<![CDATA[https://br.bolavip.com/corinthians/corinthians-sofre-nova-punicao-da-fifa-por-divida-de-charles-e-chega-ao-4o-transfer-ban]]>",
-      "time": "21/07 · 20:23",
-      "pubDate": "Tue, 21 Jul 2026 17:23:28 -0300"
-    },
-    {
       "source": "Gazeta Esportiva",
       "color": "#E5820A",
       "title": "Corinthians goleia Internacional e dorme na liderança do Brasileiro sub-17",
@@ -169,36 +193,18 @@ const LIVE_DATA = {
       "url": "<![CDATA[https://br.bolavip.com/corinthians/corinthians-ja-pagou-rs-56-milhoes-a-caixa-economica-federal]]>",
       "time": "21/07 · 19:25",
       "pubDate": "Tue, 21 Jul 2026 16:25:05 -0300"
-    },
-    {
-      "source": "Gazeta Esportiva",
-      "color": "#E5820A",
-      "title": "Corinthians não paga dívida por Charles e sofre quarto transfer ban",
-      "url": "https://www.gazetaesportiva.com/times/corinthians/corinthians-nao-paga-divida-por-charles-e-sofre-quarto-transfer-ban/",
-      "time": "21/07 · 19:05",
-      "pubDate": "Tue, 21 Jul 2026 19:05:37 +0000"
-    },
-    {
-      "source": "Gazeta Esportiva",
-      "color": "#E5820A",
-      "title": "Mirassol anuncia Gustavo Mosquito, ex-Corinthians, como reforço",
-      "url": "https://www.gazetaesportiva.com/times/mirassol/mirassol-anuncia-gustavo-mosquito-ex-corinthians-como-reforco/",
-      "time": "21/07 · 16:40",
-      "pubDate": "Tue, 21 Jul 2026 16:40:00 +0000"
-    },
-    {
-      "source": "Gazeta Esportiva",
-      "color": "#E5820A",
-      "title": "Corinthians treina na Neo Química Arena de olho no Remo",
-      "url": "https://www.gazetaesportiva.com/todas-as-noticias/corinthians-treina-na-neo-quimica-arena-de-olho-no-remo/",
-      "time": "21/07 · 16:27",
-      "pubDate": "Tue, 21 Jul 2026 16:27:52 +0000"
     }
   ],
   "upcoming": [],
   "results": [],
   "standings": [],
   "highlights": [
+    {
+      "title": "Goleiro do Corinthians celebra atuação em vitória e revela sonho de chegar ao time profissional",
+      "url": "https://www.meutimao.com.br/noticias-do-corinthians/536177/goleiro-do-corinthians-celebra-atuacao-em-vitoria-e-revela-sonho-de-chegar-ao-time-profissional",
+      "pubDate": "Wed, 22 Jul 2026 17:32:00 -0300",
+      "date": "22/07 · 20:32"
+    },
     {
       "title": "Artilheiro do Corinthians avalia goleada no Brasileiro Sub-17 e cita referências no profissional",
       "url": "https://www.meutimao.com.br/noticias-do-corinthians/536175/artilheiro-do-corinthians-avalia-goleada-no-brasileiro-sub-17-e-cita-referencias-no-profissional",
@@ -228,12 +234,6 @@ const LIVE_DATA = {
       "url": "https://www.meutimao.com.br/noticias-do-corinthians/536081/atacante-anota-primeiro-hat-trick-pelo-corinthians-em-goleada-no-paulista-sub-20",
       "pubDate": "Tue, 21 Jul 2026 16:30:00 -0300",
       "date": "21/07 · 19:30"
-    },
-    {
-      "title": "Bastidores do Corinthians trazem pedidos de atitude e elogios do treinador após goleada por 7 a 1",
-      "url": "https://www.meutimao.com.br/noticias-do-corinthians/536112/bastidores-do-corinthians-sub-20-trazem-pedidos-de-atitude-e-elogios-apos-goleada-por-7-a-1",
-      "pubDate": "Tue, 21 Jul 2026 12:03:00 -0300",
-      "date": "21/07 · 15:03"
     }
   ]
 };
