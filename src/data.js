54faa9d3bb1ff0e54c2750db25f6b5f46288d472
GitHub Action <action@github.com>
2026-07-24 22:49:15 +0000

    Dados atualizados

--- src/data.js
+++ src/data.js
@@ -1,30 +1,102 @@
-// Gerado automaticamente em 24/07/2026, 19:07:07
+// Gerado automaticamente em 24/07/2026, 22:49:15
 const LIVE_DATA = {
-  "updatedAt": "2026-07-24T19:07:07.140Z",
+  "updatedAt": "2026-07-24T22:49:15.172Z",
   "news": [
     {
       "source": "Meu Timao",
       "color": "#1a7a3a",
-      "title": "Atacante do Corinthians volta a marcar em dois jogos seguidos depois de mais de três anos",
-      "url": "https://www.meutimao.com.br/noticias-do-corinthians/536304/atacante-do-corinthians-volta-a-marcar-em-dois-jogos-seguidos-depois-de-mais-de-tres-anos",
-      "time": "24/07 · 18:30",
-      "pubDate": "Fri, 24 Jul 2026 15:30:00 -0300"
+      "title": "Corinthians ganha dois novos garçons em 2026; Labyad salta para a quarta colocação no ranking",
+      "url": "https://www.meutimao.com.br/noticias-do-corinthians/536281/corinthians-ganha-dois-novos-garcons-em-2026-labyad-salta-para-a-quarta-colocacao-no-ranking",
+      "time": "24/07 · 22:34",
+      "pubDate": "Fri, 24 Jul 2026 19:34:00 -0300"
     },
     {
       "source": "Meu Timao",
       "color": "#1a7a3a",
-      "title": "Veja como está a artilharia do Corinthians na temporada após vitória sobre o Remo",
-      "url": "https://www.meutimao.com.br/noticias-do-corinthians/536280/veja-como-esta-a-artilharia-do-corinthians-na-temporada-apos-vitoria-sobre-o-remo",
-      "time": "24/07 · 18:05",
-      "pubDate": "Fri, 24 Jul 2026 15:05:00 -0300"
+      "title": "Corinthians empata retrospecto contra o Remo no Brasileirão após vitória",
+      "url": "https://www.meutimao.com.br/noticias-do-corinthians/536275/corinthians-empata-retrospecto-contra-o-remo-no-brasileirao-apos-vitoria",
+      "time": "24/07 · 22:09",
+      "pubDate": "Fri, 24 Jul 2026 19:09:00 -0300"
     },
     {
       "source": "Meu Timao",
       "color": "#1a7a3a",
-      "title": "Yuri Alberto celebra reencontro com a torcida e prega dedicação total ao Corinthians",
-      "url": "https://www.meutimao.com.br/noticias-do-corinthians/536297/yuri-alberto-celebra-reencontro-com-a-torcida-e-prega-dedicacao-total-ao-corinthians",
-      "time": "24/07 · 17:35",
-      "pubDate": "Fri, 24 Jul 2026 14:35:00 -0300"
+      "title": "Volante do Corinthians celebra vitória contra o Remo e opina sobre gramado da Neo Química Arena",
+      "url": "https://www.meutimao.com.br/noticias-do-corinthians/536316/volante-do-corinthians-celebra-vitoria-contra-o-remo-e-opina-sobre-gramado-da-neo-quimica-arena",
+      "time": "24/07 · 21:31",
+      "pubDate": "Fri, 24 Jul 2026 18:31:00 -0300"
+    },
+    {
+      "source": "Meu Timao",
+      "color": "#1a7a3a",
+      "title": "Corinthians recebe técnico da Seleção Brasileira Sub-15 campeão pelo clube no CT Joaquim Grava",
+      "url": "https://www.meutimao.com.br/noticias-do-corinthians/536349/corinthians-recebe-tecnico-da-selecao-brasileira-sub-15-campeao-pelo-clube-no-ct-joaquim-grava",
+      "time": "24/07 · 21:02",
+      "pubDate": "Fri, 24 Jul 2026 18:02:00 -0300"
+    },
+    {
+      "source": "Meu Timao",
+      "color": "#1a7a3a",
+      "title": "Atacante de Portugal recebe camisa do Corinthians e conversa com elenco e Diniz no CT; veja imagens",
+      "url": "https://www.meutimao.com.br/noticias-do-corinthians/536361/atacante-de-portugal-recebe-camisa-do-corinthians-e-conversa-com-elenco-e-diniz-no-ct-veja-imagens",
+      "time": "24/07 · 20:55",
+      "pubDate": "Fri, 24 Jul 2026 17:55:49 -0300"
+    },
+    {
+      "source": "Gazeta Esportiva",
+      "color": "#E5820A",
+      "title": "Diniz fala sobre Memphis no Corinthians: &#8220;Seria muito bom contar com ele&#8221;",
+      "url": "https://www.gazetaesportiva.com/times/corinthians/diniz-fala-sobre-memphis-no-corinthians-seria-muito-bom-contar-com-ele-2/",
+      "time": "24/07 · 20:54",
+      "pubDate": "Fri, 24 Jul 2026 20:54:21 +0000"
+    },
+    {
+      "source": "Meu Timao",
+      "color": "#1a7a3a",
+      "title": "Corinthians entra na reta final de preparação para encarar o Vitória pelo Brasileirão",
+      "url": "https://www.meutimao.com.br/noticias-do-corinthians/536360/corinthians-entra-na-reta-final-de-preparacao-para-encarar-o-vitoria-pelo-brasileirao",
+      "time": "24/07 · 20:50",
+      "pubDate": "Fri, 24 Jul 2026 17:50:00 -0300"
+    },
+    {
+      "source": "Meu Timao",
+      "color": "#1a7a3a",
+      "title": "Corinthians engata maior sequência de vitórias no ano e encosta na liderança do Paulista Sub-20",
+      "url": "https://www.meutimao.com.br/noticias-do-corinthians/536238/corinthians-engata-maior-sequencia-de-vitorias-no-ano-e-encosta-na-lideranca-do-paulista-sub-20",
+      "time": "24/07 · 20:31",
+      "pubDate": "Fri, 24 Jul 2026 17:31:00 -0300"
+    },
+    {
+      "source": "Gazeta Esportiva",
+      "color": "#E5820A",
+      "title": "Rafael Leão usa CT do Corinthians para treinar durante as férias",
+      "url": "https://www.gazetaesportiva.com/times/corinthians/rafael-leao-usa-ct-do-corinthians-para-treinar-durante-as-ferias/",
+      "time": "24/07 · 20:06",
+      "pubDate": "Fri, 24 Jul 2026 20:06:24 +0000"
+    },
+    {
+      "source": "Meu Timao",
+      "color": "#1a7a3a",
+      "title": "Corinthians fecha primeiro turno do Brasileirão com campanhas distintas de Diniz e Dorival",
+      "url": "https://www.meutimao.com.br/noticias-do-corinthians/536276/corinthians-fecha-primeiro-turno-do-brasileirao-com-campanhas-distintas-de-diniz-e-dorival",
+      "time": "24/07 · 20:04",
+      "pubDate": "Fri, 24 Jul 2026 17:04:00 -0300"
+    },
+    {
+      "source": "Gazeta Esportiva",
+      "color": "#E5820A",
+      "title": "Corinthians se reapresenta e inicia preparação para jogo contra o Bahia",
+      "url": "https://www.gazetaesportiva.com/times/corinthians/corinthians-se-reapresenta-e-inicia-preparacao-para-jogo-contra-o-bahia/",
+      "time": "24/07 · 19:23",
+      "pubDate": "Fri, 24 Jul 2026 19:23:34 +0000"
+    },
+    {
+      "source": "Bolavip",
+      "color": "#0055A5",
+      "title": "Osmar Stabile paga multa de R$ 1,16 milhão e resolve primeiro dos quatro transfer bans do Corinthians",
+      "url": "<![CDATA[https://br.bolavip.com/corinthians/osmar-stabile-paga-multa-de-r-116-milhao-e-resolve-primeiro-dos-quatro-transfer-bans-do-corinthians]]>",
+      "time": "24/07 · 17:25",
+      "pubDate": "Fri, 24 Jul 2026 14:25:13 -0300"
     },
     {
       "source": "Gazeta Esportiva",
@@ -35,12 +107,12 @@ const LIVE_DATA = {
       "pubDate": "Fri, 24 Jul 2026 17:14:19 +0000"
     },
     {
-      "source": "Meu Timao",
-      "color": "#1a7a3a",
-      "title": "CBF divulga áudio de análise do VAR em gol validado para o Corinthians contra o Remo",
-      "url": "https://www.meutimao.com.br/noticias-do-corinthians/536329/cbf-divulga-audio-de-analise-do-var-sobre-segundo-gol-do-corinthians-contra-o-remo",
-      "time": "24/07 · 16:37",
-      "pubDate": "Fri, 24 Jul 2026 13:37:00 -0300"
+      "source": "Bolavip",
+      "color": "#0055A5",
+      "title": "Yuri Alberto encerra jejum de quatro meses no Brasileirão e brilha em vitória do Corinthians",
+      "url": "<![CDATA[https://br.bolavip.com/corinthians/yuri-alberto-encerra-jejum-de-quatro-meses-no-brasileirao-e-brilha-em-vitoria-do-corinthians]]>",
+      "time": "24/07 · 16:26",
+      "pubDate": "Fri, 24 Jul 2026 13:26:46 -0300"
     },
     {
       "source": "Gazeta Esportiva",
@@ -51,36 +123,20 @@ const LIVE_DATA = {
       "pubDate": "Fri, 24 Jul 2026 16:19:48 +0000"
     },
     {
-      "source": "Meu Timao",
-      "color": "#1a7a3a",
-      "title": "Kaio César admite desejo de ficar no Corinthians, mas diz que não há conversas por renovação",
-      "url": "https://www.meutimao.com.br/noticias-do-corinthians/536320/kaio-cesar-admite-desejo-de-ficar-no-corinthians-mas-diz-que-nao-ha-conversas-por-renovacao",
-      "time": "24/07 · 16:07",
-      "pubDate": "Fri, 24 Jul 2026 13:07:00 -0300"
+      "source": "Bolavip",
+      "color": "#0055A5",
+      "title": "Yuri Alberto encerra jejum e volta a marcar dois gols em jogo pelo Corinthians",
+      "url": "<![CDATA[https://br.bolavip.com/corinthians/yuri-alberto-encerra-jejum-e-volta-a-marcar-dois-gols-em-jogo-pelo-corinthians]]>",
+      "time": "24/07 · 15:23",
+      "pubDate": "Fri, 24 Jul 2026 12:23:47 -0300"
     },
     {
-      "source": "Meu Timao",
-      "color": "#1a7a3a",
-      "title": "Pivô se despede do Corinthians e relembra trajetória de seis temporadas no basquete",
-      "url": "https://www.meutimao.com.br/noticias-do-corinthians/536260/pivo-se-despede-do-corinthians-e-relembra-trajetoria-de-seis-temporadas-no-basquete",
-      "time": "24/07 · 15:36",
-      "pubDate": "Fri, 24 Jul 2026 12:36:00 -0300"
-    },
-    {
-      "source": "Meu Timao",
-      "color": "#1a7a3a",
-      "title": "Corinthians paga multa e se livra de um transfer ban da Fifa",
-      "url": "https://www.meutimao.com.br/noticias-do-corinthians/536334/corinthians-paga-multa-e-se-livra-de-um-transfer-ban-da-fifa",
-      "time": "24/07 · 15:08",
-      "pubDate": "Fri, 24 Jul 2026 12:08:00 -0300"
-    },
-    {
-      "source": "Meu Timao",
-      "color": "#1a7a3a",
-      "title": "Corinthians inicia venda de ingressos para jogo contra o Athletico-PR pelo Brasileirão",
-      "url": "https://www.meutimao.com.br/noticias-do-corinthians/536331/corinthians-divulga-venda-de-ingressos-para-jogo-contra-o-athletico-pr-pelo-brasileirao",
-      "time": "24/07 · 15:00",
-      "pubDate": "Fri, 24 Jul 2026 12:00:00 -0300"
+      "source": "Bolavip",
+      "color": "#0055A5",
+      "title": "Diniz rejeita rótulo de ‘mágico’ após vitória no Brasileirão e projeta sobre saídas no Corinthians",
+      "url": "<![CDATA[https://br.bolavip.com/corinthians/diniz-rejeita-rotulo-de-magico-apos-vitoria-no-brasileirao-e-projeta-sobre-saidas-no-corinthians]]>",
+      "time": "24/07 · 15:01",
+      "pubDate": "Fri, 24 Jul 2026 12:01:00 -0300"
     },
     {
       "source": "Bolavip",
@@ -97,14 +153,6 @@ const LIVE_DATA = {
       "url": "https://www.gazetaesportiva.com/times/corinthians/diniz-fala-sobre-memphis-no-corinthians-seria-muito-bom-contar-com-ele/",
       "time": "24/07 · 12:47",
       "pubDate": "Fri, 24 Jul 2026 12:47:02 +0000"
-    },
-    {
-      "source": "Bolavip",
-      "color": "#0055A5",
-      "title": "Diniz liga sinal de alerta apesar da goleada do Corinthians diante do Remo: “Baixamos a intensidade”",
-      "url": "<![CDATA[https://br.bolavip.com/corinthians/diniz-liga-sinal-de-alerta-apesar-da-goleada-do-corinthians-diante-do-remo-baixamos-a-intensidade]]>",
-      "time": "24/07 · 12:24",
-      "pubDate": "Fri, 24 Jul 2026 09:24:52 -0300"
     },
     {
       "source": "Bolavip",
@@ -145,54 +193,6 @@ const LIVE_DATA = {
       "url": "https://www.gazetaesportiva.com/times/corinthians/entenda-como-diniz-usou-a-copa-do-mundo-para-aprimorar-o-corinthians/",
       "time": "24/07 · 09:00",
       "pubDate": "Fri, 24 Jul 2026 09:00:50 +0000"
-    },
-    {
-      "source": "Gazeta Esportiva",
-      "color": "#E5820A",
-      "title": "Atuações do Corinthians: dupla de ataque brilha em vitória contra o Remo",
-      "url": "https://www.gazetaesportiva.com/times/corinthians/atuacoes-do-corinthians-dupla-de-ataque-brilha-em-vitoria-contra-o-remo/",
-      "time": "24/07 · 08:00",
-      "pubDate": "Fri, 24 Jul 2026 08:00:04 +0000"
-    },
-    {
-      "source": "Bolavip",
-      "color": "#0055A5",
-      "title": "Diniz admite saída no Corinthians, mas garante venda de apenas um titular nesta janela",
-      "url": "<![CDATA[https://br.bolavip.com/corinthians/diniz-admite-saida-no-corinthians-mas-garante-venda-de-apenas-um-titular-nesta-janela]]>",
-      "time": "24/07 · 02:26",
-      "pubDate": "Thu, 23 Jul 2026 23:26:26 -0300"
-    },
-    {
-      "source": "Gazeta Esportiva",
-      "color": "#E5820A",
-      "title": "Diniz admite necessidade de vendas no Corinthians e torce por renovação de Memphis",
-      "url": "https://www.gazetaesportiva.com/times/corinthians/diniz-admite-necessidade-de-vendas-no-corinthians-e-torce-por-renovacao-de-memphis/",
-      "time": "24/07 · 02:15",
-      "pubDate": "Fri, 24 Jul 2026 02:15:43 +0000"
-    },
-    {
-      "source": "Bolavip",
-      "color": "#0055A5",
-      "title": "Labyad tem grande atuação em vitória do Corinthians sobre Remo e é ovacionado pela Fiel",
-      "url": "<![CDATA[https://br.bolavip.com/corinthians/labyad-tem-grande-atuacao-em-vitoria-do-corinthians-sobre-remo-e-e-ovacionado-pela-fiel]]>",
-      "time": "24/07 · 02:01",
-      "pubDate": "Thu, 23 Jul 2026 23:01:33 -0300"
-    },
-    {
-      "source": "Gazeta Esportiva",
-      "color": "#E5820A",
-      "title": "Veja galeria de fotos do jogo entre Corinthians e Remo pela 19ª rodada do Brasileirão",
-      "url": "https://www.gazetaesportiva.com/campeonatos/brasileiro-serie-a/veja-galeria-de-fotos-do-jogo-entre-corinthians-e-remo-pela-19a-rodada-do-brasileirao/",
-      "time": "24/07 · 02:00",
-      "pubDate": "Fri, 24 Jul 2026 02:00:42 +0000"
-    },
-    {
-      "source": "Bolavip",
-      "color": "#0055A5",
-      "title": "Fernando Diniz impõe DNA ofensivo e Corinthians vence mais uma no Brasileirão",
-      "url": "<![CDATA[https://br.bolavip.com/corinthians/fernando-diniz-impoe-dna-ofensivo-e-corinthians-vence-mais-uma-no-brasileirao]]>",
-      "time": "24/07 · 00:33",
-      "pubDate": "Thu, 23 Jul 2026 21:33:49 -0300"
     }
   ],
   "upcoming": [],
@@ -222,18 +222,6 @@ const LIVE_DATA = {
       "url": "https://www.meutimao.com.br/videos-do-corinthians/128695/substituto-de-memphis-conexao-com-diniz-e-golaco-zona-mista-de-kaio-cesar-em-corinthians-3x0-remo",
       "pubDate": "Fri, 24 Jul 2026 00:01:55 -0300",
       "date": "24/07 · 03:01"
-    },
-    {
-      "title": "VÍDEO: Novo gramado, diferenças de Diniz e Dorival e Fiel torcida: Zona Mista de Allan",
-      "url": "https://www.meutimao.com.br/videos-do-corinthians/128688/novo-gramado-diferencas-de-diniz-e-dorival-e-fiel-torcida-zona-mista-de-allan",
-      "pubDate": "Thu, 23 Jul 2026 23:19:00 -0300",
-      "date": "24/07 · 02:19"
-    },
-    {
-      "title": "VÍDEO: Melhores Momentos: Corinthians 3X0 Remo| 19ª Rodada| Brasileirão 2026",
-      "url": "https://www.meutimao.com.br/videos-do-corinthians/128675/melhores-momentos-corinthians-3x0-remo-19-rodada-brasileirao-2026",
-      "pubDate": "Thu, 23 Jul 2026 21:42:00 -0300",
-      "date": "24/07 · 00:42"
     }
   ]
 };
