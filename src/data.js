81fae0fe5c98d60addc292f2745c65d4abce73d2
GitHub Action <action@github.com>
2026-07-28 10:18:25 +0000

    Dados atualizados

--- src/data.js
+++ src/data.js
@@ -1,46 +1,78 @@
-// Gerado automaticamente em 27/07/2026, 22:47:05
+// Gerado automaticamente em 28/07/2026, 10:18:25
 const LIVE_DATA = {
-  "updatedAt": "2026-07-27T22:47:05.686Z",
+  "updatedAt": "2026-07-28T10:18:25.936Z",
   "news": [
     {
-      "source": "Meu Timao",
-      "color": "#1a7a3a",
-      "title": "Volante exalta ponto fora de casa e cobra sequência do Corinthians de olho em eliminatórias",
-      "url": "https://www.meutimao.com.br/noticias-do-corinthians/536505/volante-exalta-ponto-fora-de-casa-e-cobra-sequencia-do-corinthians-de-olho-em-eliminatorias",
-      "time": "27/07 · 22:37",
-      "pubDate": "Mon, 27 Jul 2026 19:37:00 -0300"
+      "source": "Gazeta Esportiva",
+      "color": "#E5820A",
+      "title": "Com Angileri suspenso, Diniz ganha dor de cabeça na lateral esquerda do Corinthians",
+      "url": "https://www.gazetaesportiva.com/times/corinthians/com-angileri-suspenso-diniz-ganha-dor-de-cabeca-na-lateral-esquerda-do-corinthians/",
+      "time": "28/07 · 10:00",
+      "pubDate": "Tue, 28 Jul 2026 10:00:47 +0000"
     },
     {
       "source": "Meu Timao",
       "color": "#1a7a3a",
-      "title": "Capricho e homenagem à família marcam bastidores da goleada do Corinthians Feminino",
-      "url": "https://www.meutimao.com.br/noticias-do-corinthians/536554/capricho-e-homenagem-a-familia-marcam-bastidores-da-goleada-do-corinthians-feminino",
-      "time": "27/07 · 22:15",
-      "pubDate": "Mon, 27 Jul 2026 19:15:00 -0300"
+      "title": "Volante do Corinthians chega a sua segunda assistência no ano em empate com Bahia; veja ranking",
+      "url": "https://www.meutimao.com.br/noticias-do-corinthians/536503/volante-do-corinthians-chega-a-sua-segunda-assistencia-no-ano-em-empate-com-bahia-veja-ranking",
+      "time": "28/07 · 01:30",
+      "pubDate": "Mon, 27 Jul 2026 22:30:00 -0300"
     },
     {
       "source": "Meu Timao",
       "color": "#1a7a3a",
-      "title": "Emily Lima explica troca de volante no primeiro tempo e detalha controle de carga no Corinthians",
-      "url": "https://www.meutimao.com.br/noticias-do-corinthians/536533/emily-lima-explica-troca-de-volante-no-primeiro-tempo-e-detalha-controle-de-carga-no-corinthians",
-      "time": "27/07 · 22:04",
-      "pubDate": "Mon, 27 Jul 2026 19:04:00 -0300"
+      "title": "Vic Albuquerque comemora gols contra o Vitória e projeta sequência do Corinthians na temporada",
+      "url": "https://www.meutimao.com.br/noticias-do-corinthians/536540/vic-albuquerque-comemora-gols-contra-o-vitoria-e-projeta-sequencia-do-corinthians-na-temporada",
+      "time": "28/07 · 01:01",
+      "pubDate": "Mon, 27 Jul 2026 22:01:00 -0300"
     },
     {
       "source": "Meu Timao",
       "color": "#1a7a3a",
-      "title": "Corinthians define data de retorno de Memphis para finalizar renovação",
-      "url": "https://www.meutimao.com.br/noticias-do-corinthians/536557/corinthians-define-data-de-retorno-de-memphis-para-finalizar-renovacao",
-      "time": "27/07 · 21:50",
-      "pubDate": "Mon, 27 Jul 2026 18:50:00 -0300"
+      "title": "Corinthians estreia com goleada sobre o Umuarama no Talentos LNF",
+      "url": "https://www.meutimao.com.br/noticias-do-corinthians/536563/corinthians-estreia-com-goleada-sobre-o-umuarama-no-talentos-lnf",
+      "time": "28/07 · 00:42",
+      "pubDate": "Mon, 27 Jul 2026 21:42:00 -0300"
     },
     {
       "source": "Meu Timao",
       "color": "#1a7a3a",
-      "title": "Lista de artilheiros do Corinthians em 2026 ganha novo nome após empate com o Bahia",
-      "url": "https://www.meutimao.com.br/noticias-do-corinthians/536480/lista-de-artilheiros-do-corinthians-em-2026-ganha-novo-nome-apos-empate-com-o-bahia",
-      "time": "27/07 · 21:33",
-      "pubDate": "Mon, 27 Jul 2026 18:33:00 -0300"
+      "title": "Lateral do Corinthians fala sobre concorrência com Matheus Bidu e valoriza gol marcado",
+      "url": "https://www.meutimao.com.br/noticias-do-corinthians/536507/lateral-do-corinthians-fala-sobre-concorrencia-com-matheus-bidu-e-valoriza-gol-marcado",
+      "time": "28/07 · 00:32",
+      "pubDate": "Mon, 27 Jul 2026 21:32:00 -0300"
+    },
+    {
+      "source": "Meu Timao",
+      "color": "#1a7a3a",
+      "title": "Técnica do Corinthians admite chance de encerrar janela sem reforços e aponta carência no ataque",
+      "url": "https://www.meutimao.com.br/noticias-do-corinthians/536534/tecnica-do-corinthians-admite-chance-de-encerrar-janela-sem-reforcos-e-aponta-carencia-no-ataque",
+      "time": "28/07 · 00:01",
+      "pubDate": "Mon, 27 Jul 2026 21:01:00 -0300"
+    },
+    {
+      "source": "Meu Timao",
+      "color": "#1a7a3a",
+      "title": "Corinthians recebe cônsul dos Emirados Árabes Unidos do Brasil na Neo Química Arena",
+      "url": "https://www.meutimao.com.br/noticias-do-corinthians/536562/corinthians-recebe-consul-dos-emirados-arabes-unidos-do-brasil-na-neo-quimica-arena",
+      "time": "27/07 · 23:53",
+      "pubDate": "Mon, 27 Jul 2026 20:53:00 -0300"
+    },
+    {
+      "source": "Meu Timao",
+      "color": "#1a7a3a",
+      "title": "Vic Albuquerque acaba com jejum de quase quatro meses em goleada do Corinthians pelo Brasileirão",
+      "url": "https://www.meutimao.com.br/noticias-do-corinthians/536444/vic-albuquerque-acaba-com-jejum-de-quase-quatro-meses-em-goleada-do-corinthians-pelo-brasileirao",
+      "time": "27/07 · 23:31",
+      "pubDate": "Mon, 27 Jul 2026 20:31:00 -0300"
+    },
+    {
+      "source": "Meu Timao",
+      "color": "#1a7a3a",
+      "title": "Parte de dívida 'perdoada' e bônus menor: saiba detalhes da renovação de Memphis com o Corinthians",
+      "url": "https://www.meutimao.com.br/noticias-do-corinthians/536560/parte-de-divida-perdoada-e-bonus-menor-saiba-detalhes-da-renovacao-de-memphis-com-o-corinthians",
+      "time": "27/07 · 23:26",
+      "pubDate": "Mon, 27 Jul 2026 20:26:00 -0300"
     },
     {
       "source": "Gazeta Esportiva",
@@ -51,28 +83,36 @@ const LIVE_DATA = {
       "pubDate": "Mon, 27 Jul 2026 21:28:53 +0000"
     },
     {
-      "source": "Meu Timao",
-      "color": "#1a7a3a",
-      "title": "Promessa do Corinthians detalha preparação para Talentos LNF e sonha com título inédito",
-      "url": "https://www.meutimao.com.br/noticias-do-corinthians/536545/promessa-do-corinthians-detalha-preparacao-para-talentos-lnf-e-sonha-com-titulo-inedito",
-      "time": "27/07 · 21:00",
-      "pubDate": "Mon, 27 Jul 2026 18:00:00 -0300"
+      "source": "Bolavip",
+      "color": "#0055A5",
+      "title": "Corinthians alcança marca entre as equipes que mais empataram no Campeonato Brasileiro de 2026",
+      "url": "<![CDATA[https://br.bolavip.com/corinthians/corinthians-alcanca-marca-entre-as-equipes-que-mais-empataram-no-campeonato-brasileiro-de-2026]]>",
+      "time": "27/07 · 20:54",
+      "pubDate": "Mon, 27 Jul 2026 17:54:06 -0300"
     },
     {
-      "source": "Meu Timao",
-      "color": "#1a7a3a",
-      "title": "Técnico do Corinthians projeta disputa do Talentos LNF e destaca importância da formação",
-      "url": "https://www.meutimao.com.br/noticias-do-corinthians/536531/tecnico-do-corinthians-projeta-disputa-do-talentos-lnf-e-destaca-importancia-da-formacao",
-      "time": "27/07 · 20:01",
-      "pubDate": "Mon, 27 Jul 2026 17:01:00 -0300"
+      "source": "Bolavip",
+      "color": "#0055A5",
+      "title": "Vitinho tem lesão meniscal no joelho esquerdo confirmada e vai precisar de cirurgia no Corinthians",
+      "url": "<![CDATA[https://br.bolavip.com/corinthians/vitinho-tem-lesao-meniscal-no-joelho-esquerdo-confirmada-e-vai-precisar-de-cirurgia-no-corinthians]]>",
+      "time": "27/07 · 20:22",
+      "pubDate": "Mon, 27 Jul 2026 17:22:33 -0300"
     },
     {
-      "source": "Meu Timao",
-      "color": "#1a7a3a",
-      "title": "Zagueira volta a atuar pelo Corinthians após quase oito meses afastada por lesão",
-      "url": "https://www.meutimao.com.br/noticias-do-corinthians/536447/zagueira-volta-a-atuar-pelo-corinthians-apos-quase-oito-meses-afastada-por-lesao",
-      "time": "27/07 · 19:30",
-      "pubDate": "Mon, 27 Jul 2026 16:30:00 -0300"
+      "source": "Bolavip",
+      "color": "#0055A5",
+      "title": "Craque Neto elogia postura de Memphis Depay no Corinthians: “Abriu mão de muito dinheiro”",
+      "url": "<![CDATA[https://br.bolavip.com/corinthians/craque-neto-elogia-postura-de-memphis-depay-no-corinthians-abriu-mao-de-muito-dinheiro]]>",
+      "time": "27/07 · 20:18",
+      "pubDate": "Mon, 27 Jul 2026 17:18:59 -0300"
+    },
+    {
+      "source": "Bolavip",
+      "color": "#0055A5",
+      "title": "Raniele celebra renovação de Memphis Depay com o Corinthians: “Não tem como dar errado”",
+      "url": "<![CDATA[https://br.bolavip.com/corinthians/raniele-celebra-renovacao-de-memphis-depay-com-o-corinthians-nao-tem-como-dar-errado]]>",
+      "time": "27/07 · 19:17",
+      "pubDate": "Mon, 27 Jul 2026 16:17:46 -0300"
     },
     {
       "source": "Gazeta Esportiva",
@@ -81,14 +121,6 @@ const LIVE_DATA = {
       "url": "https://www.gazetaesportiva.com/times/corinthians/corinthians-se-reapresenta-apos-empate-e-abre-preparacao-para-enfrentar-o-athletico-pr/",
       "time": "27/07 · 18:37",
       "pubDate": "Mon, 27 Jul 2026 18:37:13 +0000"
-    },
-    {
-      "source": "Bolavip",
-      "color": "#0055A5",
-      "title": "Vitinho tem lesão meniscal no joelho esquerdo confirmada e vai precisar de cirurgia no Corinthians",
-      "url": "<![CDATA[https://br.bolavip.com/corinthians/vitinho-tem-lesao-meniscal-no-joelho-esquerdo-confirmada-e-vai-precisar-de-cirurgia-no-corinthians]]>",
-      "time": "27/07 · 18:08",
-      "pubDate": "Mon, 27 Jul 2026 15:08:54 -0300"
     },
     {
       "source": "Gazeta Esportiva",
@@ -139,22 +171,6 @@ const LIVE_DATA = {
       "pubDate": "Mon, 27 Jul 2026 14:26:20 +0000"
     },
     {
-      "source": "Bolavip",
-      "color": "#0055A5",
-      "title": "Corinthians encaminha renovação de Memphis até 2028 e prepara anúncio para terça-feira",
-      "url": "<![CDATA[https://br.bolavip.com/corinthians/corinthians-encaminha-renovacao-de-memphis-ate-2028-e-prepara-anuncio-para-terca-feira]]>",
-      "time": "27/07 · 11:40",
-      "pubDate": "Mon, 27 Jul 2026 08:40:17 -0300"
-    },
-    {
-      "source": "Bolavip",
-      "color": "#0055A5",
-      "title": "Fernando Diniz defende Kaio César após empate do Corinthians: “É uma evolução clara”",
-      "url": "<![CDATA[https://br.bolavip.com/corinthians/fernando-diniz-defende-kaio-cesar-apos-empate-do-corinthians-e-uma-evolucao-clara]]>",
-      "time": "27/07 · 11:21",
-      "pubDate": "Mon, 27 Jul 2026 08:21:31 -0300"
-    },
-    {
       "source": "Gazeta Esportiva",
       "color": "#E5820A",
       "title": "Atuações do Corinthians: elenco deixa a desejar contra o Bahia",
@@ -177,28 +193,36 @@ const LIVE_DATA = {
       "url": "https://www.gazetaesportiva.com/times/corinthians/analise-corinthians-sofre-mas-consegue-segurar-empate-contra-o-bahia/",
       "time": "27/07 · 08:00",
       "pubDate": "Mon, 27 Jul 2026 08:00:45 +0000"
-    },
-    {
-      "source": "Gazeta Esportiva",
-      "color": "#E5820A",
-      "title": "Negociação avança, e Corinthians fica perto de renovar com Memphis Depay",
-      "url": "https://www.gazetaesportiva.com/times/corinthians/negociacao-avanca-e-corinthians-fica-perto-de-renovar-com-memphis-depay/",
-      "time": "27/07 · 02:08",
-      "pubDate": "Mon, 27 Jul 2026 02:08:12 +0000"
-    },
-    {
-      "source": "Bolavip",
-      "color": "#0055A5",
-      "title": "Rogério Ceni aponta dificuldade do Corinthians diante do Bahia: “Só chutando para frente”",
-      "url": "<![CDATA[https://br.bolavip.com/corinthians/rogerio-ceni-aponta-dificuldade-do-corinthians-diante-do-bahia-so-chutando-para-frente]]>",
-      "time": "27/07 · 00:29",
-      "pubDate": "Sun, 26 Jul 2026 21:29:03 -0300"
     }
   ],
   "upcoming": [],
   "results": [],
   "standings": [],
   "highlights": [
+    {
+      "title": "Vic Albuquerque comemora gols contra o Vitória e projeta sequência do Corinthians na temporada",
+      "url": "https://www.meutimao.com.br/noticias-do-corinthians/536540/vic-albuquerque-comemora-gols-contra-o-vitoria-e-projeta-sequencia-do-corinthians-na-temporada",
+      "pubDate": "Mon, 27 Jul 2026 22:01:00 -0300",
+      "date": "28/07 · 01:01"
+    },
+    {
+      "title": "Corinthians estreia com goleada sobre o Umuarama no Talentos LNF",
+      "url": "https://www.meutimao.com.br/noticias-do-corinthians/536563/corinthians-estreia-com-goleada-sobre-o-umuarama-no-talentos-lnf",
+      "pubDate": "Mon, 27 Jul 2026 21:42:00 -0300",
+      "date": "28/07 · 00:42"
+    },
+    {
+      "title": "Lateral do Corinthians fala sobre concorrência com Matheus Bidu e valoriza gol marcado",
+      "url": "https://www.meutimao.com.br/noticias-do-corinthians/536507/lateral-do-corinthians-fala-sobre-concorrencia-com-matheus-bidu-e-valoriza-gol-marcado",
+      "pubDate": "Mon, 27 Jul 2026 21:32:00 -0300",
+      "date": "28/07 · 00:32"
+    },
+    {
+      "title": "Vic Albuquerque acaba com jejum de quase quatro meses em goleada do Corinthians pelo Brasileirão",
+      "url": "https://www.meutimao.com.br/noticias-do-corinthians/536444/vic-albuquerque-acaba-com-jejum-de-quase-quatro-meses-em-goleada-do-corinthians-pelo-brasileirao",
+      "pubDate": "Mon, 27 Jul 2026 20:31:00 -0300",
+      "date": "27/07 · 23:31"
+    },
     {
       "title": "Capricho e homenagem à família marcam bastidores da goleada do Corinthians Feminino",
       "url": "https://www.meutimao.com.br/noticias-do-corinthians/536554/capricho-e-homenagem-a-familia-marcam-bastidores-da-goleada-do-corinthians-feminino",
