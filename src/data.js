86027480e2121db19c33732e7cd816d7562bd60f
GitHub Action <action@github.com>
2026-08-03 14:08:02 +0000

    Dados atualizados

--- src/data.js
+++ src/data.js
@@ -1,7 +1,39 @@
-// Gerado automaticamente em 03/08/2026, 11:01:44
+// Gerado automaticamente em 03/08/2026, 14:08:02
 const LIVE_DATA = {
-  "updatedAt": "2026-08-03T11:01:44.529Z",
+  "updatedAt": "2026-08-03T14:08:02.930Z",
   "news": [
+    {
+      "source": "Meu Timao",
+      "color": "#1a7a3a",
+      "title": "Corinthians promove destaque da LDB ao elenco profissional de basquete",
+      "url": "https://www.meutimao.com.br/noticias-do-corinthians/537030/corinthians-promove-destaque-da-ldb-ao-elenco-profissional-de-basquete",
+      "time": "03/08 · 13:36",
+      "pubDate": "Mon, 03 Aug 2026 10:36:00 -0300"
+    },
+    {
+      "source": "Meu Timao",
+      "color": "#1a7a3a",
+      "title": "Corinthians terá semana com decisões na Copa do Brasil, LNF e estreia do basquete; veja a agenda",
+      "url": "https://www.meutimao.com.br/noticias-do-corinthians/537025/corinthians-tera-semana-com-decisoes-na-copa-do-brasil-lnf-e-estreia-do-basquete-veja-a-agenda",
+      "time": "03/08 · 13:32",
+      "pubDate": "Mon, 03 Aug 2026 10:32:00 -0300"
+    },
+    {
+      "source": "Meu Timao",
+      "color": "#1a7a3a",
+      "title": "Técnico do Corinthians reforça cautela com retorno de Yuri Alberto após lesão",
+      "url": "https://www.meutimao.com.br/noticias-do-corinthians/536998/tecnico-do-corinthians-evita-estipular-prazo-para-retorno-de-yuri-alberto-apos-lesao",
+      "time": "03/08 · 12:32",
+      "pubDate": "Mon, 03 Aug 2026 09:32:00 -0300"
+    },
+    {
+      "source": "Meu Timao",
+      "color": "#1a7a3a",
+      "title": "Saiba onde assistir à partida entre Corinthians e Flamengo pelo Brasileiro Feminino",
+      "url": "https://www.meutimao.com.br/noticias-do-corinthians/536949/saiba-onde-assistir-a-partida-entre-corinthians-e-flamengo-pelo-brasileiro-feminino",
+      "time": "03/08 · 11:30",
+      "pubDate": "Mon, 03 Aug 2026 08:30:00 -0300"
+    },
     {
       "source": "Gazeta Esportiva",
       "color": "#E5820A",
@@ -17,6 +49,14 @@ const LIVE_DATA = {
       "url": "https://www.meutimao.com.br/noticias-do-corinthians/537004/ultimas-do-corinthians-derrota-para-o-internacional-cobrancas-a-arbitragem-e-feminino-classificado",
       "time": "03/08 · 10:30",
       "pubDate": "Mon, 03 Aug 2026 07:30:00 -0300"
+    },
+    {
+      "source": "Bolavip",
+      "color": "#0055A5",
+      "title": "CBF irá avaliar a reclamação do Corinthians contra o árbitro Alex Gomes Stefano, mas punição não deve acontecer",
+      "url": "<![CDATA[https://br.bolavip.com/corinthians/cbf-ira-avaliar-a-reclamacao-do-corinthians-contra-o-arbitro-alex-gomes-stefano-mas-punicao-nao-deve-acontecer]]>",
+      "time": "03/08 · 10:01",
+      "pubDate": "Mon, 03 Aug 2026 07:01:12 -0300"
     },
     {
       "source": "Gazeta Esportiva",
@@ -75,44 +115,12 @@ const LIVE_DATA = {
       "pubDate": "Mon, 03 Aug 2026 00:01:00 -0300"
     },
     {
-      "source": "Meu Timao",
-      "color": "#1a7a3a",
-      "title": "VÍDEO: Marcelo Paz reclama a arbitragem e cita pênalti não marcado em Internacional 2X0 Corinthians",
-      "url": "https://www.meutimao.com.br/videos-do-corinthians/128884/marcelo-paz-reclama-a-arbitragem-e-cita-penalti-nao-marcado-em-internacional-2x0-corinthians",
-      "time": "03/08 · 02:55",
-      "pubDate": "Sun, 02 Aug 2026 23:55:00 -0300"
-    },
-    {
-      "source": "Meu Timao",
-      "color": "#1a7a3a",
-      "title": "VÍDEO: G. Paulista critica arbitragem, falha no gol e fala sobre virada pós Internacional 2x0 Corinthians",
-      "url": "https://www.meutimao.com.br/videos-do-corinthians/128882/g-paulista-critica-arbitragem-falha-no-gol-e-fala-sobre-virada-pos-internacional-2x0-corinthians",
-      "time": "03/08 · 02:44",
-      "pubDate": "Sun, 02 Aug 2026 23:44:00 -0300"
-    },
-    {
       "source": "Gazeta Esportiva",
       "color": "#E5820A",
       "title": "Diniz defende substituições e atribui derrota do Corinthians à desorganização",
       "url": "https://www.gazetaesportiva.com/times/corinthians/diniz-defende-substituicoes-e-atribui-derrota-do-corinthians-a-desorganizacao/",
       "time": "03/08 · 02:42",
       "pubDate": "Mon, 03 Aug 2026 02:42:00 +0000"
-    },
-    {
-      "source": "Meu Timao",
-      "color": "#1a7a3a",
-      "title": "Executivo do Corinthians volta a cobrar arbitragem e cita 'ameaça' para jogadores",
-      "url": "https://www.meutimao.com.br/noticias-do-corinthians/536989/executivo-do-corinthians-volta-a-cobrar-arbitragem-e-cita-ameaca-para-jogadores",
-      "time": "03/08 · 02:15",
-      "pubDate": "Sun, 02 Aug 2026 23:15:00 -0300"
-    },
-    {
-      "source": "Meu Timao",
-      "color": "#1a7a3a",
-      "title": "Corinthians publica vídeos com lances polêmicos contra o Internacional em crítica à arbitragem",
-      "url": "https://www.meutimao.com.br/noticias-do-corinthians/536993/corinthians-publica-videos-com-lances-polemicos-contra-o-internacional-em-critica-a-arbitragem",
-      "time": "03/08 · 02:09",
-      "pubDate": "Sun, 02 Aug 2026 23:09:00 -0300"
     },
     {
       "source": "Gazeta Esportiva",
@@ -123,12 +131,36 @@ const LIVE_DATA = {
       "pubDate": "Mon, 03 Aug 2026 02:07:56 +0000"
     },
     {
+      "source": "Bolavip",
+      "color": "#0055A5",
+      "title": "Hugo Souza falha, Corinthians perde para o Internacional e se complica na Copa do Brasil",
+      "url": "<![CDATA[https://br.bolavip.com/corinthians/hugo-souza-falha-corinthians-perde-para-o-internacional-e-se-complica-na-copa-do-brasil]]>",
+      "time": "03/08 · 01:10",
+      "pubDate": "Sun, 02 Aug 2026 22:10:35 -0300"
+    },
+    {
+      "source": "Bolavip",
+      "color": "#0055A5",
+      "title": "Diniz não supera estratégia do Internacional e Corinthians se complica com derrota na Copa do Brasil",
+      "url": "<![CDATA[https://br.bolavip.com/corinthians/diniz-nao-supera-estrategia-do-internacional-e-corinthians-se-complica-com-derrota-na-copa-do-brasil]]>",
+      "time": "03/08 · 01:04",
+      "pubDate": "Sun, 02 Aug 2026 22:04:17 -0300"
+    },
+    {
       "source": "Gazeta Esportiva",
       "color": "#E5820A",
       "title": "Garro cobra respeito da arbitragem e acredita em reação do Corinthians",
       "url": "https://www.gazetaesportiva.com/times/corinthians/garro-cobra-respeito-da-arbitragem-e-acredita-em-reacao-do-corinthians/",
       "time": "03/08 · 00:51",
       "pubDate": "Mon, 03 Aug 2026 00:51:24 +0000"
+    },
+    {
+      "source": "Bolavip",
+      "color": "#0055A5",
+      "title": "Corinthians sofre apagão na etapa final, perde para o Inter e se complica na Copa do Brasil",
+      "url": "<![CDATA[https://br.bolavip.com/corinthians/corinthians-sofre-apagao-na-etapa-final-perde-para-o-inter-e-se-complica-na-copa-do-brasil]]>",
+      "time": "03/08 · 00:30",
+      "pubDate": "Sun, 02 Aug 2026 21:30:53 -0300"
     },
     {
       "source": "Bolavip",
@@ -161,44 +193,18 @@ const LIVE_DATA = {
       "url": "<![CDATA[https://br.bolavip.com/corinthians/transfer-ban-nao-impede-renovacao-de-memphis-no-corinthians-entenda]]>",
       "time": "02/08 · 11:41",
       "pubDate": "Sun, 02 Aug 2026 08:41:39 -0300"
-    },
-    {
-      "source": "Bolavip",
-      "color": "#0055A5",
-      "title": "Gramado da Neo Química Arena vira alvo de críticas internas no Corinthians",
-      "url": "<![CDATA[https://br.bolavip.com/corinthians/gramado-da-neo-quimica-arena-vira-alvo-de-criticas-internas-no-corinthians]]>",
-      "time": "02/08 · 11:09",
-      "pubDate": "Sun, 02 Aug 2026 08:09:34 -0300"
-    },
-    {
-      "source": "Bolavip",
-      "color": "#0055A5",
-      "title": "Corinthians teme ausência de Yuri Alberto na Libertadores após lesão",
-      "url": "<![CDATA[https://br.bolavip.com/corinthians/corinthians-teme-ausencia-de-yuri-alberto-na-libertadores-apos-lesao]]>",
-      "time": "01/08 · 12:31",
-      "pubDate": "Sat, 01 Aug 2026 09:31:33 -0300"
-    },
-    {
-      "source": "Bolavip",
-      "color": "#0055A5",
-      "title": "Garro se revolta com gramado, bate boca com dirigentes e clima ferve no vestiário do Corinthians",
-      "url": "<![CDATA[https://br.bolavip.com/corinthians/garro-se-revolta-com-gramado-bate-boca-com-dirigentes-e-clima-ferve-no-vestiario-do-corinthians]]>",
-      "time": "31/07 · 21:14",
-      "pubDate": "Fri, 31 Jul 2026 18:14:38 -0300"
-    },
-    {
-      "source": "Bolavip",
-      "color": "#0055A5",
-      "title": "Zakaria Labyad rompe ligamento cruzado, tem lesão no menisco e não joga mais pelo Corinthians em 2026",
-      "url": "<![CDATA[https://br.bolavip.com/corinthians/zakaria-labyad-rompe-ligamento-cruzado-e-do-menisco-e-esta-fora-da-temporada-pelo-corinthians-em-2026]]>",
-      "time": "31/07 · 19:40",
-      "pubDate": "Fri, 31 Jul 2026 16:40:40 -0300"
     }
   ],
   "upcoming": [],
   "results": [],
   "standings": [],
   "highlights": [
+    {
+      "title": "Saiba onde assistir à partida entre Corinthians e Flamengo pelo Brasileiro Feminino",
+      "url": "https://www.meutimao.com.br/noticias-do-corinthians/536949/saiba-onde-assistir-a-partida-entre-corinthians-e-flamengo-pelo-brasileiro-feminino",
+      "pubDate": "Mon, 03 Aug 2026 08:30:00 -0300",
+      "date": "03/08 · 11:30"
+    },
     {
       "title": "VÍDEO: Análise do empate do Corinthians, situação de Yuri Alberto e mais: coletiva de Fernando Diniz",
       "url": "https://www.meutimao.com.br/videos-do-corinthians/128888/analise-do-empate-do-corinthians-situacao-de-yuri-alberto-e-mais-coletiva-de-fernando-diniz",
@@ -240,12 +246,6 @@ const LIVE_DATA = {
       "url": "https://www.meutimao.com.br/noticias-do-corinthians/536969/corinthians-iguala-pior-seca-de-gols-da-temporada-em-derrota-na-copa-do-brasil",
       "pubDate": "Sun, 02 Aug 2026 22:11:00 -0300",
       "date": "03/08 · 01:11"
-    },
-    {
-      "title": "VÍDEO: Melhores momentos: Internacional 2x0 Corinthians | Oitavas de Final (ida) | Copa do Brasil 2026",
-      "url": "https://www.meutimao.com.br/videos-do-corinthians/128877/melhores-momentos-internacional-2x0-corinthians-oitavas-de-final-ida-copa-do-brasil-2026",
-      "pubDate": "Sun, 02 Aug 2026 21:49:00 -0300",
-      "date": "03/08 · 00:49"
     }
   ]
 };
