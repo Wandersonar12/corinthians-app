37b008ee941d968c6bd7cbbeb69e7a6afbe38984
GitHub Action <action@github.com>
2026-08-04 13:29:38 +0000

    Dados atualizados

--- src/data.js
+++ src/data.js
@@ -1,7 +1,63 @@
-// Gerado automaticamente em 04/08/2026, 10:23:39
+// Gerado automaticamente em 04/08/2026, 13:29:38
 const LIVE_DATA = {
-  "updatedAt": "2026-08-04T10:23:39.955Z",
+  "updatedAt": "2026-08-04T13:29:38.885Z",
   "news": [
+    {
+      "source": "Bolavip",
+      "color": "#0055A5",
+      "title": "Corinthians chega a 12 jogos de invencibilidade contra Flamengo no futebol feminino",
+      "url": "<![CDATA[https://br.bolavip.com/corinthians/corinthians-chega-a-12-jogos-de-invencibilidade-contra-flamengo-no-futebol-feminino]]>",
+      "time": "04/08 · 13:03",
+      "pubDate": "Tue, 04 Aug 2026 10:03:48 -0300"
+    },
+    {
+      "source": "Bolavip",
+      "color": "#0055A5",
+      "title": "Memphis Depay deve chegar ao Brasil nesta semana, mas estreia pelo Corinthians ainda depende de etapas decisivas",
+      "url": "<![CDATA[https://br.bolavip.com/corinthians/memphis-depay-deve-chegar-ao-brasil-nesta-semana-mas-estreia-pelo-corinthians-ainda-depende-de-etapas-decisivas]]>",
+      "time": "04/08 · 13:01",
+      "pubDate": "Tue, 04 Aug 2026 10:01:45 -0300"
+    },
+    {
+      "source": "Meu Timao",
+      "color": "#1a7a3a",
+      "title": "Corinthians chega a 12 jogos de invencibilidade contra o Flamengo após vitória no Brasileirão",
+      "url": "https://www.meutimao.com.br/noticias-do-corinthians/537073/corinthians-chega-a-12-jogos-de-invencibilidade-contra-o-flamengo-apos-vitoria-no-brasileirao",
+      "time": "04/08 · 12:29",
+      "pubDate": "Tue, 04 Aug 2026 09:29:00 -0300"
+    },
+    {
+      "source": "Bolavip",
+      "color": "#0055A5",
+      "title": "Corinthians avalia reclamação formal após nova polêmica com arbitragem contra o Internacional",
+      "url": "<![CDATA[https://br.bolavip.com/corinthians/corinthians-avalia-reclamacao-formal-apos-nova-polemica-com-arbitragem-contra-o-internacional]]>",
+      "time": "04/08 · 12:10",
+      "pubDate": "Tue, 04 Aug 2026 09:10:57 -0300"
+    },
+    {
+      "source": "Meu Timao",
+      "color": "#1a7a3a",
+      "title": "Zagueiro projeta noite de reação do Corinthians na Neo Química Arena por vaga na Copa do Brasil",
+      "url": "https://www.meutimao.com.br/noticias-do-corinthians/537018/zagueiro-projeta-noite-de-reacao-do-corinthians-na-neo-quimica-arena-por-vaga-na-copa-do-brasil",
+      "time": "04/08 · 11:33",
+      "pubDate": "Tue, 04 Aug 2026 08:33:00 -0300"
+    },
+    {
+      "source": "Bolavip",
+      "color": "#0055A5",
+      "title": "Corinthians muda discurso e cria impasse para renovar com Memphis Depay",
+      "url": "<![CDATA[https://br.bolavip.com/corinthians/corinthians-muda-discurso-e-cria-impasse-para-renovar-com-memphis-depay]]>",
+      "time": "04/08 · 11:30",
+      "pubDate": "Tue, 04 Aug 2026 08:30:51 -0300"
+    },
+    {
+      "source": "Meu Timao",
+      "color": "#1a7a3a",
+      "title": "Últimas do Corinthians: voo holandês, críticas de presidente e retorno cauteloso",
+      "url": "https://www.meutimao.com.br/noticias-do-corinthians/537076/ultimas-do-corinthians-voo-holandes-criticas-de-presidente-e-retorno-cauteloso",
+      "time": "04/08 · 10:30",
+      "pubDate": "Tue, 04 Aug 2026 07:30:00 -0300"
+    },
     {
       "source": "Meu Timao",
       "color": "#1a7a3a",
@@ -27,6 +83,14 @@ const LIVE_DATA = {
       "pubDate": "Tue, 04 Aug 2026 02:35:22 +0000"
     },
     {
+      "source": "Bolavip",
+      "color": "#0055A5",
+      "title": "Vídeo: Comentarista pede Memphis Depay contra o Inter e defende força-tarefa no Corinthians",
+      "url": "<![CDATA[https://br.bolavip.com/corinthians/video-comentarista-pede-memphis-depay-contra-o-inter-e-defende-forca-tarefa-no-corinthians]]>",
+      "time": "04/08 · 02:05",
+      "pubDate": "Mon, 03 Aug 2026 23:05:51 -0300"
+    },
+    {
       "source": "Meu Timao",
       "color": "#1a7a3a",
       "title": "Dupla de conselheiros é expulsa do quadro associativo do Corinthians",
@@ -41,6 +105,22 @@ const LIVE_DATA = {
       "url": "https://www.meutimao.com.br/noticias-do-corinthians/537022/neto-critica-diretoria-por-ausencia-de-memphis-em-derrota-do-corinthians-para-o-internacional",
       "time": "04/08 · 01:30",
       "pubDate": "Mon, 03 Aug 2026 22:30:00 -0300"
+    },
+    {
+      "source": "Bolavip",
+      "color": "#0055A5",
+      "title": "Vídeo: influenciador vê o Corinthians em condição de virar contra o Internacional, mas cobra melhora",
+      "url": "<![CDATA[https://br.bolavip.com/corinthians/influenciador-corinthians-virada-internacional]]>",
+      "time": "04/08 · 01:29",
+      "pubDate": "Mon, 03 Aug 2026 22:29:45 -0300"
+    },
+    {
+      "source": "Bolavip",
+      "color": "#0055A5",
+      "title": "Memphis Depay impõe nova exigência e renovação com o Corinthians segue indefinida",
+      "url": "<![CDATA[https://br.bolavip.com/corinthians/memphis-depay-impoe-nova-exigencia-e-renovacao-com-o-corinthians-segue-indefinida]]>",
+      "time": "04/08 · 01:14",
+      "pubDate": "Mon, 03 Aug 2026 22:14:28 -0300"
     },
     {
       "source": "Meu Timao",
@@ -59,22 +139,6 @@ const LIVE_DATA = {
       "pubDate": "Mon, 03 Aug 2026 21:08:00 -0300"
     },
     {
-      "source": "Meu Timao",
-      "color": "#1a7a3a",
-      "title": "Corinthians mantém vantagem sobre rivais e garante mando de campo nas quartas do Brasileiro Feminino",
-      "url": "https://www.meutimao.com.br/noticias-do-corinthians/537069/corinthians-mantem-vantagem-sobre-rivais-e-garante-mando-de-campo-nas-quartas-do-brasileiro-feminino",
-      "time": "04/08 · 00:03",
-      "pubDate": "Mon, 03 Aug 2026 21:03:00 -0300"
-    },
-    {
-      "source": "Meu Timao",
-      "color": "#1a7a3a",
-      "title": "Meia da base do Corinthians é anunciada por escola de formação dos Estados Unidos",
-      "url": "https://www.meutimao.com.br/noticias-do-corinthians/537054/meia-da-base-do-corinthians-e-anunciada-por-escola-de-formacao-dos-estados-unidos",
-      "time": "04/08 · 00:02",
-      "pubDate": "Mon, 03 Aug 2026 21:02:00 -0300"
-    },
-    {
       "source": "Gazeta Esportiva",
       "color": "#E5820A",
       "title": "Corinthians vence Flamengo com uma a menos e retoma liderança do Brasileiro feminino",
@@ -83,12 +147,12 @@ const LIVE_DATA = {
       "pubDate": "Mon, 03 Aug 2026 23:49:00 +0000"
     },
     {
-      "source": "Meu Timao",
-      "color": "#1a7a3a",
-      "title": "Zakaria Labyad fala em 'dias difíceis' e lamenta lesão que o tirou da temporada do Corinthians; veja",
-      "url": "https://www.meutimao.com.br/noticias-do-corinthians/537067/zakaria-labyad-fala-em-dias-dificeis-e-lamenta-lesao-que-o-tirou-da-temporada-do-corinthians-veja",
-      "time": "03/08 · 23:46",
-      "pubDate": "Mon, 03 Aug 2026 20:46:00 -0300"
+      "source": "Bolavip",
+      "color": "#0055A5",
+      "title": "Opinião: Garro precisa voltar a ser protagonista no Corinthians e tem chance de ouro",
+      "url": "<![CDATA[https://br.bolavip.com/corinthians/opiniao-garro-precisa-voltar-a-ser-protagonista-no-corinthians-e-tem-chance-de-ouro]]>",
+      "time": "03/08 · 23:08",
+      "pubDate": "Mon, 03 Aug 2026 20:08:24 -0300"
     },
     {
       "source": "Gazeta Esportiva",
@@ -107,30 +171,6 @@ const LIVE_DATA = {
       "pubDate": "Mon, 03 Aug 2026 21:51:04 +0000"
     },
     {
-      "source": "Bolavip",
-      "color": "#0055A5",
-      "title": "Corinthians conta com Memphis Depay contra o Internacional e holandês é aguardado nesta terça",
-      "url": "<![CDATA[https://br.bolavip.com/corinthians/corinthians-conta-com-memphis-depay-contra-o-internacional-e-holandes-e-aguardado-nesta-terca]]>",
-      "time": "03/08 · 21:04",
-      "pubDate": "Mon, 03 Aug 2026 18:04:39 -0300"
-    },
-    {
-      "source": "Bolavip",
-      "color": "#0055A5",
-      "title": "Corinthians pede áudio à CBF após intimidação de árbitro a Matheuzinho: “Quero ver você fazer isso lá fora”",
-      "url": "<![CDATA[https://br.bolavip.com/corinthians/corinthians-pede-audio-a-cbf-apos-intimidacao-de-arbitro-a-matheuzinho-quero-ver-voce-fazer-isso-la-fora]]>",
-      "time": "03/08 · 21:03",
-      "pubDate": "Mon, 03 Aug 2026 18:03:12 -0300"
-    },
-    {
-      "source": "Bolavip",
-      "color": "#0055A5",
-      "title": "Opinião: Corinthians se apega à arbitragem e ignora o verdadeiro motivo da derrota para o Inter",
-      "url": "<![CDATA[https://br.bolavip.com/corinthians/opiniao-corinthians-se-apega-a-arbitragem-e-ignora-o-verdadeiro-motivo-da-derrota-para-o-inter]]>",
-      "time": "03/08 · 20:54",
-      "pubDate": "Mon, 03 Aug 2026 17:54:30 -0300"
-    },
-    {
       "source": "Gazeta Esportiva",
       "color": "#E5820A",
       "title": "Presidente do Inter vê pressão do Corinthians sobre a arbitragem: &#8220;Não aceitaremos&#8221;",
@@ -147,52 +187,12 @@ const LIVE_DATA = {
       "pubDate": "Mon, 03 Aug 2026 20:20:30 +0000"
     },
     {
-      "source": "Bolavip",
-      "color": "#0055A5",
-      "title": "Opinião: Problemas financeiros e erros na gestão de Osmar Stabile prejudicam resultados do Corinthians",
-      "url": "<![CDATA[https://br.bolavip.com/corinthians/opiniao-problemas-financeiros-e-erros-na-gestao-de-osmar-stabile-prejudicam-resultados-do-corinthians]]>",
-      "time": "03/08 · 17:45",
-      "pubDate": "Mon, 03 Aug 2026 14:45:09 -0300"
-    },
-    {
-      "source": "Bolavip",
-      "color": "#0055A5",
-      "title": "Presidente do Cuiabá ironiza renovação de Memphis e pressiona CNRD contra o Corinthians",
-      "url": "<![CDATA[https://br.bolavip.com/corinthians/presidente-do-cuiaba-ironiza-renovacao-de-memphis-e-pressiona-cnrd-contra-o-corinthians]]>",
-      "time": "03/08 · 17:39",
-      "pubDate": "Mon, 03 Aug 2026 14:39:12 -0300"
-    },
-    {
-      "source": "Bolavip",
-      "color": "#0055A5",
-      "title": "Opinião: O Corinthians não pode sonhar alto sem um reserva confiável para o ataque",
-      "url": "<![CDATA[https://br.bolavip.com/corinthians/opiniao-o-corinthians-nao-pode-sonhar-alto-sem-um-reserva-confiavel-para-o-ataque]]>",
-      "time": "03/08 · 16:35",
-      "pubDate": "Mon, 03 Aug 2026 13:35:01 -0300"
-    },
-    {
-      "source": "Bolavip",
-      "color": "#0055A5",
-      "title": "Marcelo Paz denuncia intimidação da arbitragem o Corinthians: “Causa estranheza”",
-      "url": "<![CDATA[https://br.bolavip.com/corinthians/marcelo-paz-denuncia-intimidacao-da-arbitragem-o-corinthians-causa-estranheza]]>",
-      "time": "03/08 · 15:39",
-      "pubDate": "Mon, 03 Aug 2026 12:39:45 -0300"
-    },
-    {
       "source": "Gazeta Esportiva",
       "color": "#E5820A",
       "title": "Pezzolano analisa &#8216;virada de chave&#8217; do Inter após bater o Corinthians",
       "url": "https://www.gazetaesportiva.com/todas-as-noticias/pezzolano-analisa-virada-de-chave-do-inter-apos-bater-o-corinthians/",
       "time": "03/08 · 14:19",
       "pubDate": "Mon, 03 Aug 2026 14:19:25 +0000"
-    },
-    {
-      "source": "Bolavip",
-      "color": "#0055A5",
-      "title": "Memphis dá demonstração de compromisso, e Corinthians acerta ao apostar na renovação",
-      "url": "<![CDATA[https://br.bolavip.com/corinthians/memphis-da-demonstracao-de-compromisso-e-corinthians-acerta-ao-apostar-na-renovacao]]>",
-      "time": "03/08 · 12:05",
-      "pubDate": "Mon, 03 Aug 2026 09:05:36 -0300"
     }
   ],
   "upcoming": [],
