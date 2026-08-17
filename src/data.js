748744760413c66062cb531fa647e6fdcaa97ee9
GitHub Action <action@github.com>
2026-08-17 08:27:34 +0000

    Dados atualizados

--- src/data.js
+++ src/data.js
@@ -1,78 +1,142 @@
-// Gerado automaticamente em 16/08/2026, 22:07:22
+// Gerado automaticamente em 17/08/2026, 08:27:34
 const LIVE_DATA = {
-  "updatedAt": "2026-08-16T22:07:22.152Z",
+  "updatedAt": "2026-08-17T08:27:34.682Z",
   "news": [
     {
-      "source": "Meu Timao",
-      "color": "#1a7a3a",
-      "title": "Zagueiro apresenta quadro de labirintite e desfalca o Corinthians contra o Cruzeiro",
-      "url": "https://www.meutimao.com.br/noticias-do-corinthians/538032/zagueiro-apresenta-quadro-de-labirintite-e-desfalca-o-corinthians-contra-o-cruzeiro",
-      "time": "16/08 · 21:39",
-      "pubDate": "Sun, 16 Aug 2026 18:39:00 -0300"
+      "source": "Gazeta Esportiva",
+      "color": "#E5820A",
+      "title": "Atuações do Corinthians: Matheuzinho tem erros decisivos, e Gustavo Henrique se destaca com golaço",
+      "url": "https://www.gazetaesportiva.com/times/corinthians/atuacoes-do-corinthians-matheuzinho-tem-erros-decisivos-e-gustavo-henrique-se-destaca-com-golaco/",
+      "time": "17/08 · 08:00",
+      "pubDate": "Mon, 17 Aug 2026 08:00:32 +0000"
     },
     {
       "source": "Gazeta Esportiva",
       "color": "#E5820A",
-      "title": "Diniz escala Corinthians com mudança no meio para enfrentar o Cruzeiro; veja escalações",
-      "url": "https://www.gazetaesportiva.com/times/corinthians/escalacoes-corinthians-cruzeiro-brasileirao-23-rodada-16-08-2026/",
-      "time": "16/08 · 21:38",
-      "pubDate": "Sun, 16 Aug 2026 21:38:29 +0000"
+      "title": "Diniz justifica titulares contra o Cruzeiro e vê Corinthians preparado para decisão na Libertadores",
+      "url": "https://www.gazetaesportiva.com/times/corinthians/diniz-justifica-titulares-contra-o-cruzeiro-e-ve-corinthians-preparado-para-decisao-na-libertadores/",
+      "time": "17/08 · 03:20",
+      "pubDate": "Mon, 17 Aug 2026 03:20:22 +0000"
     },
     {
       "source": "Meu Timao",
       "color": "#1a7a3a",
-      "title": "Corinthians é escalado com força máxima para encarar o Cruzeiro pelo Brasileirão",
-      "url": "https://www.meutimao.com.br/noticias-do-corinthians/538016/corinthians-e-escalado-com-forca-maxima-para-encarar-o-cruzeiro-pelo-brasileirao",
-      "time": "16/08 · 21:30",
-      "pubDate": "Sun, 16 Aug 2026 18:30:00 -0300"
+      "title": "Lateral comenta possível retorno de Memphis e destaca importância do atacante ao Corinthians",
+      "url": "https://www.meutimao.com.br/noticias-do-corinthians/538094/lateral-comenta-possivel-retorno-de-memphis-e-destaca-importancia-do-atacante-ao-corinthians",
+      "time": "17/08 · 02:57",
+      "pubDate": "Sun, 16 Aug 2026 23:57:00 -0300"
+    },
+    {
+      "source": "Gazeta Esportiva",
+      "color": "#E5820A",
+      "title": "Corinthians reabre negociações para renovar com Memphis; veja detalhes",
+      "url": "https://www.gazetaesportiva.com/times/corinthians/reviravolta-corinthians-reabre-negociacoes-para-renovar-com-memphis-veja-detalhes/",
+      "time": "17/08 · 02:55",
+      "pubDate": "Mon, 17 Aug 2026 02:55:03 +0000"
     },
     {
       "source": "Meu Timao",
       "color": "#1a7a3a",
-      "title": "Cori marca reunião para tratar de renovação de Memphis Depay no Corinthians",
-      "url": "https://www.meutimao.com.br/noticias-do-corinthians/538027/cori-marca-reuniao-para-tratar-de-renovacao-de-memphis-depay-no-corinthians",
-      "time": "16/08 · 20:56",
-      "pubDate": "Sun, 16 Aug 2026 17:56:00 -0300"
+      "title": "Técnico do Corinthians lamenta falhas contra o Cruzeiro e justifica preservação de titulares",
+      "url": "https://www.meutimao.com.br/noticias-do-corinthians/538081/tecnico-do-corinthians-lamenta-falhas-contra-o-cruzeiro-e-justifica-preservacao-de-titulares",
+      "time": "17/08 · 02:50",
+      "pubDate": "Sun, 16 Aug 2026 23:50:00 -0300"
     },
     {
       "source": "Meu Timao",
       "color": "#1a7a3a",
-      "title": "Duelo direto e reencontros: veja como chega o Cruzeiro para enfrentar o Corinthians no Brasileirão",
-      "url": "https://www.meutimao.com.br/noticias-do-corinthians/538017/duelo-direto-e-reencontros-veja-como-chega-o-cruzeiro-para-enfrentar-o-corinthians-no-brasileirao",
-      "time": "16/08 · 20:33",
-      "pubDate": "Sun, 16 Aug 2026 17:33:00 -0300"
+      "title": "Técnico do Corinthians não garante retorno próximo de Yuri Alberto e detalha situação de zagueiro",
+      "url": "https://www.meutimao.com.br/noticias-do-corinthians/538082/tecnico-do-corinthians-nao-garante-retorno-proximo-de-yuri-alberto-e-detalha-situacao-de-zagueiro",
+      "time": "17/08 · 02:45",
+      "pubDate": "Sun, 16 Aug 2026 23:45:00 -0300"
     },
     {
       "source": "Meu Timao",
       "color": "#1a7a3a",
-      "title": "Corinthians perde novamente para a Ferroviária e se despede do Brasileirão Feminino Sub-17",
-      "url": "https://www.meutimao.com.br/noticias-do-corinthians/538026/corinthians-perde-novamente-para-a-ferroviaria-e-se-despede-do-brasileirao-feminino-sub-17",
-      "time": "16/08 · 20:02",
-      "pubDate": "Sun, 16 Aug 2026 17:02:00 -0300"
+      "title": "VÍDEO: Fagner fala que diretoria pediu sua saída do Corinthians e pede transparência para o clube",
+      "url": "https://www.meutimao.com.br/videos-do-corinthians/129145/fagner-fala-que-diretoria-pediu-sua-saida-do-corinthians-e-pede-trabsparencia-por-parte-do-clube",
+      "time": "17/08 · 02:29",
+      "pubDate": "Sun, 16 Aug 2026 23:29:32 -0300"
     },
     {
       "source": "Meu Timao",
       "color": "#1a7a3a",
-      "title": "Corinthians busca ampliar retrospecto positivo contra o Cruzeiro; veja os números",
-      "url": "https://www.meutimao.com.br/noticias-do-corinthians/538014/corinthians-busca-ampliar-retrospecto-positivo-contra-o-cruzeiro-veja-os-numeros",
-      "time": "16/08 · 19:34",
-      "pubDate": "Sun, 16 Aug 2026 16:34:00 -0300"
+      "title": "VÍDEO: Derrota em casa, lesão de Gabriel Paulsita e mais: coletiva de Fernando Diniz no Corinthians",
+      "url": "https://www.meutimao.com.br/videos-do-corinthians/129143/derrota-em-casa-lesao-de-gabriel-paulsita-e-mais-coletiva-de-fernando-diniz-no-corinthians",
+      "time": "17/08 · 02:25",
+      "pubDate": "Sun, 16 Aug 2026 23:25:42 -0300"
     },
     {
       "source": "Meu Timao",
       "color": "#1a7a3a",
-      "title": "Corinthians caótico e Cruzeiro em alta: o que aconteceu desde a semifinal da Copa do Brasil",
-      "url": "https://www.meutimao.com.br/noticias-do-corinthians/537968/corinthians-caotico-e-cruzeiro-em-alta-o-que-aconteceu-desde-a-semifinal-da-copa-do-brasil",
-      "time": "16/08 · 18:32",
-      "pubDate": "Sun, 16 Aug 2026 15:32:00 -0300"
+      "title": "Fagner diz que foi dispensado do Corinthians por telefone e expõe bastidores: 'Torcida merece saber'",
+      "url": "https://www.meutimao.com.br/noticias-do-corinthians/538075/fagner-diz-que-foi-dispensado-do-corinthians-por-telefone-e-expoe-bastidores-torcida-merece-saber",
+      "time": "17/08 · 02:14",
+      "pubDate": "Sun, 16 Aug 2026 23:14:00 -0300"
     },
     {
       "source": "Meu Timao",
       "color": "#1a7a3a",
-      "title": "Corinthians enfrenta o Cruzeiro em confronto direto por vaga no G5 do Brasileirão; veja tabela",
-      "url": "https://www.meutimao.com.br/noticias-do-corinthians/538015/corinthians-enfrenta-o-cruzeiro-em-confronto-direto-por-vaga-no-g5-do-brasileirao-veja-tabela",
-      "time": "16/08 · 17:34",
-      "pubDate": "Sun, 16 Aug 2026 14:34:00 -0300"
+      "title": "Fiel 'salva' zagueiro e distribui notas baixas ao elenco do Corinthians em derrota no Brasileirão",
+      "url": "https://www.meutimao.com.br/noticias-do-corinthians/538076/fiel-salva-zagueiro-e-distribui-notas-baixas-ao-elenco-do-corinthians-em-derrota-no-brasileirao",
+      "time": "17/08 · 02:07",
+      "pubDate": "Sun, 16 Aug 2026 23:07:00 -0300"
+    },
+    {
+      "source": "Meu Timao",
+      "color": "#1a7a3a",
+      "title": "VÍDEO: Derrota amarga e preparação contra o Rosario: Zons mista Matheus Bidu | Corinthians 1X2 Cruzeiro",
+      "url": "https://www.meutimao.com.br/videos-do-corinthians/129141/derrota-amarga-e-preparacao-contra-o-rosario-zons-mista-matheus-bidu-corinthians-1x2-cruzeiro",
+      "time": "17/08 · 02:02",
+      "pubDate": "Sun, 16 Aug 2026 23:02:00 -0300"
+    },
+    {
+      "source": "Gazeta Esportiva",
+      "color": "#E5820A",
+      "title": "Torcida do Corinthians manda recado ao elenco antes de decisão na Libertadores: &#8220;É guerra&#8221;",
+      "url": "https://www.gazetaesportiva.com/times/corinthians/torcida-do-corinthians-manda-recado-ao-elenco-antes-de-decisao-na-libertadores-e-guerra/",
+      "time": "17/08 · 01:39",
+      "pubDate": "Mon, 17 Aug 2026 01:39:57 +0000"
+    },
+    {
+      "source": "Gazeta Esportiva",
+      "color": "#E5820A",
+      "title": "Garro lamenta derrota do Corinthians no Brasileiro e projeta decisão na Libertadores: &#8220;Jogo do ano&#8221;",
+      "url": "https://www.gazetaesportiva.com/times/corinthians/garro-lamenta-derrota-do-corinthians-no-brasileiro-e-projeta-decisao-na-libertadores-jogo-do-ano/",
+      "time": "17/08 · 01:07",
+      "pubDate": "Mon, 17 Aug 2026 01:07:15 +0000"
+    },
+    {
+      "source": "Gazeta Esportiva",
+      "color": "#E5820A",
+      "title": "Rodrigo Garro toma o terceiro cartão amarelo e vira desfalque no Corinthians contra o Coritiba",
+      "url": "https://www.gazetaesportiva.com/times/corinthians/rodrigo-garro-toma-o-terceiro-cartao-amarelo-e-vira-desfalque-no-corinthians-contra-o-coritiba/",
+      "time": "17/08 · 00:49",
+      "pubDate": "Mon, 17 Aug 2026 00:49:15 +0000"
+    },
+    {
+      "source": "Gazeta Esportiva",
+      "color": "#E5820A",
+      "title": "Corinthians perde do Cruzeiro e desperdiça chance de entrar no G5 do Brasileirão",
+      "url": "https://www.gazetaesportiva.com/times/corinthians/corinthians-x-cruzeiro-acompanhe-o-tempo-real-da-partida-da-23a-rodada-do-brasileirao/",
+      "time": "17/08 · 00:28",
+      "pubDate": "Mon, 17 Aug 2026 00:28:02 +0000"
+    },
+    {
+      "source": "Gazeta Esportiva",
+      "color": "#E5820A",
+      "title": "Gabriel Paulista deixa jogo contra o Cruzeiro com dores e vira preocupação no Corinthians",
+      "url": "https://www.gazetaesportiva.com/times/corinthians/gabriel-paulista-corinthians-cruzeiro-preocupacao/",
+      "time": "17/08 · 00:14",
+      "pubDate": "Mon, 17 Aug 2026 00:14:20 +0000"
+    },
+    {
+      "source": "Bolavip",
+      "color": "#0055A5",
+      "title": "Opinião: Diniz acerta ao mesclar titulares para priorizar decisão do Corinthians na Libertadores",
+      "url": "<![CDATA[https://br.bolavip.com/corinthians/opiniao-diniz-acerta-ao-mesclar-titulares-para-priorizar-decisao-do-corinthians-na-libertadores]]>",
+      "time": "16/08 · 19:20",
+      "pubDate": "Sun, 16 Aug 2026 16:20:47 -0300"
     },
     {
       "source": "Bolavip",
@@ -91,44 +155,12 @@ const LIVE_DATA = {
       "pubDate": "Sun, 16 Aug 2026 13:32:07 -0300"
     },
     {
-      "source": "Gazeta Esportiva",
-      "color": "#E5820A",
-      "title": "Confira como foi o último treino do Corinthians para o duelo contra o Cruzeiro",
-      "url": "https://www.gazetaesportiva.com/times/corinthians/confira-como-foi-o-ultimo-treino-do-corinthians-para-duelo-o-contra-o-cruzeiro/",
-      "time": "16/08 · 15:11",
-      "pubDate": "Sun, 16 Aug 2026 15:11:45 +0000"
-    },
-    {
       "source": "Bolavip",
       "color": "#0055A5",
       "title": "Diniz prepara Corinthians alternativo antes de decisão na Libertadores contra o Rosario",
       "url": "<![CDATA[https://br.bolavip.com/corinthians/diniz-prepara-corinthians-alternativo-antes-de-decisao-na-libertadores-contra-o-rosario]]>",
       "time": "16/08 · 13:04",
       "pubDate": "Sun, 16 Aug 2026 10:04:01 -0300"
-    },
-    {
-      "source": "Gazeta Esportiva",
-      "color": "#E5820A",
-      "title": "Corinthians aposta em retrospecto na Arena para vencer o Cruzeiro",
-      "url": "https://www.gazetaesportiva.com/times/corinthians/corinthians-aposta-em-retrospecto-na-arena-para-vencer-o-cruzeiro/",
-      "time": "16/08 · 10:00",
-      "pubDate": "Sun, 16 Aug 2026 10:00:10 +0000"
-    },
-    {
-      "source": "Gazeta Esportiva",
-      "color": "#E5820A",
-      "title": "Corinthians x Cruzeiro pelo Campeonato Brasileiro: veja prováveis escalações e onde assistir",
-      "url": "https://www.gazetaesportiva.com/todas-as-noticias/corinthians-x-cruzeiro-pelo-campeonato-brasileiro-veja-provaveis-escalacoes-e-onde-assistir/",
-      "time": "15/08 · 23:00",
-      "pubDate": "Sat, 15 Aug 2026 23:00:56 +0000"
-    },
-    {
-      "source": "Gazeta Esportiva",
-      "color": "#E5820A",
-      "title": "Corinthians sai na frente, leva empate mas segue no topo do Brasileirão Feminino",
-      "url": "https://www.gazetaesportiva.com/times/corinthians/corinthians-empata-com-bahia-e-segue-na-lideranca/",
-      "time": "15/08 · 22:40",
-      "pubDate": "Sat, 15 Aug 2026 22:40:00 +0000"
     },
     {
       "source": "Bolavip",
@@ -161,38 +193,6 @@ const LIVE_DATA = {
       "url": "<![CDATA[https://br.bolavip.com/corinthians/pedro-raul-e-desfalque-do-corinthians-contra-o-cruzeiro-apos-trauma-na-costela]]>",
       "time": "15/08 · 18:10",
       "pubDate": "Sat, 15 Aug 2026 15:10:51 -0300"
-    },
-    {
-      "source": "Gazeta Esportiva",
-      "color": "#E5820A",
-      "title": "Ex-Corinthians, Malcom aproveita sobra e gira rápido para marcar na volta do Saudita; veja o lance",
-      "url": "https://www.gazetaesportiva.com/todas-as-noticias/ex-corinthians-malcom-aproveita-sobra-e-gira-rapido-para-marcar-na-volta-do-saudita-veja-o-lance/",
-      "time": "15/08 · 17:07",
-      "pubDate": "Sat, 15 Aug 2026 17:07:13 +0000"
-    },
-    {
-      "source": "Gazeta Esportiva",
-      "color": "#E5820A",
-      "title": "Com Diniz, Corinthians tem 2º melhor aproveitamento entre times paulistas",
-      "url": "https://www.gazetaesportiva.com/times/corinthians/com-diniz-corinthians-tem-2o-melhor-aproveitamento-entre-times-paulistas/",
-      "time": "15/08 · 10:00",
-      "pubDate": "Sat, 15 Aug 2026 10:00:53 +0000"
-    },
-    {
-      "source": "Gazeta Esportiva",
-      "color": "#E5820A",
-      "title": "Corinthians x Cruzeiro: veja onde assistir ao jogo pelo Brasileiro",
-      "url": "https://www.gazetaesportiva.com/campeonatos/brasileiro-serie-a/corinthians-x-cruzeiro-veja-onde-assistir-ao-jogo-pelo-brasileiro/",
-      "time": "15/08 · 09:00",
-      "pubDate": "Sat, 15 Aug 2026 09:00:04 +0000"
-    },
-    {
-      "source": "Bolavip",
-      "color": "#0055A5",
-      "title": "Rosario Central ataca torcida do Corinthians e duvida de denúncia de Hugo Souza em nota",
-      "url": "<![CDATA[https://br.bolavip.com/corinthians/rosario-central-ataca-torcida-do-corinthians-e-duvida-de-denuncia-de-hugo-souza-em-nota]]>",
-      "time": "15/08 · 02:00",
-      "pubDate": "Fri, 14 Aug 2026 23:00:26 -0300"
     }
   ],
   "upcoming": [],
@@ -200,22 +200,52 @@ const LIVE_DATA = {
   "standings": [],
   "highlights": [
     {
-      "title": "Saiba onde assistir à partida entre Corinthians e Cruzeiro pelo Brasileirão",
-      "url": "https://www.meutimao.com.br/noticias-do-corinthians/537990/saiba-onde-assistir-a-partida-entre-corinthians-e-cruzeiro-pelo-brasileirao",
-      "pubDate": "Sun, 16 Aug 2026 09:08:00 -0300",
-      "date": "16/08 · 12:08"
+      "title": "VÍDEO: Fagner fala que diretoria pediu sua saída do Corinthians e pede transparência para o clube",
+      "url": "https://www.meutimao.com.br/videos-do-corinthians/129145/fagner-fala-que-diretoria-pediu-sua-saida-do-corinthians-e-pede-trabsparencia-por-parte-do-clube",
+      "pubDate": "Sun, 16 Aug 2026 23:29:32 -0300",
+      "date": "17/08 · 02:29"
     },
     {
-      "title": "Corinthians se apoia em melhor momento defensivo desde abril para encarar o Cruzeiro",
-      "url": "https://www.meutimao.com.br/noticias-do-corinthians/537898/corinthians-se-apoia-em-melhor-momento-defensivo-desde-abril-para-encarar-o-cruzeiro",
-      "pubDate": "Sat, 15 Aug 2026 22:30:00 -0300",
-      "date": "16/08 · 01:30"
+      "title": "VÍDEO: Sem Yuri Alberto, quem faz gol? Kaio César fala sobre responsabilidade de fazer gols",
+      "url": "https://www.meutimao.com.br/videos-do-corinthians/129144/sem-yuri-alberto-quem-faz-gol-kaio-cesar-fala-sobre-responsabilidade-de-fazer-gols",
+      "pubDate": "Sun, 16 Aug 2026 23:28:32 -0300",
+      "date": "17/08 · 02:28"
     },
     {
-      "title": "Corinthians se isola como o time da Série A com mais jogos sem sofrer gols em mata-mata desde 2025",
-      "url": "https://www.meutimao.com.br/noticias-do-corinthians/537954/corinthians-se-isola-como-o-time-da-serie-a-com-mais-jogos-sem-sofrer-gols-em-mata-mata-desde-2025",
-      "pubDate": "Sat, 15 Aug 2026 20:45:00 -0300",
-      "date": "15/08 · 23:45"
+      "title": "VÍDEO: Derrota em casa, lesão de Gabriel Paulsita e mais: coletiva de Fernando Diniz no Corinthians",
+      "url": "https://www.meutimao.com.br/videos-do-corinthians/129143/derrota-em-casa-lesao-de-gabriel-paulsita-e-mais-coletiva-de-fernando-diniz-no-corinthians",
+      "pubDate": "Sun, 16 Aug 2026 23:25:42 -0300",
+      "date": "17/08 · 02:25"
+    },
+    {
+      "title": "VÍDEO: Derrota amarga e preparação contra o Rosario: Zons mista Matheus Bidu | Corinthians 1X2 Cruzeiro",
+      "url": "https://www.meutimao.com.br/videos-do-corinthians/129141/derrota-amarga-e-preparacao-contra-o-rosario-zons-mista-matheus-bidu-corinthians-1x2-cruzeiro",
+      "pubDate": "Sun, 16 Aug 2026 23:02:00 -0300",
+      "date": "17/08 · 02:02"
+    },
+    {
+      "title": "VÍDEO: Lari Agostini e Tabata Amaral falam domovimento e projetos de lei contra patrocínio da Fatal Fans",
+      "url": "https://www.meutimao.com.br/videos-do-corinthians/129139/lari-agostini-e-tabaata-amaral-falam-domovimento-e-projetos-de-lei-contra-patrocinio-da-fatal-fans",
+      "pubDate": "Sun, 16 Aug 2026 22:32:00 -0300",
+      "date": "17/08 · 01:32"
+    },
+    {
+      "title": "VÍDEO: Melhores Momentos: Corinthians 1x2 Cruzeiro | 23ª rodada | Campeonato Brasileiro 2026",
+      "url": "https://www.meutimao.com.br/videos-do-corinthians/129137/melhores-momentos-corinthians-1x2-cruzeiro-23-rodada-campeonato-brasileiro-2026",
+      "pubDate": "Sun, 16 Aug 2026 22:26:36 -0300",
+      "date": "17/08 · 01:26"
+    },
+    {
+      "title": "Gustavo Henrique marca gol de voleio em derrota e se isola na vice-artilharia do Corinthians em 2026",
+      "url": "https://www.meutimao.com.br/noticias-do-corinthians/538053/gustavo-henrique-marca-gol-de-voleio-em-derrota-e-se-isola-na-vice-artilharia-do-corinthians-em-2026",
+      "pubDate": "Sun, 16 Aug 2026 21:57:00 -0300",
+      "date": "17/08 · 00:57"
+    },
+    {
+      "title": "Golaço de zagueiro e derrota em casa: Fiel repercute jogo do Corinthians contra o Cruzeiro",
+      "url": "https://www.meutimao.com.br/noticias-do-corinthians/538059/golaco-de-zagueiro-e-derrota-em-casa-fiel-repercute-jogo-do-corinthians-contra-o-cruzeiro",
+      "pubDate": "Sun, 16 Aug 2026 21:38:00 -0300",
+      "date": "17/08 · 00:38"
     }
   ]
 };
