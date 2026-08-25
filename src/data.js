e4903410194160e2fda31dd65297a9fe844d220f
GitHub Action <action@github.com>
2026-08-25 08:25:59 +0000

    Dados atualizados

--- src/data.js
+++ src/data.js
@@ -1,7 +1,71 @@
-// Gerado automaticamente em 24/08/2026, 22:12:25
+// Gerado automaticamente em 25/08/2026, 08:25:59
 const LIVE_DATA = {
-  "updatedAt": "2026-08-24T22:12:25.536Z",
+  "updatedAt": "2026-08-25T08:25:59.406Z",
   "news": [
+    {
+      "source": "Meu Timao",
+      "color": "#1a7a3a",
+      "title": "Volante cobra regularidade do Corinthians no Brasileirão para gerir disputa na Libertadores",
+      "url": "https://www.meutimao.com.br/noticias-do-corinthians/538654/volante-cobra-regularidade-do-corinthians-no-brasileirao-para-gerir-disputa-na-libertadores",
+      "time": "25/08 · 01:30",
+      "pubDate": "Mon, 24 Aug 2026 22:30:00 -0300"
+    },
+    {
+      "source": "Meu Timao",
+      "color": "#1a7a3a",
+      "title": "Corinthians paga um dos meses atrasados de direitos de imagem; outras dívidas seguem pendentes",
+      "url": "https://www.meutimao.com.br/noticias-do-corinthians/538703/corinthians-paga-um-dos-meses-atrasados-de-direitos-de-imagem-outras-dividas-seguem-pendentes",
+      "time": "25/08 · 01:15",
+      "pubDate": "Mon, 24 Aug 2026 22:15:00 -0300"
+    },
+    {
+      "source": "Meu Timao",
+      "color": "#1a7a3a",
+      "title": "Corinthians perde uma posição ao fim da 24ª rodada do Brasileirão; veja a tabela",
+      "url": "https://www.meutimao.com.br/noticias-do-corinthians/538702/corinthians-perde-uma-posicao-ao-fim-da-24-rodada-do-brasileirao-veja-a-tabela",
+      "time": "25/08 · 00:58",
+      "pubDate": "Mon, 24 Aug 2026 21:58:00 -0300"
+    },
+    {
+      "source": "Meu Timao",
+      "color": "#1a7a3a",
+      "title": "Belén Aquino ultrapassa zagueira e vira a quarta estrangeira com mais jogos pelo Corinthians",
+      "url": "https://www.meutimao.com.br/noticias-do-corinthians/538672/belen-aquino-ultrapassa-zagueira-e-vira-a-quarta-estrangeira-com-mais-jogos-pelo-corinthians",
+      "time": "25/08 · 00:33",
+      "pubDate": "Mon, 24 Aug 2026 21:33:00 -0300"
+    },
+    {
+      "source": "Meu Timao",
+      "color": "#1a7a3a",
+      "title": "Jogadores do basquete visitam o Memorial do Corinthians no Parque São Jorge",
+      "url": "https://www.meutimao.com.br/noticias-do-corinthians/538701/jogadores-do-basquete-visitam-o-memorial-do-corinthians-no-parque-sao-jorge",
+      "time": "24/08 · 23:49",
+      "pubDate": "Mon, 24 Aug 2026 20:49:00 -0300"
+    },
+    {
+      "source": "Meu Timao",
+      "color": "#1a7a3a",
+      "title": "Técnico do Corinthians sai em defesa de Jesse Lingard e relembra luto vivido pelo inglês",
+      "url": "https://www.meutimao.com.br/noticias-do-corinthians/538649/tecnico-do-corinthians-sai-em-defesa-de-jesse-lingard-e-relembra-luto-vivido-pelo-ingles",
+      "time": "24/08 · 23:28",
+      "pubDate": "Mon, 24 Aug 2026 20:28:00 -0300"
+    },
+    {
+      "source": "Meu Timao",
+      "color": "#1a7a3a",
+      "title": "Volante do Corinthians iguala maior sequência como titular na temporada após seis meses",
+      "url": "https://www.meutimao.com.br/noticias-do-corinthians/538638/volante-do-corinthians-iguala-maior-sequencia-como-titular-na-temporada-apos-seis-meses",
+      "time": "24/08 · 22:36",
+      "pubDate": "Mon, 24 Aug 2026 19:36:00 -0300"
+    },
+    {
+      "source": "Bolavip",
+      "color": "#0055A5",
+      "title": "Presidente do Cuiabá chama Corinthians de “caloteiro” ao criticar dívidas de clubes",
+      "url": "<![CDATA[https://br.bolavip.com/corinthians/presidente-do-cuiaba-chama-corinthians-de-caloteiro-ao-criticar-dividas-de-clubes]]>",
+      "time": "24/08 · 21:58",
+      "pubDate": "Mon, 24 Aug 2026 18:58:49 -0300"
+    },
     {
       "source": "Meu Timao",
       "color": "#1a7a3a",
@@ -11,60 +75,20 @@ const LIVE_DATA = {
       "pubDate": "Mon, 24 Aug 2026 18:30:00 -0300"
     },
     {
-      "source": "Meu Timao",
-      "color": "#1a7a3a",
-      "title": "Corinthians vive expectativa por retorno de Yuri Alberto diante do Santos",
-      "url": "https://www.meutimao.com.br/noticias-do-corinthians/538696/corinthians-vive-expectativa-por-retorno-de-yuri-alberto-diante-do-santos",
-      "time": "24/08 · 21:23",
-      "pubDate": "Mon, 24 Aug 2026 18:23:00 -0300"
+      "source": "Bolavip",
+      "color": "#0055A5",
+      "title": "Opinião: Corinthians sofre com elenco fraco e precisa urgentemente resolver transfer bans para contratar",
+      "url": "<![CDATA[https://br.bolavip.com/corinthians/opiniao-corinthians-sofre-com-elenco-fraco-e-precisa-urgentemente-resolver-transfer-bans-para-contratar]]>",
+      "time": "24/08 · 21:10",
+      "pubDate": "Mon, 24 Aug 2026 18:10:20 -0300"
     },
     {
-      "source": "Meu Timao",
-      "color": "#1a7a3a",
-      "title": "Jogador do Corinthians volta a participar de gol após quatro anos",
-      "url": "https://www.meutimao.com.br/noticias-do-corinthians/538640/jogador-do-corinthians-volta-a-participar-de-gol-apos-quatro-anos",
-      "time": "24/08 · 20:31",
-      "pubDate": "Mon, 24 Aug 2026 17:31:00 -0300"
-    },
-    {
-      "source": "Meu Timao",
-      "color": "#1a7a3a",
-      "title": "Meia do Corinthians valoriza período na Seleção e prega foco no Brasileiro Sub-17",
-      "url": "https://www.meutimao.com.br/noticias-do-corinthians/538694/meia-do-corinthians-valoriza-periodo-na-selecao-e-prega-foco-no-brasileiro-sub-17",
-      "time": "24/08 · 19:49",
-      "pubDate": "Mon, 24 Aug 2026 16:49:00 -0300"
-    },
-    {
-      "source": "Meu Timao",
-      "color": "#1a7a3a",
-      "title": "Duda Sampaio admite oscilação do Corinthians e projeta mata-mata do Brasileirão contra o Cruzeiro",
-      "url": "https://www.meutimao.com.br/noticias-do-corinthians/538583/duda-sampaio-admite-oscilacao-do-corinthians-e-projeta-mata-mata-do-brasileirao-contra-o-cruzeiro",
-      "time": "24/08 · 19:29",
-      "pubDate": "Mon, 24 Aug 2026 16:29:00 -0300"
-    },
-    {
-      "source": "Meu Timao",
-      "color": "#1a7a3a",
-      "title": "Laterais reservas voltam a atuar juntos pelo Corinthians após quase três meses",
-      "url": "https://www.meutimao.com.br/noticias-do-corinthians/538627/laterais-reservas-voltam-a-atuar-juntos-pelo-corinthians-apos-quase-tres-meses",
-      "time": "24/08 · 18:31",
-      "pubDate": "Mon, 24 Aug 2026 15:31:00 -0300"
-    },
-    {
-      "source": "Meu Timao",
-      "color": "#1a7a3a",
-      "title": "Volante do Corinthians lamenta derrota para Coritiba e cobra mais concentração do time no Brasileiro",
-      "url": "https://www.meutimao.com.br/noticias-do-corinthians/538655/volante-do-corinthians-lamenta-derrota-para-coritiba-e-cobra-mais-concentracao-do-time-no-brasileiro",
-      "time": "24/08 · 17:32",
-      "pubDate": "Mon, 24 Aug 2026 14:32:00 -0300"
-    },
-    {
-      "source": "Meu Timao",
-      "color": "#1a7a3a",
-      "title": "Raniele iguala meio-campista na quinta colocação entre os artilheiros do Corinthians em 2026",
-      "url": "https://www.meutimao.com.br/noticias-do-corinthians/538621/raniele-iguala-meio-campista-na-quinta-colocacao-entre-os-artilheiros-do-corinthians-em-2026",
-      "time": "24/08 · 16:34",
-      "pubDate": "Mon, 24 Aug 2026 13:34:00 -0300"
+      "source": "Bolavip",
+      "color": "#0055A5",
+      "title": "OPINIÃO: Corinthians de Fernando Diniz é obrigado a vencer Santos e Chapecoense no Campeonato Brasileiro",
+      "url": "<![CDATA[https://br.bolavip.com/corinthians/opiniao-corinthians-de-fernando-diniz-e-obrigado-a-vencer-santos-e-chapecoense-no-campeonato-brasileiro]]>",
+      "time": "24/08 · 19:00",
+      "pubDate": "Mon, 24 Aug 2026 16:00:52 -0300"
     },
     {
       "source": "Bolavip",
@@ -105,30 +129,6 @@ const LIVE_DATA = {
       "url": "<![CDATA[https://br.bolavip.com/corinthians/mudancas-de-fernando-diniz-nao-causam-efeito-e-corinthians-perde-para-o-coritiba-no-brasileirao]]>",
       "time": "24/08 · 01:23",
       "pubDate": "Sun, 23 Aug 2026 22:23:58 -0300"
-    },
-    {
-      "source": "Bolavip",
-      "color": "#0055A5",
-      "title": "Corinthians cai de rendimento após valorizar a posse, volta a finalizar pouco e perde para o Coritiba",
-      "url": "<![CDATA[https://br.bolavip.com/corinthians/corinthians-cai-de-rendimento-apos-valorizar-a-posse-volta-a-finalizar-pouco-e-perde-para-o-coritiba]]>",
-      "time": "24/08 · 00:32",
-      "pubDate": "Sun, 23 Aug 2026 21:32:34 -0300"
-    },
-    {
-      "source": "Bolavip",
-      "color": "#0055A5",
-      "title": "Corinthians poupará Hugo Souza, Matheuzinho e Bidu contra o Coritiba",
-      "url": "<![CDATA[https://br.bolavip.com/corinthians/corinthians-poupara-hugo-souza-matheuzinho-e-bidu-contra-o-coritiba]]>",
-      "time": "23/08 · 16:59",
-      "pubDate": "Sun, 23 Aug 2026 13:59:21 -0300"
-    },
-    {
-      "source": "Bolavip",
-      "color": "#0055A5",
-      "title": "Diniz prepara mudança e Pedro Milans deve ganhar chance no Corinthians",
-      "url": "<![CDATA[https://br.bolavip.com/corinthians/diniz-prepara-mudanca-e-pedro-milans-deve-ganhar-chance-no-corinthians]]>",
-      "time": "23/08 · 14:07",
-      "pubDate": "Sun, 23 Aug 2026 11:07:47 -0300"
     },
     {
       "source": "Gazeta Esportiva",
@@ -216,24 +216,6 @@ const LIVE_DATA = {
       "url": "https://www.meutimao.com.br/videos-do-corinthians/129298/dificuldades-do-jogo-e-foco-na-libertadores-zona-mista-raniele-coritiba-2x1-corinthians",
       "pubDate": "Sun, 23 Aug 2026 23:12:08 -0300",
       "date": "24/08 · 02:12"
-    },
-    {
-      "title": "VÍDEO: Confusão em Curitiba! Veja imagens e relato dos eventos após Coritiba 2x1 Corinthians",
-      "url": "https://www.meutimao.com.br/videos-do-corinthians/129296/confusao-em-curitiba-veja-imagens-e-relato-dos-eventos-apos-coritiba-2x1-corinthians",
-      "pubDate": "Sun, 23 Aug 2026 22:43:23 -0300",
-      "date": "24/08 · 01:43"
-    },
-    {
-      "title": "VÍDEO: Melhores momentos: Coritiba 2x1 Corinthians | 24ª rodada | Campeonato Brasileiro 2026",
-      "url": "https://www.meutimao.com.br/videos-do-corinthians/129295/melhores-momentos-coritiba-2x1-corinthians-24-rodada-campeonato-brasileiro-2026",
-      "pubDate": "Sun, 23 Aug 2026 22:41:23 -0300",
-      "date": "24/08 · 01:41"
-    },
-    {
-      "title": "Jovem goleiro volta a atuar pelo Corinthians em jogos oficiais após quase quatro meses",
-      "url": "https://www.meutimao.com.br/noticias-do-corinthians/538609/jovem-goleiro-volta-a-atuar-pelo-corinthians-em-jogos-oficiais-apos-quase-quatro-meses",
-      "pubDate": "Sun, 23 Aug 2026 22:31:00 -0300",
-      "date": "24/08 · 01:31"
     }
   ]
 };
