961e572dd6d27d560c399561143a14deda8ed4c2
GitHub Action <action@github.com>
2026-08-28 05:51:12 +0000

    Dados atualizados

--- src/data.js
+++ src/data.js
@@ -1,22 +1,110 @@
-// Gerado automaticamente em 27/08/2026, 18:26:44
+// Gerado automaticamente em 28/08/2026, 05:51:12
 const LIVE_DATA = {
-  "updatedAt": "2026-08-27T18:26:44.056Z",
+  "updatedAt": "2026-08-28T05:51:12.353Z",
   "news": [
     {
       "source": "Meu Timao",
       "color": "#1a7a3a",
-      "title": "Corinthians conhece datas e horários das quartas de final da Libertadores",
-      "url": "https://www.meutimao.com.br/noticias-do-corinthians/538839/corinthians-conhece-datas-e-horarios-das-quartas-de-final-da-libertadores",
-      "time": "27/08 · 18:21",
-      "pubDate": "Thu, 27 Aug 2026 15:21:27 -0300"
+      "title": "Goleada do Corinthians contra o Taubaté movimenta artilharia do feminino em 2026; confira lista",
+      "url": "https://www.meutimao.com.br/noticias-do-corinthians/538805/goleada-do-corinthians-contra-o-taubate-movimenta-artilharia-do-feminino-em-2026-confira-lista",
+      "time": "28/08 · 01:30",
+      "pubDate": "Thu, 27 Aug 2026 22:30:00 -0300"
     },
     {
       "source": "Meu Timao",
       "color": "#1a7a3a",
-      "title": "Presidente do Corinthians apresenta defesa ao MP-SP em investigação sobre empresa de segurança",
-      "url": "https://www.meutimao.com.br/noticias-do-corinthians/538851/presidente-do-corinthians-apresenta-defesa-ao-mp-sp-em-investigacao-sobre-empresa-de-seguranca",
-      "time": "27/08 · 17:57",
-      "pubDate": "Thu, 27 Aug 2026 14:57:38 -0300"
+      "title": "Bastidores do Corinthians têm noite artilheira de Ariel e dedicatória especial de Tamires",
+      "url": "https://www.meutimao.com.br/noticias-do-corinthians/538875/bastidores-do-corinthians-tem-noite-artilheira-de-ariel-e-dedicatoria-especial-de-tamires",
+      "time": "28/08 · 00:51",
+      "pubDate": "Thu, 27 Aug 2026 21:51:30 -0300"
+    },
+    {
+      "source": "Meu Timao",
+      "color": "#1a7a3a",
+      "title": "Corinthians é dominado pelo Magnus e vê série invicta chegar ao fim no Paulista de Futsal",
+      "url": "https://www.meutimao.com.br/noticias-do-corinthians/538698/corinthians-e-dominado-pelo-magnus-e-ve-serie-invicta-chegar-ao-fim-no-paulista-de-futsal",
+      "time": "28/08 · 00:43",
+      "pubDate": "Thu, 27 Aug 2026 21:43:00 -0300"
+    },
+    {
+      "source": "Meu Timao",
+      "color": "#1a7a3a",
+      "title": "Clube inglês prepara homenagem ao Corinthians antes do aniversário de 116 anos",
+      "url": "https://www.meutimao.com.br/noticias-do-corinthians/538857/clube-ingles-prepara-homenagem-ao-corinthians-antes-do-aniversario-de-116-anos",
+      "time": "28/08 · 00:30",
+      "pubDate": "Thu, 27 Aug 2026 21:30:00 -0300"
+    },
+    {
+      "source": "Meu Timao",
+      "color": "#1a7a3a",
+      "title": "Projeto do Corinthians mira 20 jogadores da base no elenco profissional em 2030",
+      "url": "https://www.meutimao.com.br/noticias-do-corinthians/538863/projeto-do-corinthians-mira-20-jogadores-da-base-no-elenco-profissional-em-2030",
+      "time": "28/08 · 00:00",
+      "pubDate": "Thu, 27 Aug 2026 21:00:00 -0300"
+    },
+    {
+      "source": "Meu Timao",
+      "color": "#1a7a3a",
+      "title": "Corinthians terá ajuda integral de patrocinadora em renovação contratual de atacante",
+      "url": "https://www.meutimao.com.br/noticias-do-corinthians/538874/corinthians-tera-ajuda-integral-de-patrocinadora-em-renovacao-contratual-de-atacante",
+      "time": "27/08 · 23:47",
+      "pubDate": "Thu, 27 Aug 2026 20:47:00 -0300"
+    },
+    {
+      "source": "Meu Timao",
+      "color": "#1a7a3a",
+      "title": "Vice-presidente Corinthians minimiza 'atrito' com Memphis e defende trabalho no Corinthians",
+      "url": "https://www.meutimao.com.br/noticias-do-corinthians/538854/vice-presidente-corinthians-minimiza-atrito-com-memphis-e-defende-trabalho-no-corinthians",
+      "time": "27/08 · 23:31",
+      "pubDate": "Thu, 27 Aug 2026 20:31:00 -0300"
+    },
+    {
+      "source": "Meu Timao",
+      "color": "#1a7a3a",
+      "title": "Emily Lima minimiza pressão no Corinthians e reforça compromisso com o clube",
+      "url": "https://www.meutimao.com.br/noticias-do-corinthians/538822/emily-lima-minimiza-pressao-no-corinthians-e-reforca-compromisso-com-o-clube",
+      "time": "27/08 · 23:28",
+      "pubDate": "Thu, 27 Aug 2026 20:28:00 -0300"
+    },
+    {
+      "source": "Gazeta Esportiva",
+      "color": "#E5820A",
+      "title": "Corinthians encaminha empréstimo do jovem atacante Favela a clube europeu",
+      "url": "https://www.gazetaesportiva.com/times/corinthians/corinthians-encaminha-emprestimo-do-jovem-atacante-favela-a-clube-europeu/",
+      "time": "27/08 · 22:35",
+      "pubDate": "Thu, 27 Aug 2026 22:35:27 +0000"
+    },
+    {
+      "source": "Gazeta Esportiva",
+      "color": "#E5820A",
+      "title": "Corinthians e SAFiel têm reunião de quase 5 horas para debater projeto",
+      "url": "https://www.gazetaesportiva.com/times/corinthians/corinthians-e-safiel-tem-reuniao-de-quase-5-horas-para-debater-projeto/",
+      "time": "27/08 · 21:50",
+      "pubDate": "Thu, 27 Aug 2026 21:50:30 +0000"
+    },
+    {
+      "source": "Gazeta Esportiva",
+      "color": "#E5820A",
+      "title": "Diniz tem ausências e esboça Corinthians para clássico com Memphis titular",
+      "url": "https://www.gazetaesportiva.com/times/corinthians/diniz-tem-ausencia-de-dupla-e-esboca-corinthians-para-pegar-o-santos/",
+      "time": "27/08 · 19:40",
+      "pubDate": "Thu, 27 Aug 2026 19:40:59 +0000"
+    },
+    {
+      "source": "Gazeta Esportiva",
+      "color": "#E5820A",
+      "title": "Veja galeria de fotos do treino do Corinthians nesta quinta-feira",
+      "url": "https://www.gazetaesportiva.com/times/corinthians/veja-galeria-de-fotos-do-treino-do-corinthians-nesta-quinta-feira-9/",
+      "time": "27/08 · 19:22",
+      "pubDate": "Thu, 27 Aug 2026 19:22:01 +0000"
+    },
+    {
+      "source": "Gazeta Esportiva",
+      "color": "#E5820A",
+      "title": "Conmebol define detalhes das quartas da Libertadores; veja quando jogam Corinthians e Palmeiras",
+      "url": "https://www.gazetaesportiva.com/campeonatos/libertadores-da-america/conmebol-define-detalhes-das-quartas-da-libertadores-veja-quando-jogam-corinthians-e-palmeiras/",
+      "time": "27/08 · 19:21",
+      "pubDate": "Thu, 27 Aug 2026 19:21:41 +0000"
     },
     {
       "source": "Gazeta Esportiva",
@@ -27,46 +115,6 @@ const LIVE_DATA = {
       "pubDate": "Thu, 27 Aug 2026 17:48:38 +0000"
     },
     {
-      "source": "Meu Timao",
-      "color": "#1a7a3a",
-      "title": "Volante do Corinthians volta a ser titular após mais de quatro meses",
-      "url": "https://www.meutimao.com.br/noticias-do-corinthians/538798/volante-do-corinthians-volta-a-ser-titular-apos-mais-de-quatro-meses",
-      "time": "27/08 · 17:37",
-      "pubDate": "Thu, 27 Aug 2026 14:37:00 -0300"
-    },
-    {
-      "source": "Meu Timao",
-      "color": "#1a7a3a",
-      "title": "Corinthians contrata fisioterapeuta ex-Palmeiras para reforçar comissão do time feminino",
-      "url": "https://www.meutimao.com.br/noticias-do-corinthians/538840/corinthians-contrata-fisioterapeuta-ex-palmeiras-para-reforcar-comissao-do-time-feminino",
-      "time": "27/08 · 17:07",
-      "pubDate": "Thu, 27 Aug 2026 14:07:00 -0300"
-    },
-    {
-      "source": "Meu Timao",
-      "color": "#1a7a3a",
-      "title": "Quem deve substituir Breno Bidon no Corinthians contra o Santos? Vote na enquete do Meu Timão!",
-      "url": "https://www.meutimao.com.br/noticias-do-corinthians/538786/quem-deve-substituir-breno-bidon-no-classico-do-corinthians-diante-do-santos-vote-na-enquete",
-      "time": "27/08 · 16:31",
-      "pubDate": "Thu, 27 Aug 2026 13:31:00 -0300"
-    },
-    {
-      "source": "Meu Timao",
-      "color": "#1a7a3a",
-      "title": "Corinthians promete pagamento da primeira parcela da dívida com time feminino nesta sexta",
-      "url": "https://www.meutimao.com.br/noticias-do-corinthians/538844/corinthians-promete-pagamento-da-primeira-parcela-da-divida-com-time-feminino-nesta-sexta",
-      "time": "27/08 · 16:04",
-      "pubDate": "Thu, 27 Aug 2026 13:04:00 -0300"
-    },
-    {
-      "source": "Meu Timao",
-      "color": "#1a7a3a",
-      "title": "Coletivo de sócios do Corinthians convoca protesto no Parque São Jorge por mudanças no clube",
-      "url": "https://www.meutimao.com.br/noticias-do-corinthians/538846/coletivo-de-socios-do-corinthians-convoca-protesto-no-parque-sao-jorge-por-mudancas-no-clube",
-      "time": "27/08 · 16:02",
-      "pubDate": "Thu, 27 Aug 2026 13:02:00 -0300"
-    },
-    {
       "source": "Gazeta Esportiva",
       "color": "#E5820A",
       "title": "Ex-Corinthians e Santos, Steven Mendoza alcança marca de 500 jogos na carreira",
@@ -75,12 +123,12 @@ const LIVE_DATA = {
       "pubDate": "Thu, 27 Aug 2026 15:57:54 +0000"
     },
     {
-      "source": "Meu Timao",
-      "color": "#1a7a3a",
-      "title": "Corinthians estabelece valores para venda de André Luiz ao Nottingham Forest",
-      "url": "https://www.meutimao.com.br/noticias-do-corinthians/538845/corinthians-estabelece-valores-para-venda-de-andre-luiz-ao-nottingham-forest",
-      "time": "27/08 · 15:44",
-      "pubDate": "Thu, 27 Aug 2026 12:44:00 -0300"
+      "source": "Bolavip",
+      "color": "#0055A5",
+      "title": "Corinthians sobe a meta e André tem valor detalhado para ser vendido ao Nottingham Forest",
+      "url": "<![CDATA[https://br.bolavip.com/corinthians/corinthians-sobe-a-meta-e-andre-tem-valor-detalhado-para-ser-vendido-ao-nottingham-forest]]>",
+      "time": "27/08 · 15:54",
+      "pubDate": "Thu, 27 Aug 2026 12:54:33 -0300"
     },
     {
       "source": "Gazeta Esportiva",
@@ -91,28 +139,12 @@ const LIVE_DATA = {
       "pubDate": "Thu, 27 Aug 2026 12:32:12 +0000"
     },
     {
-      "source": "Gazeta Esportiva",
-      "color": "#E5820A",
-      "title": "Corinthians pode ter até sete reforços para clássico contra o Santos",
-      "url": "https://www.gazetaesportiva.com/times/corinthians/corinthians-pode-ter-ate-sete-reforcos-para-classico-contra-o-santos/",
-      "time": "27/08 · 09:00",
-      "pubDate": "Thu, 27 Aug 2026 09:00:29 +0000"
-    },
-    {
-      "source": "Gazeta Esportiva",
-      "color": "#E5820A",
-      "title": "Corinthians se reapresenta e Gustavo Henrique comenta tempo para treinos",
-      "url": "https://www.gazetaesportiva.com/times/corinthians/corinthians-se-reapresenta-e-gustavo-henrique-comenta-tempo-para-treinos/",
-      "time": "27/08 · 01:02",
-      "pubDate": "Thu, 27 Aug 2026 01:02:12 +0000"
-    },
-    {
-      "source": "Gazeta Esportiva",
-      "color": "#E5820A",
-      "title": "Palmeiras avança às semifinais do Paulistão F; Corinthians, Santos e São Paulo vão ao play-in",
-      "url": "https://www.gazetaesportiva.com/campeonatos/brasileirao-feminino/palmeiras-avanca-as-semifinais-do-paulistao-f-corinthians-santos-e-sao-paulo-vao-ao-play-in/",
-      "time": "27/08 · 00:11",
-      "pubDate": "Thu, 27 Aug 2026 00:11:03 +0000"
+      "source": "Bolavip",
+      "color": "#0055A5",
+      "title": "Vídeo: Podcast LivePorTi indaga se Diniz poderia fazer André se valorizar com mais tempo no Corinthians",
+      "url": "<![CDATA[https://br.bolavip.com/corinthians/video-podcast-liveporti-indaga-se-diniz-poderia-fazer-andre-se-valorizar-com-mais-tempo-no-corinthians]]>",
+      "time": "27/08 · 03:07",
+      "pubDate": "Thu, 27 Aug 2026 00:07:52 -0300"
     },
     {
       "source": "Bolavip",
@@ -123,28 +155,12 @@ const LIVE_DATA = {
       "pubDate": "Wed, 26 Aug 2026 19:46:40 -0300"
     },
     {
-      "source": "Gazeta Esportiva",
-      "color": "#E5820A",
-      "title": "Com André em campo, Corinthians abre preparação para clássico",
-      "url": "https://www.gazetaesportiva.com/times/corinthians/com-andre-em-campo-corinthians-abre-preparacao-para-classico/",
-      "time": "26/08 · 21:21",
-      "pubDate": "Wed, 26 Aug 2026 21:21:25 +0000"
-    },
-    {
       "source": "Bolavip",
       "color": "#0055A5",
       "title": "Opinião: Corinthians não pode nem sonhar em renovar com Lingard, Matheus Pereira, Allan e Vitinho",
       "url": "<![CDATA[https://br.bolavip.com/corinthians/opiniao-corinthians-nao-pode-nem-sonhar-em-renovar-com-lingard-matheus-pereira-allan-e-vitinho]]>",
       "time": "26/08 · 20:56",
       "pubDate": "Wed, 26 Aug 2026 17:56:14 -0300"
-    },
-    {
-      "source": "Gazeta Esportiva",
-      "color": "#E5820A",
-      "title": "Yuri Alberto inicia transição e se aproxima de retorno no Corinthians",
-      "url": "https://www.gazetaesportiva.com/times/corinthians/yuri-alberto-inicia-transicao-e-se-aproxima-de-retorno-no-corinthians/",
-      "time": "26/08 · 20:54",
-      "pubDate": "Wed, 26 Aug 2026 20:54:55 +0000"
     },
     {
       "source": "Bolavip",
@@ -177,22 +193,6 @@ const LIVE_DATA = {
       "url": "<![CDATA[https://br.bolavip.com/corinthians/influenciador-ambiente-andre-prejudica-corinthians]]>",
       "time": "25/08 · 23:53",
       "pubDate": "Tue, 25 Aug 2026 20:53:38 -0300"
-    },
-    {
-      "source": "Bolavip",
-      "color": "#0055A5",
-      "title": "OPINIÃO: Corinthians precisa quitar transfer bans e contratar novo centroavante com dinheiro de André",
-      "url": "<![CDATA[https://br.bolavip.com/corinthians/opiniao-corinthians-precisa-quitar-transfer-bans-e-contratar-novo-centroavante-com-dinheiro-de-andre]]>",
-      "time": "25/08 · 19:08",
-      "pubDate": "Tue, 25 Aug 2026 16:08:08 -0300"
-    },
-    {
-      "source": "Bolavip",
-      "color": "#0055A5",
-      "title": "Opinião: Corinthians precisa vender para sobreviver, mas corre o risco de enfraquecer o próprio time",
-      "url": "<![CDATA[https://br.bolavip.com/corinthians/opiniao-corinthians-precisa-vender-para-sobreviver-mas-corre-o-risco-de-enfraquecer-o-proprio-time]]>",
-      "time": "25/08 · 17:18",
-      "pubDate": "Tue, 25 Aug 2026 14:18:15 -0300"
     }
   ],
   "upcoming": [],
@@ -200,34 +200,16 @@ const LIVE_DATA = {
   "standings": [],
   "highlights": [
     {
-      "title": "Técnica do Corinthians celebra goleada e analisa classificação ao play-in do Paulistão Feminino",
-      "url": "https://www.meutimao.com.br/noticias-do-corinthians/538824/tecnica-do-corinthians-celebra-goleada-e-analisa-classificacao-ao-play-in-do-paulistao-feminino",
-      "pubDate": "Thu, 27 Aug 2026 10:39:00 -0300",
-      "date": "27/08 · 13:39"
+      "title": "Goleada do Corinthians contra o Taubaté movimenta artilharia do feminino em 2026; confira lista",
+      "url": "https://www.meutimao.com.br/noticias-do-corinthians/538805/goleada-do-corinthians-contra-o-taubate-movimenta-artilharia-do-feminino-em-2026-confira-lista",
+      "pubDate": "Thu, 27 Aug 2026 22:30:00 -0300",
+      "date": "28/08 · 01:30"
     },
     {
-      "title": "VÍDEO: Vice-presidente do Corinthians destaca importância de reunião com a Safiel",
-      "url": "https://www.meutimao.com.br/videos-do-corinthians/129355/vice-presidente-do-corinthians-destaca-importancia-de-reuniao-com-a-safiel",
-      "pubDate": "Wed, 26 Aug 2026 23:05:01 -0300",
-      "date": "27/08 · 02:05"
-    },
-    {
-      "title": "VÍDEO: Safiel no Corinthians: representantes detalham reunião com diretoria do Timão",
-      "url": "https://www.meutimao.com.br/videos-do-corinthians/129354/safiel-no-corinthians-representantes-detalham-reuniao-com-diretoria-do-timao",
-      "pubDate": "Wed, 26 Aug 2026 22:40:46 -0300",
-      "date": "27/08 · 01:40"
-    },
-    {
-      "title": "Jair Ventura recorda vice do Corinthians na Copa do Brasil e lamenta gol anulado de Pedrinho",
-      "url": "https://www.meutimao.com.br/noticias-do-corinthians/538771/jair-ventura-recorda-vice-do-corinthians-na-copa-do-brasil-e-lamenta-gol-anulado-de-pedrinho",
-      "pubDate": "Wed, 26 Aug 2026 21:33:00 -0300",
-      "date": "27/08 · 00:33"
-    },
-    {
-      "title": "Corinthians fecha a primeira fase do Paulista Feminino com goleada sobre o Taubaté",
-      "url": "https://www.meutimao.com.br/noticias-do-corinthians/538790/corinthians-fecha-a-primeira-fase-do-paulista-feminino-com-goleada-sobre-o-taubate",
-      "pubDate": "Wed, 26 Aug 2026 20:31:00 -0300",
-      "date": "26/08 · 23:31"
+      "title": "Tamires celebra gol especial pelo Corinthians e pede foco para sequência do segundo semestre",
+      "url": "https://www.meutimao.com.br/noticias-do-corinthians/538827/tamires-celebra-gol-especial-pelo-corinthians-e-pede-foco-para-sequencia-do-segundo-semestre",
+      "pubDate": "Thu, 27 Aug 2026 18:34:00 -0300",
+      "date": "27/08 · 21:34"
     }
   ]
 };
