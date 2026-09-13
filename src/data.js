e3a92f79a50697d09660f3fc69877b4459ed6087
GitHub Action <action@github.com>
2026-09-13 12:52:51 +0000

    Dados atualizados

--- src/data.js
+++ src/data.js
@@ -1,102 +1,78 @@
-// Gerado automaticamente em 12/09/2026, 23:28:49
+// Gerado automaticamente em 13/09/2026, 12:52:51
 const LIVE_DATA = {
-  "updatedAt": "2026-09-12T23:28:49.359Z",
+  "updatedAt": "2026-09-13T12:52:51.768Z",
   "news": [
     {
       "source": "Meu Timao",
       "color": "#1a7a3a",
-      "title": "Meio-campista do Corinthians projeta semifinal do Brasileirão Feminino e mira sequência da temporada",
-      "url": "https://www.meutimao.com.br/noticias-do-corinthians/539900/meio-campista-do-corinthians-projeta-semifinal-do-brasileirao-feminino-e-mira-sequencia-da-temporada",
-      "time": "12/09 · 23:06",
-      "pubDate": "Sat, 12 Sep 2026 20:06:00 -0300"
+      "title": "Corinthians encara o Taubaté buscando assumir a liderança do Paulistão Feminino Sub-17; saiba tudo",
+      "url": "https://www.meutimao.com.br/noticias-do-corinthians/539905/corinthians-encara-o-taubate-buscando-assumir-a-lideranca-do-paulistao-feminino-sub-17-saiba-tudo",
+      "time": "13/09 · 12:31",
+      "pubDate": "Sun, 13 Sep 2026 09:31:00 -0300"
     },
     {
       "source": "Meu Timao",
       "color": "#1a7a3a",
-      "title": "Estudiantes poupa titulares no Campeonato Argentino de olho em duelo contra o Corinthians",
-      "url": "https://www.meutimao.com.br/noticias-do-corinthians/539920/estudiantes-poupa-titulares-no-campeonato-argentino-de-olho-em-duelo-contra-o-corinthians",
-      "time": "12/09 · 23:03",
-      "pubDate": "Sat, 12 Sep 2026 20:03:00 -0300"
-    },
-    {
-      "source": "Gazeta Esportiva",
-      "color": "#E5820A",
-      "title": "Flamengo x Corinthians pelo Brasileirão: onde assistir, estatísticas, histórico e escalações",
-      "url": "https://www.gazetaesportiva.com/campeonatos/brasileiro-serie-a/flamengo-corinthians-brasileirao-13-09-2026/",
-      "time": "12/09 · 23:00",
-      "pubDate": "Sat, 12 Sep 2026 23:00:47 +0000"
+      "title": "Saiba onde assistir ao clássico entre Corinthians e Flamengo pelo Brasileirão",
+      "url": "https://www.meutimao.com.br/noticias-do-corinthians/539919/saiba-onde-assistir-ao-classico-entre-corinthians-e-flamengo-pelo-brasileirao",
+      "time": "13/09 · 11:30",
+      "pubDate": "Sun, 13 Sep 2026 08:30:00 -0300"
     },
     {
       "source": "Meu Timao",
       "color": "#1a7a3a",
-      "title": "Técnico do Sub-20 celebra zagueiros relacionados para partida do Corinthians contra o Flamengo",
-      "url": "https://www.meutimao.com.br/noticias-do-corinthians/539918/tecnico-do-sub-20-celebra-zagueiros-relacionados-para-partida-do-corinthians-contra-o-flamengo",
-      "time": "12/09 · 22:15",
-      "pubDate": "Sat, 12 Sep 2026 19:15:02 -0300"
+      "title": "Últimas do Corinthians: escalação alternativa, declaração de Memphis e janela vazia",
+      "url": "https://www.meutimao.com.br/noticias-do-corinthians/539929/ultimas-do-corinthians-escalacao-alternativa-declaracao-de-memphis-e-janela-vazia",
+      "time": "13/09 · 10:30",
+      "pubDate": "Sun, 13 Sep 2026 07:30:00 -0300"
     },
     {
       "source": "Meu Timao",
       "color": "#1a7a3a",
-      "title": "VÍDEO: Corinthians desembarca no Rio de Janeiro para confronto com o Flamengo no Brasileirão",
-      "url": "https://www.meutimao.com.br/videos-do-corinthians/129626/corinthians-desembarca-no-rio-de-janeiro-para-confronto-com-o-flamengo-no-brasileirao",
-      "time": "12/09 · 22:03",
-      "pubDate": "Sat, 12 Sep 2026 19:03:20 -0300"
+      "title": "Corinthians visita o Flamengo buscando voltar a vencer no Campeonato Brasileiro; saiba tudo",
+      "url": "https://www.meutimao.com.br/noticias-do-corinthians/539907/corinthians-visita-o-flamengo-buscando-voltar-a-vencer-no-campeonato-brasileiro-saiba-tudo",
+      "time": "13/09 · 03:01",
+      "pubDate": "Sun, 13 Sep 2026 00:01:00 -0300"
     },
     {
       "source": "Meu Timao",
       "color": "#1a7a3a",
-      "title": "Corinthians tenta quebrar tabu contra o Flamengo que já perdura há quase quatro anos; saiba qual",
-      "url": "https://www.meutimao.com.br/noticias-do-corinthians/539785/corinthians-tenta-quebrar-tabu-contra-o-flamengo-que-ja-perdura-ha-quase-quatro-anos-saiba-qual",
-      "time": "12/09 · 22:03",
-      "pubDate": "Sat, 12 Sep 2026 19:03:00 -0300"
+      "title": "Corinthians visita o S��o José pelo Paulista Sub-17 para se classificar antecipadamente; saiba tudo",
+      "url": "https://www.meutimao.com.br/noticias-do-corinthians/539928/corinthians-visita-o-sao-jose-pelo-paulista-sub-17-para-se-classificar-antecipadamente-saiba-tudo",
+      "time": "13/09 · 02:00",
+      "pubDate": "Sat, 12 Sep 2026 23:00:00 -0300"
     },
     {
       "source": "Meu Timao",
       "color": "#1a7a3a",
-      "title": "Meio-campista do Corinthians projeta duelo contra o Taubaté pelo Paulista Sub-17",
-      "url": "https://www.meutimao.com.br/noticias-do-corinthians/539916/meio-campista-do-corinthians-projeta-duelo-contra-o-taubate-pelo-paulista-sub-17",
-      "time": "12/09 · 21:41",
-      "pubDate": "Sat, 12 Sep 2026 18:41:00 -0300"
+      "title": "Corinthians planeja avanço em recuperação de Yuri Alberto e retorno deve ocorrer após a Data Fifa",
+      "url": "https://www.meutimao.com.br/noticias-do-corinthians/539931/corinthians-planeja-avanco-em-recuperacao-de-yuri-alberto-e-retorno-deve-ocorrer-apos-a-data-fifa",
+      "time": "13/09 · 01:00",
+      "pubDate": "Sat, 12 Sep 2026 22:00:00 -0300"
     },
     {
       "source": "Meu Timao",
       "color": "#1a7a3a",
-      "title": "Corinthians desembarca no Rio de Janeiro com 24 relacionados para duelo contra o Flamengo",
-      "url": "https://www.meutimao.com.br/noticias-do-corinthians/539902/corinthians-desembarca-no-rio-de-janeiro-com-24-relacionados-para-duelo-contra-o-flamengo",
-      "time": "12/09 · 21:19",
-      "pubDate": "Sat, 12 Sep 2026 18:19:00 -0300"
-    },
-    {
-      "source": "Gazeta Esportiva",
-      "color": "#E5820A",
-      "title": "Corinthians goleia América-SP e segue na cola do líder Santos no Paulista-sub 20",
-      "url": "https://www.gazetaesportiva.com/times/corinthians/corinthians-goleia-america-sp-e-segue-na-cola-do-lider-santos-no-paulista-sub-20/",
-      "time": "12/09 · 20:25",
-      "pubDate": "Sat, 12 Sep 2026 20:25:46 +0000"
+      "title": "Atacante marca pela primeira vez na temporada e movimenta artilharia do Corinthians Sub-20",
+      "url": "https://www.meutimao.com.br/noticias-do-corinthians/539923/atacante-marca-pela-primeira-vez-na-temporada-e-movimenta-artilharia-do-corinthians-sub-20",
+      "time": "13/09 · 00:35",
+      "pubDate": "Sat, 12 Sep 2026 21:35:00 -0300"
     },
     {
       "source": "Meu Timao",
       "color": "#1a7a3a",
-      "title": "Corinthians conhece detalhes do Desafio Internacional de Futsal 2026",
-      "url": "https://www.meutimao.com.br/noticias-do-corinthians/539913/corinthians-conhece-detalhes-do-desafio-internacional-de-futsal-2026",
-      "time": "12/09 · 20:25",
-      "pubDate": "Sat, 12 Sep 2026 17:25:00 -0300"
+      "title": "Ex-atacante acompanha treinos do Corinthians no CT Joaquim Grava",
+      "url": "https://www.meutimao.com.br/noticias-do-corinthians/539903/ex-atacante-acompanha-treinos-do-corinthians-no-ct-joaquim-grava",
+      "time": "13/09 · 00:02",
+      "pubDate": "Sat, 12 Sep 2026 21:02:00 -0300"
     },
     {
       "source": "Bolavip",
       "color": "#0055A5",
       "title": "Corinthians trabalha para ter André de volta e mira retorno gradual após lesão",
       "url": "<![CDATA[https://br.bolavip.com/corinthians/corinthians-trabalha-para-ter-andre-de-volta-e-mira-retorno-gradual-apos-lesao]]>",
-      "time": "12/09 · 19:40",
-      "pubDate": "Sat, 12 Sep 2026 16:40:52 -0300"
-    },
-    {
-      "source": "Gazeta Esportiva",
-      "color": "#E5820A",
-      "title": "Escalação do Corinthians: Diniz deve poupar titulares contra o Flamengo",
-      "url": "https://www.gazetaesportiva.com/times/corinthians/escalacao-do-corinthians-diniz-deve-poupar-titulares-contra-o-flamengo/",
-      "time": "12/09 · 19:33",
-      "pubDate": "Sat, 12 Sep 2026 19:33:23 +0000"
+      "time": "12/09 · 22:59",
+      "pubDate": "Sat, 12 Sep 2026 19:59:28 -0300"
     },
     {
       "source": "Bolavip",
@@ -105,46 +81,6 @@ const LIVE_DATA = {
       "url": "<![CDATA[https://br.bolavip.com/corinthians/corinthians-poupa-forcas-para-decisao-e-diniz-prepara-time-reserva-com-novidades-contra-o-flamengo]]>",
       "time": "12/09 · 19:24",
       "pubDate": "Sat, 12 Sep 2026 16:24:12 -0300"
-    },
-    {
-      "source": "Gazeta Esportiva",
-      "color": "#E5820A",
-      "title": "Veja galeria de fotos do treino do Corinthians neste sábado",
-      "url": "https://www.gazetaesportiva.com/times/corinthians/veja-galeria-de-fotos-do-treino-do-corinthians-neste-sabado-13/",
-      "time": "12/09 · 18:38",
-      "pubDate": "Sat, 12 Sep 2026 18:38:35 +0000"
-    },
-    {
-      "source": "Gazeta Esportiva",
-      "color": "#E5820A",
-      "title": "Corinthians x América-SP: veja onde assistir à partida pelo Paulista sub-20",
-      "url": "https://www.gazetaesportiva.com/times/corinthians/corinthians-x-america-sp-veja-onde-assistir-a-partida-pelo-paulista-sub-20/",
-      "time": "12/09 · 11:00",
-      "pubDate": "Sat, 12 Sep 2026 11:00:34 +0000"
-    },
-    {
-      "source": "Gazeta Esportiva",
-      "color": "#E5820A",
-      "title": "Flamengo x Corinthians: onde assistir ao jogo pela 27ª rodada do Brasileirão",
-      "url": "https://www.gazetaesportiva.com/campeonatos/brasileiro-serie-a/flamengo-x-corinthians-onde-assistir-ao-jogo-pela-27a-rodada-do-brasileirao/",
-      "time": "12/09 · 10:00",
-      "pubDate": "Sat, 12 Sep 2026 10:00:31 +0000"
-    },
-    {
-      "source": "Gazeta Esportiva",
-      "color": "#E5820A",
-      "title": "Respaldado por Diniz, Garro busca retomar protagonismo no Corinthians",
-      "url": "https://www.gazetaesportiva.com/times/corinthians/respaldado-por-diniz-garro-busca-retomar-protagonismo-no-corinthians/",
-      "time": "12/09 · 09:00",
-      "pubDate": "Sat, 12 Sep 2026 09:00:53 +0000"
-    },
-    {
-      "source": "Gazeta Esportiva",
-      "color": "#E5820A",
-      "title": "Corinthians treina para duelo contra o Flamengo no Maracanã",
-      "url": "https://www.gazetaesportiva.com/times/corinthians/corinthians-treina-para-duelo-contra-o-flamengo-no-maracana/",
-      "time": "11/09 · 22:35",
-      "pubDate": "Fri, 11 Sep 2026 22:35:29 +0000"
     },
     {
       "source": "Bolavip",
@@ -193,12 +129,82 @@ const LIVE_DATA = {
       "url": "<![CDATA[https://br.bolavip.com/corinthians/kaio-cesar-cai-na-pilha-dos-argentinos-e-recebe-cartao-amarelo-pelo-corinthians]]>",
       "time": "10/09 · 01:17",
       "pubDate": "Wed, 09 Sep 2026 22:17:20 -0300"
+    },
+    {
+      "source": "Gazeta Esportiva",
+      "color": "#E5820A",
+      "title": "Allan aponta foco do Corinthians para buscar melhor campanha da Libertadores",
+      "url": "https://www.gazetaesportiva.com/times/corinthians/allan-aponta-foco-do-corinthians-para-buscar-melhor-campanha-da-libertadores/",
+      "time": "27/05 · 00:00",
+      "pubDate": "Wed, 27 May 2026 00:00:28 +0000"
+    },
+    {
+      "source": "Gazeta Esportiva",
+      "color": "#E5820A",
+      "title": "Corinthians x Platense pela Libertadores: onde assistir ao vivo, estatísticas e prováveis escalações",
+      "url": "https://www.gazetaesportiva.com/campeonatos/libertadores-da-america/corinthians-x-platense-libertadores-27-05-26/",
+      "time": "26/05 · 23:00",
+      "pubDate": "Tue, 26 May 2026 23:00:00 +0000"
+    },
+    {
+      "source": "Gazeta Esportiva",
+      "color": "#E5820A",
+      "title": "Corinthians faz treino tático e fecha preparação para pegar o Platense; veja provável escalação",
+      "url": "https://www.gazetaesportiva.com/times/corinthians/corinthians-faz-treino-tatico-e-fecha-preparacao-para-pegar-o-platense-veja-provavel-escalacao/",
+      "time": "26/05 · 22:26",
+      "pubDate": "Tue, 26 May 2026 22:26:53 +0000"
+    },
+    {
+      "source": "Gazeta Esportiva",
+      "color": "#E5820A",
+      "title": "Veja fotos do último treino do Corinthians antes de enfrentar o Platense",
+      "url": "https://www.gazetaesportiva.com/times/corinthians/veja-fotos-do-ultimo-treino-do-corinthians-antes-de-enfrentar-o-platense/",
+      "time": "26/05 · 22:26",
+      "pubDate": "Tue, 26 May 2026 22:26:51 +0000"
+    },
+    {
+      "source": "Gazeta Esportiva",
+      "color": "#E5820A",
+      "title": "Diretor do Corinthians atualiza situação de Memphis e abre o jogo sobre Yuri Alberto",
+      "url": "https://www.gazetaesportiva.com/times/corinthians/diretor-do-corinthians-atualiza-situacao-de-memphis-e-abre-o-jogo-sobre-yuri-alberto/",
+      "time": "26/05 · 17:45",
+      "pubDate": "Tue, 26 May 2026 17:45:53 +0000"
+    },
+    {
+      "source": "Gazeta Esportiva",
+      "color": "#E5820A",
+      "title": "Memphis pode fazer último jogo na Arena, e Corinthians corre contra o tempo por renovação",
+      "url": "https://www.gazetaesportiva.com/times/corinthians/memphis-pode-fazer-ultimo-jogo-na-arena-e-corinthians-corre-contra-o-tempo-por-renovacao/",
+      "time": "26/05 · 10:00",
+      "pubDate": "Tue, 26 May 2026 10:00:49 +0000"
+    },
+    {
+      "source": "Gazeta Esportiva",
+      "color": "#E5820A",
+      "title": "Corinthians x Platense: veja onde assistir ao duelo pela última rodada da Libertadores",
+      "url": "https://www.gazetaesportiva.com/times/corinthians/corinthians-x-platense-veja-onde-assistir-ao-duelo-pela-ultima-rodada-da-libertadores/",
+      "time": "26/05 · 09:00",
+      "pubDate": "Tue, 26 May 2026 09:00:32 +0000"
+    },
+    {
+      "source": "Gazeta Esportiva",
+      "color": "#E5820A",
+      "title": "Corinthians goleia o Mixto e assume a ponta do Brasileiro feminino",
+      "url": "https://www.gazetaesportiva.com/times/corinthians/corinthians-goleia-o-mixto-e-assume-a-ponta-do-brasileiro-feminino/",
+      "time": "26/05 · 02:04",
+      "pubDate": "Tue, 26 May 2026 02:04:37 +0000"
     }
   ],
   "upcoming": [],
   "results": [],
   "standings": [],
   "highlights": [
+    {
+      "title": "Saiba onde assistir ao clássico entre Corinthians e Flamengo pelo Brasileirão",
+      "url": "https://www.meutimao.com.br/noticias-do-corinthians/539919/saiba-onde-assistir-ao-classico-entre-corinthians-e-flamengo-pelo-brasileirao",
+      "pubDate": "Sun, 13 Sep 2026 08:30:00 -0300",
+      "date": "13/09 · 11:30"
+    },
     {
       "title": "VÍDEO: Corinthians desembarca no Rio de Janeiro para confronto com o Flamengo no Brasileirão",
       "url": "https://www.meutimao.com.br/videos-do-corinthians/129626/corinthians-desembarca-no-rio-de-janeiro-para-confronto-com-o-flamengo-no-brasileirao",
