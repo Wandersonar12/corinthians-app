aeeeec44fc0fe3df8171238f38c91a9a8c220584
GitHub Action <action@github.com>
2026-09-16 23:56:04 +0000

    Dados atualizados

--- src/data.js
+++ src/data.js
@@ -1,7 +1,71 @@
-// Gerado automaticamente em 16/09/2026, 20:30:16
+// Gerado automaticamente em 16/09/2026, 23:56:04
 const LIVE_DATA = {
-  "updatedAt": "2026-09-16T20:30:16.853Z",
+  "updatedAt": "2026-09-16T23:56:04.131Z",
   "news": [
+    {
+      "source": "Gazeta Esportiva",
+      "color": "#E5820A",
+      "title": "Corinthians terá André no banco contra o Estudiantes; veja escalações",
+      "url": "https://www.gazetaesportiva.com/times/corinthians/corinthians-tera-andre-no-banco-contra-o-estudiantes-veja-escalacoes/",
+      "time": "16/09 · 23:11",
+      "pubDate": "Wed, 16 Sep 2026 23:11:17 +0000"
+    },
+    {
+      "source": "Meu Timao",
+      "color": "#1a7a3a",
+      "title": "Corinthians está escalado para decisão contra o Estudiantes na Libertadores",
+      "url": "https://www.meutimao.com.br/noticias-do-corinthians/540155/corinthians-esta-escalado-para-decisao-contra-o-estudiantes-na-libertadores",
+      "time": "16/09 · 23:08",
+      "pubDate": "Wed, 16 Sep 2026 20:08:00 -0300"
+    },
+    {
+      "source": "Meu Timao",
+      "color": "#1a7a3a",
+      "title": "Atacante entra no pódio da artilharia do Corinthians após vitória no Brasileirão Feminino",
+      "url": "https://www.meutimao.com.br/noticias-do-corinthians/540135/atacante-entra-no-podio-da-artilharia-do-corinthians-apos-vitoria-no-brasileirao-feminino",
+      "time": "16/09 · 22:34",
+      "pubDate": "Wed, 16 Sep 2026 19:34:00 -0300"
+    },
+    {
+      "source": "Meu Timao",
+      "color": "#1a7a3a",
+      "title": "Ídolo do Corinthians repercute chance de retorno do clube à semifinal da Libertadores após 14 anos",
+      "url": "https://www.meutimao.com.br/noticias-do-corinthians/540190/idolo-do-corinthians-repercute-chance-de-retorno-do-clube-a-semifinal-da-libertadores-apos-14-anos",
+      "time": "16/09 · 21:24",
+      "pubDate": "Wed, 16 Sep 2026 18:24:01 -0300"
+    },
+    {
+      "source": "Gazeta Esportiva",
+      "color": "#E5820A",
+      "title": "Corinthians quita salários atrasados do elenco antes de decisão na Libertadores",
+      "url": "https://www.gazetaesportiva.com/times/corinthians/corinthians-quita-salarios-atrasados-do-elenco-antes-de-decisao-na-libertadores/",
+      "time": "16/09 · 21:09",
+      "pubDate": "Wed, 16 Sep 2026 21:09:59 +0000"
+    },
+    {
+      "source": "Meu Timao",
+      "color": "#1a7a3a",
+      "title": "FPF repudia e lamenta episódios de homofobia sofridos por jogadoras do Corinthians",
+      "url": "https://www.meutimao.com.br/noticias-do-corinthians/540184/fpf-repudia-e-lamenta-episodios-de-homofobia-sofridos-por-jogadoras-do-corinthians",
+      "time": "16/09 · 20:48",
+      "pubDate": "Wed, 16 Sep 2026 17:48:00 -0300"
+    },
+    {
+      "source": "Meu Timao",
+      "color": "#1a7a3a",
+      "title": "Corinthians quita salários atrasados do elenco masculino antes de duelo contra o Estudiantes",
+      "url": "https://www.meutimao.com.br/noticias-do-corinthians/540188/corinthians-quita-salarios-atrasados-do-elenco-masculino-antes-de-duelo-contra-o-estudiantes",
+      "time": "16/09 · 20:33",
+      "pubDate": "Wed, 16 Sep 2026 17:33:00 -0300"
+    },
+    {
+      "source": "Meu Timao",
+      "color": "#1a7a3a",
+      "title": "Corinthians conhece adversário da segunda fase do Campeonato Paulista Sub-20; saiba os detalhes",
+      "url": "https://www.meutimao.com.br/noticias-do-corinthians/540187/corinthians-conhece-adversario-da-segunda-fase-do-campeonato-paulista-sub-20-saiba-os-detalhes",
+      "time": "16/09 · 20:31",
+      "pubDate": "Wed, 16 Sep 2026 17:31:00 -0300"
+    },
     {
       "source": "Gazeta Esportiva",
       "color": "#E5820A",
@@ -27,60 +91,12 @@ const LIVE_DATA = {
       "pubDate": "Wed, 16 Sep 2026 16:31:00 -0300"
     },
     {
-      "source": "Meu Timao",
-      "color": "#1a7a3a",
-      "title": "Corinthians busca voltar a se classificar às semifinais da Libertadores após 14 anos",
-      "url": "https://www.meutimao.com.br/noticias-do-corinthians/540060/corinthians-busca-voltar-a-se-classificar-as-semifinais-da-libertadores-apos-14-anos",
-      "time": "16/09 · 18:30",
-      "pubDate": "Wed, 16 Sep 2026 15:30:00 -0300"
-    },
-    {
-      "source": "Meu Timao",
-      "color": "#1a7a3a",
-      "title": "Lula sugere venda da Fazendinha para Corinthians quitar dívida da Neo Química Arena com a Caixa",
-      "url": "https://www.meutimao.com.br/noticias-do-corinthians/540175/lula-sugere-venda-da-fazendinha-para-corinthians-quitar-divida-da-neo-quimica-arena-com-a-caixa",
-      "time": "16/09 · 18:02",
-      "pubDate": "Wed, 16 Sep 2026 15:02:00 -0300"
-    },
-    {
-      "source": "Meu Timao",
-      "color": "#1a7a3a",
-      "title": "Romero provoca São Paulo após eliminação na Sul-Americana e relembra tempos de Corinthians: 'Normal'",
-      "url": "https://www.meutimao.com.br/noticias-do-corinthians/540176/romero-provoca-sao-paulo-apos-eliminacao-na-sul-americana-e-relembra-tempos-de-corinthians-normal",
-      "time": "16/09 · 17:45",
-      "pubDate": "Wed, 16 Sep 2026 14:45:00 -0300"
-    },
-    {
-      "source": "Meu Timao",
-      "color": "#1a7a3a",
-      "title": "Quanto o Corinthians pode faturar se eliminar o Estudiantes na Libertadores?",
-      "url": "https://www.meutimao.com.br/noticias-do-corinthians/540153/quanto-o-corinthians-pode-faturar-se-eliminar-o-estudiantes-na-libertadores",
-      "time": "16/09 · 17:37",
-      "pubDate": "Wed, 16 Sep 2026 14:37:00 -0300"
-    },
-    {
       "source": "Gazeta Esportiva",
       "color": "#E5820A",
       "title": "Fábio Santos completa 41 anos: confira gols do lateral com a camisa do Corinthians",
       "url": "https://www.gazetaesportiva.com/times/corinthians/fabio-santos-completa-41-anos-confira-gols-do-lateral-com-a-camisa-do-corinthians/",
       "time": "16/09 · 16:43",
       "pubDate": "Wed, 16 Sep 2026 16:43:36 +0000"
-    },
-    {
-      "source": "Meu Timao",
-      "color": "#1a7a3a",
-      "title": "Corinthians busca evitar feito negativo no Sub-17 que não acontece desde 2018",
-      "url": "https://www.meutimao.com.br/noticias-do-corinthians/540114/corinthians-busca-evitar-feito-negativo-no-sub-17-que-nao-acontece-desde-2018",
-      "time": "16/09 · 16:27",
-      "pubDate": "Wed, 16 Sep 2026 13:27:00 -0300"
-    },
-    {
-      "source": "Meu Timao",
-      "color": "#1a7a3a",
-      "title": "Nike inaugura nova loja e confirma data de lançamento da terceira camisa do Corinthians",
-      "url": "https://www.meutimao.com.br/noticias-do-corinthians/540170/nike-lanca-nova-loja-e-confirma-data-de-lancamento-da-terceira-camisa-do-corinthians",
-      "time": "16/09 · 16:08",
-      "pubDate": "Wed, 16 Sep 2026 13:08:00 -0300"
     },
     {
       "source": "Gazeta Esportiva",
@@ -99,6 +115,14 @@ const LIVE_DATA = {
       "pubDate": "Wed, 16 Sep 2026 14:56:25 +0000"
     },
     {
+      "source": "Bolavip",
+      "color": "#0055A5",
+      "title": "Opinião: Corinthians chega ao jogo do ano precisando ser maior que a própria crise",
+      "url": "<![CDATA[https://br.bolavip.com/corinthians/opiniao-corinthians-chega-ao-jogo-do-ano-precisando-ser-maior-que-a-propria-crise]]>",
+      "time": "16/09 · 14:18",
+      "pubDate": "Wed, 16 Sep 2026 11:18:47 -0300"
+    },
+    {
       "source": "Gazeta Esportiva",
       "color": "#E5820A",
       "title": "Corinthians pronto para decisão; Kaio César projeta partida",
@@ -115,36 +139,12 @@ const LIVE_DATA = {
       "pubDate": "Wed, 16 Sep 2026 08:00:50 +0000"
     },
     {
-      "source": "Gazeta Esportiva",
-      "color": "#E5820A",
-      "title": "Corinthians vence Bahia fora de casa e abre vantagem na semifinal do Brasileiro feminino",
-      "url": "https://www.gazetaesportiva.com/times/corinthians/corinthians-vence-bahia-fora-de-casa-e-abre-vantagem-na-semifinal-do-brasileiro-feminino/",
-      "time": "16/09 · 02:48",
-      "pubDate": "Wed, 16 Sep 2026 02:48:18 +0000"
-    },
-    {
       "source": "Bolavip",
       "color": "#0055A5",
       "title": "André Ramalho quer permanecer no Corinthians, mas renovação ainda não foi discutida",
       "url": "<![CDATA[https://br.bolavip.com/corinthians/andre-ramalho-quer-permanecer-no-corinthians-mas-renovacao-ainda-nao-foi-discutida]]>",
       "time": "16/09 · 00:34",
       "pubDate": "Tue, 15 Sep 2026 21:34:29 -0300"
-    },
-    {
-      "source": "Bolavip",
-      "color": "#0055A5",
-      "title": "Opinião: Corinthians chega ao jogo do ano precisando ser maior que a própria crise",
-      "url": "<![CDATA[https://br.bolavip.com/corinthians/opiniao-corinthians-chega-ao-jogo-do-ano-precisando-ser-maior-que-a-propria-crise]]>",
-      "time": "16/09 · 00:33",
-      "pubDate": "Tue, 15 Sep 2026 21:33:58 -0300"
-    },
-    {
-      "source": "Gazeta Esportiva",
-      "color": "#E5820A",
-      "title": "Escalação: André deve reforçar Corinthians contra o Estudiantes, mas Yuri segue fora",
-      "url": "https://www.gazetaesportiva.com/times/corinthians/escalacao-andre-deve-reforcar-corinthians-contra-o-estudiantes-mas-yuri-segue-fora/",
-      "time": "16/09 · 00:23",
-      "pubDate": "Wed, 16 Sep 2026 00:23:04 +0000"
     },
     {
       "source": "Bolavip",
@@ -204,12 +204,6 @@ const LIVE_DATA = {
       "url": "https://www.meutimao.com.br/noticias-do-corinthians/540108/saiba-onde-assistir-a-decisao-entre-corinthians-e-estudiantes-nas-quartas-de-final-da-libertadores",
       "pubDate": "Wed, 16 Sep 2026 08:30:00 -0300",
       "date": "16/09 · 11:30"
-    },
-    {
-      "title": "Saiba como assistir à dupla do Corinthians contra os EUA pelas oitavas da Copa do Mundo Sub-20",
-      "url": "https://www.meutimao.com.br/noticias-do-corinthians/540081/saiba-como-assistir-a-dupla-do-corinthians-contra-os-eua-pelas-oitavas-da-copa-do-mundo-sub-20",
-      "pubDate": "Tue, 15 Sep 2026 22:30:00 -0300",
-      "date": "16/09 · 01:30"
     }
   ]
 };
