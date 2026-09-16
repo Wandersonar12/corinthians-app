11343ee9e8ab00899a8d6b9334785cb7bcc1a0b4
GitHub Action <action@github.com>
2026-09-16 12:52:29 +0000

    Dados atualizados

--- src/data.js
+++ src/data.js
@@ -1,62 +1,110 @@
-// Gerado automaticamente em 15/09/2026, 23:47:05
+// Gerado automaticamente em 16/09/2026, 12:52:29
 const LIVE_DATA = {
-  "updatedAt": "2026-09-15T23:47:05.515Z",
+  "updatedAt": "2026-09-16T12:52:29.340Z",
   "news": [
     {
       "source": "Meu Timao",
       "color": "#1a7a3a",
-      "title": "Atacante projeta decisão contra o Estudiantes e confia em apoio da torcida do Corinthians",
-      "url": "https://www.meutimao.com.br/noticias-do-corinthians/540129/atacante-projeta-decisao-contra-o-estudiantes-e-confia-em-apoio-da-torcida-do-corinthians",
-      "time": "15/09 · 23:40",
-      "pubDate": "Tue, 15 Sep 2026 20:40:00 -0300"
-    },
-    {
-      "source": "Meu Timao",
-      "color": "#1a7a3a",
-      "title": "Corinthians disputará seu 86º jogo de mata-mata na Neo Química Arena; relembre todos",
-      "url": "https://www.meutimao.com.br/noticias-do-corinthians/540041/corinthians-disputara-seu-86-jogo-de-mata-mata-na-neo-quimica-arena-relembre-todos",
-      "time": "15/09 · 23:31",
-      "pubDate": "Tue, 15 Sep 2026 20:31:00 -0300"
-    },
-    {
-      "source": "Meu Timao",
-      "color": "#1a7a3a",
-      "title": "Corinthians está escalado para o primeiro jogo das semifinais do Brasileirão Feminino",
-      "url": "https://www.meutimao.com.br/noticias-do-corinthians/540082/corinthians-esta-escalado-para-o-primeiro-jogo-das-semifinais-do-brasileirao-feminino",
-      "time": "15/09 · 23:30",
-      "pubDate": "Tue, 15 Sep 2026 20:30:00 -0300"
-    },
-    {
-      "source": "Meu Timao",
-      "color": "#1a7a3a",
-      "title": "Corinthians encerra preparação para decisão contra o Estudiantes; veja a provável escalação",
-      "url": "https://www.meutimao.com.br/noticias-do-corinthians/540102/corinthians-encerra-preparacao-para-decisao-contra-o-estudiantes-veja-a-provavel-escalacao",
-      "time": "15/09 · 23:26",
-      "pubDate": "Tue, 15 Sep 2026 20:26:00 -0300"
-    },
-    {
-      "source": "Meu Timao",
-      "color": "#1a7a3a",
-      "title": "Gaviões da Fiel pede 'apuração rigorosa' em denúncia do MP-SP contra presidente do Corinthians",
-      "url": "https://www.meutimao.com.br/noticias-do-corinthians/540128/gavioes-da-fiel-pede-apuracao-rigorosa-em-denuncia-do-mp-sp-contra-presidente-do-corinthians",
-      "time": "15/09 · 23:23",
-      "pubDate": "Tue, 15 Sep 2026 20:23:00 -0300"
-    },
-    {
-      "source": "Meu Timao",
-      "color": "#1a7a3a",
-      "title": "Corinthians se junta a atletas olímpicas em resposta a comercial polêmico de atriz americana",
-      "url": "https://www.meutimao.com.br/noticias-do-corinthians/540123/corinthians-se-junta-a-atletas-olimpicas-em-resposta-a-comercial-polemico-de-atriz-americana",
-      "time": "15/09 · 23:02",
-      "pubDate": "Tue, 15 Sep 2026 20:02:00 -0300"
+      "title": "Corinthians recebe o Atlético Mineiro em busca de voltar a vencer no Brasileiro Sub-17; saiba tudo",
+      "url": "https://www.meutimao.com.br/noticias-do-corinthians/540110/corinthians-recebe-o-atletico-mineiro-em-busca-de-voltar-a-vencer-no-brasileiro-sub-17-saiba-tudo",
+      "time": "16/09 · 12:34",
+      "pubDate": "Wed, 16 Sep 2026 09:34:00 -0300"
     },
     {
       "source": "Gazeta Esportiva",
       "color": "#E5820A",
-      "title": "Corinthians x Estudiantes pela Libertadores: onde assistir, estatísticas, histórico e escalações",
-      "url": "https://www.gazetaesportiva.com/campeonatos/libertadores-da-america/corinthians-estudiantes-libertadores-16-09-2026/",
-      "time": "15/09 · 23:00",
-      "pubDate": "Tue, 15 Sep 2026 23:00:07 +0000"
+      "title": "Corinthians pronto para decisão; Kaio César projeta partida",
+      "url": "https://www.gazetaesportiva.com/times/corinthians/corinthians-pronto-para-decisao-kaio-cesar-projeta-partida/",
+      "time": "16/09 · 12:20",
+      "pubDate": "Wed, 16 Sep 2026 12:20:34 +0000"
+    },
+    {
+      "source": "Meu Timao",
+      "color": "#1a7a3a",
+      "title": "Tamires leva cinco pontos na cabeça após vitória do Corinthians sobre o Bahia",
+      "url": "https://www.meutimao.com.br/noticias-do-corinthians/540148/tamires-leva-cinco-pontos-na-cabeca-apos-vitoria-do-corinthians-sobre-o-bahia",
+      "time": "16/09 · 12:00",
+      "pubDate": "Wed, 16 Sep 2026 09:00:00 -0300"
+    },
+    {
+      "source": "Meu Timao",
+      "color": "#1a7a3a",
+      "title": "Saiba onde assistir à decisão entre Corinthians e Estudiantes nas quartas de final da Libertadores",
+      "url": "https://www.meutimao.com.br/noticias-do-corinthians/540108/saiba-onde-assistir-a-decisao-entre-corinthians-e-estudiantes-nas-quartas-de-final-da-libertadores",
+      "time": "16/09 · 11:30",
+      "pubDate": "Wed, 16 Sep 2026 08:30:00 -0300"
+    },
+    {
+      "source": "Meu Timao",
+      "color": "#1a7a3a",
+      "title": "Castán relembra batalha contra o Vasco em 2012 e vê Corinthians de Diniz forte em mata-matas",
+      "url": "https://www.meutimao.com.br/noticias-do-corinthians/540120/castan-relembra-batalha-contra-o-vasco-em-2012-e-ve-corinthians-de-diniz-forte-em-mata-matas",
+      "time": "16/09 · 11:00",
+      "pubDate": "Wed, 16 Sep 2026 08:00:00 -0300"
+    },
+    {
+      "source": "Meu Timao",
+      "color": "#1a7a3a",
+      "title": "Últimas do Corinthians: vitória no feminino, presidente denunciado e revelação da SAFiel",
+      "url": "https://www.meutimao.com.br/noticias-do-corinthians/540134/ultimas-do-corinthians-vitoria-no-feminino-presidente-denunciado-e-revelacao-da-safiel",
+      "time": "16/09 · 10:30",
+      "pubDate": "Wed, 16 Sep 2026 07:30:00 -0300"
+    },
+    {
+      "source": "Gazeta Esportiva",
+      "color": "#E5820A",
+      "title": "Premiação, jejum e vaga em 2027: o que está em jogo para o Corinthians na Libertadores",
+      "url": "https://www.gazetaesportiva.com/times/corinthians/premiacao-jejum-e-vaga-em-2027-o-que-esta-em-jogo-para-o-corinthians-na-libertadores/",
+      "time": "16/09 · 08:00",
+      "pubDate": "Wed, 16 Sep 2026 08:00:50 +0000"
+    },
+    {
+      "source": "Meu Timao",
+      "color": "#1a7a3a",
+      "title": "Corinthians recebe o Estudiantes por duelo decisivo nas quartas de final da Libertadores; saiba tudo",
+      "url": "https://www.meutimao.com.br/noticias-do-corinthians/540109/corinthians-recebe-o-estudiantes-por-duelo-decisivo-nas-quartas-de-final-da-libertadores-saiba-tudo",
+      "time": "16/09 · 03:01",
+      "pubDate": "Wed, 16 Sep 2026 00:01:00 -0300"
+    },
+    {
+      "source": "Gazeta Esportiva",
+      "color": "#E5820A",
+      "title": "Corinthians vence Bahia fora de casa e abre vantagem na semifinal do Brasileiro feminino",
+      "url": "https://www.gazetaesportiva.com/times/corinthians/corinthians-vence-bahia-fora-de-casa-e-abre-vantagem-na-semifinal-do-brasileiro-feminino/",
+      "time": "16/09 · 02:48",
+      "pubDate": "Wed, 16 Sep 2026 02:48:18 +0000"
+    },
+    {
+      "source": "Meu Timao",
+      "color": "#1a7a3a",
+      "title": "Corinthians derrota o Bahia e abre vantagem nas semifinais do Brasileirão Feminino",
+      "url": "https://www.meutimao.com.br/noticias-do-corinthians/540130/corinthians-derrota-o-bahia-e-abre-vantagem-nas-semifinais-do-brasileirao-feminino",
+      "time": "16/09 · 02:32",
+      "pubDate": "Tue, 15 Sep 2026 23:32:00 -0300"
+    },
+    {
+      "source": "Meu Timao",
+      "color": "#1a7a3a",
+      "title": "Saiba como assistir à dupla do Corinthians contra os EUA pelas oitavas da Copa do Mundo Sub-20",
+      "url": "https://www.meutimao.com.br/noticias-do-corinthians/540081/saiba-como-assistir-a-dupla-do-corinthians-contra-os-eua-pelas-oitavas-da-copa-do-mundo-sub-20",
+      "time": "16/09 · 01:30",
+      "pubDate": "Tue, 15 Sep 2026 22:30:00 -0300"
+    },
+    {
+      "source": "Gazeta Esportiva",
+      "color": "#E5820A",
+      "title": "Escalação: André deve reforçar Corinthians contra o Estudiantes, mas Yuri segue fora",
+      "url": "https://www.gazetaesportiva.com/times/corinthians/escalacao-andre-deve-reforcar-corinthians-contra-o-estudiantes-mas-yuri-segue-fora/",
+      "time": "16/09 · 00:23",
+      "pubDate": "Wed, 16 Sep 2026 00:23:04 +0000"
+    },
+    {
+      "source": "Gazeta Esportiva",
+      "color": "#E5820A",
+      "title": "Veja galeria de fotos do treino do Corinthians nesta terça-feira",
+      "url": "https://www.gazetaesportiva.com/times/corinthians/veja-galeria-de-fotos-do-treino-do-corinthians-nesta-terca-feira-25/",
+      "time": "16/09 · 00:18",
+      "pubDate": "Wed, 16 Sep 2026 00:18:37 +0000"
     },
     {
       "source": "Gazeta Esportiva",
@@ -67,20 +115,36 @@ const LIVE_DATA = {
       "pubDate": "Tue, 15 Sep 2026 23:00:07 +0000"
     },
     {
-      "source": "Meu Timao",
-      "color": "#1a7a3a",
-      "title": "Técnico do Corinthians prega foco em jogos antes da data Fifa e exalta convocados à Seleção",
-      "url": "https://www.meutimao.com.br/noticias-do-corinthians/540006/tecnico-do-corinthians-prega-foco-em-jogos-antes-da-data-fifa-e-exalta-convocados-a-selecao",
-      "time": "15/09 · 22:35",
-      "pubDate": "Tue, 15 Sep 2026 19:35:00 -0300"
+      "source": "Gazeta Esportiva",
+      "color": "#E5820A",
+      "title": "Corinthians x Estudiantes pela Libertadores: onde assistir, estatísticas, histórico e escalações",
+      "url": "https://www.gazetaesportiva.com/campeonatos/libertadores-da-america/corinthians-estudiantes-libertadores-16-09-2026/",
+      "time": "15/09 · 23:00",
+      "pubDate": "Tue, 15 Sep 2026 23:00:07 +0000"
     },
     {
-      "source": "Meu Timao",
-      "color": "#1a7a3a",
-      "title": "Técnico do Corinthians Sub-17 comenta sobre calendário e projeta duelo contra o Atlético Mineiro",
-      "url": "https://www.meutimao.com.br/noticias-do-corinthians/540121/tecnico-do-corinthians-sub-17-comenta-sobre-calendario-e-projeta-duelo-contra-o-atletico-mineiro",
-      "time": "15/09 · 22:12",
-      "pubDate": "Tue, 15 Sep 2026 19:12:00 -0300"
+      "source": "Bolavip",
+      "color": "#0055A5",
+      "title": "Opinião: Corinthians chega ao jogo do ano precisando ser maior que a própria crise",
+      "url": "<![CDATA[https://br.bolavip.com/corinthians/opiniao-corinthians-chega-ao-jogo-do-ano-precisando-ser-maior-que-a-propria-crise]]>",
+      "time": "15/09 · 22:32",
+      "pubDate": "Tue, 15 Sep 2026 19:32:57 -0300"
+    },
+    {
+      "source": "Bolavip",
+      "color": "#0055A5",
+      "title": "Corinthians regulariza direitos de imagem atrasados do feminino antes de decisão",
+      "url": "<![CDATA[https://br.bolavip.com/corinthians/corinthians-regulariza-direitos-de-imagem-atrasados-do-feminino-antes-de-decisao]]>",
+      "time": "15/09 · 21:17",
+      "pubDate": "Tue, 15 Sep 2026 18:17:26 -0300"
+    },
+    {
+      "source": "Bolavip",
+      "color": "#0055A5",
+      "title": "Raio-X do Atleta: como Breno Bidon ganhou espaço como peça versátil do Corinthians",
+      "url": "<![CDATA[https://br.bolavip.com/corinthians/raio-x-do-atleta-como-breno-bidon-ganhou-espaco-como-peca-versatil-do-corinthians]]>",
+      "time": "15/09 · 20:49",
+      "pubDate": "Tue, 15 Sep 2026 17:49:34 -0300"
     },
     {
       "source": "Bolavip",
@@ -123,82 +187,30 @@ const LIVE_DATA = {
       "pubDate": "Tue, 15 Sep 2026 14:50:22 -0300"
     },
     {
-      "source": "Gazeta Esportiva",
-      "color": "#E5820A",
-      "title": "MP denuncia Stabile e pede afastamento do presidente do Corinthians",
-      "url": "https://www.gazetaesportiva.com/times/corinthians/mp-stabile-afastamento-corinthians/",
-      "time": "15/09 · 17:12",
-      "pubDate": "Tue, 15 Sep 2026 17:12:26 +0000"
-    },
-    {
-      "source": "Bolavip",
-      "color": "#0055A5",
-      "title": "Raio-X do Atleta: como Gui Negão retomou espaço no ataque do Corinthians",
-      "url": "<![CDATA[https://br.bolavip.com/corinthians/raio-x-do-atleta-como-gui-negao-retomou-espaco-no-ataque-do-corinthians]]>",
-      "time": "15/09 · 16:56",
-      "pubDate": "Tue, 15 Sep 2026 13:56:30 -0300"
-    },
-    {
       "source": "Bolavip",
       "color": "#0055A5",
       "title": "Yuri Alberto dá novo passo na recuperação após regressão e vai ao campo no CT do Corinthians",
       "url": "<![CDATA[https://br.bolavip.com/corinthians/yuri-alberto-da-novo-passo-na-recuperacao-apos-regressao-e-vai-ao-campo-no-ct-do-corinthians]]>",
       "time": "15/09 · 16:54",
       "pubDate": "Tue, 15 Sep 2026 13:54:38 -0300"
-    },
-    {
-      "source": "Gazeta Esportiva",
-      "color": "#E5820A",
-      "title": "Corinthians retoma preparação para duelo decisivo contra o Estudiantes",
-      "url": "https://www.gazetaesportiva.com/todas-as-noticias/corinthians-retoma-preparacao-para-duelo-decisivo-contra-o-estudiantes/",
-      "time": "15/09 · 15:01",
-      "pubDate": "Tue, 15 Sep 2026 15:01:02 +0000"
-    },
-    {
-      "source": "Gazeta Esportiva",
-      "color": "#E5820A",
-      "title": "Gui Negão encerra jejum de mais de sete meses e busca espaço no Corinthians",
-      "url": "https://www.gazetaesportiva.com/times/corinthians/gui-negao-encerra-jejum-de-mais-de-sete-meses-e-busca-espaco-no-corinthians/",
-      "time": "15/09 · 10:00",
-      "pubDate": "Tue, 15 Sep 2026 10:00:18 +0000"
-    },
-    {
-      "source": "Gazeta Esportiva",
-      "color": "#E5820A",
-      "title": "Corinthians x Estudiantes: saiba onde assistir ao jogo de volta das quartas da Libertadores",
-      "url": "https://www.gazetaesportiva.com/campeonatos/libertadores-da-america/corinthians-x-estudiantes-saiba-onde-assistir-ao-jogo-de-volta-das-quartas-da-libertadores/",
-      "time": "15/09 · 09:00",
-      "pubDate": "Tue, 15 Sep 2026 09:00:37 +0000"
-    },
-    {
-      "source": "Gazeta Esportiva",
-      "color": "#E5820A",
-      "title": "Com Corinthians em semana decisiva, Memphis faz reflexão sobre “tempos difíceis”",
-      "url": "https://www.gazetaesportiva.com/times/corinthians/com-corinthians-em-semana-decisiva-memphis-faz-reflexao-sobre-tempos-dificeis/",
-      "time": "15/09 · 02:05",
-      "pubDate": "Tue, 15 Sep 2026 02:05:22 +0000"
-    },
-    {
-      "source": "Bolavip",
-      "color": "#0055A5",
-      "title": "Opinião: Jesse Lingard tem atuação abaixo, e Corinthians precisa discutir como recuperar o inglês",
-      "url": "<![CDATA[https://br.bolavip.com/corinthians/opiniao-jesse-lingard-tem-atuacao-abaixo-e-corinthians-precisa-discutir-como-recuperar-o-ingles]]>",
-      "time": "14/09 · 21:00",
-      "pubDate": "Mon, 14 Sep 2026 18:00:38 -0300"
-    },
-    {
-      "source": "Bolavip",
-      "color": "#0055A5",
-      "title": "Opinião: Iago Machado acende uma luz, e Corinthians pode ter mais respostas dentro de casa",
-      "url": "<![CDATA[https://br.bolavip.com/corinthians/opiniao-iago-machado-acende-uma-luz-e-corinthians-pode-ter-mais-respostas-dentro-de-casa]]>",
-      "time": "14/09 · 20:04",
-      "pubDate": "Mon, 14 Sep 2026 17:04:04 -0300"
     }
   ],
   "upcoming": [],
   "results": [],
   "standings": [],
   "highlights": [
+    {
+      "title": "Saiba onde assistir à decisão entre Corinthians e Estudiantes nas quartas de final da Libertadores",
+      "url": "https://www.meutimao.com.br/noticias-do-corinthians/540108/saiba-onde-assistir-a-decisao-entre-corinthians-e-estudiantes-nas-quartas-de-final-da-libertadores",
+      "pubDate": "Wed, 16 Sep 2026 08:30:00 -0300",
+      "date": "16/09 · 11:30"
+    },
+    {
+      "title": "Saiba como assistir à dupla do Corinthians contra os EUA pelas oitavas da Copa do Mundo Sub-20",
+      "url": "https://www.meutimao.com.br/noticias-do-corinthians/540081/saiba-como-assistir-a-dupla-do-corinthians-contra-os-eua-pelas-oitavas-da-copa-do-mundo-sub-20",
+      "pubDate": "Tue, 15 Sep 2026 22:30:00 -0300",
+      "date": "16/09 · 01:30"
+    },
     {
       "title": "VÍDEO: Corinthians x Estudiantes: Provável escalação para jogo do Corinthians na Conmebol Libertadores",
       "url": "https://www.meutimao.com.br/videos-do-corinthians/129670/corinthians-x-estudiantes-provavel-escalacao-para-jogo-do-corinthians-na-conmebol-libertadores",
@@ -210,12 +222,6 @@ const LIVE_DATA = {
       "url": "https://www.meutimao.com.br/noticias-do-corinthians/540092/jogador-do-corinthians-disputa-mundial-de-futsal-down-e-ajuda-brasil-em-goleada-sobre-argentina",
       "pubDate": "Tue, 15 Sep 2026 14:37:00 -0300",
       "date": "15/09 · 17:37"
-    },
-    {
-      "title": "Saiba onde assistir à partida entre Corinthians e Bahia pela semifinal do Brasileirão Feminino",
-      "url": "https://www.meutimao.com.br/noticias-do-corinthians/540052/saiba-onde-assistir-a-partida-entre-corinthians-e-bahia-pela-semifinal-do-brasileirao-feminino",
-      "pubDate": "Tue, 15 Sep 2026 08:30:00 -0300",
-      "date": "15/09 · 11:30"
     }
   ]
 };
