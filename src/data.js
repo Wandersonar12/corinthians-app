7b78cd91047bce6e5bf3524d40a63698ead5f09d
GitHub Action <action@github.com>
2026-09-24 20:50:35 +0000

    Dados atualizados

--- src/data.js
+++ src/data.js
@@ -1,38 +1,94 @@
-// Gerado automaticamente em 24/09/2026, 12:56:45
+// Gerado automaticamente em 24/09/2026, 20:50:35
 const LIVE_DATA = {
-  "updatedAt": "2026-09-24T12:56:45.952Z",
+  "updatedAt": "2026-09-24T20:50:35.100Z",
   "news": [
     {
       "source": "Meu Timao",
       "color": "#1a7a3a",
-      "title": "Corinthians Feminino ultrapassa marca de R$ 900 mil em bilheteria na temporada 2026",
-      "url": "https://www.meutimao.com.br/noticias-do-corinthians/540657/corinthians-feminino-ultrapassa-marca-de-r-900-mil-em-bilheteria-na-temporada-2026",
-      "time": "24/09 · 12:36",
-      "pubDate": "Thu, 24 Sep 2026 09:36:00 -0300"
+      "title": "Meio-campista do Corinthians destaca evolução da equipe após 'choque' na primeira fase do Brasileiro",
+      "url": "https://www.meutimao.com.br/noticias-do-corinthians/540640/meio-campista-do-corinthians-destaca-evolucao-da-equipe-apos-choque-na-primeira-fase-do-brasileiro",
+      "time": "24/09 · 20:35",
+      "pubDate": "Thu, 24 Sep 2026 17:35:00 -0300"
     },
     {
       "source": "Meu Timao",
       "color": "#1a7a3a",
-      "title": "Membros das últimas quatro gestões do Corinthians acumulam problemas no clube e na Justiça",
-      "url": "https://www.meutimao.com.br/noticias-do-corinthians/540714/membros-das-ultimas-quatro-gestoes-do-corinthians-acumulam-problemas-no-clube-e-na-justica",
-      "time": "24/09 · 11:30",
-      "pubDate": "Thu, 24 Sep 2026 08:30:00 -0300"
+      "title": "Treinadora do Corinthians vive expectativa de conquistar o Brasileirão Feminino pela primeira vez",
+      "url": "https://www.meutimao.com.br/noticias-do-corinthians/540625/treinadora-do-corinthians-vive-expectativa-de-conquistar-o-brasileirao-feminino-pela-primeira-vez",
+      "time": "24/09 · 20:04",
+      "pubDate": "Thu, 24 Sep 2026 17:04:00 -0300"
     },
     {
       "source": "Meu Timao",
       "color": "#1a7a3a",
-      "title": "Ancelotti confirma dupla do Corinthians como titular da Seleção Brasileira contra a Austrália",
-      "url": "https://www.meutimao.com.br/noticias-do-corinthians/540750/ancelotti-confirma-dupla-do-corinthians-como-titular-da-selecao-brasileira-contra-a-australia",
-      "time": "24/09 · 11:20",
-      "pubDate": "Thu, 24 Sep 2026 08:20:00 -0300"
+      "title": "Diretora do Corinthians detalha as condições para renovar contratos com as Brabas",
+      "url": "https://www.meutimao.com.br/noticias-do-corinthians/540755/diretora-do-corinthians-detalha-as-condicoes-para-renovar-contratos-com-as-brabas",
+      "time": "24/09 · 19:32",
+      "pubDate": "Thu, 24 Sep 2026 16:32:00 -0300"
     },
     {
       "source": "Meu Timao",
       "color": "#1a7a3a",
-      "title": "Últimas do Corinthians: retorno de quarteto, marca centenária na base e convocados na Seleção",
-      "url": "https://www.meutimao.com.br/noticias-do-corinthians/540739/ultimas-do-corinthians-retorno-de-quarteto-marca-centenaria-na-base-e-convocados-na-selecao",
-      "time": "24/09 · 10:30",
-      "pubDate": "Thu, 24 Sep 2026 07:30:00 -0300"
+      "title": "Corinthians atrasa parcelas com a União e corre risco de perder renegociação de acordo bilionário",
+      "url": "https://www.meutimao.com.br/noticias-do-corinthians/540787/corinthians-atrasa-parcelas-com-a-uniao-e-corre-risco-de-perder-renegociacao-de-acordo-bilionario",
+      "time": "24/09 · 19:23",
+      "pubDate": "Thu, 24 Sep 2026 16:23:00 -0300"
+    },
+    {
+      "source": "Gazeta Esportiva",
+      "color": "#E5820A",
+      "title": "Corinthians: Neo Química Arena recebe certificação por práticas de gestão ambiental",
+      "url": "https://www.gazetaesportiva.com/times/corinthians/corinthians-neo-quimica-arena-recebe-certificacao-por-praticas-de-gestao-ambiental/",
+      "time": "24/09 · 19:20",
+      "pubDate": "Thu, 24 Sep 2026 19:20:26 +0000"
+    },
+    {
+      "source": "Meu Timao",
+      "color": "#1a7a3a",
+      "title": "Dupla do Corinthians comenta sobre expectativa de Neo Química Arena lotada e importância da torcida",
+      "url": "https://www.meutimao.com.br/noticias-do-corinthians/540781/dupla-do-corinthians-comenta-sobre-expectativa-de-neo-quimica-arena-lotada-e-importancia-da-torcida",
+      "time": "24/09 · 19:01",
+      "pubDate": "Thu, 24 Sep 2026 16:01:00 -0300"
+    },
+    {
+      "source": "Meu Timao",
+      "color": "#1a7a3a",
+      "title": "Corinthians receberá R$ 10 milhões da cota de direitos de transmissão negociada com FFU",
+      "url": "https://www.meutimao.com.br/noticias-do-corinthians/540785/corinthians-recebera-r-10-milhoes-da-cota-de-direitos-de-transmissao-negociada-com-ffu",
+      "time": "24/09 · 18:59",
+      "pubDate": "Thu, 24 Sep 2026 15:59:00 -0300"
+    },
+    {
+      "source": "Meu Timao",
+      "color": "#1a7a3a",
+      "title": "Atacante fala sobre atuar em 'nova' posição e minimiza críticas ao Corinthians",
+      "url": "https://www.meutimao.com.br/noticias-do-corinthians/540637/atacante-fala-sobre-atuar-em-nova-posicao-e-minimiza-criticas-ao-corinthians",
+      "time": "24/09 · 18:32",
+      "pubDate": "Thu, 24 Sep 2026 15:32:00 -0300"
+    },
+    {
+      "source": "Meu Timao",
+      "color": "#1a7a3a",
+      "title": "VÍDEO: Temporada do Corinthians, reedição da final de 2024 e mais: coletiva de Tamires e Andressa Alves",
+      "url": "https://www.meutimao.com.br/videos-do-corinthians/129832/temporada-do-corinthians-reedicao-da-final-de-2024-e-mais-coletiva-de-tamires-e-andressa-alves",
+      "time": "24/09 · 17:58",
+      "pubDate": "Thu, 24 Sep 2026 14:58:48 -0300"
+    },
+    {
+      "source": "Bolavip",
+      "color": "#0055A5",
+      "title": "Corinthians descarta qualquer contato por Luís Castro e segue acreditando no trabalho de Fernando Diniz",
+      "url": "<![CDATA[https://br.bolavip.com/corinthians/corinthians-descarta-qualquer-contato-por-luis-castro-e-segue-acreditando-no-trabalho-de-fernando-diniz]]>",
+      "time": "24/09 · 11:57",
+      "pubDate": "Thu, 24 Sep 2026 08:57:30 -0300"
+    },
+    {
+      "source": "Bolavip",
+      "color": "#0055A5",
+      "title": "Opinião: Corinthians deve receber R$ 10 milhões da FFU e terá uma ajuda importante no caixa",
+      "url": "<![CDATA[https://br.bolavip.com/corinthians/opiniao-corinthians-deve-receber-r-10-milhoes-da-ffu-e-tera-uma-ajuda-importante-no-caixa]]>",
+      "time": "24/09 · 11:48",
+      "pubDate": "Thu, 24 Sep 2026 08:48:32 -0300"
     },
     {
       "source": "Gazeta Esportiva",
@@ -43,52 +99,12 @@ const LIVE_DATA = {
       "pubDate": "Thu, 24 Sep 2026 10:00:10 +0000"
     },
     {
-      "source": "Meu Timao",
-      "color": "#1a7a3a",
-      "title": "Técnica do Corinthians comenta emoção de primeira final com as Brabas",
-      "url": "https://www.meutimao.com.br/noticias-do-corinthians/540628/tecnica-do-corinthians-comenta-emocao-de-primeira-final-com-as-brabas",
-      "time": "24/09 · 01:30",
-      "pubDate": "Wed, 23 Sep 2026 22:30:00 -0300"
-    },
-    {
-      "source": "Meu Timao",
-      "color": "#1a7a3a",
-      "title": "Corinthians perde para o Mogi e se complica na semifinal do Paulista",
-      "url": "https://www.meutimao.com.br/noticias-do-corinthians/540738/corinthians-perde-para-o-mogi-e-se-complica-na-semifinal-do-paulista",
-      "time": "24/09 · 01:02",
-      "pubDate": "Wed, 23 Sep 2026 22:02:00 -0300"
-    },
-    {
-      "source": "Meu Timao",
-      "color": "#1a7a3a",
-      "title": "Ex-zagueiro do Corinthians relembra parceria com Diniz e comenta processo de formação na base",
-      "url": "https://www.meutimao.com.br/noticias-do-corinthians/540690/ex-zagueiro-do-corinthians-relembra-parceria-com-diniz-e-comenta-processo-de-formacao-na-base",
-      "time": "24/09 · 00:31",
-      "pubDate": "Wed, 23 Sep 2026 21:31:00 -0300"
-    },
-    {
-      "source": "Meu Timao",
-      "color": "#1a7a3a",
-      "title": "Corinthians conhece grupo e detalhes da Copinha Feminina de 2026",
-      "url": "https://www.meutimao.com.br/noticias-do-corinthians/540720/corinthians-conhece-grupo-e-detalhes-da-copinha-feminina-de-2026",
-      "time": "24/09 · 00:20",
-      "pubDate": "Wed, 23 Sep 2026 21:20:00 -0300"
-    },
-    {
       "source": "Gazeta Esportiva",
       "color": "#E5820A",
       "title": "Corinthians avança em conversas com a Caixa por dívida da Neo Química Arena",
       "url": "https://www.gazetaesportiva.com/times/corinthians/corinthians-avanca-em-conversas-com-a-caixa-por-divida-da-neo-quimica-arena/",
       "time": "23/09 · 23:02",
       "pubDate": "Wed, 23 Sep 2026 23:02:08 +0000"
-    },
-    {
-      "source": "Bolavip",
-      "color": "#0055A5",
-      "title": "Opinião: Corinthians deve receber R$ 10 milhões da FFU e terá uma ajuda importante no caixa",
-      "url": "<![CDATA[https://br.bolavip.com/corinthians/opiniao-corinthians-deve-receber-r-10-milhoes-da-ffu-e-tera-uma-ajuda-importante-no-caixa]]>",
-      "time": "23/09 · 22:28",
-      "pubDate": "Wed, 23 Sep 2026 19:28:44 -0300"
     },
     {
       "source": "Gazeta Esportiva",
@@ -177,22 +193,6 @@ const LIVE_DATA = {
       "url": "https://www.gazetaesportiva.com/times/corinthians/ex-corinthians-e-santos-meia-giuliano-anuncia-aposentadoria/",
       "time": "22/09 · 23:18",
       "pubDate": "Tue, 22 Sep 2026 23:18:24 +0000"
-    },
-    {
-      "source": "Bolavip",
-      "color": "#0055A5",
-      "title": "Opinião: Diniz precisa repensar esquema para fazer Corinthians voltar a funcionar",
-      "url": "<![CDATA[https://br.bolavip.com/corinthians/opiniao-diniz-precisa-repensar-esquema-para-fazer-corinthians-voltar-a-funcionar]]>",
-      "time": "22/09 · 22:39",
-      "pubDate": "Tue, 22 Sep 2026 19:39:32 -0300"
-    },
-    {
-      "source": "Gazeta Esportiva",
-      "color": "#E5820A",
-      "title": "Corinthians faz seis no Juventude e garante vaga no mata-mata do Brasileiro sub-17",
-      "url": "https://www.gazetaesportiva.com/times/corinthians/corinthians-faz-seis-no-juventude-e-garante-vaga-no-mata-mata-do-brasileiro-sub-17/",
-      "time": "22/09 · 20:34",
-      "pubDate": "Tue, 22 Sep 2026 20:34:29 +0000"
     }
   ],
   "upcoming": [],
@@ -200,10 +200,22 @@ const LIVE_DATA = {
   "standings": [],
   "highlights": [
     {
-      "title": "Miguel Moscardo completa 100 gols em seu terceiro ano na base do Corinthians",
-      "url": "https://www.meutimao.com.br/noticias-do-corinthians/540698/miguel-moscardo-completa-100-gols-em-seu-terceiro-ano-na-base-do-corinthians",
-      "pubDate": "Wed, 23 Sep 2026 11:25:00 -0300",
-      "date": "23/09 · 14:25"
+      "title": "VÍDEO: Temporada do Corinthians, reedição da final de 2024 e mais: coletiva de Tamires e Andressa Alves",
+      "url": "https://www.meutimao.com.br/videos-do-corinthians/129832/temporada-do-corinthians-reedicao-da-final-de-2024-e-mais-coletiva-de-tamires-e-andressa-alves",
+      "pubDate": "Thu, 24 Sep 2026 14:58:48 -0300",
+      "date": "24/09 · 17:58"
+    },
+    {
+      "title": "Goleada do Corinthians sobre o Juventude movimenta artilharia do Sub-17",
+      "url": "https://www.meutimao.com.br/noticias-do-corinthians/540716/goleada-do-corinthians-sobre-o-juventude-movimenta-artilharia-do-sub-17",
+      "pubDate": "Thu, 24 Sep 2026 13:32:00 -0300",
+      "date": "24/09 · 16:32"
+    },
+    {
+      "title": "VÍDEO: Treino aberto do Corinthians antes do jogo de ida da final do Brasileirão Feminino",
+      "url": "https://www.meutimao.com.br/videos-do-corinthians/129828/treino-aberto-do-corinthians-antes-do-jogo-de-ida-da-final-do-brasileirao-feminino",
+      "pubDate": "Thu, 24 Sep 2026 11:34:23 -0300",
+      "date": "24/09 · 14:34"
     }
   ]
 };
