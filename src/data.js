acbffa627cd9407e3e2e55a51962e4023c96738e
GitHub Action <action@github.com>
2026-09-25 00:06:35 +0000

    Dados atualizados

--- src/data.js
+++ src/data.js
@@ -1,7 +1,95 @@
-// Gerado automaticamente em 24/09/2026, 20:50:35
+// Gerado automaticamente em 25/09/2026, 00:06:34
 const LIVE_DATA = {
-  "updatedAt": "2026-09-24T20:50:35.100Z",
+  "updatedAt": "2026-09-25T00:06:34.960Z",
   "news": [
+    {
+      "source": "Meu Timao",
+      "color": "#1a7a3a",
+      "title": "Pioneira do Corinthians celebra evolução do futebol feminino e relembra tempos no Parque São Jorge",
+      "url": "https://www.meutimao.com.br/noticias-do-corinthians/540763/pioneira-do-corinthians-celebra-evolucao-do-futebol-feminino-e-relembra-tempos-no-parque-sao-jorge",
+      "time": "24/09 · 23:31",
+      "pubDate": "Thu, 24 Sep 2026 20:31:00 -0300"
+    },
+    {
+      "source": "Meu Timao",
+      "color": "#1a7a3a",
+      "title": "Dupla do Corinthians projeta final do Brasileirão Feminino contra o São Paulo",
+      "url": "https://www.meutimao.com.br/noticias-do-corinthians/540782/dupla-do-corinthians-projeta-final-do-brasileirao-feminino-contra-o-sao-paulo",
+      "time": "24/09 · 23:03",
+      "pubDate": "Thu, 24 Sep 2026 20:03:00 -0300"
+    },
+    {
+      "source": "Gazeta Esportiva",
+      "color": "#E5820A",
+      "title": "Corinthians x XV de Jaú: veja prováveis escalações e onde assistir ao jogo do Paulista sub-20",
+      "url": "https://www.gazetaesportiva.com/times/corinthians/corinthians-x-xv-de-jau-veja-provaveis-escalacoes-e-onde-assistir-ao-jogo-do-paulista-sub-20/",
+      "time": "24/09 · 23:00",
+      "pubDate": "Thu, 24 Sep 2026 23:00:39 +0000"
+    },
+    {
+      "source": "Meu Timao",
+      "color": "#1a7a3a",
+      "title": "Emily Lima comenta sobre críticas ao seu trabalho e exalta mentalidade do elenco do Corinthians",
+      "url": "https://www.meutimao.com.br/noticias-do-corinthians/540632/emily-lima-comenta-sobre-criticas-ao-seu-trabalho-e-exalta-mentalidade-do-elenco-do-corinthians",
+      "time": "24/09 · 22:33",
+      "pubDate": "Thu, 24 Sep 2026 19:33:00 -0300"
+    },
+    {
+      "source": "Meu Timao",
+      "color": "#1a7a3a",
+      "title": "Goleiro de Corinthians se torna novo embaixador da 7K Poker",
+      "url": "https://www.meutimao.com.br/noticias-do-corinthians/540779/goleiro-de-corinthians-se-torna-novo-embaixador-da-7k-poker",
+      "time": "24/09 · 22:01",
+      "pubDate": "Thu, 24 Sep 2026 19:01:00 -0300"
+    },
+    {
+      "source": "Gazeta Esportiva",
+      "color": "#E5820A",
+      "title": "São Paulo divulga venda de ingressos para decisão do Brasileirão Feminino contra o Corinthians",
+      "url": "https://www.gazetaesportiva.com/times/sao-paulo/sao-paulo-ingressos-final-brasileirao-feminino-corinthians/",
+      "time": "24/09 · 21:53",
+      "pubDate": "Thu, 24 Sep 2026 21:53:29 +0000"
+    },
+    {
+      "source": "Meu Timao",
+      "color": "#1a7a3a",
+      "title": "Atacante do Corinthians projeta duelo por vaga nas oitavas do Paulista Sub-20",
+      "url": "https://www.meutimao.com.br/noticias-do-corinthians/540794/atacante-do-corinthians-projeta-duelo-por-vaga-nas-oitavas-do-paulista-sub-20",
+      "time": "24/09 · 21:43",
+      "pubDate": "Thu, 24 Sep 2026 18:43:25 -0300"
+    },
+    {
+      "source": "Meu Timao",
+      "color": "#1a7a3a",
+      "title": "Técnico do Corinthians aponta falhas contra o Mogi e comenta postura frente à arbitragem",
+      "url": "https://www.meutimao.com.br/noticias-do-corinthians/540752/tecnico-do-corinthians-aponta-falhas-contra-o-mogi-e-comenta-postura-frente-a-arbitragem",
+      "time": "24/09 · 21:35",
+      "pubDate": "Thu, 24 Sep 2026 18:35:00 -0300"
+    },
+    {
+      "source": "Meu Timao",
+      "color": "#1a7a3a",
+      "title": "Ju Passero comenta expectativa pela disputa da Copinha Feminina com o Corinthians Sub-20",
+      "url": "https://www.meutimao.com.br/noticias-do-corinthians/540761/ju-passero-comenta-expectativa-pela-disputa-da-copinha-feminina-com-o-corinthians-sub-20",
+      "time": "24/09 · 21:01",
+      "pubDate": "Thu, 24 Sep 2026 18:01:00 -0300"
+    },
+    {
+      "source": "Bolavip",
+      "color": "#0055A5",
+      "title": "Opinião: Kaio César merece titularidade com Diniz no Corinthians mesmo com retorno de Yuri Alberto",
+      "url": "<![CDATA[https://br.bolavip.com/corinthians/opiniao-kaio-cesar-merece-titularidade-com-diniz-no-corinthians-mesmo-com-retorno-de-yuri-alberto]]>",
+      "time": "24/09 · 20:40",
+      "pubDate": "Thu, 24 Sep 2026 17:40:29 -0300"
+    },
+    {
+      "source": "Bolavip",
+      "color": "#0055A5",
+      "title": "Arthur Sousa é destaque jogando nos Estados Unidos em 2026 e pode gerar lucro aos cofres do Corinthians",
+      "url": "<![CDATA[https://br.bolavip.com/corinthians/arthur-sousa-e-destaque-jogando-nos-estados-unidos-em-2026-e-pode-gerar-lucro-aos-cofres-do-corinthians]]>",
+      "time": "24/09 · 20:39",
+      "pubDate": "Thu, 24 Sep 2026 17:39:52 -0300"
+    },
     {
       "source": "Meu Timao",
       "color": "#1a7a3a",
@@ -9,30 +97,6 @@ const LIVE_DATA = {
       "url": "https://www.meutimao.com.br/noticias-do-corinthians/540640/meio-campista-do-corinthians-destaca-evolucao-da-equipe-apos-choque-na-primeira-fase-do-brasileiro",
       "time": "24/09 · 20:35",
       "pubDate": "Thu, 24 Sep 2026 17:35:00 -0300"
-    },
-    {
-      "source": "Meu Timao",
-      "color": "#1a7a3a",
-      "title": "Treinadora do Corinthians vive expectativa de conquistar o Brasileirão Feminino pela primeira vez",
-      "url": "https://www.meutimao.com.br/noticias-do-corinthians/540625/treinadora-do-corinthians-vive-expectativa-de-conquistar-o-brasileirao-feminino-pela-primeira-vez",
-      "time": "24/09 · 20:04",
-      "pubDate": "Thu, 24 Sep 2026 17:04:00 -0300"
-    },
-    {
-      "source": "Meu Timao",
-      "color": "#1a7a3a",
-      "title": "Diretora do Corinthians detalha as condições para renovar contratos com as Brabas",
-      "url": "https://www.meutimao.com.br/noticias-do-corinthians/540755/diretora-do-corinthians-detalha-as-condicoes-para-renovar-contratos-com-as-brabas",
-      "time": "24/09 · 19:32",
-      "pubDate": "Thu, 24 Sep 2026 16:32:00 -0300"
-    },
-    {
-      "source": "Meu Timao",
-      "color": "#1a7a3a",
-      "title": "Corinthians atrasa parcelas com a União e corre risco de perder renegociação de acordo bilionário",
-      "url": "https://www.meutimao.com.br/noticias-do-corinthians/540787/corinthians-atrasa-parcelas-com-a-uniao-e-corre-risco-de-perder-renegociacao-de-acordo-bilionario",
-      "time": "24/09 · 19:23",
-      "pubDate": "Thu, 24 Sep 2026 16:23:00 -0300"
     },
     {
       "source": "Gazeta Esportiva",
@@ -43,36 +107,20 @@ const LIVE_DATA = {
       "pubDate": "Thu, 24 Sep 2026 19:20:26 +0000"
     },
     {
-      "source": "Meu Timao",
-      "color": "#1a7a3a",
-      "title": "Dupla do Corinthians comenta sobre expectativa de Neo Química Arena lotada e importância da torcida",
-      "url": "https://www.meutimao.com.br/noticias-do-corinthians/540781/dupla-do-corinthians-comenta-sobre-expectativa-de-neo-quimica-arena-lotada-e-importancia-da-torcida",
-      "time": "24/09 · 19:01",
-      "pubDate": "Thu, 24 Sep 2026 16:01:00 -0300"
+      "source": "Bolavip",
+      "color": "#0055A5",
+      "title": "Análise: Garro, Yuri Alberto e Depay juntos tem aproveitamento abaixo do esperado em 2026 pelo Corinthians",
+      "url": "<![CDATA[https://br.bolavip.com/corinthians/analise-garro-yuri-alberto-e-depay-juntos-tem-aproveitamento-abaixo-do-esperado-em-2026-pelo-corinthians]]>",
+      "time": "24/09 · 16:57",
+      "pubDate": "Thu, 24 Sep 2026 13:57:19 -0300"
     },
     {
-      "source": "Meu Timao",
-      "color": "#1a7a3a",
-      "title": "Corinthians receberá R$ 10 milhões da cota de direitos de transmissão negociada com FFU",
-      "url": "https://www.meutimao.com.br/noticias-do-corinthians/540785/corinthians-recebera-r-10-milhoes-da-cota-de-direitos-de-transmissao-negociada-com-ffu",
-      "time": "24/09 · 18:59",
-      "pubDate": "Thu, 24 Sep 2026 15:59:00 -0300"
-    },
-    {
-      "source": "Meu Timao",
-      "color": "#1a7a3a",
-      "title": "Atacante fala sobre atuar em 'nova' posição e minimiza críticas ao Corinthians",
-      "url": "https://www.meutimao.com.br/noticias-do-corinthians/540637/atacante-fala-sobre-atuar-em-nova-posicao-e-minimiza-criticas-ao-corinthians",
-      "time": "24/09 · 18:32",
-      "pubDate": "Thu, 24 Sep 2026 15:32:00 -0300"
-    },
-    {
-      "source": "Meu Timao",
-      "color": "#1a7a3a",
-      "title": "VÍDEO: Temporada do Corinthians, reedição da final de 2024 e mais: coletiva de Tamires e Andressa Alves",
-      "url": "https://www.meutimao.com.br/videos-do-corinthians/129832/temporada-do-corinthians-reedicao-da-final-de-2024-e-mais-coletiva-de-tamires-e-andressa-alves",
-      "time": "24/09 · 17:58",
-      "pubDate": "Thu, 24 Sep 2026 14:58:48 -0300"
+      "source": "Bolavip",
+      "color": "#0055A5",
+      "title": "Opinião: Corinthians deve receber R$ 10 milhões da FFU e terá uma ajuda importante no caixa",
+      "url": "<![CDATA[https://br.bolavip.com/corinthians/opiniao-corinthians-deve-receber-r-10-milhoes-da-ffu-e-tera-uma-ajuda-importante-no-caixa]]>",
+      "time": "24/09 · 15:42",
+      "pubDate": "Thu, 24 Sep 2026 12:42:22 -0300"
     },
     {
       "source": "Bolavip",
@@ -81,14 +129,6 @@ const LIVE_DATA = {
       "url": "<![CDATA[https://br.bolavip.com/corinthians/corinthians-descarta-qualquer-contato-por-luis-castro-e-segue-acreditando-no-trabalho-de-fernando-diniz]]>",
       "time": "24/09 · 11:57",
       "pubDate": "Thu, 24 Sep 2026 08:57:30 -0300"
-    },
-    {
-      "source": "Bolavip",
-      "color": "#0055A5",
-      "title": "Opinião: Corinthians deve receber R$ 10 milhões da FFU e terá uma ajuda importante no caixa",
-      "url": "<![CDATA[https://br.bolavip.com/corinthians/opiniao-corinthians-deve-receber-r-10-milhoes-da-ffu-e-tera-uma-ajuda-importante-no-caixa]]>",
-      "time": "24/09 · 11:48",
-      "pubDate": "Thu, 24 Sep 2026 08:48:32 -0300"
     },
     {
       "source": "Gazeta Esportiva",
@@ -147,58 +187,24 @@ const LIVE_DATA = {
       "pubDate": "Wed, 23 Sep 2026 14:04:19 -0300"
     },
     {
-      "source": "Bolavip",
-      "color": "#0055A5",
-      "title": "Lingard está longe da meta de renovação automática e diretoria não garante permanência no Corinthians",
-      "url": "<![CDATA[https://br.bolavip.com/corinthians/lingard-esta-longe-da-meta-de-renovacao-automatica-e-diretoria-nao-garante-permanencia-no-corinthians]]>",
-      "time": "23/09 · 16:58",
-      "pubDate": "Wed, 23 Sep 2026 13:58:47 -0300"
-    },
-    {
       "source": "Gazeta Esportiva",
       "color": "#E5820A",
       "title": "Corinthians x São Paulo: veja arbitragem da ida da final do Brasileiro feminino",
       "url": "https://www.gazetaesportiva.com/campeonatos/brasileirao-feminino/corinthians-x-sao-paulo-arbitragem-final-brasileirao-feminino/",
       "time": "23/09 · 16:24",
       "pubDate": "Wed, 23 Sep 2026 16:24:49 +0000"
-    },
-    {
-      "source": "Bolavip",
-      "color": "#0055A5",
-      "title": "Análise: Fernando Diniz insiste e Corinthians tem aproveitamento 15,4% melhor com Allan",
-      "url": "<![CDATA[https://br.bolavip.com/corinthians/analise-fernando-diniz-insiste-e-corinthians-tem-aproveitamento-154-melhor-com-allan]]>",
-      "time": "23/09 · 14:45",
-      "pubDate": "Wed, 23 Sep 2026 11:45:57 -0300"
-    },
-    {
-      "source": "Bolavip",
-      "color": "#0055A5",
-      "title": "Opinião: Corinthians não demitiu Fernando Diniz por falta de opções no mercado e multa de R$ 5 milhões",
-      "url": "<![CDATA[https://br.bolavip.com/corinthians/opiniao-corinthians-nao-demitiu-fernando-diniz-por-falta-de-opcoes-no-mercado-e-multa-de-r-5-milhoes]]>",
-      "time": "23/09 · 13:09",
-      "pubDate": "Wed, 23 Sep 2026 10:09:57 -0300"
-    },
-    {
-      "source": "Gazeta Esportiva",
-      "color": "#E5820A",
-      "title": "Multa milionária pesa contra demissão de Diniz no Corinthians",
-      "url": "https://www.gazetaesportiva.com/times/corinthians/multa-milionaria-pesa-contra-demissao-de-diniz-no-corinthians/",
-      "time": "23/09 · 08:00",
-      "pubDate": "Wed, 23 Sep 2026 08:00:04 +0000"
-    },
-    {
-      "source": "Gazeta Esportiva",
-      "color": "#E5820A",
-      "title": "Ex-Corinthians e Santos, meia Giuliano anuncia aposentadoria",
-      "url": "https://www.gazetaesportiva.com/times/corinthians/ex-corinthians-e-santos-meia-giuliano-anuncia-aposentadoria/",
-      "time": "22/09 · 23:18",
-      "pubDate": "Tue, 22 Sep 2026 23:18:24 +0000"
     }
   ],
   "upcoming": [],
   "results": [],
   "standings": [],
   "highlights": [
+    {
+      "title": "Goleiro de Corinthians se torna novo embaixador da 7K Poker",
+      "url": "https://www.meutimao.com.br/noticias-do-corinthians/540779/goleiro-de-corinthians-se-torna-novo-embaixador-da-7k-poker",
+      "pubDate": "Thu, 24 Sep 2026 19:01:00 -0300",
+      "date": "24/09 · 22:01"
+    },
     {
       "title": "VÍDEO: Temporada do Corinthians, reedição da final de 2024 e mais: coletiva de Tamires e Andressa Alves",
       "url": "https://www.meutimao.com.br/videos-do-corinthians/129832/temporada-do-corinthians-reedicao-da-final-de-2024-e-mais-coletiva-de-tamires-e-andressa-alves",
